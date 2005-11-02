d77e1a9642fe1efe9aa5f737a640354c27d04e02
Daniel Veillard <veillard@redhat.com>
2005-11-02 12:50:21 +0000

    Initial revision

---
+++ include/libxen.h
@@ -0,0 +1,80 @@
+/*
+ * libxen.h: interface for the libxen library to handle Xen domains
+ *           from a process running in domain 0
+ *
+ * Copyright (C) 2005 Red Hat, Inc.
+ *
+ * See COPYING.LIB for the License of this software
+ *
+ * Daniel Veillard <veillard@redhat.com>
+ */
+
+#ifndef __XEN_XENLIB_H__
+#define __XEN_XENLIB_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/**
+ * xenConnect:
+ *
+ * a xenConnect is a private structure representing a connection to
+ * the Xen Hypervisor.
+ */
+typedef struct _xenConnect xenConnect;
+
+/**
+ * xenConnectPtr:
+ *
+ * a xenConnectPtr is pointer to a xenConnect private structure, this is the
+ * type used to reference a connection to the Xen Hypervisor in the API.
+ */
+typedef xenConnect *xenConnectPtr;
+
+/**
+ * xenDomain:
+ *
+ * a xenDomain is a private structure representing a Xen domain.
+ */
+typedef struct _xenDomain xenDomain;
+
+/**
+ * xenDomainPtr:
+ *
+ * a xenDomainPtr is pointer to a xenDomain private structure, this is the
+ * type used to reference a Xen domain in the API.
+ */
+typedef xenDomain *xenDomainPtr;
+
+
+/**
+ * xenDomainFlags:
+ *
+ * Flags OR'ed together to provide specific behaviour when creating a
+ * Domain.
+ */
+typedef enum {
+     XEN_DOMAIN_NONE = 0
+} xenDomainFlags;
+
+/*
+ * Connection and disconnections to the Hypervisor
+ */
+xenConnectPtr		xenOpenConnect		(const char *name);
+int			xenCloseConnect		(xenConnectPtr conn);
+unsigned long		xenGetVersion		(xenConnectPtr conn);
+
+/*
+ * Domain creation and destruction
+ */
+xenDomainPtr		xenCreateLinuxDomain	(xenConnectPtr conn,
+						 const char *kernel_path,
+						 const char *initrd_path,
+						 const char *cmdline,
+						 unsigned int flags);
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __XEN_XENLIB_H__ */
---
+++ src/libxen.c
@@ -0,0 +1,76 @@
+/*
+ * libxen.h: Main interfaces for the libxen library to handle virtualization
+ *           domains from a process running in domain 0
+ *
+ * Copyright (C) 2005 Red Hat, Inc.
+ *
+ * See COPYING.LIB for the License of this software
+ *
+ * Daniel Veillard <veillard@redhat.com>
+ */
+
+#include "libxen.h"
+
+#include "memory.h"
+#include "internal.h"
+
+/*
+ * TODO:
+ * - use lock to protect against concurrent accesses ?
+ * - use reference counting to garantee coherent pointer state ?
+ */
+
+#define XEN_CONNECT_MAGIC 0x4F23DEAD
+/**
+ * _xenConnect:
+ *
+ * Internal structure associated to a connection
+ */
+struct _xenConnect {
+    unsigned int magic;		/* specific value to check */
+    int	         handle;	/* internal handle used for hypercall */
+}
+
+/**
+ * xenGetConnect:
+ * @name: optional argument currently unused, pass NULL
+ *
+ * This function should be called first to get a connection to the 
+ * Hypervisor
+ *
+ * Returns a pointer to the hypervisor connection or NULL in case of error
+ */
+xenConnectPtr
+xenOpenConnect(const char *name ATTRIBUTE_UNUSED) {
+    return(NULL);
+}
+
+/**
+ * xenCloseConnect:
+ * @conn: pointer to the hypervisor connection
+ *
+ * This function closes the connection to the Hypervisor. This should
+ * not be called if further interaction with the Hypervisor are needed
+ * especially if there is running domain which need further monitoring by
+ * the application.
+ *
+ * Returns 0 in case of success or -1 in case of error.
+ */
+int
+xenCloseConnect(xenConnectPtr conn) {
+    if ((conn == NULL) || (conn->magic != XEN_CONNECT_MAGIC))
+        return(-1);
+    /*
+     * TODO:
+     * Free the domain pointers associated to this connection
+     */
+    conn->magic = -1;
+    free(conn);
+    return(0);
+}
+
+/**
+ * xenGetVersion:
+ * @conn: pointer to the hypervisor connection
+ *
+ * Get the version level of the Hypervisor running
