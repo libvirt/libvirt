4fc5f43399ab35059a3f29ff18953da1c0208e9b
Daniel Veillard <veillard@redhat.com>
2006-11-09 10:14:01 +0000

    * tests/reconnect.c: fixed the reconnect test when running as non-root Daniel

--- tests/reconnect.c
+++ tests/reconnect.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <libvirt/libvirt.h>
+#include <libvirt/virterror.h>
+
+static void errorHandler(void *userData, virErrorPtr error) {
+}
 
 int main(void) {
     int id = 0;
+    int ro = 0;
     virConnectPtr conn;
     virDomainPtr dom;
 
-    conn = virConnectOpen("");
+    virSetErrorFunc(NULL, errorHandler);
+
+    conn = virConnectOpen(NULL);
+    if (conn == NULL) {
+        ro = 1;
+	conn = virConnectOpenReadOnly(NULL);
+    }
     if (conn == NULL) {
         fprintf(stderr, "First virConnectOpen() failed\n");
 	exit(1);
@@ -19,7 +30,10 @@ int main(void) {
     }
     virDomainFree(dom);
     virConnectClose(conn);
-    conn = virConnectOpen("");
+    if (ro == 1)
+	conn = virConnectOpenReadOnly(NULL);
+    else
+	conn = virConnectOpen(NULL);
     if (conn == NULL) {
         fprintf(stderr, "Second virConnectOpen() failed\n");
 	exit(1);
