b50353f80f91db5343c3a59565b1b5d84a14fd3c
Daniel Veillard <veillard@redhat.com>
2007-03-16 22:26:55 +0000

    last changes for 0.2.1, daniel

--- include/libvirt/libvirt.h
+++ include/libvirt/libvirt.h
@@ -214,7 +214,7 @@ typedef virNodeInfo *virNodeInfoPtr;
  * version * 1,000,000 + minor * 1000 + micro
  */
 
-#define LIBVIR_VERSION_NUMBER 2000
+#define LIBVIR_VERSION_NUMBER 2001
 
 int			virGetVersion		(unsigned long *libVer,
 						 const char *type,
