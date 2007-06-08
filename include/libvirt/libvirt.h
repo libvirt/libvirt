29e65f9a9808b03005c49ff5fd9c26bc9ca4a6d2
Daniel Veillard <veillard@redhat.com>
2007-06-08 11:11:56 +0000

    * configure.in libvirt.spec.in include/libvirt/libvirt.h   docs/*: prepared release of 0.2.3 Daniel

--- include/libvirt/libvirt.h
+++ include/libvirt/libvirt.h
@@ -279,7 +279,7 @@ typedef virNodeInfo *virNodeInfoPtr;
  * version * 1,000,000 + minor * 1000 + micro
  */
 
-#define LIBVIR_VERSION_NUMBER 2002
+#define LIBVIR_VERSION_NUMBER 2003
 
 int			virGetVersion		(unsigned long *libVer,
 						 const char *type,
