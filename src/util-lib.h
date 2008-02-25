9b1cd481b83239aa46369a1c7c57c289f5a24d9f
Daniel Veillard <veillard@redhat.com>
2008-02-25 13:55:56 +0000

    fix compilation against static libraries * src/Makefile.am src/util-lib.h: fix done with Jim to allow   compilation of virsh with the static libraries. Daniel

--- src/util-lib.h
+++ src/util-lib.h
@@ -10,6 +10,15 @@
 
 #include <sys/types.h>
 
+/*
+ * To avoid a double definition of the function when compiling
+ * programs using both util-lib and libvirt, like virsh
+ */
+#ifdef IN_LIBVIRT
+#define saferead libvirt_saferead
+#define safewrite libvirt_safewrite
+#endif
+
 int saferead(int fd, void *buf, size_t count);
 ssize_t safewrite(int fd, const void *buf, size_t count);
 
