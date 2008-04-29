eb6c33cd97f5597e93f87433a2deceb25f53d7d9
Jim Meyering <meyering@redhat.com>
2008-04-29 21:39:38 +0000

    * gnulib/tests/test-EOVERFLOW.c: Add another new file from gnulib.

---
+++ gnulib/tests/test-EOVERFLOW.c
@@ -0,0 +1,32 @@
+/* Test of EOVERFLOW macro.
+   Copyright (C) 2008 Free Software Foundation, Inc.
+
+   This program is free software: you can redistribute it and/or modify
+   it under the terms of the GNU General Public License as published by
+   the Free Software Foundation; either version 3 of the License, or
+   (at your option) any later version.
+
+   This program is distributed in the hope that it will be useful,
+   but WITHOUT ANY WARRANTY; without even the implied warranty of
+   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+   GNU General Public License for more details.
+
+   You should have received a copy of the GNU General Public License
+   along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+#include <config.h>
+
+#include <errno.h>
+
+/* Check that it can be used as an initializer outside of a function.  */
+static int err = EOVERFLOW;
+
+int
+main ()
+{
+  /* snprintf() callers want to distinguish EINVAL and EOVERFLOW.  */
+  if (err == EINVAL)
+    return 1;
+
+  return 0;
+}
