7967662a526df2ba4f3f78cd55a07940753b9b18
Jim Meyering <meyering@redhat.com>
2008-05-09 14:03:15 +0000

    Add new files from gnulib.
    * gnulib/lib/c-ctype.c: New file.
    * gnulib/lib/c-ctype.h: Likewise.
    * gnulib/tests/test-c-ctype.c: Likewise.
    * gnulib/tests/test-vc-list-files-git.sh: Likewise.
    * gnulib/tests/test-vc-list-files-cvs.sh: Likewise.

---
+++ gnulib/tests/test-vc-list-files-git.sh
@@ -0,0 +1,44 @@
+#!/bin/sh
+# Unit tests for vc-list-files
+# Copyright (C) 2008 Free Software Foundation, Inc.
+# This file is part of the GNUlib Library.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.  */
+
+if ( diff --version < /dev/null 2>&1 | grep GNU ) 2>&1 > /dev/null; then
+  compare() { diff -u "$@"; }
+elif ( cmp --version < /dev/null 2>&1 | grep GNU ) 2>&1 > /dev/null; then
+  compare() { cmp -s "$@"; }
+else
+  compare() { cmp "$@"; }
+fi
+
+tmpdir=vc-git-$$
+trap 'st=$?; cd '"`pwd`"' && rm -rf $tmpdir; exit $st' 0
+trap '(exit $?); exit $?' 1 2 13 15
+
+fail=1
+mkdir $tmpdir && cd $tmpdir &&
+  # without git, skip the test
+  { git init -q || exit 77; } &&
+  mkdir d &&
+  touch d/a b c &&
+  git add . > /dev/null &&
+  git commit -q -a -m log &&
+  printf '%s\n' b c d/a > expected &&
+  vc-list-files > actual &&
+  compare expected actual &&
+  fail=0
+
+(exit $fail); exit $fail
