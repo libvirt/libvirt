9d3d43436eac5aee19754a70b88d140c65c78462
Daniel Veillard <veillard@redhat.com>
2008-09-08 14:32:18 +0000

    * configure.in libvirt.spec.in docs/*: preparing release of   libvirt-0.4.5 * po/*: rebuilt and updated de.po daniel

--- include/libvirt/libvirt.h
+++ include/libvirt/libvirt.h
@@ -380,7 +380,7 @@ extern virConnectAuthPtr virConnectAuthPtrDefault;
  * version * 1,000,000 + minor * 1000 + micro
  */
 
-#define LIBVIR_VERSION_NUMBER 4004
+#define LIBVIR_VERSION_NUMBER 4005
 
 int                     virGetVersion           (unsigned long *libVer,
                                                  const char *type,
