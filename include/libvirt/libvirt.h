4a5dfb847b620aef8be6a52255b2dc8b11f13763
Daniel Veillard <veillard@redhat.com>
2009-01-31 11:46:29 +0000

    Release of 0.6.0 * configure.in docs/* NEWS: release of 0.6.0 * po/*: regenerated and updated brazilian and polish localizations daniel

--- include/libvirt/libvirt.h
+++ include/libvirt/libvirt.h
@@ -380,7 +380,7 @@ extern virConnectAuthPtr virConnectAuthPtrDefault;
  * version * 1,000,000 + minor * 1000 + micro
  */
 
-#define LIBVIR_VERSION_NUMBER 5001
+#define LIBVIR_VERSION_NUMBER 6000
 
 int                     virGetVersion           (unsigned long *libVer,
                                                  const char *type,
