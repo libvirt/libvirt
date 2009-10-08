39a7be470cc5477a96a4d53194291bf4c7b07185
Cole Robinson <crobinso@redhat.com>
2009-10-08 18:06:59 -0400

    tests: Break out duplicate schema verification functionality.
    All schema tests have identical functionality, so avoid the duplication.

---
+++ tests/schematestutils.sh
@@ -0,0 +1,39 @@
+#!/bin/sh
+
+test -z "$srcdir" && srcdir=`pwd`
+test -z "$abs_srcdir" && abs_srcdir=`pwd`
+
+function check_schema {
+
+DIRS=$1
+SCHEMA="$srcdir/../docs/schemas/$2"
+
+n=0
+f=0
+for dir in $DIRS
+do
+  XML=`find $abs_srcdir/$dir -name '*.xml'` || exit 1
+
+  for xml in $XML
+  do
+    n=`expr $n + 1`
+    printf "%4d) %.60s  " $n $(basename $(dirname $xml))"/"$(basename $xml)
+    cmd="xmllint --relaxng $SCHEMA --noout $xml"
+    result=`$cmd 2>&1`
+    ret=$?
+    if test $ret = 0; then
+        echo "OK"
+    else
+        echo "FAILED"
+        echo -e "$cmd\n$result"
+        f=`expr $f + 1`
+    fi
+  done
+done
+echo "Validated $n files, $f failed"
+
+ret=0
+test $f != 0 && ret=255
+exit $ret
+
+}
