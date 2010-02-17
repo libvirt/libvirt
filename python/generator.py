148114e3258803630362d4bbae9a767134194f07
Cole Robinson <crobinso@redhat.com>
2010-02-17 15:55:46 -0500

    python: Actually add virConnectGetVersion to generated bindings
    The recent commit to implement a python version of this function
    didn't drop an explicit 'ignore' check in the generator, so this
    never ended up in the bindings.

--- python/generator.py
+++ python/generator.py
@@ -319,7 +319,6 @@ skip_function = (
     'virConnectListDomains', # Python API is called virConectListDomainsID for unknown reasons
     'virConnSetErrorFunc', # Not used in Python API  XXX is this a bug ?
     'virResetError', # Not used in Python API  XXX is this a bug ?
-    'virConnectGetVersion', # Not used in Python API  XXX is this a bug ?
     'virGetVersion', # Python C code is manually written
     'virSetErrorFunc', # Python API is called virRegisterErrorHandler for unknown reasons
     'virConnCopyLastError', # Python API is called virConnGetLastError instead
