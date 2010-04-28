4e041189f85bc1b0f8d5278c0cafdd4aba55beaf
Daniel Veillard <veillard@redhat.com>
2010-04-28 15:38:47 +0200

    Move dnsmasq host file to a separate directory
    use /var/lib/libvirt/dnsmasq since /var/lib/libvirt/network is
    unreadable by the dnsmasq binary
    
    * src/network/bridge_driver.c: update DNSMASQ_STATE_DIR
    * src/Makefile.am: create it on make install
    * libvirt.spec.in: take the new directory into account

--- src/network/bridge_driver.c
+++ src/network/bridge_driver.c
@@ -62,7 +62,7 @@
 #define NETWORK_PID_DIR LOCAL_STATE_DIR "/run/libvirt/network"
 #define NETWORK_STATE_DIR LOCAL_STATE_DIR "/lib/libvirt/network"
 
-#define DNSMASQ_STATE_DIR LOCAL_STATE_DIR "/lib/libvirt/network"
+#define DNSMASQ_STATE_DIR LOCAL_STATE_DIR "/lib/libvirt/dnsmasq"
 
 #define VIR_FROM_THIS VIR_FROM_NETWORK
 
