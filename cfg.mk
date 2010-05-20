7a3267cfcf47c17e66138bf134e971e41a1b65bd
Jim Meyering <meyering@redhat.com>
2010-05-20 08:56:48 +0200

    maint: enforce policy wrt VIR_DEBUG and VIR_DEBUG0
    * cfg.mk (sc_prohibit_gettext_markup): Just like VIR_WARN*.

--- cfg.mk
+++ cfg.mk
@@ -439,7 +439,7 @@ sc_copyright_format:
 # Some functions/macros produce messages intended solely for developers
 # and maintainers.  Do not mark them for translation.
 sc_prohibit_gettext_markup:
-	@prohibit='\<VIR_WARN0? *\(_\('					\
+	@prohibit='\<VIR_(WARN|DEBUG)0? *\(_\('				\
 	halt='do not mark these strings for translation'		\
 	  $(_sc_search_regexp)
 
