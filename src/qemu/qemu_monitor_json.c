72c791e430baa2f6193e6f6a85f6a476587c72fe
Jiri Denemark <jdenemar@redhat.com>
2010-08-19 15:59:25 +0200

    qemu: Fix JSON migrate_set_downtime command

--- src/qemu/qemu_monitor_json.c
+++ src/qemu/qemu_monitor_json.c
@@ -1481,17 +1481,12 @@ int qemuMonitorJSONSetMigrationDowntime(qemuMonitorPtr mon,
                                         unsigned long long downtime)
 {
     int ret;
-    char *downtimestr;
     virJSONValuePtr cmd;
     virJSONValuePtr reply = NULL;
-    if (virAsprintf(&downtimestr, "%llums", downtime) < 0) {
-        virReportOOMError();
-        return -1;
-    }
+
     cmd = qemuMonitorJSONMakeCommand("migrate_set_downtime",
-                                     "s:value", downtimestr,
+                                     "d:value", downtime / 1000.0,
                                      NULL);
-    VIR_FREE(downtimestr);
     if (!cmd)
         return -1;
 
