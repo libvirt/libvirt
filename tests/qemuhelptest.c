e414dab4d613da6a0e88c46e1fc5c4e6fd8d021a
Eric Blake <eblake@redhat.com>
2010-12-08 10:10:51 -0700

    tests: test Fedora 14 qemu-kvm -help parsing
    * tests/qemuhelpdata/qemu-kvm-0.13.0: New file.
    * tests/qemuhelptest.c (mymain): New test from Fedora 14 qemu-kvm,
    which covers some options (like -fstype passthrough) not tested elsewhere.

--- tests/qemuhelptest.c
+++ tests/qemuhelptest.c
@@ -362,6 +362,49 @@ mymain(int argc, char **argv)
             QEMUD_CMD_FLAG_SMBIOS_TYPE |
             QEMUD_CMD_FLAG_VGA_NONE,
             12003, 1,  0);
+    DO_TEST("qemu-kvm-0.13.0",
+            QEMUD_CMD_FLAG_VNC_COLON |
+            QEMUD_CMD_FLAG_NO_REBOOT |
+            QEMUD_CMD_FLAG_DRIVE |
+            QEMUD_CMD_FLAG_DRIVE_BOOT |
+            QEMUD_CMD_FLAG_NAME |
+            QEMUD_CMD_FLAG_UUID |
+            QEMUD_CMD_FLAG_VNET_HDR |
+            QEMUD_CMD_FLAG_MIGRATE_QEMU_TCP |
+            QEMUD_CMD_FLAG_MIGRATE_QEMU_EXEC |
+            QEMUD_CMD_FLAG_DRIVE_CACHE_V2 |
+            QEMUD_CMD_FLAG_KVM |
+            QEMUD_CMD_FLAG_DRIVE_FORMAT |
+            QEMUD_CMD_FLAG_DRIVE_SERIAL |
+            QEMUD_CMD_FLAG_XEN_DOMID |
+            QEMUD_CMD_FLAG_DRIVE_READONLY |
+            QEMUD_CMD_FLAG_VGA |
+            QEMUD_CMD_FLAG_0_10 |
+            QEMUD_CMD_FLAG_PCIDEVICE |
+            QEMUD_CMD_FLAG_MEM_PATH |
+            QEMUD_CMD_FLAG_SDL |
+            QEMUD_CMD_FLAG_MIGRATE_QEMU_UNIX |
+            QEMUD_CMD_FLAG_CHARDEV |
+            QEMUD_CMD_FLAG_ENABLE_KVM |
+            QEMUD_CMD_FLAG_MONITOR_JSON |
+            QEMUD_CMD_FLAG_BALLOON |
+            QEMUD_CMD_FLAG_DEVICE |
+            QEMUD_CMD_FLAG_SMP_TOPOLOGY |
+            QEMUD_CMD_FLAG_NETDEV |
+            QEMUD_CMD_FLAG_RTC |
+            QEMUD_CMD_FLAG_VNET_HOST |
+            QEMUD_CMD_FLAG_NO_HPET |
+            QEMUD_CMD_FLAG_NO_KVM_PIT |
+            QEMUD_CMD_FLAG_TDF |
+            QEMUD_CMD_FLAG_NODEFCONFIG |
+            QEMUD_CMD_FLAG_BOOT_MENU |
+            QEMUD_CMD_FLAG_FSDEV |
+            QEMUD_CMD_FLAG_NESTING |
+            QEMUD_CMD_FLAG_NAME_PROCESS |
+            QEMUD_CMD_FLAG_SMBIOS_TYPE |
+            QEMUD_CMD_FLAG_SPICE |
+            QEMUD_CMD_FLAG_VGA_NONE,
+            13000, 1,  0);
 
     return ret == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
