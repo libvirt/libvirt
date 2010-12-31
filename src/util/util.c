libvirt/libvirt

commit c3568ec2892c420c931c018719974c01000a18f3

virExec: avoid undefined behavior
* src/util/util.c (__virExec): Don't use FD_ISSET on out-of-bounds fd.

src/util/util.c
@@ -570,8 +570,7 @@ __virExec(const char *const*argv,
             i != null &&
             i != childout &&
             i != childerr &&
-            (!keepfd ||
-             !FD_ISSET(i, keepfd))) {
+            (!keepfd || (i < FD_SETSIZE && !FD_ISSET(i, keepfd)))) {
             tmpfd = i;
             VIR_FORCE_CLOSE(tmpfd);
         }

commit e80ed3fd3bd6d6d100b46f06474b8cb8406cdfc5

virExec: fix logic bug
As pointed out in https://bugzilla.redhat.com/show_bug.cgi?id=659855#c9,
commit c3568ec2 introduced a regression where we no longer close any
fd's beyond FD_SETSIZE.

* src/util/util.c (__virExec): Continue to close fd's beyond
keepfd range.
Reported by Stefan Praszalowicz.

src/util/util.c
@@ -570,7 +570,7 @@ __virExec(const char *const*argv,
             i != null &&
             i != childout &&
             i != childerr &&
-            (!keepfd || (i < FD_SETSIZE && !FD_ISSET(i, keepfd)))) {
+            (!keepfd || i >= FD_SETSIZE || !FD_ISSET(i, keepfd))) {
             tmpfd = i;
             VIR_FORCE_CLOSE(tmpfd);
         }