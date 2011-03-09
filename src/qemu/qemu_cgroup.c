c52cbe487c1694a6f4c1c17eb96ee015bb46e8a4
Eric Blake <eblake@redhat.com>
2011-03-09 15:05:00 -0700

    qemu: don't request cgroup ACL access for /dev/net/tun
    Since libvirt always passes /dev/net/tun to qemu via fd, we should
    never trigger the cases where qemu tries to directly open the
    device.  Therefore, it is safer to deny the cgroup device ACL.
    
    * src/qemu/qemu_cgroup.c (defaultDeviceACL): Remove /dev/net/tun.
    * src/qemu/qemu.conf (cgroup_device_acl): Reflect this change.

--- src/qemu/qemu_cgroup.c
+++ src/qemu/qemu_cgroup.c
@@ -37,7 +37,7 @@ static const char *const defaultDeviceACL[] = {
     "/dev/null", "/dev/full", "/dev/zero",
     "/dev/random", "/dev/urandom",
     "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
-    "/dev/rtc", "/dev/hpet", "/dev/net/tun",
+    "/dev/rtc", "/dev/hpet",
     NULL,
 };
 #define DEVICE_PTY_MAJOR 136
