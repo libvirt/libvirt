libvirt/libvirt

commit 2215050edd8adefbf0ff21c5cbf09685877492d6

Only initialize/cleanup libpciaccess once
libpciaccess has many bugs in its pci_system_init/cleanup
functions that makes calling them multiple times unwise.
eg it will double close() FDs, and leak other FDs.

* src/node_device/node_device_udev.c: Only initialize
  libpciaccess once

src/node_device/node_device_udev.c
@@ -363,17 +363,9 @@ static int udevTranslatePCIIds(unsigned int vendor,
                                char **vendor_string,
                                char **product_string)
 {
-    int ret = -1, pciret;
+    int ret = -1;
     struct pci_id_match m;
     const char *vendor_name = NULL, *device_name = NULL;
-
-    if ((pciret = pci_system_init()) != 0) {
-        char ebuf[256];
-        VIR_INFO("Failed to initialize libpciaccess: %s",
-                 virStrerror(pciret, ebuf, sizeof ebuf));
-        ret = 0;
-        goto out;
-    }
 
     m.vendor_id = vendor;
     m.device_id = product;
@@ -405,9 +397,6 @@ static int udevTranslatePCIIds(unsigned int vendor,
             goto out;
         }
     }
-
-    /* pci_system_cleanup returns void */
-    pci_system_cleanup();
 
     ret = 0;
 
@@ -1426,6 +1415,9 @@ static int udevDeviceMonitorShutdown(void)
         ret = -1;
     }
 
+    /* pci_system_cleanup returns void */
+    pci_system_cleanup();
+
     return ret;
 }
 
@@ -1593,6 +1585,15 @@ static int udevDeviceMonitorStartup(int privileged ATTRIBUTE_UNUSED)
     udevPrivate *priv = NULL;
     struct udev *udev = NULL;
     int ret = 0;
+    int pciret;
+
+    if ((pciret = pci_system_init()) != 0) {
+        char ebuf[256];
+        VIR_INFO("Failed to initialize libpciaccess: %s",
+                 virStrerror(pciret, ebuf, sizeof ebuf));
+        ret = -1;
+        goto out;
+    }
 
     if (VIR_ALLOC(priv) < 0) {
         virReportOOMError();

commit 4c7508b4decbade955ad1b6d28dad7f03cdb482b

udev: fix regression with qemu:///session
https://bugzilla.redhat.com/show_bug.cgi?id=684655 points out
a regression introduced in commit 2215050edd - non-root users
can't connect to qemu:///session because libvirtd dies when
it can't use pciaccess initialization.

* src/node_device/node_device_udev.c (udevDeviceMonitorStartup):
Don't abort udev driver (and libvirtd overall) if non-root user
can't use pciaccess.

src/node_device/node_device_udev.c
@@ -1,7 +1,7 @@
 /*
  * node_device_udev.c: node device enumeration - libudev implementation
  *
- * Copyright (C) 2009-2010 Red Hat, Inc.
+ * Copyright (C) 2009-2011 Red Hat, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -1589,7 +1589,7 @@ out:
     return ret;
 }
 
-static int udevDeviceMonitorStartup(int privileged ATTRIBUTE_UNUSED)
+static int udevDeviceMonitorStartup(int privileged)
 {
     udevPrivate *priv = NULL;
     struct udev *udev = NULL;
@@ -1597,11 +1597,16 @@ static int udevDeviceMonitorStartup(int privileged ATTRIBUTE_UNUSED)
     int pciret;
 
     if ((pciret = pci_system_init()) != 0) {
-        char ebuf[256];
-        VIR_ERROR(_("Failed to initialize libpciaccess: %s"),
-                  virStrerror(pciret, ebuf, sizeof ebuf));
-        ret = -1;
-        goto out;
+        /* Ignore failure as non-root; udev is not as helpful in that
+         * situation, but a non-privileged user won't benefit much
+         * from udev in the first place.  */
+        if (privileged || errno != EACCES) {
+            char ebuf[256];
+            VIR_ERROR(_("Failed to initialize libpciaccess: %s"),
+                      virStrerror(pciret, ebuf, sizeof ebuf));
+            ret = -1;
+            goto out;
+        }
     }
 
     if (VIR_ALLOC(priv) < 0) {