libvirt/libvirt

commit 36deff04996fbab3d95b20cbbcb02134318957dd

virsh: Remove two 4kb stack allocations

tools/virsh.c
@@ -2052,7 +2052,7 @@ cmdDominfo(vshControl *ctl, const vshCmd *cmd)
     virDomainInfo info;
     virDomainPtr dom;
     virSecurityModel secmodel;
-    virSecurityLabel seclabel;
+    virSecurityLabelPtr seclabel;
     int persistent = 0;
     int ret = TRUE, autostart;
     unsigned int id;
@@ -2138,15 +2138,22 @@ cmdDominfo(vshControl *ctl, const vshCmd *cmd)
             vshPrint(ctl, "%-15s %s\n", _("Security DOI:"), secmodel.doi);
 
             /* Security labels are only valid for active domains */
-            memset(&seclabel, 0, sizeof seclabel);
-            if (virDomainGetSecurityLabel(dom, &seclabel) == -1) {
+            if (VIR_ALLOC(seclabel) < 0) {
                 virDomainFree(dom);
                 return FALSE;
-            } else {
-                if (seclabel.label[0] != '\0')
-                    vshPrint(ctl, "%-15s %s (%s)\n", _("Security label:"),
-                             seclabel.label, seclabel.enforcing ? "enforcing" : "permissive");
             }
+
+            if (virDomainGetSecurityLabel(dom, seclabel) == -1) {
+                virDomainFree(dom);
+                VIR_FREE(seclabel);
+                return FALSE;
+            } else {
+                if (seclabel->label[0] != '\0')
+                    vshPrint(ctl, "%-15s %s (%s)\n", _("Security label:"),
+                             seclabel->label, seclabel->enforcing ? "enforcing" : "permissive");
+            }
+
+            VIR_FREE(seclabel);
         }
     }
     virDomainFree(dom);
@@ -12141,13 +12148,15 @@ vshOpenLogFile(vshControl *ctl)
 static void
 vshOutputLogFile(vshControl *ctl, int log_level, const char *msg_format, va_list ap)
 {
-    char msg_buf[MSG_BUFFER];
+    char *msg_buf;
     const char *lvl = "";
     struct timeval stTimeval;
     struct tm *stTm;
 
     if (ctl->log_fd == -1)
         return;
+
+    msg_buf = vshMalloc(ctl, MSG_BUFFER);
 
     /**
      * create log format
@@ -12199,6 +12208,8 @@ vshOutputLogFile(vshControl *ctl, int log_level, const char *msg_format, va_list
         vshCloseLogFile(ctl);
         vshError(ctl, "%s", _("failed to write the log file"));
     }
+
+    VIR_FREE(msg_buf);
 }
 
 /**

commit 0431551435e79664b46f2ae2a7dca394e4314f05

virsh: fix regression in log to file
Commit 36deff04 introduced a regression due to which virsh is not able
to log to a file - msg_buf was changed from an array to a pointer
without corresponding change to usage of "sizeof()".

Fix regression in virsh logging

Signed-off-by: Supriya Kannery <supriyak@in.ibm.com>

AUTHORS
@@ -169,6 +169,7 @@ Patches have also been contributed by:
   Richard Laager       <rlaager@wiktel.com>
   Mark Wu              <dwu@redhat.com>
   Yufang Zhang         <yuzhang@redhat.com>
+  Supriya Kannery      <supriyak@in.ibm.com>
 
   [....send patches to get your name here....]
 

tools/virsh.c
@@ -12227,7 +12227,8 @@ vshOpenLogFile(vshControl *ctl)
  * Outputting an error to log file.
  */
 static void
-vshOutputLogFile(vshControl *ctl, int log_level, const char *msg_format, va_list ap)
+vshOutputLogFile(vshControl *ctl, int log_level, const char *msg_format,
+                 va_list ap)
 {
     char *msg_buf;
     const char *lvl = "";
@@ -12246,7 +12247,7 @@ vshOutputLogFile(vshControl *ctl, int log_level, const char *msg_format, va_list
     */
     gettimeofday(&stTimeval, NULL);
     stTm = localtime(&stTimeval.tv_sec);
-    snprintf(msg_buf, sizeof(msg_buf),
+    snprintf(msg_buf, MSG_BUFFER,
              "[%d.%02d.%02d %02d:%02d:%02d ",
              (1900 + stTm->tm_year),
              (1 + stTm->tm_mon),
@@ -12254,7 +12255,7 @@ vshOutputLogFile(vshControl *ctl, int log_level, const char *msg_format, va_list
              (stTm->tm_hour),
              (stTm->tm_min),
              (stTm->tm_sec));
-    snprintf(msg_buf + strlen(msg_buf), sizeof(msg_buf) - strlen(msg_buf),
+    snprintf(msg_buf + strlen(msg_buf), MSG_BUFFER - strlen(msg_buf),
              "%s] ", SIGN_NAME);
     switch (log_level) {
         case VSH_ERR_DEBUG:
@@ -12276,13 +12277,13 @@ vshOutputLogFile(vshControl *ctl, int log_level, const char *msg_format, va_list
             lvl = LVL_DEBUG;
             break;
     }
-    snprintf(msg_buf + strlen(msg_buf), sizeof(msg_buf) - strlen(msg_buf),
+    snprintf(msg_buf + strlen(msg_buf), MSG_BUFFER - strlen(msg_buf),
              "%s ", lvl);
-    vsnprintf(msg_buf + strlen(msg_buf), sizeof(msg_buf) - strlen(msg_buf),
+    vsnprintf(msg_buf + strlen(msg_buf), MSG_BUFFER - strlen(msg_buf),
               msg_format, ap);
 
     if (msg_buf[strlen(msg_buf) - 1] != '\n')
-        snprintf(msg_buf + strlen(msg_buf), sizeof(msg_buf) - strlen(msg_buf), "\n");
+        snprintf(msg_buf + strlen(msg_buf), MSG_BUFFER - strlen(msg_buf), "\n");
 
     /* write log */
     if (safewrite(ctl->log_fd, msg_buf, strlen(msg_buf)) < 0) {