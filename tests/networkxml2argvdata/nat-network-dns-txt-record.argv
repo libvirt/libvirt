libvirt/libvirt

commit 60b9c693138f42254317f19a5dc238c2c0c30332

Network: Add regression tests for the command-line arguments
The regression testing done by comparison of command-line
generated from the network XML file and the expected
command-line arguments (read from file).

Signed-off-by: Michal Novotny <minovotn@redhat.com>

tests/networkxml2argvdata/isolated-network.argv
@@ -1,0 +1,1 @@
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= --except-interface lo --dhcp-option=3 --listen-address 192.168.152.1 --dhcp-range 192.168.152.2,192.168.152.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/private.leases --dhcp-lease-max=253 --dhcp-no-override

tests/networkxml2argvdata/nat-network-dns-txt-record.argv
@@ -1,0 +1,1 @@
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= --except-interface lo --txt-record=example,example value --listen-address 192.168.122.1 --listen-address 192.168.123.1 --listen-address 2001:db8:ac10:fe01::1 --listen-address 2001:db8:ac10:fd01::1 --listen-address 10.24.10.1 --dhcp-range 192.168.122.2,192.168.122.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/default.leases --dhcp-lease-max=253 --dhcp-no-override

tests/networkxml2argvdata/nat-network.argv
@@ -1,0 +1,1 @@
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= --except-interface lo --listen-address 192.168.122.1 --listen-address 192.168.123.1 --listen-address 2001:db8:ac10:fe01::1 --listen-address 2001:db8:ac10:fd01::1 --listen-address 10.24.10.1 --dhcp-range 192.168.122.2,192.168.122.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/default.leases --dhcp-lease-max=253 --dhcp-no-override

tests/networkxml2argvdata/netboot-network.argv
@@ -1,0 +1,1 @@
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --domain example.com --conf-file= --except-interface lo --listen-address 192.168.122.1 --dhcp-range 192.168.122.2,192.168.122.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/netboot.leases --dhcp-lease-max=253 --dhcp-no-override --enable-tftp --tftp-root /var/lib/tftproot --dhcp-boot pxeboot.img

tests/networkxml2argvdata/netboot-proxy-network.argv
@@ -1,0 +1,1 @@
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --domain example.com --conf-file= --except-interface lo --listen-address 192.168.122.1 --dhcp-range 192.168.122.2,192.168.122.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/netboot.leases --dhcp-lease-max=253 --dhcp-no-override --dhcp-boot pxeboot.img,,10.20.30.40

tests/networkxml2argvdata/routed-network.argv
@@ -1,0 +1,1 @@
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= --except-interface lo --listen-address 192.168.122.1

tests/Makefile.am
@@ -50,6 +50,7 @@ EXTRA_DIST =		\
 	networkschematest \
 	networkxml2xmlin \
 	networkxml2xmlout \
+	networkxml2argvdata \
 	nodedevschemadata \
 	nodedevschematest \
 	nodeinfodata     \
@@ -112,6 +113,8 @@ check_PROGRAMS += object-locking
 endif
 
 check_PROGRAMS += networkxml2xmltest
+
+check_PROGRAMS += networkxml2argvtest
 
 check_PROGRAMS += nwfilterxml2xmltest
 
@@ -225,6 +228,8 @@ TESTS += eventtest
 endif
 
 TESTS += networkxml2xmltest
+
+TESTS += networkxml2argvtest
 
 TESTS += storagevolxml2xmltest storagepoolxml2xmltest
 
@@ -352,6 +357,11 @@ networkxml2xmltest_SOURCES = \
 	networkxml2xmltest.c \
 	testutils.c testutils.h
 networkxml2xmltest_LDADD = $(LDADDS)
+
+networkxml2argvtest_SOURCES = \
+	networkxml2argvtest.c \
+	testutils.c testutils.h
+networkxml2argvtest_LDADD = ../src/libvirt_driver_network.la $(LDADDS)
 
 nwfilterxml2xmltest_SOURCES = \
 	nwfilterxml2xmltest.c \

tests/networkxml2argvdata/isolated-network.xml
@@ -1,0 +1,11 @@
+<network>
+  <name>private</name>
+  <uuid>81ff0d90-c91e-6742-64da-4a736edb9a9b</uuid>
+  <bridge name='virbr2' stp='on' delay='0' />
+  <mac address='52:54:00:17:3F:37'/>
+  <ip address='192.168.152.1' netmask='255.255.255.0'>
+    <dhcp>
+      <range start='192.168.152.2' end='192.168.152.254' />
+    </dhcp>
+  </ip>
+</network>

commit 2abb4616a068085cae6b20592d036e7b67a948d5

build: avoid long line tests
'make syntax-check' regression introduced in commit 60b9c69.

* tests/networkxml2argvdata/*.argv: Break long lines.

.gitignore
@@ -57,6 +57,7 @@
 /tests/*.log
 /tests/cputest
 /tests/hashtest
+/tests/networkxml2argvtest
 /tests/nwfilterxml2xmltest
 /tests/openvzutilstest
 /update.log

tests/networkxml2argvdata/isolated-network.argv
@@ -1,1 +1,5 @@
-/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= --except-interface lo --dhcp-option=3 --listen-address 192.168.152.1 --dhcp-range 192.168.152.2,192.168.152.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/private.leases --dhcp-lease-max=253 --dhcp-no-override
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= \
+--except-interface lo --dhcp-option=3 --listen-address 192.168.152.1 \
+--dhcp-range 192.168.152.2,192.168.152.254 \
+--dhcp-leasefile=/var/lib/libvirt/dnsmasq/private.leases --dhcp-lease-max=253 \
+--dhcp-no-override\

tests/networkxml2argvdata/nat-network-dns-hosts.argv
@@ -1,1 +1,3 @@
-/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= --except-interface lo --listen-address 192.168.122.1 --addn-hosts=/var/lib/libvirt/dnsmasq/default.addnhosts
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= \
+--except-interface lo --listen-address 192.168.122.1 \
+--addn-hosts=/var/lib/libvirt/dnsmasq/default.addnhosts\

tests/networkxml2argvdata/nat-network-dns-txt-record.argv
@@ -1,1 +1,8 @@
-/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= --except-interface lo --txt-record=example,example value --listen-address 192.168.122.1 --listen-address 192.168.123.1 --listen-address 2001:db8:ac10:fe01::1 --listen-address 2001:db8:ac10:fd01::1 --listen-address 10.24.10.1 --dhcp-range 192.168.122.2,192.168.122.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/default.leases --dhcp-lease-max=253 --dhcp-no-override
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= \
+--except-interface lo --txt-record=example,example value \
+--listen-address 192.168.122.1 --listen-address 192.168.123.1 \
+--listen-address 2001:db8:ac10:fe01::1 \
+--listen-address 2001:db8:ac10:fd01::1 --listen-address 10.24.10.1 \
+--dhcp-range 192.168.122.2,192.168.122.254 \
+--dhcp-leasefile=/var/lib/libvirt/dnsmasq/default.leases \
+--dhcp-lease-max=253 --dhcp-no-override\

tests/networkxml2argvdata/nat-network.argv
@@ -1,1 +1,7 @@
-/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= --except-interface lo --listen-address 192.168.122.1 --listen-address 192.168.123.1 --listen-address 2001:db8:ac10:fe01::1 --listen-address 2001:db8:ac10:fd01::1 --listen-address 10.24.10.1 --dhcp-range 192.168.122.2,192.168.122.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/default.leases --dhcp-lease-max=253 --dhcp-no-override
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= \
+--except-interface lo --listen-address 192.168.122.1 \
+--listen-address 192.168.123.1 --listen-address 2001:db8:ac10:fe01::1 \
+--listen-address 2001:db8:ac10:fd01::1 --listen-address 10.24.10.1 \
+--dhcp-range 192.168.122.2,192.168.122.254 \
+--dhcp-leasefile=/var/lib/libvirt/dnsmasq/default.leases \
+--dhcp-lease-max=253 --dhcp-no-override\

tests/networkxml2argvdata/netboot-network.argv
@@ -1,1 +1,6 @@
-/usr/sbin/dnsmasq --strict-order --bind-interfaces --domain example.com --conf-file= --except-interface lo --listen-address 192.168.122.1 --dhcp-range 192.168.122.2,192.168.122.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/netboot.leases --dhcp-lease-max=253 --dhcp-no-override --enable-tftp --tftp-root /var/lib/tftproot --dhcp-boot pxeboot.img
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --domain example.com \
+--conf-file= --except-interface lo --listen-address 192.168.122.1 \
+--dhcp-range 192.168.122.2,192.168.122.254 \
+--dhcp-leasefile=/var/lib/libvirt/dnsmasq/netboot.leases \
+--dhcp-lease-max=253 --dhcp-no-override --enable-tftp \
+--tftp-root /var/lib/tftproot --dhcp-boot pxeboot.img\

tests/networkxml2argvdata/netboot-proxy-network.argv
@@ -1,1 +1,5 @@
-/usr/sbin/dnsmasq --strict-order --bind-interfaces --domain example.com --conf-file= --except-interface lo --listen-address 192.168.122.1 --dhcp-range 192.168.122.2,192.168.122.254 --dhcp-leasefile=/var/lib/libvirt/dnsmasq/netboot.leases --dhcp-lease-max=253 --dhcp-no-override --dhcp-boot pxeboot.img,,10.20.30.40
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --domain example.com \
+--conf-file= --except-interface lo --listen-address 192.168.122.1 \
+--dhcp-range 192.168.122.2,192.168.122.254 \
+--dhcp-leasefile=/var/lib/libvirt/dnsmasq/netboot.leases \
+--dhcp-lease-max=253 --dhcp-no-override --dhcp-boot pxeboot.img,,10.20.30.40\

tests/networkxml2argvdata/routed-network.argv
@@ -1,1 +1,2 @@
-/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= --except-interface lo --listen-address 192.168.122.1
+/usr/sbin/dnsmasq --strict-order --bind-interfaces --conf-file= \
+--except-interface lo --listen-address 192.168.122.1\