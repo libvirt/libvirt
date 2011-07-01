ab0b2c1996e94a198480d81f87036153d34d4429
Matthias Bolte <matthias.bolte@googlemail.com>
2011-07-01 19:58:15 +0200

    tests: Add a general util test
    Move non-esx specific tests from esxutilstest there and add a
    test for virParseVersionString.

--- tests/esxutilstest.c
+++ tests/esxutilstest.c
@@ -24,74 +24,6 @@ testQuietError(void *userData ATTRIBUTE_UNUSED,
 
 
 
-static const char* names[] = {
-    "sda",  "sdb",  "sdc",  "sdd",  "sde",  "sdf",  "sdg",  "sdh",  "sdi",  "sdj",  "sdk",  "sdl",  "sdm",  "sdn",  "sdo",  "sdp",  "sdq",  "sdr",  "sds",  "sdt",  "sdu",  "sdv",  "sdw",  "sdx",  "sdy",  "sdz",
-    "sdaa", "sdab", "sdac", "sdad", "sdae", "sdaf", "sdag", "sdah", "sdai", "sdaj", "sdak", "sdal", "sdam", "sdan", "sdao", "sdap", "sdaq", "sdar", "sdas", "sdat", "sdau", "sdav", "sdaw", "sdax", "sday", "sdaz",
-    "sdba", "sdbb", "sdbc", "sdbd", "sdbe", "sdbf", "sdbg", "sdbh", "sdbi", "sdbj", "sdbk", "sdbl", "sdbm", "sdbn", "sdbo", "sdbp", "sdbq", "sdbr", "sdbs", "sdbt", "sdbu", "sdbv", "sdbw", "sdbx", "sdby", "sdbz",
-    "sdca", "sdcb", "sdcc", "sdcd", "sdce", "sdcf", "sdcg", "sdch", "sdci", "sdcj", "sdck", "sdcl", "sdcm", "sdcn", "sdco", "sdcp", "sdcq", "sdcr", "sdcs", "sdct", "sdcu", "sdcv", "sdcw", "sdcx", "sdcy", "sdcz",
-    "sdda", "sddb", "sddc", "sddd", "sdde", "sddf", "sddg", "sddh", "sddi", "sddj", "sddk", "sddl", "sddm", "sddn", "sddo", "sddp", "sddq", "sddr", "sdds", "sddt", "sddu", "sddv", "sddw", "sddx", "sddy", "sddz",
-    "sdea", "sdeb", "sdec", "sded", "sdee", "sdef", "sdeg", "sdeh", "sdei", "sdej", "sdek", "sdel", "sdem", "sden", "sdeo", "sdep", "sdeq", "sder", "sdes", "sdet", "sdeu", "sdev", "sdew", "sdex", "sdey", "sdez",
-    "sdfa", "sdfb", "sdfc", "sdfd", "sdfe", "sdff", "sdfg", "sdfh", "sdfi", "sdfj", "sdfk", "sdfl", "sdfm", "sdfn", "sdfo", "sdfp", "sdfq", "sdfr", "sdfs", "sdft", "sdfu", "sdfv", "sdfw", "sdfx", "sdfy", "sdfz",
-    "sdga", "sdgb", "sdgc", "sdgd", "sdge", "sdgf", "sdgg", "sdgh", "sdgi", "sdgj", "sdgk", "sdgl", "sdgm", "sdgn", "sdgo", "sdgp", "sdgq", "sdgr", "sdgs", "sdgt", "sdgu", "sdgv", "sdgw", "sdgx", "sdgy", "sdgz",
-    "sdha", "sdhb", "sdhc", "sdhd", "sdhe", "sdhf", "sdhg", "sdhh", "sdhi", "sdhj", "sdhk", "sdhl", "sdhm", "sdhn", "sdho", "sdhp", "sdhq", "sdhr", "sdhs", "sdht", "sdhu", "sdhv", "sdhw", "sdhx", "sdhy", "sdhz",
-    "sdia", "sdib", "sdic", "sdid", "sdie", "sdif", "sdig", "sdih", "sdii", "sdij", "sdik", "sdil", "sdim", "sdin", "sdio", "sdip", "sdiq", "sdir", "sdis", "sdit", "sdiu", "sdiv", "sdiw", "sdix", "sdiy", "sdiz"
-};
-
-static int
-testIndexToDiskName(const void *data ATTRIBUTE_UNUSED)
-{
-    int i;
-    char *name = NULL;
-
-    for (i = 0; i < ARRAY_CARDINALITY(names); ++i) {
-        VIR_FREE(name);
-
-        name = virIndexToDiskName(i, "sd");
-
-        if (STRNEQ(names[i], name)) {
-            virtTestDifference(stderr, names[i], name);
-            VIR_FREE(name);
-
-            return -1;
-        }
-    }
-
-    VIR_FREE(name);
-
-    return 0;
-}
-
-static int
-testDiskNameToIndex(const void *data ATTRIBUTE_UNUSED)
-{
-    int i, k;
-    char *name = NULL;
-
-    for (i = 0; i < 100000; ++i) {
-        VIR_FREE(name);
-
-        name = virIndexToDiskName(i, "sd");
-        k = virDiskNameToIndex(name);
-
-        if (k != i) {
-            if (virTestGetDebug() > 0) {
-                fprintf(stderr, "\nExpect [%d]\n", i);
-                fprintf(stderr, "Actual [%d]\n", k);
-            }
-
-            VIR_FREE(name);
-
-            return -1;
-        }
-    }
-
-    VIR_FREE(name);
-
-    return 0;
-}
-
-
-
 struct testPath {
     const char *datastorePath;
     int result;
@@ -336,8 +268,6 @@ mymain(void)
             }                                                                 \
         } while (0)
 
-    DO_TEST(IndexToDiskName);
-    DO_TEST(DiskNameToIndex);
     DO_TEST(ParseDatastorePath);
     DO_TEST(ConvertDateTimeToCalendarTime);
     DO_TEST(EscapeDatastoreItem);
