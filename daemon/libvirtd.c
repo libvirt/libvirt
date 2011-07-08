libvirt/libvirt

commit 8e2e47803c0028b42b72c45b81e91de7b0cbb7fe

Don't exit if the libvirtd config does not exist
It is common for the $HOME/.libvirt/libvirtd.conf file to not
exist. Treat this situation as non-fatal since we can carry
on with our default settings just fine.

* daemon/libvirtd.c: Treat ENOENT as non-fatal when loading
  config

daemon/libvirtd.c
@@ -1028,6 +1028,10 @@ daemonConfigLoad(struct daemonConfig *data,
 {
     virConfPtr conf;
 
+    if (access(filename, R_OK) == -1 &&
+        errno == ENOENT)
+        return 0;
+
     conf = virConfReadFile (filename, 0);
     if (!conf)
         return -1;

commit a07c81c4d0befc1e92b9cf3ce25260488a6bdcdb

libvirtd: diagnose explicitly requested but missing conf file
Fixes test regression introduced in commit 8e2e4780.

* daemon/libvirtd.c (daemonConfigLoad): Add argument.
(main): Update caller.

daemon/libvirtd.c
@@ -1024,11 +1024,13 @@ daemonConfigFree(struct daemonConfig *data)
  */
 static int
 daemonConfigLoad(struct daemonConfig *data,
-                 const char *filename)
+                 const char *filename,
+                 bool allow_missing)
 {
     virConfPtr conf;
 
-    if (access(filename, R_OK) == -1 &&
+    if (allow_missing &&
+        access(filename, R_OK) == -1 &&
         errno == ENOENT)
         return 0;
 
@@ -1282,6 +1284,7 @@ int main(int argc, char **argv) {
     int ipsock = 0;
     struct daemonConfig *config;
     bool privileged = geteuid() == 0 ? true : false;
+    bool implicit_conf = false;
 
     struct option opts[] = {
         { "verbose", no_argument, &verbose, 1},
@@ -1367,14 +1370,16 @@ int main(int argc, char **argv) {
         exit(EXIT_FAILURE);
 
     /* No explicit config, so try and find a default one */
-    if (remote_config_file == NULL &&
-        daemonConfigFilePath(privileged,
-                             &remote_config_file) < 0)
-        exit(EXIT_FAILURE);
+    if (remote_config_file == NULL) {
+        implicit_conf = true;
+        if (daemonConfigFilePath(privileged,
+                                 &remote_config_file) < 0)
+            exit(EXIT_FAILURE);
+    }
 
     /* Read the config file if it exists*/
     if (remote_config_file &&
-        daemonConfigLoad(config, remote_config_file) < 0)
+        daemonConfigLoad(config, remote_config_file, implicit_conf) < 0)
         exit(EXIT_FAILURE);
 
     if (config->host_uuid &&