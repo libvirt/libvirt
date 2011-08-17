libvirt/libvirt

commit 5d30db09a682ef905551d56fed806f118e9c1b63

Support changing UNIX socket owner in virNetSocketNewListenUNIX
This patch allows owner's UID to be changed as well.

src/rpc/virnetsocket.c
@@ -302,6 +302,7 @@ error:
 #if HAVE_SYS_UN_H
 int virNetSocketNewListenUNIX(const char *path,
                               mode_t mask,
+                              uid_t user,
                               gid_t grp,
                               virNetSocketPtr *retsock)
 {
@@ -344,10 +345,10 @@ int virNetSocketNewListenUNIX(const char *path,
     /* chown() doesn't work for abstract sockets but we use them only
      * if libvirtd runs unprivileged
      */
-    if (grp != 0 && chown(path, -1, grp)) {
+    if (grp != 0 && chown(path, user, grp)) {
         virReportSystemError(errno,
-                             _("Failed to change group ID of '%s' to %u"),
-                             path, (unsigned int) grp);
+                             _("Failed to change ownership of '%s' to %d:%d"),
+                             path, (int) user, (int) grp);
         goto error;
     }
 

src/rpc/virnetserverservice.c
@@ -182,6 +182,7 @@ virNetServerServicePtr virNetServerServiceNewUNIX(const char *path,
 
     if (virNetSocketNewListenUNIX(path,
                                   mask,
+                                  -1,
                                   grp,
                                   &svc->socks[0]) < 0)
         goto error;

src/rpc/virnetsocket.h
@@ -47,6 +47,7 @@ int virNetSocketNewListenTCP(const char *nodename,
 
 int virNetSocketNewListenUNIX(const char *path,
                               mode_t mask,
+                              uid_t user,
                               gid_t grp,
                               virNetSocketPtr *addr);
 

tests/virnetsockettest.c
@@ -214,7 +214,7 @@ static int testSocketUNIXAccept(const void *data ATTRIBUTE_UNUSED)
         }
     }
 
-    if (virNetSocketNewListenUNIX(path, 0700, getgid(), &lsock) < 0)
+    if (virNetSocketNewListenUNIX(path, 0700, -1, getgid(), &lsock) < 0)
         goto cleanup;
 
     if (virNetSocketListen(lsock, 0) < 0)
@@ -263,7 +263,7 @@ static int testSocketUNIXAddrs(const void *data ATTRIBUTE_UNUSED)
         }
     }
 
-    if (virNetSocketNewListenUNIX(path, 0700, getgid(), &lsock) < 0)
+    if (virNetSocketNewListenUNIX(path, 0700, -1, getgid(), &lsock) < 0)
         goto cleanup;
 
     if (STRNEQ(virNetSocketLocalAddrString(lsock), "127.0.0.1;0")) {

commit 310b09ec270f34f71095f0c9e107b27babcf1c9c

build: fix virnetsocket on mingw
Regression introduced in commit 5d30db0.

* src/rpc/virnetsocket.c (virNetSocketNewListenUNIX) [WIN32]: Use
correct signature.

src/rpc/virnetsocket.c
@@ -366,6 +366,7 @@ error:
 #else
 int virNetSocketNewListenUNIX(const char *path ATTRIBUTE_UNUSED,
                               mode_t mask ATTRIBUTE_UNUSED,
+                              uid_t user ATTRIBUTE_UNUSED,
                               gid_t grp ATTRIBUTE_UNUSED,
                               virNetSocketPtr *retsock ATTRIBUTE_UNUSED)
 {