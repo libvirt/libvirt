libvirt/libvirt

commit 74c75671331d284e1f777f9692b72e9737520bf0

daemon: initialize GnuTLS
When spice_tls is set but listen_tls is not, we don't initialize
GnuTLS library. So any later gnutls call (e.g. during migration,
where we initialize a certificate) will access uninitialized GnuTLS
internal structs and throws an error.

Although, we might now initialize GnuTLS twice, it is safe according
to the documentation:

    This function can be called many times,
    but will only do something the first time.

This patch creates 2 functions: virNetTLSInit and virNetTLSDeinit
with respect to written above.

daemon/libvirtd.c
@@ -1516,6 +1516,7 @@ int main(int argc, char **argv) {
     virHookCall(VIR_HOOK_DRIVER_DAEMON, "-", VIR_HOOK_DAEMON_OP_START,
                 0, "start", NULL);
 
+    virNetTLSInit();
     if (daemonSetupNetworking(srv, config,
                               sock_file, sock_file_ro,
                               ipsock, privileged) < 0) {
@@ -1554,6 +1555,7 @@ cleanup:
     virNetServerProgramFree(qemuProgram);
     virNetServerClose(srv);
     virNetServerFree(srv);
+    virNetTLSDeinit();
     if (statuswrite != -1) {
         if (ret != 0) {
             /* Tell parent of daemon what failed */

src/rpc/virnettlscontext.c
@@ -679,9 +679,6 @@ static virNetTLSContextPtr virNetTLSContextNew(const char *cacert,
 
     ctxt->refs = 1;
 
-    /* Initialise GnuTLS. */
-    gnutls_global_init();
-
     if ((gnutlsdebug = getenv("LIBVIRT_GNUTLS_DEBUG")) != NULL) {
         int val;
         if (virStrToLong_i(gnutlsdebug, NULL, 10, &val) < 0)
@@ -1398,4 +1395,26 @@ void virNetTLSSessionFree(virNetTLSSessionPtr sess)
     virMutexUnlock(&sess->lock);
     virMutexDestroy(&sess->lock);
     VIR_FREE(sess);
+}
+
+/*
+ * This function MUST be called before any
+ * virNetTLS* because it initializes
+ * underlying GnuTLS library. According to
+ * it's documentation, it's safe to be called
+ * many times, but is not thread safe. Each
+ * call SHOULD be later followed by
+ * virNetTLSContextDeinit.
+ */
+void virNetTLSInit(void)
+{
+    gnutls_global_init();
+}
+
+/*
+ * See virNetTLSInit
+ */
+void virNetTLSDeinit(void)
+{
+    gnutls_global_deinit();
 }

src/rpc/virnettlscontext.h
@@ -30,6 +30,9 @@ typedef struct _virNetTLSSession virNetTLSSession;
 typedef virNetTLSSession *virNetTLSSessionPtr;
 
 
+void virNetTLSInit(void);
+void virNetTLSDeinit(void);
+
 virNetTLSContextPtr virNetTLSContextNewServerPath(const char *pkipath,
                                                   bool tryUserPkiPath,
                                                   const char *const*x509dnWhitelist,

commit eaddec976ef06457fee4a4ce86b8c7ee906183b7

daemon: Move TLS initialization to virInitialize
My previous patch 74c75671331d284e1f777f9692b72e9737520bf0
introduced a regression by removing TLS initialization from client.

daemon/libvirtd.c
@@ -1516,7 +1516,6 @@ int main(int argc, char **argv) {
     virHookCall(VIR_HOOK_DRIVER_DAEMON, "-", VIR_HOOK_DAEMON_OP_START,
                 0, "start", NULL);
 
-    virNetTLSInit();
     if (daemonSetupNetworking(srv, config,
                               sock_file, sock_file_ro,
                               ipsock, privileged) < 0) {
@@ -1555,7 +1554,6 @@ cleanup:
     virNetServerProgramFree(qemuProgram);
     virNetServerClose(srv);
     virNetServerFree(srv);
-    virNetTLSDeinit();
     if (statuswrite != -1) {
         if (ret != 0) {
             /* Tell parent of daemon what failed */

src/libvirt.c
@@ -40,6 +40,7 @@
 #include "memory.h"
 #include "configmake.h"
 #include "intprops.h"
+#include "rpc/virnettlscontext.h"
 
 #ifndef WITH_DRIVER_MODULES
 # ifdef WITH_TEST
@@ -412,6 +413,8 @@ virInitialize(void)
     gcry_check_version(NULL);
 
     virLogSetFromEnv();
+
+    virNetTLSInit();
 
     VIR_DEBUG("register drivers");
 

src/rpc/virnettlscontext.c
@@ -1410,11 +1410,3 @@ void virNetTLSInit(void)
 {
     gnutls_global_init();
 }
-
-/*
- * See virNetTLSInit
- */
-void virNetTLSDeinit(void)
-{
-    gnutls_global_deinit();
-}

src/rpc/virnettlscontext.h
@@ -31,7 +31,6 @@ typedef virNetTLSSession *virNetTLSSessionPtr;
 
 
 void virNetTLSInit(void);
-void virNetTLSDeinit(void);
 
 virNetTLSContextPtr virNetTLSContextNewServerPath(const char *pkipath,
                                                   bool tryUserPkiPath,