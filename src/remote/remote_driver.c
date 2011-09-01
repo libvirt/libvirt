libvirt/libvirt

commit d4b53ef6cfd7c8614b8a58325fad660e830503ed

remote: Cleanup improper VIR_ERR_NO_SUPPORT use

src/remote/remote_driver.c
@@ -2928,7 +2928,7 @@ static int remoteDomainEventRegister(virConnectPtr conn,
     remoteDriverLock(priv);
 
     if (priv->domainEventState->timer < 0) {
-         remoteError(VIR_ERR_NO_SUPPORT, "%s", _("no event support"));
+         remoteError(VIR_ERR_OPERATION_INVALID, "%s", _("no event support"));
          goto done;
     }
 
@@ -3277,7 +3277,7 @@ remoteSecretGetValue (virSecretPtr secret, size_t *value_size,
 
     /* internalFlags intentionally do not go over the wire */
     if (internalFlags) {
-        remoteError(VIR_ERR_NO_SUPPORT, "%s", _("no internalFlags support"));
+        remoteError(VIR_ERR_OPERATION_INVALID, "%s", _("no internalFlags support"));
         goto done;
     }
 
@@ -3541,7 +3541,7 @@ static int remoteDomainEventRegisterAny(virConnectPtr conn,
     remoteDriverLock(priv);
 
     if (priv->domainEventState->timer < 0) {
-         remoteError(VIR_ERR_NO_SUPPORT, "%s", _("no event support"));
+         remoteError(VIR_ERR_OPERATION_INVALID, "%s", _("no event support"));
          goto done;
     }
 

commit fd038a337bb370c545ba0d7319611cfc4f8c1731

remote: Fix incorrect error codes
Introduced by d4b53ef6c. For "no internalFlags support", the
error code is changed into INTERNAL_ERROR.

src/remote/remote_driver.c
@@ -2936,7 +2936,7 @@ static int remoteDomainEventRegister(virConnectPtr conn,
     remoteDriverLock(priv);
 
     if (priv->domainEventState->timer < 0) {
-         remoteError(VIR_ERR_OPERATION_INVALID, "%s", _("no event support"));
+         remoteError(VIR_ERR_NO_SUPPORT, "%s", _("no event support"));
          goto done;
     }
 
@@ -3285,7 +3285,7 @@ remoteSecretGetValue (virSecretPtr secret, size_t *value_size,
 
     /* internalFlags intentionally do not go over the wire */
     if (internalFlags) {
-        remoteError(VIR_ERR_OPERATION_INVALID, "%s", _("no internalFlags support"));
+        remoteError(VIR_ERR_INTERNAL_ERROR, "%s", _("no internalFlags support"));
         goto done;
     }
 
@@ -3549,7 +3549,7 @@ static int remoteDomainEventRegisterAny(virConnectPtr conn,
     remoteDriverLock(priv);
 
     if (priv->domainEventState->timer < 0) {
-         remoteError(VIR_ERR_OPERATION_INVALID, "%s", _("no event support"));
+         remoteError(VIR_ERR_NO_SUPPORT, "%s", _("no event support"));
          goto done;
     }
 