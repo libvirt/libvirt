776124e6f0a05989d7779735ea44e841439c3596
Laine Stump <laine@laine.org>
2011-09-29 13:00:32 -0400

    qemu: make PCI multifunction support more manual
    When support for was added for PCI multifunction cards (in commit
    9f8baf, first included in libvirt 0.9.3), it was done by always
    turning on the multifunction bit for all PCI devices. Since that time
    it has been realized that this is not an ideal solution, and that the
    multifunction bit must be selectively turned on. For example, see
    
      https://bugzilla.redhat.com/show_bug.cgi?id=728174
    
    and the discussion before and after
    
      https://www.redhat.com/archives/libvir-list/2011-September/msg01036.html
    
    This patch modifies multifunction support so that the multifunction=on
    option is only added to the qemu commandline for a device if its PCI
    <address> definition has the attribute "multifunction='on'", e.g.:
    
      <address type='pci' domain='0x0000' bus='0x00'
               slot='0x04' function='0x0' multifunction='on'/>
    
    In practice, the multifunction bit should only be turned on if
    function='0' AND other functions will be used in the same slot - it
    usually isn't needed for functions 1-7 (although there are apparently
    some exceptions, e.g. the Intel X53 according to the QEMU source
    code), and should never be set if only function 0 will be used in the
    slot. The test cases have been changed accordingly to illustrate.
    
    With this patch in place, if a user attempts to assign multiple
    functions in a slot without setting the multifunction bit for function
    0, libvirt will issue an error when the domain is defined, and the
    define operation will fail. In the future, we may decide to detect
    this situation and automatically add multifunction=on to avoid the
    error; even then it will still be useful to have a manual method of
    turning on multifunction since, as stated above, there are some
    devices that excpect it to be turned on for all functions in a slot.
    
    A side effect of this patch is that attempts to use the same PCI
    address for two different devices will now log an error (previously
    this would cause the domain define operation to fail, but there would
    be no log message generated). Because the function doing this log was
    almost completely rewritten, I didn't think it worthwhile to make a
    separate patch for that fix (the entire patch would immediately be
    obsoleted).
    (cherry picked from commit c329db7180d77c8077b9f9cd167a71d7f347227a)

--- src/conf/domain_conf.c
+++ src/conf/domain_conf.c
@@ -138,6 +138,12 @@ VIR_ENUM_IMPL(virDomainDeviceAddress, VIR_DOMAIN_DEVICE_ADDRESS_TYPE_LAST,
               "ccid",
               "usb")
 
+VIR_ENUM_IMPL(virDomainDeviceAddressPciMulti,
+              VIR_DOMAIN_DEVICE_ADDRESS_PCI_MULTI_LAST,
+              "default",
+              "on",
+              "off")
+
 VIR_ENUM_IMPL(virDomainDisk, VIR_DOMAIN_DISK_TYPE_LAST,
               "block",
               "file",
@@ -1645,6 +1651,10 @@ virDomainDeviceInfoFormat(virBufferPtr buf,
                           info->addr.pci.bus,
                           info->addr.pci.slot,
                           info->addr.pci.function);
+        if (info->addr.pci.multi) {
+           virBufferAsprintf(buf, " multifunction='%s'",
+                             virDomainDeviceAddressPciMultiTypeToString(info->addr.pci.multi));
+        }
         break;
 
     case VIR_DOMAIN_DEVICE_ADDRESS_TYPE_DRIVE:
@@ -1689,7 +1699,7 @@ static int
 virDomainDevicePCIAddressParseXML(xmlNodePtr node,
                                   virDomainDevicePCIAddressPtr addr)
 {
-    char *domain, *slot, *bus, *function;
+    char *domain, *slot, *bus, *function, *multi;
     int ret = -1;
 
     memset(addr, 0, sizeof(*addr));
@@ -1698,6 +1708,7 @@ virDomainDevicePCIAddressParseXML(xmlNodePtr node,
     bus      = virXMLPropString(node, "bus");
     slot     = virXMLPropString(node, "slot");
     function = virXMLPropString(node, "function");
+    multi    = virXMLPropString(node, "multifunction");
 
     if (domain &&
         virStrToLong_ui(domain, NULL, 0, &addr->domain) < 0) {
@@ -1727,6 +1738,14 @@ virDomainDevicePCIAddressParseXML(xmlNodePtr node,
         goto cleanup;
     }
 
+    if (multi &&
+        ((addr->multi = virDomainDeviceAddressPciMultiTypeFromString(multi)) <= 0)) {
+        virDomainReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                             _("Unknown value '%s' for <address> 'multifunction' attribute"),
+                             multi);
+        goto cleanup;
+
+    }
     if (!virDomainDevicePCIAddressIsValid(addr)) {
         virDomainReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                              _("Insufficient specification for PCI address"));
@@ -1740,6 +1759,7 @@ cleanup:
     VIR_FREE(bus);
     VIR_FREE(slot);
     VIR_FREE(function);
+    VIR_FREE(multi);
     return ret;
 }
 
