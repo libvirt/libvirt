49bc39be662744024358691b9bdb33adc4cd2228
Marc-André Lureau <marcandre.lureau@gmail.com>
2011-10-01 03:39:14 +0200

    Set to NULL members that have been freed to prevent crashes
    Do not crash if virStreamFinish is called after error.
    
    ==11000== Invalid read of size 4
    ==11000==    at 0x373A8099A0: pthread_mutex_lock (pthread_mutex_lock.c:51)
    ==11000==    by 0x4C7CADE: virMutexLock (threads-pthread.c:85)
    ==11000==    by 0x4D57C31: virNetClientStreamRaiseError (virnetclientstream.c:203)
    ==11000==    by 0x4D385E4: remoteStreamFinish (remote_driver.c:3541)
    ==11000==    by 0x4D182F9: virStreamFinish (libvirt.c:14157)
    ==11000==    by 0x40FDC4: cmdScreenshot (virsh.c:3075)
    ==11000==    by 0x42BA40: vshCommandRun (virsh.c:14922)
    ==11000==    by 0x42ECCA: main (virsh.c:16381)
    ==11000==  Address 0x59b86c0 is 16 bytes inside a block of size 216 free'd
    ==11000==    at 0x4A06928: free (vg_replace_malloc.c:427)
    ==11000==    by 0x4C69E2B: virFree (memory.c:310)
    ==11000==    by 0x4D57B56: virNetClientStreamFree (virnetclientstream.c:184)
    ==11000==    by 0x4D3DB7A: remoteDomainScreenshot (remote_client_bodies.h:1812)
    ==11000==    by 0x4CFD245: virDomainScreenshot (libvirt.c:2903)
    ==11000==    by 0x40FB73: cmdScreenshot (virsh.c:3029)
    ==11000==    by 0x42BA40: vshCommandRun (virsh.c:14922)
    ==11000==    by 0x42ECCA: main (virsh.c:16381)
    (cherry picked from commit be5ec76630c4af83404e8fbf2b5a6b7cb46656e7)

--- src/rpc/gendispatch.pl
+++ src/rpc/gendispatch.pl
@@ -1478,6 +1478,8 @@ elsif ($opt_k) {
         if ($call->{streamflag} ne "none") {
             print "        virNetClientRemoveStream(priv->client, netst);\n";
             print "        virNetClientStreamFree(netst);\n";
+            print "        st->driver = NULL;\n";
+            print "        st->privateData = NULL;\n";
         }
 
         print "        goto done;\n";
