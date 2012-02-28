c430248643613e1d1ea453027f4f2911fa01f650
Osier Yang <jyang@redhat.com>
2012-02-28 14:01:31 +0800

    tests: Add tests for virtio-scsi and ibmvscsi controllers

--- tests/qemuxml2xmltest.c
+++ tests/qemuxml2xmltest.c
@@ -40,7 +40,6 @@ testCompareXMLToXMLFiles(const char *inxml, const char *outxml, bool live)
     if (!(actual = virDomainDefFormat(def, VIR_DOMAIN_XML_SECURE)))
         goto fail;
 
-
     if (STRNEQ(outXmlData, actual)) {
         virtTestDifference(stderr, outXmlData, actual);
         goto fail;
@@ -150,6 +149,8 @@ mymain(void)
     DO_TEST("disk-drive-cache-v1-wb");
     DO_TEST("disk-drive-cache-v1-none");
     DO_TEST("disk-scsi-device");
+    DO_TEST("disk-scsi-vscsi");
+    DO_TEST("disk-scsi-virtio-scsi");
     DO_TEST("graphics-listen-network");
     DO_TEST("graphics-vnc");
     DO_TEST("graphics-vnc-sasl");
