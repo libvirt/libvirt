19d309025b33e6e3ac5700566da39369ead587a7
Daniel P. Berrange <berrange@redhat.com>
2012-05-14 11:16:22 +0100

    Fix logic for assigning PCI addresses to USB2 companion controllers
    Currently each USB2 companion controller gets put on a separate
    PCI slot. Not only is this wasteful of PCI slots, but it is not
    in compliance with the spec for USB2 controllers. The master
    echi1 and all companion controllers should be in the same slot,
    with echi1 in function 7, and uhci1-3 in functions 0-2 respectively.
    
    * src/qemu/qemu_command.c: Special case handling of USB2 controllers
      to apply correct pci slot assignment
    * tests/qemuxml2argvdata/qemuxml2argv-usb-ich9-ehci-addr.args,
      tests/qemuxml2argvdata/qemuxml2argv-usb-ich9-ehci-addr.xml: Expand
      test to cover automatic slot assignment
    (cherry picked from commit 1ebd52cb871f87b7868503b28448e96d59e41d63)
    
    Conflicts:
    
    	tests/qemuxml2xmltest.c

--- tests/qemuxml2xmltest.c
+++ tests/qemuxml2xmltest.c
@@ -222,6 +222,7 @@ mymain(void)
     DO_TEST_DIFFERENT("serial-target-port-auto");
     DO_TEST_DIFFERENT("graphics-listen-network2");
     DO_TEST_DIFFERENT("graphics-spice-timeout");
+    DO_TEST_DIFFERENT("usb-ich9-ehci-addr");
 
     DO_TEST_DIFFERENT("metadata");
 
