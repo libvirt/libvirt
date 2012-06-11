libvirt/libvirt

commit 521cc44700f81b0f31711cc6d1ec3fad20c62ae9

snapshot: virsh fallback for snapshot-list --descendants --from
Given a list of snapshots and their parents, finding all descendants
requires a hairy traversal.  This code is O(n^3); it could maybe be
made to scale O(n^2) with the use of a hash table, but that costs more
memory.  Hopefully there aren't too many people with a hierarchy
so large as to approach REMOTE_DOMAIN_SNAPSHOT_LIST_NAMES_MAX (1024).

* tools/virsh.c (cmdSnapshotList): Add final fallback.

tools/virsh.c
@@ -13177,6 +13177,7 @@ cmdSnapshotList(vshControl *ctl, const vshCmd *cmd)
     bool tree = vshCommandOptBool(cmd, "tree");
     const char *from = NULL;
     virDomainSnapshotPtr start = NULL;
+    int start_index = -1;
     bool descendants = false;
 
     if (vshCommandOptString(cmd, "from", &from) < 0) {
@@ -13231,10 +13232,8 @@ cmdSnapshotList(vshControl *ctl, const vshCmd *cmd)
         numsnaps = ctl->useSnapshotOld ? -1 :
             virDomainSnapshotNumChildren(start, flags);
         if (numsnaps < 0) {
-            /* XXX also want to emulate --descendants without --tree */
-            if ((!descendants || tree) &&
-                (ctl->useSnapshotOld ||
-                 last_error->code == VIR_ERR_NO_SUPPORT)) {
+            if (ctl->useSnapshotOld ||
+                last_error->code == VIR_ERR_NO_SUPPORT) {
                 /* We can emulate --from.  */
                 virFreeError(last_error);
                 last_error = NULL;
@@ -13312,6 +13311,11 @@ cmdSnapshotList(vshControl *ctl, const vshCmd *cmd)
     if (tree || ctl->useSnapshotOld) {
         parents = vshCalloc(ctl, sizeof(char *), actual);
         for (i = (from && !ctl->useSnapshotOld); i < actual; i++) {
+            if (ctl->useSnapshotOld && STREQ(names[i], from)) {
+                start_index = i;
+                continue;
+            }
+
             /* free up memory from previous iterations of the loop */
             if (snapshot)
                 virDomainSnapshotFree(snapshot);
@@ -13345,12 +13349,61 @@ cmdSnapshotList(vshControl *ctl, const vshCmd *cmd)
         goto cleanup;
     } else {
         if (ctl->useSnapshotOld && descendants) {
-            /* XXX emulate --descendants as well */
-            goto cleanup;
+            bool changed = false;
+
+            /* Make multiple passes over the list - first pass NULLs
+             * out all roots except start, remaining passes NULL out
+             * any entry whose parent is not still in list.  Also, we
+             * NULL out parent when name is known to be in list.
+             * Sorry, this is O(n^3) - hope your hierarchy isn't huge.  */
+            if (start_index < 0) {
+                vshError(ctl, _("snapshot %s disappeared from list"), from);
+                goto cleanup;
+            }
+            for (i = 0; i < actual; i++) {
+                if (i == start_index)
+                    continue;
+                if (!parents[i]) {
+                    VIR_FREE(names[i]);
+                } else if (STREQ(parents[i], from)) {
+                    VIR_FREE(parents[i]);
+                    changed = true;
+                }
+            }
+            if (!changed) {
+                ret = true;
+                goto cleanup;
+            }
+            while (changed) {
+                changed = false;
+                for (i = 0; i < actual; i++) {
+                    bool found = false;
+                    int j;
+
+                    if (!names[i] || !parents[i])
+                        continue;
+                    for (j = 0; j < actual; j++) {
+                        if (!names[j] || i == j)
+                            continue;
+                        if (STREQ(parents[i], names[j])) {
+                            found = true;
+                            if (!parents[j])
+                                VIR_FREE(parents[i]);
+                            break;
+                        }
+                    }
+                    if (!found) {
+                        changed = true;
+                        VIR_FREE(names[i]);
+                    }
+                }
+            }
+            VIR_FREE(names[start_index]);
         }
 
         for (i = 0; i < actual; i++) {
-            if (ctl->useSnapshotOld && STRNEQ_NULLABLE(parents[i], from))
+            if (ctl->useSnapshotOld &&
+                (descendants ? !names[i] : STRNEQ_NULLABLE(parents[i], from)))
                 continue;
 
             /* free up memory from previous iterations of the loop */

commit d9f606659829e666803d95d05aee9ca1ecced955

snapshot: avoid virsh crash with older servers
Commits 51082301, 16d7b39, and 521cc447 introduced support for
'virsh snapshot-list --from' when talking to a server older than
0.9.5, but broke support for plain 'virsh snapshot-list' for the
same old server in the process.  Because the code is not properly
gated, we end up with a SIGSEGV during a strcmp with a NULL argument.

* tools/virsh.c (cmdSnapshotList): Don't waste time on fallbacks
when --from is not present.

tools/virsh.c
@@ -16740,10 +16740,10 @@ cmdSnapshotList(vshControl *ctl, const vshCmd *cmd)
 
     qsort(&names[0], actual, sizeof(char*), namesorter);
 
-    if (tree || ctl->useSnapshotOld) {
+    if (tree || (from && ctl->useSnapshotOld)) {
         parents = vshCalloc(ctl, sizeof(char *), actual);
         for (i = (from && !ctl->useSnapshotOld); i < actual; i++) {
-            if (ctl->useSnapshotOld && STREQ(names[i], from)) {
+            if (from && ctl->useSnapshotOld && STREQ(names[i], from)) {
                 start_index = i;
                 continue;
             }
@@ -16765,7 +16765,8 @@ cmdSnapshotList(vshControl *ctl, const vshCmd *cmd)
         char indentBuf[INDENT_BUFLEN];
         for (i = 0 ; i < actual ; i++) {
             memset(indentBuf, '\0', sizeof(indentBuf));
-            if (ctl->useSnapshotOld ? STREQ(names[i], from) : !parents[i])
+            if ((from && ctl->useSnapshotOld) ? STREQ(names[i], from) :
+                !parents[i])
                 cmdNodeListDevicesPrint(ctl,
                                         names,
                                         parents,
@@ -16834,7 +16835,7 @@ cmdSnapshotList(vshControl *ctl, const vshCmd *cmd)
         }
 
         for (i = 0; i < actual; i++) {
-            if (ctl->useSnapshotOld &&
+            if (from && ctl->useSnapshotOld &&
                 (descendants ? !names[i] : STRNEQ_NULLABLE(parents[i], from)))
                 continue;
 