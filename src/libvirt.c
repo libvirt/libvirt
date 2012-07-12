a69e46813f8693e40074baae6209e1e93154269a
Jiri Denemark <jdenemar@redhat.com>
2012-07-12 15:27:18 +0200

    Clarify direct migration
    When --direct is used when migrating a domain running on a hypervisor
    that does not support direct migration (such as QEMU), the caller would
    get the following error message:
    
        this function is not supported by the connection driver:
        virDomainMigrateToURI2
    
    which is a complete nonsense since qemu driver implements
    virDomainMigrateToURI2. This patch would emit a more sensible error in
    this case:
    
        Requested operation is not valid: direct migration is not supported
        by the connection driver
    (cherry picked from commit 3189dfb1636da22d426d2fc07cc9f60304b16c5c)

--- src/libvirt.c
+++ src/libvirt.c
@@ -5685,7 +5685,9 @@ virDomainMigrateToURI (virDomainPtr domain,
                 goto error;
         } else {
             /* Cannot do a migration with only the perform step */
-            virLibConnError(VIR_ERR_NO_SUPPORT, __FUNCTION__);
+            virLibConnError(VIR_ERR_OPERATION_INVALID, "%s",
+                            _("direct migration is not supported by the"
+                              " connection driver"));
             goto error;
         }
     }
@@ -5819,7 +5821,9 @@ virDomainMigrateToURI2(virDomainPtr domain,
                 goto error;
         } else {
             /* Cannot do a migration with only the perform step */
-            virLibConnError(VIR_ERR_NO_SUPPORT, __FUNCTION__);
+            virLibConnError(VIR_ERR_OPERATION_INVALID, "%s",
+                            _("direct migration is not supported by the"
+                              " connection driver"));
             goto error;
         }
     }
