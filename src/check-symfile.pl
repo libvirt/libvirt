536a1d7d0a722383d45c70173f4a021eef3ad639
Daniel P. Berrange <berrange@redhat.com>
2012-07-24 14:37:48 +0100

    Add a test case that checks there are no bogus entries in .syms
    During refactoring of code, it has proved common to forget to
    remove old symbols from the .syms file. While the Win32 linker
    will complain about this, the Linux ELF linker does not. The
    new test case validates that every symbol listed in the .syms
    file actually exists in the built ELF libraries.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

---
+++ src/check-symfile.pl
@@ -0,0 +1,54 @@
+#!/usr/bin/perl
+
+die "syntax: $0 SYMFILE ELFLIB(S)" unless int(@ARGV) >= 2;
+
+my $symfile = shift @ARGV;
+my @elflibs = @ARGV;
+
+my @wantsyms;
+my %gotsyms;
+
+# Skip on non-linux
+if ($^O ne "linux") {
+    return 77; # Automake's skip code
+}
+
+open SYMFILE, $symfile or die "cannot read $symfile: $!";
+
+while (<SYMFILE>) {
+    next if /{/;
+    next if /}/;
+    next if /global:/;
+    next if /local:/;
+    next if /^\s*$/;
+    next if /^\s*#/;
+    next if /\*/;
+
+    die "malformed line $_" unless /^\s*(\S+);$/;
+
+    push @wantsyms, $1;
+}
+close SYMFILE;
+
+foreach my $elflib (@elflibs) {
+    open NM, "-|", "nm", $elflib or die "cannot run 'nm $elflib': $!";
+
+    while (<NM>) {
+	next unless /^\S+\s(?:T|D)\s(\S+)\s*$/;
+
+	$gotsyms{$1} = 1;
+    }
+
+    close NM;
+}
+
+my $ret = 0;
+
+foreach my $sym (@wantsyms) {
+    next if exists $gotsyms{$sym};
+
+    print STDERR "Expected symbol $sym is not in ELF library\n";
+    $ret = 1;
+}
+
+exit($ret);
