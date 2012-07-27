a4bcefbcff01c9cdc257127df376929cd80304b1
Osier Yang <jyang@redhat.com>
2012-07-27 17:39:53 +0800

    maint: Use consistent copyright.
    This is a follow up patch of commit f9ce7dad6, it modifies all
    the files which declare the copyright like "See COPYING.LIB for
    the License of this software" to use the detailed/consistent one.
    
    And deserts the outdated comments like:
    
     * libvirt-qemu.h:
     * Summary: qemu specific interfaces
     * Description: Provides the interfaces of the libvirt library to handle
     *              qemu specific methods
     *
     * Copy:  Copyright (C) 2010, 2012 Red Hat, Inc.
    
    Uses the more compact style like:
    
     * libvirt-qemu.h: Interfaces specific for QEMU/KVM driver
     *
     * Copyright (C) 2010, 2012 Red Hat, Inc.

--- include/libvirt/libvirt-qemu.h
+++ include/libvirt/libvirt-qemu.h
@@ -1,12 +1,21 @@
 /* -*- c -*-
- * libvirt-qemu.h:
- * Summary: qemu specific interfaces
- * Description: Provides the interfaces of the libvirt library to handle
- *              qemu specific methods
+ * libvirt-qemu.h: Interfaces specific for QEMU/KVM driver
  *
- * Copy:  Copyright (C) 2010, 2012 Red Hat, Inc.
+ * Copyright (C) 2010, 2012 Red Hat, Inc.
  *
- * See COPYING.LIB for the License of this software
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library;  If not, see
+ * <http://www.gnu.org/licenses/>.
  *
  * Author: Chris Lalancette <clalance@redhat.com>
  */
--- src/util/conf.h
+++ src/util/conf.h
@@ -3,7 +3,19 @@
  *
  * Copyright (C) 2006, 2007 Red Hat, Inc.
  *
- * See COPYING.LIB for the License of this software
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library;  If not, see
+ * <http://www.gnu.org/licenses/>.
  *
  * Daniel Veillard <veillard@redhat.com>
  */
