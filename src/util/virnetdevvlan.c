cfbdd005e9ce58894eb21577d5b99ef5a3dfb672
Laine Stump <laine@laine.org>
2012-08-11 13:44:27 -0400

    util: add virNetDevVlanType
    To allow for the possibility of vlan "trunks", which have more than
    one vlan tag associated with them, we need a vlan struct. Since it
    will be used by multiple files in src/util, src/conf, src/network, and
    src/qemu, it must be defined in src/util. Unfortunately there isn't
    currently a common file for simple netdev data definitions, so I
    created a new file.

---
+++ src/util/virnetdevvlan.c
@@ -0,0 +1,94 @@
+/*
+ * Copyright (C) 2009-2012 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library;  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *      Laine Stump <laine@redhat.com>
+ */
+
+#include <config.h>
+
+#include "internal.h"
+#include "virterror_internal.h"
+#include "virnetdevvlan.h"
+#include "memory.h"
+
+#define VIR_FROM_THIS VIR_FROM_NONE
+
+void
+virNetDevVlanClear(virNetDevVlanPtr vlan)
+{
+    VIR_FREE(vlan->tag);
+    vlan->nTags = 0;
+}
+
+void
+virNetDevVlanFree(virNetDevVlanPtr vlan)
+{
+    if (vlan)
+        virNetDevVlanClear(vlan);
+    VIR_FREE(vlan);
+}
+
+int
+virNetDevVlanEqual(const virNetDevVlanPtr a, const virNetDevVlanPtr b)
+{
+    int ai, bi;
+
+    if (!(a || b))
+        return true;
+    if (!a || !b)
+        return false;
+
+    if (a->trunk != b->trunk ||
+        a->nTags != b->nTags) {
+        return false;
+    }
+
+    for (ai = 0; ai < a->nTags; ai++) {
+        for (bi = 0; bi < b->nTags; bi++) {
+            if (a->tag[ai] == b->tag[bi])
+                break;
+        }
+        if (bi >= b->nTags) {
+            /* no matches for a->tag[ai] anywhere in b->tag */
+            return false;
+        }
+    }
+    return true;
+}
+
+/*
+ * virNetDevVlanCopy - copy from src into (already existing) dst.
+ *                     If src is NULL, dst will have nTags set to 0.
+ *                     dst is assumed to be empty on entry.
+ */
+int
+virNetDevVlanCopy(virNetDevVlanPtr dst, const virNetDevVlanPtr src)
+{
+    if (!src || src->nTags == 0)
+        return 0;
+
+    if (VIR_ALLOC_N(dst->tag, src->nTags) < 0) {
+        virReportOOMError();
+        return -1;
+    }
+
+    dst->trunk = src->trunk;
+    dst->nTags = src->nTags;
+    memcpy(dst->tag, src->tag, src->nTags * sizeof(*src->tag));
+    return 0;
+}
---
+++ src/util/virnetdevvlan.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright (C) 2009-2012 Red Hat, Inc.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library;  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Authors:
+ *      Laine Stump <laine@redhat.com>
+ */
+
+#ifndef __VIR_NETDEV_VLAN_H__
+# define __VIR_NETDEV_VLAN_H__
+
+typedef struct _virNetDevVlan virNetDevVlan;
+typedef virNetDevVlan *virNetDevVlanPtr;
+struct _virNetDevVlan {
+    bool trunk;        /* true if this is a trunk */
+    int nTags;          /* number of tags in array */
+    unsigned int *tag; /* pointer to array of tags */
+};
+
+void virNetDevVlanClear(virNetDevVlanPtr vlan);
+void virNetDevVlanFree(virNetDevVlanPtr vlan);
+int virNetDevVlanEqual(const virNetDevVlanPtr a, const virNetDevVlanPtr b);
+int virNetDevVlanCopy(virNetDevVlanPtr dst, const virNetDevVlanPtr src);
+
+#endif /* __VIR_NETDEV_VLAN_H__ */
