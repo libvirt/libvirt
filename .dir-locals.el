libvirt/libvirt

commit f299ddd6b1d686c32758a9245a4761e4dc0c199a

Remove duplicate symbols and add test case
When the event symbols were added to the public API, not all
of them were removed from the private exports list. Solaris
gets unhappy when there are duplicated symbols. Extend the
symfile check to test for this scenario

src/check-symfile.pl
@@ -5,8 +5,10 @@ die "syntax: $0 SYMFILE ELFLIB(S)" unless int(@ARGV) >= 2;
 my $symfile = shift @ARGV;
 my @elflibs = @ARGV;
 
-my @wantsyms;
+my %wantsyms;
 my %gotsyms;
+
+my $ret = 0;
 
 open SYMFILE, $symfile or die "cannot read $symfile: $!";
 
@@ -21,7 +23,12 @@ while (<SYMFILE>) {
 
     die "malformed line $_" unless /^\s*(\S+);$/;
 
-    push @wantsyms, $1;
+    if (exists $wantsyms{$1}) {
+	print STDERR "Symbol $1 is listed twice\n";
+	$ret = 1;
+    } else {
+	$wantsyms{$1} = 1;
+    }
 }
 close SYMFILE;
 
@@ -36,8 +43,6 @@ foreach my $elflib (@elflibs) {
 
     close NM;
 }
-
-my $ret = 0;
 
 foreach my $sym (@wantsyms) {
     next if exists $gotsyms{$sym};

src/libvirt_private.syms
@@ -1178,8 +1178,6 @@ virDirCreate;
 virDoubleToStr;
 virEnumFromString;
 virEnumToString;
-virEventAddHandle;
-virEventRemoveHandle;
 virFileAbsPath;
 virFileAccessibleAs;
 virFileBuildPath;

commit b6a14aec5f29073b07a84188849a9cba95090588

build: avoid tabs that failed syntax-check
Introduced in commit f299ddd6.

* src/check-symfile.pl: Fix whitespace.
* .dir-locals.el (perl-mode): Prevent future occurrences.

.dir-locals.el
@@ -14,4 +14,7 @@
  (nxml-mode . (
 	       (indent-tabs-mode . nil)
 	       ))
+ (perl-mode . (
+	       (indent-tabs-mode . nil)
+	       ))
  )

src/check-symfile.pl
@@ -24,10 +24,10 @@ while (<SYMFILE>) {
     die "malformed line $_" unless /^\s*(\S+);$/;
 
     if (exists $wantsyms{$1}) {
-	print STDERR "Symbol $1 is listed twice\n";
-	$ret = 1;
+        print STDERR "Symbol $1 is listed twice\n";
+        $ret = 1;
     } else {
-	$wantsyms{$1} = 1;
+        $wantsyms{$1} = 1;
     }
 }
 close SYMFILE;