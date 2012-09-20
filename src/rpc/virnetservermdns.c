2de152d20d9981ac1fbc9f2292c9c7cba42c61ff
Daniel P. Berrange <berrange@redhat.com>
2012-09-20 15:21:13 +0100

    Convert HAVE_AVAHI to WITH_AVAHI
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- src/rpc/virnetservermdns.c
+++ src/rpc/virnetservermdns.c
@@ -29,7 +29,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#if HAVE_AVAHI
+#if WITH_AVAHI
 # include <avahi-client/client.h>
 # include <avahi-client/publish.h>
 
@@ -56,7 +56,7 @@ struct _virNetServerMDNSEntry {
 
 struct _virNetServerMDNSGroup {
     virNetServerMDNSPtr mdns;
-#if HAVE_AVAHI
+#if WITH_AVAHI
     AvahiEntryGroup *handle;
 #endif
     char *name;
@@ -65,14 +65,14 @@ struct _virNetServerMDNSGroup {
 };
 
 struct _virNetServerMDNS {
-#if HAVE_AVAHI
+#if WITH_AVAHI
     AvahiClient *client;
     AvahiPoll *poller;
 #endif
     virNetServerMDNSGroupPtr group;
 };
 
-#if HAVE_AVAHI
+#if WITH_AVAHI
 /* Avahi API requires this struct name in the app :-( */
 struct AvahiWatch {
     int watch;
@@ -619,7 +619,7 @@ void virNetServerMDNSEntryFree(virNetServerMDNSEntryPtr entry)
     VIR_FREE(entry);
 }
 
-#else /* ! HAVE_AVAHI */
+#else /* ! WITH_AVAHI */
 
 static const char *unsupported = N_("avahi not available at build time");
 
@@ -692,4 +692,4 @@ virNetServerMDNSEntryFree(virNetServerMDNSEntryPtr entry ATTRIBUTE_UNUSED)
     VIR_DEBUG("%s", _(unsupported));
 }
 
-#endif /* ! HAVE_AVAHI */
+#endif /* ! WITH_AVAHI */
