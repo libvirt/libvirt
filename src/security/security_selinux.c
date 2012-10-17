4c2a38d1edbee7cdceed665e8d42bfa23728d960
Guannan Ren <gren@redhat.com>
2012-10-17 11:28:28 +0800

    selinux: remove unused variables in socket labelling (cherry picked from commit d37a3a1d6c6508f235965185453602ba310cc66e)

--- src/security/security_selinux.c
+++ src/security/security_selinux.c
@@ -1626,8 +1626,6 @@ virSecuritySELinuxSetSecurityDaemonSocketLabel(virSecurityManagerPtr mgr,
 {
     /* TODO: verify DOI */
     virSecurityLabelDefPtr secdef;
-    context_t execcon = NULL;
-    context_t proccon = NULL;
     security_context_t scon = NULL;
     char *str = NULL;
     int rc = -1;
@@ -1670,8 +1668,6 @@ done:
 
     if (security_getenforce() != 1)
         rc = 0;
-    if (execcon) context_free(execcon);
-    if (proccon) context_free(proccon);
     freecon(scon);
     VIR_FREE(str);
     return rc;
