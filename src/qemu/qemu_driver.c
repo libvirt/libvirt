b50e896ceebd7518c6add45b8fd6dde78a69e4da
Cole Robinson <crobinso@redhat.com>
2012-10-20 22:28:34 -0400

    qemu: Fix domxml-to-native network model conversion
    https://bugzilla.redhat.com/show_bug.cgi?id=636832
    (cherry picked from commit 9a2975786b74aa0fe75e3eadb39aa0f9b37e57bb)
    
    Conflicts:
    	src/qemu/qemu_driver.c

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -4608,6 +4608,8 @@ static char *qemuDomainXMLToNative(virConnectPtr conn,
     for (i = 0 ; i < def->nnets ; i++) {
         virDomainNetDefPtr net = def->nets[i];
         int bootIndex = net->info.bootIndex;
+        char *model = net->model;
+
         if (net->type == VIR_DOMAIN_NET_TYPE_NETWORK) {
             int actualType = virDomainNetGetActualType(net);
             const char *brname;
@@ -4665,7 +4667,9 @@ static char *qemuDomainXMLToNative(virConnectPtr conn,
             net->data.ethernet.dev = brname;
             net->data.ethernet.ipaddr = ipaddr;
         }
+
         net->info.bootIndex = bootIndex;
+        net->model = model;
     }
     for (i = 0 ; i < def->ngraphics ; i++) {
         if (def->graphics[i]->type == VIR_DOMAIN_GRAPHICS_TYPE_VNC &&
