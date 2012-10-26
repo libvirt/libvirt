81af5336acf4c765ef1201e7762d003ae0b0011e
Vladislav Bogdanov <bubble@hoster-ok.com>
2012-10-26 09:09:22 +0000

    qemu: pass -usb and usb hubs earlier, so USB disks with static address are handled properly

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -5083,6 +5083,20 @@ qemuBuildCommandLine(virConnectPtr conn,
         }
     }
 
+    if (usbcontroller == 0)
+        virCommandAddArg(cmd, "-usb");
+
+    for (i = 0 ; i < def->nhubs ; i++) {
+        virDomainHubDefPtr hub = def->hubs[i];
+        char *optstr;
+
+        virCommandAddArg(cmd, "-device");
+        if (!(optstr = qemuBuildHubDevStr(hub, caps)))
+            goto error;
+        virCommandAddArg(cmd, optstr);
+        VIR_FREE(optstr);
+    }
+
     /* If QEMU supports -drive param instead of old -hda, -hdb, -cdrom .. */
     if (qemuCapsGet(caps, QEMU_CAPS_DRIVE)) {
         int bootCD = 0, bootFloppy = 0, bootDisk = 0;
@@ -5815,20 +5829,6 @@ qemuBuildCommandLine(virConnectPtr conn,
                            NULLSTR(virDomainChrConsoleTargetTypeToString(console->targetType)));
             goto error;
         }
-    }
-
-    if (usbcontroller == 0)
-        virCommandAddArg(cmd, "-usb");
-
-    for (i = 0 ; i < def->nhubs ; i++) {
-        virDomainHubDefPtr hub = def->hubs[i];
-        char *optstr;
-
-        virCommandAddArg(cmd, "-device");
-        if (!(optstr = qemuBuildHubDevStr(hub, caps)))
-            goto error;
-        virCommandAddArg(cmd, optstr);
-        VIR_FREE(optstr);
     }
 
     for (i = 0 ; i < def->ninputs ; i++) {
