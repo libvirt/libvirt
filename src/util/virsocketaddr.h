fe98b65947e4880f5b1e8bae16eeacc57d6e99f4
Laine Stump <laine@laine.org>
2012-11-21 21:17:30 -0500

    util: new virSocketAddrIsPrivate function
    This new function returns true if the given address is in the range of
    any "private" or "local" networks as defined in RFC1918 (IPv4) or
    RFC3484/RFC4193 (IPv6), otherwise they return false.
    
    These ranges are:
    
       192.168.0.0/16
       172.16.0.0/16
       10.0.0.0/24
       FC00::/7
       FEC0::/10
    
    (cherry picked from commit bf402e77b6d53a4e569b3aa76aef9c7d589c0cf2)
    Conflicts:
        src/util/virsocketaddr.c
        src/util/virsocketaddr.h
         * both of these files had new functions that had been added
           at the same place virSocketAddrIsPrivate was being added,
           so the context on the branch didn't match.

--- src/util/virsocketaddr.h
+++ src/util/virsocketaddr.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2009-2011 Red Hat, Inc.
+ * Copyright (C) 2009-2012 Red Hat, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -100,5 +100,6 @@ int virSocketAddrGetNumNetmaskBits(const virSocketAddrPtr netmask);
 int virSocketAddrPrefixToNetmask(unsigned int prefix,
                                  virSocketAddrPtr netmask,
                                  int family);
+bool virSocketAddrIsPrivate(const virSocketAddrPtr addr);
 
 #endif /* __VIR_SOCKETADDR_H__ */
