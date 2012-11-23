dd35c8dadd3af1aae761606d4259752b31c723ff
Daniel P. Berrange <berrange@redhat.com>
2012-11-23 16:40:16 +0000

    Ensure transient def is removed if LXC start fails
    When starting a container, newDef is initialized to a
    copy of 'def', but when startup fails newDef is never
    removed. This cause later attempts to use 'virDomainDefine'
    to lose the new data being defined.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    (cherry picked from commit 9d2bfc1ca78eb7106e5d189b9d21ad47e1826e50)

--- src/lxc/lxc_process.c
+++ src/lxc/lxc_process.c
@@ -1184,6 +1184,10 @@ cleanup:
         VIR_FREE(veths[i]);
     }
     if (rc != 0) {
+        if (vm->newDef) {
+            virDomainDefFree(vm->newDef);
+            vm->newDef = NULL;
+        }
         if (priv->monitor) {
             virObjectUnref(priv->monitor);
             priv->monitor = NULL;
