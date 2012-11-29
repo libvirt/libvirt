caf1377a12dd9994f910b7009058cb9399130f2c
Ján Tomko <jtomko@redhat.com>
2012-11-29 11:46:00 +0100

    virsh: do timing even for unusable connections
    Time values were uninitialized if the connection wasn't usable.
    (cherry picked from commit 8b235d40576d5b5077a4ab2ee52a1897211f5e4d)

--- tools/virsh.c
+++ tools/virsh.c
@@ -1549,19 +1549,19 @@ vshCommandRun(vshControl *ctl, const vshCmd *cmd)
             !(cmd->def->flags & VSH_CMD_FLAG_NOCONNECT))
             vshReconnect(ctl);
 
+        if (enable_timing)
+            GETTIMEOFDAY(&before);
+
         if ((cmd->def->flags & VSH_CMD_FLAG_NOCONNECT) ||
             vshConnectionUsability(ctl, ctl->conn)) {
-            if (enable_timing)
-                GETTIMEOFDAY(&before);
-
             ret = cmd->def->handler(ctl, cmd);
-
-            if (enable_timing)
-                GETTIMEOFDAY(&after);
         } else {
             /* connection is not usable, return error */
             ret = false;
         }
+
+        if (enable_timing)
+            GETTIMEOFDAY(&after);
 
         /* try to automatically catch disconnections */
         if (!ret &&
