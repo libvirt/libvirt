8d0e7eb4c11ac7bf6437c0d70a47ee5cc35081fb
Ján Tomko <jtomko@redhat.com>
2012-12-13 14:08:01 +0100

    virsh: use vshReconnect for non-default connections too
    For non-default connections (specified by the environment variable or
    the command line option) we call virConnectOpenAuth without registering
    the vshCatchDisconnect callback.
    
    This calls vshReconnect instead which takes care of it.

--- tools/virsh.c
+++ tools/virsh.c
@@ -2189,10 +2189,7 @@ vshInit(vshControl *ctl)
     ctl->eventLoopStarted = true;
 
     if (ctl->name) {
-        ctl->conn = virConnectOpenAuth(ctl->name,
-                                       virConnectAuthPtrDefault,
-                                       ctl->readonly ? VIR_CONNECT_RO : 0);
-
+        vshReconnect(ctl);
         /* Connecting to a named connection must succeed, but we delay
          * connecting to the default connection until we need it
          * (since the first command might be 'connect' which allows a
@@ -2201,7 +2198,6 @@ vshInit(vshControl *ctl)
          */
         if (!ctl->conn) {
             vshReportError(ctl);
-            vshError(ctl, "%s", _("failed to connect to the hypervisor"));
             return false;
         }
     }
