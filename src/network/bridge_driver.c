libvirt/libvirt

commit 0211fd6e04cdc402da20818df54299c6ded3d3cb

net: Mark network persistent when assigning persistent definition
When assigning the new persistent definition for a transient network
(thus making it persistent) the network needs to be marked persistent
before actually atempting to assign the definition.

src/network/bridge_driver.c
@@ -2822,7 +2822,7 @@ cleanup:
 
 static virNetworkPtr networkDefine(virConnectPtr conn, const char *xml) {
     struct network_driver *driver = conn->networkPrivateData;
-    virNetworkDefPtr def;
+    virNetworkDefPtr def = NULL;
     bool freeDef = true;
     virNetworkObjPtr network = NULL;
     virNetworkPtr ret = NULL;
@@ -2835,11 +2835,17 @@ static virNetworkPtr networkDefine(virConnectPtr conn, const char *xml) {
     if (networkValidate(driver, def, false) < 0)
        goto cleanup;
 
-    if (!(network = virNetworkAssignDef(&driver->networks, def, false)))
-        goto cleanup;
-    freeDef = false;
+    if ((network = virNetworkFindByName(&driver->networks, def->name))) {
+        network->persistent = 1;
+        if (virNetworkObjAssignDef(network, def, false) < 0)
+            goto cleanup;
+    } else {
+        if (!(network = virNetworkAssignDef(&driver->networks, def, false)))
+            goto cleanup;
+    }
 
-    network->persistent = 1;
+    /* def was asigned */
+    freeDef = false;
 
     if (virNetworkSaveConfig(driver->networkConfigDir, def) < 0) {
         virNetworkRemoveInactive(&driver->networks, network);

commit bb19491cf5e3b78f2d72bbcbcc06e985d29b7f4b

network: bridge: Fix regression when defining persistent networks
Commit 0211fd6e04cdc402da20818df54299c6ded3d3cb introduced regression
where newly defined networks were not made persistent.

This patch makes the network persistent on each successful definition.

src/network/bridge_driver.c
@@ -3124,6 +3124,9 @@ static virNetworkPtr networkDefine(virConnectPtr conn, const char *xml) {
             goto cleanup;
     }
 
+    /* define makes the network persistent - always */
+    network->persistent = 1;
+
     /* def was asigned */
     freeDef = false;
 