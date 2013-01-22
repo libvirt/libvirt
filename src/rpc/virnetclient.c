dd36cc3f97b712ba2d2db57ae9398a79b0313e8e
John Ferlan <jferlan@redhat.com>
2013-01-22 17:09:27 -0500

    rpc: Revert Coverity tag message
    Turns out the fix for VIR_FREE made this particular Coverity tag
    unnecessary, so I'm removing it.

--- src/rpc/virnetclient.c
+++ src/rpc/virnetclient.c
@@ -430,7 +430,6 @@ virNetClientPtr virNetClientNewLibSSH2(const char *host,
                 VIR_FREE(privkey);
             /* DSA */
             if (!privkey) {
-                /* coverity[dead_error_begin] */
                 virBufferAsprintf(&buf, "%s/.ssh/id_dsa", homedir);
                 if (!(privkey = virBufferContentAndReset(&buf)))
                     goto no_memory;
