73cdac3f72d202ab435d57841821b0e73fd4161d
John Ferlan <jferlan@redhat.com>
2013-01-22 17:09:26 -0500

    lxc_process: Avoid passing NULL iface->iname
    A followon to commit id: 68dceb635 - if iface->iname is NULL, then
    neither virNetDevOpenvswitchRemovePort() nor virNetDevVethDelete()
    should be called.  Found by Coverity.

--- src/lxc/lxc_process.c
+++ src/lxc/lxc_process.c
@@ -262,13 +262,15 @@ static void virLXCProcessCleanup(virLXCDriverPtr driver,
     for (i = 0 ; i < vm->def->nnets ; i++) {
         virDomainNetDefPtr iface = vm->def->nets[i];
         vport = virDomainNetGetActualVirtPortProfile(iface);
-        if (iface->ifname)
+        if (iface->ifname) {
             ignore_value(virNetDevSetOnline(iface->ifname, false));
-        if (vport && vport->virtPortType == VIR_NETDEV_VPORT_PROFILE_OPENVSWITCH)
-            ignore_value(virNetDevOpenvswitchRemovePort(
-                            virDomainNetGetActualBridgeName(iface),
-                            iface->ifname));
-        ignore_value(virNetDevVethDelete(iface->ifname));
+            if (vport &&
+                vport->virtPortType == VIR_NETDEV_VPORT_PROFILE_OPENVSWITCH)
+                ignore_value(virNetDevOpenvswitchRemovePort(
+                                virDomainNetGetActualBridgeName(iface),
+                                iface->ifname));
+            ignore_value(virNetDevVethDelete(iface->ifname));
+        }
         networkReleaseActualDevice(iface);
     }
 
