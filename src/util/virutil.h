fe03d58621035a8598c2c2797603233bc930e6e8
Laine Stump <laine@laine.org>
2013-03-13 15:26:35 -0400

    util: fix clear_emulator_capabilities=0
    My commit 7a2e845a865dc7fa82d2393ea2a770cfc8cf00b4 (and its
    prerequisites) managed to effectively ignore the
    clear_emulator_capabilities setting in qemu.conf (visible in the code
    as the VIR_EXEC_CLEAR_CAPS flag when qemu is being exec'ed), with the
    result that the capabilities are always cleared regardless of the
    qemu.conf setting. This patch fixes it by passing the flag through to
    virSetUIDGIDWithCaps(), which uses it to decide whether or not to
    clear existing capabilities before adding in those that were
    requested.
    
    Note that the existing capabilities are *always* cleared if the new
    process is going to run as non-root, since the whole point of running
    non-root is to have the capabilities removed (it's still possible to
    maintain individual capabilities as needed using the capBits argument
    though).
    (cherry picked from commit 2639c6320647778c1496008058dde57dfca35b92)

--- src/util/virutil.h
+++ src/util/virutil.h
@@ -54,7 +54,8 @@ int virPipeReadUntilEOF(int outfd, int errfd,
                         char **outbuf, char **errbuf);
 
 int virSetUIDGID(uid_t uid, gid_t gid);
-int virSetUIDGIDWithCaps(uid_t uid, gid_t gid, unsigned long long capBits);
+int virSetUIDGIDWithCaps(uid_t uid, gid_t gid, unsigned long long capBits,
+                         bool clearExistingCaps);
 
 int virFileReadLimFD(int fd, int maxlen, char **buf) ATTRIBUTE_RETURN_CHECK;
 
