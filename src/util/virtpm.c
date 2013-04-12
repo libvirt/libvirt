06ba4bff9157bb3d529f8616cbb2b5c6ad389bbc
Stefan Berger <stefanb@linux.vnet.ibm.com>
2013-04-12 16:55:45 -0400

    Helper functions for host TPM support
    Implement helper function to create the TPM's sysfs cancel file.
    
    Signed-off-by: Stefan Berger <stefanb@linux.vnet.ibm.com>
    Reviewed-by: Corey Bryant <coreyb@linux.vnet.ibm.com>
    Tested-by: Corey Bryant <coreyb@linux.vnet.ibm.com>

---
+++ src/util/virtpm.c
@@ -0,0 +1,65 @@
+/*
+ * virtpm.c: TPM support
+ *
+ * Copyright (C) 2013 IBM Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Author: Stefan Berger <stefanb@linux.vnet.ibm.com>
+ */
+
+#include <config.h>
+
+#include <sys/stat.h>
+
+#include "virutil.h"
+#include "virerror.h"
+#include "virtpm.h"
+
+#define VIR_FROM_THIS VIR_FROM_NONE
+
+/**
+ * virTPMCreateCancelPath:
+ * @devpath: Path to the TPM device
+ *
+ * Create the cancel path given the path to the TPM device
+ */
+char *
+virTPMCreateCancelPath(const char *devpath)
+{
+    char *path = NULL;
+    const char *dev;
+
+    if (devpath) {
+        dev = strrchr(devpath, '/');
+        if (dev) {
+            dev++;
+            if (virAsprintf(&path, "/sys/class/misc/%s/device/cancel",
+                            dev) < 0) {
+                virReportOOMError();
+                goto cleanup;
+            }
+        } else {
+            virReportError(VIR_ERR_INTERNAL_ERROR,
+                           _("TPM device path %s is invalid"), devpath);
+        }
+    } else {
+        virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
+                       _("Missing TPM device path"));
+    }
+
+cleanup:
+    return path;
+}
---
+++ src/util/virtpm.h
@@ -0,0 +1,27 @@
+/*
+ * virtpm.h: TPM support
+ *
+ * Copyright (C) 2013 IBM Corporation
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ *
+ * Author: Stefan Berger <stefanb@linux.vnet.ibm.com>
+ */
+#ifndef __VIR_TPM_H__
+# define __VIR_TPM_H__
+
+char *virTPMCreateCancelPath(const char *devpath);
+
+#endif /* __VIR_TPM_H__ */
