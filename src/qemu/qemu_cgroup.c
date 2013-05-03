97633c868294e2ff5b206fd0fcdab4e0e2712076
Laine Stump <laine@laine.org>
2013-05-03 14:30:55 -0400

    qemu: fix stupid typos in VFIO cgroup setup/teardown
    I must have looked at this a couple dozen times before I noticed it
    had "!=" instead of "==". Not doing this setup prevented qemu from
    doing anything with the vfio group device.
    (cherry picked from commit 52ba0f6e1cf29e791be4e079408c0de851100b65)

--- src/qemu/qemu_cgroup.c
+++ src/qemu/qemu_cgroup.c
@@ -238,7 +238,7 @@ qemuSetupHostdevCGroup(virDomainObjPtr vm,
         switch (dev->source.subsys.type) {
         case VIR_DOMAIN_HOSTDEV_SUBSYS_TYPE_PCI:
             if (dev->source.subsys.u.pci.backend
-                != VIR_DOMAIN_HOSTDEV_PCI_BACKEND_VFIO) {
+                == VIR_DOMAIN_HOSTDEV_PCI_BACKEND_VFIO) {
                 int rc;
 
                 pci = virPCIDeviceNew(dev->source.subsys.u.pci.addr.domain,
@@ -324,7 +324,7 @@ qemuTeardownHostdevCgroup(virDomainObjPtr vm,
         switch (dev->source.subsys.type) {
         case VIR_DOMAIN_HOSTDEV_SUBSYS_TYPE_PCI:
             if (dev->source.subsys.u.pci.backend
-                != VIR_DOMAIN_HOSTDEV_PCI_BACKEND_VFIO) {
+                == VIR_DOMAIN_HOSTDEV_PCI_BACKEND_VFIO) {
                 int rc;
 
                 pci = virPCIDeviceNew(dev->source.subsys.u.pci.addr.domain,
