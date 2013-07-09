43af7332c09a751c6b1ced69b9d9d7fb890449fe
Gao feng <gaofeng@cn.fujitsu.com>
2013-07-09 11:16:20 +0100

    LXC: hostdev: create parent directory for hostdev
    Create parent directroy for hostdev automatically when we
    start a lxc domain or attach a hostdev to a lxc domain.
    
    Signed-off-by: Gao feng <gaofeng@cn.fujitsu.com>
    (cherry picked from commit 468ee0bc4d6007f4af51235838cac7499fa22773)

--- src/lxc/lxc_driver.c
+++ src/lxc/lxc_driver.c
@@ -3544,6 +3544,13 @@ lxcDomainAttachDeviceHostdevStorageLive(virLXCDriverPtr driver,
         goto cleanup;
     }
 
+    if (lxcContainerSetupHostdevCapsMakePath(dst) < 0) {
+        virReportSystemError(errno,
+                             _("Unable to create directroy for device %s"),
+                             dst);
+        goto cleanup;
+    }
+
     mode = 0700 | S_IFBLK;
 
     VIR_DEBUG("Creating dev %s (%d,%d)",
@@ -3645,6 +3652,13 @@ lxcDomainAttachDeviceHostdevMiscLive(virLXCDriverPtr driver,
 
     if (VIR_REALLOC_N(vm->def->hostdevs, vm->def->nhostdevs+1) < 0) {
         virReportOOMError();
+        goto cleanup;
+    }
+
+    if (lxcContainerSetupHostdevCapsMakePath(dst) < 0) {
+        virReportSystemError(errno,
+                             _("Unable to create directroy for device %s"),
+                             dst);
         goto cleanup;
     }
 
