0776809659ea014f325e862fe441b5a43be3de9e
John Ferlan <jferlan@redhat.com>
2013-07-11 07:19:37 -0400

    hellolibvirt: Resolve Coverity issues
    Recent changes uncovered a NEGATIVE_RETURNS when processing 'numNames'
    in 'showDomains' in the for loop after a possible -1 return.

--- examples/hellolibvirt/hellolibvirt.c
+++ examples/hellolibvirt/hellolibvirt.c
@@ -90,6 +90,13 @@ showDomains(virConnectPtr conn)
     numNames = virConnectListAllDomains(conn,
                                         &nameList,
                                         flags);
+    if (numNames == -1) {
+        ret = 1;
+        printf("Failed to get a list of all domains: %s\n",
+               virGetLastErrorMessage());
+        goto out;
+    }
+
     for (i = 0; i < numNames; i++) {
         int active = virDomainIsActive(nameList[i]);
         printf("  %8s (%s)\n",
