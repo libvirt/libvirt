99fa1ade4bb20c2a450324ddb61db2ca8123fa79
Martin Kletzander <mkletzan@redhat.com>
2013-08-27 13:19:24 +0200

    virsh: Fix debugging
    Commit a0b6a36f "fixed" what abfff210 broke (URI precedence), but
    there was still one more thing missing to fix.  When using virsh
    parameters to setup debugging, those weren't honored, because at the
    time debugging was initializing, arguments weren't parsed yet.  To
    make ewerything work as expected, we need to initialize the debugging
    twice, once before debugging (so we can debug option parsing properly)
    and then again after these options are parsed.
    
    As a side effect, this patch also fixes a leak when virsh is ran with
    multiple '-l' parameters.
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>
    (cherry picked from commit ac43da705f0e7c23dffd87c0705ff01711b88ac0)

--- tools/virsh.c
+++ tools/virsh.c
@@ -2326,10 +2326,9 @@ vshInitDebug(vshControl *ctl)
         debugEnv = getenv("VIRSH_LOG_FILE");
         if (debugEnv && *debugEnv) {
             ctl->logfile = vshStrdup(ctl, debugEnv);
+            vshOpenLogFile(ctl);
         }
     }
-
-    vshOpenLogFile(ctl);
 }
 
 /*
@@ -2338,6 +2337,10 @@ vshInitDebug(vshControl *ctl)
 static bool
 vshInit(vshControl *ctl)
 {
+    /* Since we have the commandline arguments parsed, we need to
+     * re-initialize all the debugging to make it work properly */
+    vshInitDebug(ctl);
+
     if (ctl->conn)
         return false;
 
@@ -3044,7 +3047,9 @@ vshParseArgv(vshControl *ctl, int argc, char **argv)
             ctl->readonly = true;
             break;
         case 'l':
+            vshCloseLogFile(ctl);
             ctl->logfile = vshStrdup(ctl, optarg);
+            vshOpenLogFile(ctl);
             break;
         case 'e':
             len = strlen(optarg);
