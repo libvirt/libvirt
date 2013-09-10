ffb7ccaa7059aed3a2dfdf5630a4b1aa321d2551
Tomas Meszaros <exo@tty.sk>
2013-09-10 17:54:26 +0200

    virsh: Add vshCompleter to each option
    completer and completer_flags added to the _vshCmdOptDef
    structure so it will be possible for completion generators to
    conveniently call option completer functions with desired flags.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>

--- tools/virsh.h
+++ tools/virsh.h
@@ -148,6 +148,8 @@ typedef struct _vshCmdOptDef vshCmdOptDef;
 typedef struct _vshControl vshControl;
 typedef struct _vshCtrlData vshCtrlData;
 
+typedef char **(*vshCompleter)(unsigned int flags);
+
 /*
  * vshCmdInfo -- name/value pair for information about command
  *
@@ -169,6 +171,8 @@ struct _vshCmdOptDef {
     unsigned int flags;         /* flags */
     const char *help;           /* non-NULL help string; or for VSH_OT_ALIAS
                                  * the name of a later public option */
+    vshCompleter completer;         /* option completer */
+    unsigned int completer_flags;   /* option completer flags */
 };
 
 /*
