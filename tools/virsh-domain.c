f084caae7c5db8ae03e7fafce164c73f65681843
Jiri Denemark <jdenemar@redhat.com>
2013-09-11 15:49:48 +0200

    virsh domjobinfo: Do not return 1 if job is NONE
    https://bugzilla.redhat.com/show_bug.cgi?id=1006864
    
    Commit 38ab1225 changed the default value of ret from true to false but
    forgot to set ret = true when job is NONE. Thus, virsh domjobinfo
    returned 1 when there was no job running for a domain but it used to
    (and should) return 0 in this case.

--- tools/virsh-domain.c
+++ tools/virsh-domain.c
@@ -5018,6 +5018,7 @@ cmdDomjobinfo(vshControl *ctl, const vshCmd *cmd)
     case VIR_DOMAIN_JOB_NONE:
     default:
         vshPrint(ctl, "%-12s\n", _("None"));
+        ret = true;
         goto cleanup;
     }
 
