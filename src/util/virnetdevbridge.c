68b1813043a620c796adaaac65b827a775c22ff8
Eric Blake <eblake@redhat.com>
2013-09-13 10:11:26 -0600

    build: fix build with latest rawhide kernel headers
    Bother those kernel developers.  In the latest rawhide, kernel
    and glibc have now been unified so that <netinet/in.h> and
    <linux/in6.h> no longer clash; but <linux/if_bridge.h> is still
    not self-contained.  Because of the latest header change, the
    build is failing with:
    
    checking for linux/param.h... no
    configure: error: You must install kernel-headers in order to compile libvirt with QEMU or LXC support
    
    with details:
    
    In file included from conftest.c:561:0:
    /usr/include/linux/in6.h:71:18: error: field 'flr_dst' has incomplete type
      struct in6_addr flr_dst;
    
    We need a workaround to avoid our workaround :)
    
    * configure.ac (NETINET_LINUX_WORKAROUND): New test.
    * src/util/virnetdevbridge.c (includes): Use it.
    
    Signed-off-by: Eric Blake <eblake@redhat.com>
    (cherry picked from commit e62e0094dcd0ca1484491a9cc62919473b647f11)

--- src/util/virnetdevbridge.c
+++ src/util/virnetdevbridge.c
@@ -39,22 +39,26 @@
 #ifdef __linux__
 # include <linux/sockios.h>
 # include <linux/param.h>     /* HZ                 */
+# if NETINET_LINUX_WORKAROUND
 /* Depending on the version of kernel vs. glibc, there may be a collision
  * between <net/in.h> and kernel IPv6 structures.  The different types
  * are ABI compatible, but choke the C type system; work around it by
  * using temporary redefinitions.  */
-# define in6_addr in6_addr_
-# define sockaddr_in6 sockaddr_in6_
-# define ipv6_mreq ipv6_mreq_
-# define in6addr_any in6addr_any_
-# define in6addr_loopback in6addr_loopback_
+#  define in6_addr in6_addr_
+#  define sockaddr_in6 sockaddr_in6_
+#  define ipv6_mreq ipv6_mreq_
+#  define in6addr_any in6addr_any_
+#  define in6addr_loopback in6addr_loopback_
+# endif
 # include <linux/in6.h>
 # include <linux/if_bridge.h> /* SYSFS_BRIDGE_ATTR  */
-# undef in6_addr
-# undef sockaddr_in6
-# undef ipv6_mreq
-# undef in6addr_any
-# undef in6addr_loopback
+# if NETINET_LINUX_WORKAROUND
+#  undef in6_addr
+#  undef sockaddr_in6
+#  undef ipv6_mreq
+#  undef in6addr_any
+#  undef in6addr_loopback
+# endif
 
 # define JIFFIES_TO_MS(j) (((j)*1000)/HZ)
 # define MS_TO_JIFFIES(ms) (((ms)*HZ)/1000)
