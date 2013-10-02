586a5bc0c6ca98101f6c9d7dafe4c3524f39b7e7
Michal Privoznik <mprivozn@redhat.com>
2013-10-02 18:18:13 +0200

    qemuMonitorJSONSendKey: Avoid double free
    After successful @cmd construction the memory where @keys points to is
    part of @cmd. Avoid double freeing it.
    (cherry picked from commit 3e8343e1510741623aa5bc1dfb74ec39fde868dd)

--- src/qemu/qemu_monitor_json.c
+++ src/qemu/qemu_monitor_json.c
@@ -3440,6 +3440,9 @@ int qemuMonitorJSONSendKey(qemuMonitorPtr mon,
     if (!cmd)
         goto cleanup;
 
+    /* @keys is part of @cmd now. Avoid double free */
+    keys = NULL;
+
     if ((ret = qemuMonitorJSONCommand(mon, cmd, &reply)) < 0)
         goto cleanup;
 
