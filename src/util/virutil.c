eb892701c76c2aa495eb439821a444dc7fbfd0c1
Daniel P. Berrange <berrange@redhat.com>
2013-10-09 11:19:27 +0100

    Block all use of getenv with syntax-check
    The use of getenv is typically insecure, and we want people
    to use our wrappers, to force them to think about setuid
    needs.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>
    (cherry picked from commit 71b21f12bece1127b28b404f11f57b4c2d48983a)

--- src/util/virutil.c
+++ src/util/virutil.c
@@ -2140,7 +2140,7 @@ cleanup:
  */
 const char *virGetEnvBlockSUID(const char *name)
 {
-    return secure_getenv(name);
+    return secure_getenv(name); /* exempt from syntax-check-rules */
 }
 
 
@@ -2154,7 +2154,7 @@ const char *virGetEnvBlockSUID(const char *name)
  */
 const char *virGetEnvAllowSUID(const char *name)
 {
-    return getenv(name);
+    return getenv(name); /* exempt from syntax-check-rules */
 }
 
 
