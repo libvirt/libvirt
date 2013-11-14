928a1a519310ab81861e369c8e2c082525049cad
Michal Privoznik <mprivozn@redhat.com>
2013-11-14 10:33:30 +0100

    virDomainEventCallbackListFree: Don't leak @list->callbacks
    The @list->callbacks is an array that is inflated whenever a new event
    is added, e.g. via virDomainEventCallbackListAddID(). However, when we
    are freeing the array, we free the items within it but forgot to
    actually free it.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    (cherry picked from commit ea13a759f5b32204e1a7bdf6ee228235acb42bcb)

--- src/conf/domain_event.c
+++ src/conf/domain_event.c
@@ -144,6 +144,7 @@ virDomainEventCallbackListFree(virDomainEventCallbackListPtr list)
             (*freecb)(list->callbacks[i]->opaque);
         VIR_FREE(list->callbacks[i]);
     }
+    VIR_FREE(list->callbacks);
     VIR_FREE(list);
 }
 
