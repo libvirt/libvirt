libvirt/libvirt

commit 21685c955e546676a5b2a01f7b788d222e0ee0f5

virpcitest: Introduce testVirPCIDeviceReset
This addition, however, requires some refactoring to be done.  First of
all, to match the best practice we should detach the device prior
resetting it. That's why testVirPCIDeviceDetach is detaching all devices
within 0000:00:01.0 and 0000:00:03.0 range. Then, the brand new test
will reset the 0000:00:02.0 device, so the last testVirPCIDeviceReattach
can reattach all the devices back.

In order to perform a PCI device reset, the dummy config file is not
sufficient anymore and must be replaced with real PCI config (binary
mess). Such config files are to be stored under tests/virpcitestdata/
and ought to have '.config' suffix.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

tests/virpcimock.c
@@ -302,9 +302,12 @@ pci_device_new_from_stub(const struct pciDevice *data)
 {
     struct pciDevice *dev;
     char *devpath;
+    char *configSrc, *configDst;
     char tmp[32];
+    struct stat sb;
 
     if (VIR_ALLOC_QUIET(dev) < 0 ||
+        virAsprintfQuiet(&configSrc, "%s/virpcitestdata/%s.config", abs_builddir, data->id) < 0 ||
         virAsprintfQuiet(&devpath, "%s/devices/%s", fakesysfsdir, data->id) < 0)
         ABORT_OOM();
 
@@ -313,7 +316,21 @@ pci_device_new_from_stub(const struct pciDevice *data)
     if (virFileMakePath(devpath) < 0)
         ABORT("Unable to create: %s", devpath);
 
-    make_file(devpath, "config", "some dummy config");
+    /* If there is a config file for the device within virpcitestdata dir,
+     * symlink it. Otherwise create a dummy config file. */
+    if ((realstat && realstat(configSrc, &sb) == 0) ||
+        (real__xstat && real__xstat(_STAT_VER, configSrc, &sb) == 0)) {
+        /* On success make symlink to @configSrc */
+        if (virAsprintfQuiet(&configDst, "%s/config", devpath) < 0)
+            ABORT_OOM();
+
+        if (symlink(configSrc, configDst) < 0)
+            ABORT("Unable to create symlink: %s", configDst);
+    } else {
+        /* If there's no config data in the virpcitestdata dir, create a dummy
+         * config file */
+        make_file(devpath, "config", "some dummy config");
+    }
 
     if (snprintf(tmp, sizeof(tmp),  "0x%.4x", dev->vendor) < 0)
         ABORT("@tmp overflow");

cfg.mk
@@ -971,7 +971,7 @@ exclude_file_name_regexp--sc_prohibit_close = \
   (\.p[yl]$$|^docs/|^(src/util/virfile\.c|src/libvirt\.c|tests/vir(cgroup|pci)mock\.c)$$)
 
 exclude_file_name_regexp--sc_prohibit_empty_lines_at_EOF = \
-  (^tests/(qemuhelp|nodeinfo)data/|\.(gif|ico|png|diff)$$)
+  (^tests/(qemuhelp|nodeinfo|virpcitest)data/|\.(gif|ico|png|diff)$$)
 
 _src2=src/(util/vircommand|libvirt|lxc/lxc_controller|locking/lock_daemon)
 exclude_file_name_regexp--sc_prohibit_fork_wrappers = \

tests/virpcitest.c
@@ -61,7 +61,7 @@ cleanup:
     if ((count = virPCIDeviceListCount(list)) != cnt) {                 \
         virReportError(VIR_ERR_INTERNAL_ERROR,                          \
                        "Unexpected count of items in " #list ": %d, "   \
-                       "expecting " #cnt, count);                       \
+                       "expecting %zu", count, (size_t) cnt);           \
         goto cleanup;                                                   \
     }
 
@@ -69,39 +69,115 @@ static int
 testVirPCIDeviceDetach(const void *oaque ATTRIBUTE_UNUSED)
 {
     int ret = -1;
-    virPCIDevicePtr dev = NULL, unbindedDev = NULL;
+    virPCIDevicePtr dev[] = {NULL, NULL, NULL};
+    size_t i, nDev = ARRAY_CARDINALITY(dev);
     virPCIDeviceListPtr activeDevs = NULL, inactiveDevs = NULL;
     int count;
 
-    if (!(dev = virPCIDeviceNew(0, 0, 1, 0)) ||
-        !(unbindedDev = virPCIDeviceNew(0, 0, 3, 0)) ||
-        !(activeDevs = virPCIDeviceListNew()) ||
+    if (!(activeDevs = virPCIDeviceListNew()) ||
         !(inactiveDevs = virPCIDeviceListNew()))
         goto cleanup;
 
     CHECK_LIST_COUNT(activeDevs, 0);
     CHECK_LIST_COUNT(inactiveDevs, 0);
 
-    if (virPCIDeviceSetStubDriver(dev, "pci-stub") < 0 ||
-        virPCIDeviceSetStubDriver(unbindedDev, "pci-stub") < 0)
-        goto cleanup;
+    for (i = 0; i < nDev; i++) {
+        if (!(dev[i] = virPCIDeviceNew(0, 0, i + 1, 0)) ||
+            virPCIDeviceSetStubDriver(dev[i], "pci-stub") < 0)
+            goto cleanup;
 
-    if (virPCIDeviceDetach(dev, activeDevs, inactiveDevs) < 0)
-        goto cleanup;
+        if (virPCIDeviceDetach(dev[i], activeDevs, inactiveDevs) < 0)
+            goto cleanup;
 
-    CHECK_LIST_COUNT(activeDevs, 0);
-    CHECK_LIST_COUNT(inactiveDevs, 1);
-
-    if (virPCIDeviceDetach(unbindedDev, activeDevs, inactiveDevs) < 0)
-        goto cleanup;
-
-    CHECK_LIST_COUNT(activeDevs, 0);
-    CHECK_LIST_COUNT(inactiveDevs, 2);
+        CHECK_LIST_COUNT(activeDevs, 0);
+        CHECK_LIST_COUNT(inactiveDevs, i + 1);
+    }
 
     ret = 0;
 cleanup:
-    virPCIDeviceFree(dev);
-    virPCIDeviceFree(unbindedDev);
+    for (i = 0; i < nDev; i++)
+        virPCIDeviceFree(dev[i]);
+    virObjectUnref(activeDevs);
+    virObjectUnref(inactiveDevs);
+    return ret;
+}
+
+static int
+testVirPCIDeviceReset(const void *opaque ATTRIBUTE_UNUSED)
+{
+    int ret = -1;
+    virPCIDevicePtr dev[] = {NULL, NULL, NULL};
+    size_t i, nDev = ARRAY_CARDINALITY(dev);
+    virPCIDeviceListPtr activeDevs = NULL, inactiveDevs = NULL;
+    int count;
+
+    if (!(activeDevs = virPCIDeviceListNew()) ||
+        !(inactiveDevs = virPCIDeviceListNew()))
+        goto cleanup;
+
+    CHECK_LIST_COUNT(activeDevs, 0);
+    CHECK_LIST_COUNT(inactiveDevs, 0);
+
+    for (i = 0; i < nDev; i++) {
+        if (!(dev[i] = virPCIDeviceNew(0, 0, i + 1, 0)) ||
+            virPCIDeviceSetStubDriver(dev[i], "pci-stub") < 0)
+            goto cleanup;
+
+        if (virPCIDeviceReset(dev[i], activeDevs, inactiveDevs) < 0)
+            goto cleanup;
+    }
+
+    ret = 0;
+cleanup:
+    for (i = 0; i < nDev; i++)
+        virPCIDeviceFree(dev[i]);
+    virObjectUnref(activeDevs);
+    virObjectUnref(inactiveDevs);
+    return ret;
+}
+
+static int
+testVirPCIDeviceReattach(const void *opaque ATTRIBUTE_UNUSED)
+{
+    int ret = -1;
+    virPCIDevicePtr dev[] = {NULL, NULL, NULL};
+    size_t i, nDev = ARRAY_CARDINALITY(dev);
+    virPCIDeviceListPtr activeDevs = NULL, inactiveDevs = NULL;
+    int count;
+
+    if (!(activeDevs = virPCIDeviceListNew()) ||
+        !(inactiveDevs = virPCIDeviceListNew()))
+        goto cleanup;
+
+    for (i = 0; i < nDev; i++) {
+        if (!(dev[i] = virPCIDeviceNew(0, 0, i + 1, 0)))
+            goto cleanup;
+
+        if (virPCIDeviceListAdd(inactiveDevs, dev[i]) < 0) {
+            virPCIDeviceFree(dev[i]);
+            goto cleanup;
+        }
+
+        CHECK_LIST_COUNT(activeDevs, 0);
+        CHECK_LIST_COUNT(inactiveDevs, i + 1);
+
+        if (virPCIDeviceSetStubDriver(dev[i], "pci-stub") < 0)
+            goto cleanup;
+    }
+
+    CHECK_LIST_COUNT(activeDevs, 0);
+    CHECK_LIST_COUNT(inactiveDevs, nDev);
+
+    for (i = 0; i < nDev; i++) {
+        if (virPCIDeviceReattach(dev[i], activeDevs, inactiveDevs) < 0)
+            goto cleanup;
+
+        CHECK_LIST_COUNT(activeDevs, 0);
+        CHECK_LIST_COUNT(inactiveDevs, nDev - i - 1);
+    }
+
+    ret = 0;
+cleanup:
     virObjectUnref(activeDevs);
     virObjectUnref(inactiveDevs);
     return ret;
@@ -109,55 +185,6 @@ cleanup:
 
 # define FAKESYSFSDIRTEMPLATE abs_builddir "/fakesysfsdir-XXXXXX"
 
-static int
-testVirPCIDeviceReattach(const void *oaque ATTRIBUTE_UNUSED)
-{
-    int ret = -1;
-    virPCIDevicePtr dev = NULL, unbindedDev = NULL;
-    virPCIDeviceListPtr activeDevs = NULL, inactiveDevs = NULL;
-    int count;
-
-    if (!(dev = virPCIDeviceNew(0, 0, 1, 0)) ||
-        !(unbindedDev = virPCIDeviceNew(0, 0, 3, 0)) ||
-        !(activeDevs = virPCIDeviceListNew()) ||
-        !(inactiveDevs = virPCIDeviceListNew()))
-        goto cleanup;
-
-    if (virPCIDeviceListAdd(inactiveDevs, dev) < 0) {
-        virPCIDeviceFree(dev);
-        virPCIDeviceFree(unbindedDev);
-        goto cleanup;
-    }
-
-    if (virPCIDeviceListAdd(inactiveDevs, unbindedDev) < 0) {
-        virPCIDeviceFree(unbindedDev);
-        goto cleanup;
-    }
-
-    CHECK_LIST_COUNT(activeDevs, 0);
-    CHECK_LIST_COUNT(inactiveDevs, 2);
-
-    if (virPCIDeviceSetStubDriver(dev, "pci-stub") < 0)
-        goto cleanup;
-
-    if (virPCIDeviceReattach(dev, activeDevs, inactiveDevs) < 0)
-        goto cleanup;
-
-    CHECK_LIST_COUNT(activeDevs, 0);
-    CHECK_LIST_COUNT(inactiveDevs, 1);
-
-    if (virPCIDeviceReattach(unbindedDev, activeDevs, inactiveDevs) < 0)
-        goto cleanup;
-
-    CHECK_LIST_COUNT(activeDevs, 0);
-    CHECK_LIST_COUNT(inactiveDevs, 0);
-
-    ret = 0;
-cleanup:
-    virObjectUnref(activeDevs);
-    virObjectUnref(inactiveDevs);
-    return ret;
-}
 static int
 mymain(void)
 {
@@ -184,6 +211,7 @@ mymain(void)
 
     DO_TEST(testVirPCIDeviceNew);
     DO_TEST(testVirPCIDeviceDetach);
+    DO_TEST(testVirPCIDeviceReset);
     DO_TEST(testVirPCIDeviceReattach);
 
     if (getenv("LIBVIRT_SKIP_CLEANUP") == NULL)

commit e821de2c5a7e3d9f122f7f30e4e97edc30d6506a

tests: fix virpcitest with read-only srcdir
'make distcheck' has been broken since commit 21685c9; basically,
it emulates the case of a read-only $(srcdir) (such as building
from a tarball exploded onto a CD-ROM), but we were creating our
fake pci device as a symlink into $(srcdir) and failing when that
requires opening the config file for writing:

 3) testVirPCIDeviceReset                                             ... libvirt:  error : Failed to open config space file '/sys/bus/pci/devices/0000:00:01.0/config': Permission denied

Fix it by copying rather than symlinking.

* tests/virpcimock.c (make_file): Add parameter to allow binary
creation; adjust all callers.
(pci_device_new_from_stub): Copy rather than symlink.

Signed-off-by: Eric Blake <eblake@redhat.com>

tests/virpcimock.c
@@ -154,10 +154,13 @@ static int pci_driver_handle_remove_id(const char *path);
 static void
 make_file(const char *path,
           const char *name,
-          const char *value)
+          const char *value,
+          ssize_t len)
 {
     int fd = -1;
     char *filepath = NULL;
+    if (value && len == -1)
+        len = strlen(value);
 
     if (virAsprintfQuiet(&filepath, "%s/%s", path, name) < 0)
         ABORT_OOM();
@@ -165,7 +168,7 @@ make_file(const char *path,
     if ((fd = realopen(filepath, O_CREAT|O_WRONLY, 0666)) < 0)
         ABORT("Unable to open: %s", filepath);
 
-    if (value && safewrite(fd, value, strlen(value)) != strlen(value))
+    if (value && safewrite(fd, value, len) != len)
         ABORT("Unable to write: %s", filepath);
 
     VIR_FORCE_CLOSE(fd);
@@ -302,7 +305,7 @@ pci_device_new_from_stub(const struct pciDevice *data)
 {
     struct pciDevice *dev;
     char *devpath;
-    char *configSrc, *configDst;
+    char *configSrc;
     char tmp[32];
     struct stat sb;
 
@@ -321,25 +324,32 @@ pci_device_new_from_stub(const struct pciDevice *data)
      * symlink it. Otherwise create a dummy config file. */
     if ((realstat && realstat(configSrc, &sb) == 0) ||
         (real__xstat && real__xstat(_STAT_VER, configSrc, &sb) == 0)) {
-        /* On success make symlink to @configSrc */
-        if (virAsprintfQuiet(&configDst, "%s/config", devpath) < 0)
-            ABORT_OOM();
+        /* On success, copy @configSrc into the destination (a copy,
+         * rather than a symlink, is required since we write into the
+         * file, and parallel VPATH builds must not stomp on the
+         * original; besides, 'make distcheck' requires the original
+         * to be read-only */
+        char *buf;
+        ssize_t len;
 
-        if (symlink(configSrc, configDst) < 0)
-            ABORT("Unable to create symlink: %s", configDst);
+        if ((len = virFileReadAll(configSrc, 4096, &buf)) < 0)
+            ABORT("Unable to read config file '%s'", configSrc);
+
+        make_file(devpath, "config", buf, len);
+        VIR_FREE(buf);
     } else {
         /* If there's no config data in the virpcitestdata dir, create a dummy
          * config file */
-        make_file(devpath, "config", "some dummy config");
+        make_file(devpath, "config", "some dummy config", -1);
     }
 
     if (snprintf(tmp, sizeof(tmp),  "0x%.4x", dev->vendor) < 0)
         ABORT("@tmp overflow");
-    make_file(devpath, "vendor", tmp);
+    make_file(devpath, "vendor", tmp, -1);
 
     if (snprintf(tmp, sizeof(tmp),  "0x%.4x", dev->device) < 0)
         ABORT("@tmp overflow");
-    make_file(devpath, "device", tmp);
+    make_file(devpath, "device", tmp, -1);
 
     if (pci_device_autobind(dev) < 0)
         ABORT("Unable to bind: %s", data->id);
@@ -348,6 +358,7 @@ pci_device_new_from_stub(const struct pciDevice *data)
         ABORT_OOM();
 
     VIR_FREE(devpath);
+    VIR_FREE(configSrc);
 }
 
 static struct pciDevice *
@@ -425,10 +436,10 @@ pci_driver_new(const char *name, ...)
 
     va_end(args);
 
-    make_file(driverpath, "bind", NULL);
-    make_file(driverpath, "unbind", NULL);
-    make_file(driverpath, "new_id", NULL);
-    make_file(driverpath, "remove_id", NULL);
+    make_file(driverpath, "bind", NULL, -1);
+    make_file(driverpath, "unbind", NULL, -1);
+    make_file(driverpath, "new_id", NULL, -1);
+    make_file(driverpath, "remove_id", NULL, -1);
 
     if (VIR_APPEND_ELEMENT_QUIET(pciDrivers, nPciDrivers, driver) < 0)
         ABORT_OOM();