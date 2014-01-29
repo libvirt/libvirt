libvirt/libvirt

commit df36af589f09e94af9d338b1cfb6136089889c4a

maint: Kill usage of atoi()
Kill the use of atoi() and introduce syntax check to forbid it and it's
friends (atol, atoll, atof, atoq).

Also fix a typo in variable name holding the cylinders count of a disk
pool (apparently unused).

examples/domsuspend/suspend.c will need a larger scale refactor as the
whole example file is broken thus it will be exempted from the syntax
check for now.

src/xen/xend_internal.c
@@ -290,10 +290,19 @@ xend_req(int fd, char **content)
         if (STREQ(buffer, "\r\n"))
             break;
 
-        if (istartswith(buffer, "Content-Length: "))
-            content_length = atoi(buffer + 16);
-        else if (istartswith(buffer, "HTTP/1.1 "))
-            retcode = atoi(buffer + 9);
+        if (istartswith(buffer, "Content-Length: ")) {
+            if (virStrToLong_i(buffer + 16, NULL, 10, &content_length) < 0) {
+                virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
+                               _("failed to parse Xend response content length"));
+                return -1;
+            }
+        } else if (istartswith(buffer, "HTTP/1.1 ")) {
+            if (virStrToLong_i(buffer + 9, NULL, 10, &retcode) < 0) {
+                virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
+                               _("failed to parse Xend response return code"));
+                return -1;
+            }
+        }
     }
 
     VIR_FREE(buffer);

cfg.mk
@@ -869,6 +869,12 @@ sc_prohibit_getenv:
 	halt='Use virGetEnv{Allow,Block}SUID instead of getenv'		\
 	  $(_sc_search_regexp)
 
+sc_prohibit_atoi:
+	@prohibit='\bato(i|f|l|ll|q) *\('	\
+	halt='Use virStrToLong* instead of atoi, atol, atof, atoq, atoll' \
+	  $(_sc_search_regexp)
+
+
 # We don't use this feature of maint.mk.
 prev_version_file = /dev/null
 

src/conf/storage_conf.h
@@ -229,7 +229,7 @@ struct _virStoragePoolSourceDevice {
      * the geometry data is needed
      */
     struct _geometry {
-        int cyliders;
+        int cylinders;
         int heads;
         int sectors;
     } geometry;

src/storage/storage_backend_disk.c
@@ -280,12 +280,16 @@ virStorageBackendDiskMakePoolGeometry(virStoragePoolObjPtr pool,
                                       char **const groups,
                                       void *data ATTRIBUTE_UNUSED)
 {
+    virStoragePoolSourceDevicePtr device = &(pool->def->source.devices[0]);
+    if (virStrToLong_i(groups[0], NULL, 0, &device->geometry.cylinders) < 0 ||
+        virStrToLong_i(groups[1], NULL, 0, &device->geometry.heads) < 0 ||
+        virStrToLong_i(groups[2], NULL, 0, &device->geometry.sectors) < 0) {
+        virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
+                       _("Failed to create disk pool geometry"));
+        return -1;
+    }
 
-       pool->def->source.devices[0].geometry.cyliders = atoi(groups[0]);
-       pool->def->source.devices[0].geometry.heads = atoi(groups[1]);
-       pool->def->source.devices[0].geometry.sectors = atoi(groups[2]);
-
-       return 0;
+    return 0;
 }
 
 static int

commit 37564b471da7e9080284149f8350241afd448515

xen: fix parsing xend http response
Commit df36af58 broke parsing of http response from xend.  The prior
use of atoi() would happily parse e.g. a string containing "200 OK\r\n",
whereas virStrToLong_i() will fail when called with a NULL end_ptr.
Change the calls to virStrToLong_i() to provide a non-NULL end_ptr.

src/xen/xend_internal.c
@@ -282,6 +282,7 @@ xend_req(int fd, char **content)
     size_t buffer_size = 4096;
     int content_length = 0;
     int retcode = 0;
+    char *end_ptr;
 
     if (VIR_ALLOC_N(buffer, buffer_size) < 0)
         return -1;
@@ -291,13 +292,13 @@ xend_req(int fd, char **content)
             break;
 
         if (istartswith(buffer, "Content-Length: ")) {
-            if (virStrToLong_i(buffer + 16, NULL, 10, &content_length) < 0) {
+            if (virStrToLong_i(buffer + 16, &end_ptr, 10, &content_length) < 0) {
                 virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                                _("failed to parse Xend response content length"));
                 return -1;
             }
         } else if (istartswith(buffer, "HTTP/1.1 ")) {
-            if (virStrToLong_i(buffer + 9, NULL, 10, &retcode) < 0) {
+            if (virStrToLong_i(buffer + 9, &end_ptr, 10, &retcode) < 0) {
                 virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                                _("failed to parse Xend response return code"));
                 return -1;