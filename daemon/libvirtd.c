34542473e555dcac35abace2bd4e88232ef67f0e
Nehal J Wani <nehaljw.kkd1@gmail.com>
2014-04-03 23:43:38 +0530

    Fix Memory Leak in daemon/libvirtd.c
    Fixes leak introduced by e562e82f
    
    ==4937== 64 bytes in 1 blocks are definitely lost in loss record 270 of 405
    ==4937==    at 0x4A06BE0: realloc (vg_replace_malloc.c:662)
    ==4937==    by 0x6FA41C4: __vasprintf_chk (vasprintf_chk.c:90)
    ==4937==    by 0x50C8D29: virVasprintfInternal (stdio2.h:199)
    ==4937==    by 0x50C8E3A: virAsprintfInternal (virstring.c:362)
    ==4937==    by 0x11D01A: main (libvirtd.c:1170)
    
    Signed-off-by: Ján Tomko <jtomko@redhat.com>

--- daemon/libvirtd.c
+++ daemon/libvirtd.c
@@ -1182,6 +1182,7 @@ int main(int argc, char **argv) {
         virDriverModuleInitialize(driverdir);
 #endif
         cpuMapOverride(cpumap);
+        VIR_FREE(cpumap);
         *tmp = '/';
         /* Must not free 'driverdir' - it is still used */
     }
