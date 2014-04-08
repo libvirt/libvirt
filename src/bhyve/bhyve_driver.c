libvirt/libvirt

commit 17b17565e6464e6219866ffea74e1af310a45195

Simplify bhyveDriverGetCapabilities()
The driver passed as the only argument to the function should never be
NULL so there's no need to check it. After removing it, the whole
function collapses to a single line doing ref over driver
capabilities.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

src/bhyve/bhyve_driver.c
@@ -2,6 +2,7 @@
  * bhyve_driver.c: core driver methods for managing bhyve guests
  *
  * Copyright (C) 2014 Roman Bogorodskiy
+ * Copyright (C) 2014 Red Hat, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
@@ -123,16 +124,10 @@ bhyveAutostartDomains(bhyveConnPtr driver)
  * Returns: a reference to a virCapsPtr instance or NULL
  */
 static virCapsPtr
-bhyveDriverGetCapabilities(bhyveConnPtr driver)
+bhyveDriverGetCapabilities(bhyveConnPtr driver ATTRIBUTE_NONNULL)
 {
-    virCapsPtr ret = NULL;
 
-    if (driver == NULL)
-        return NULL;
-
-    ret = virObjectRef(driver->caps);
-
-    return ret;
+    return virObjectRef(driver->caps);
 }
 
 static char *

commit 986a07c7099488a4abdb04415fd3af42e65b87b6

bhyve: fix ATTRIBUTE_NONNULL usage
Fix incorrect ATTRIBUTE_NONNULL usage introduced in 17b17565
which caused build failure:

bhyve/bhyve_driver.c:127:48: error: expected ')'
bhyveDriverGetCapabilities(bhyveConnPtr driver ATTRIBUTE_NONNULL)
                                               ^
bhyve/bhyve_driver.c:127:27: note: to match this '('
bhyveDriverGetCapabilities(bhyveConnPtr driver ATTRIBUTE_NONNULL)

Pushed under the build breaker rule.

src/bhyve/bhyve_driver.c
@@ -123,8 +123,8 @@ bhyveAutostartDomains(bhyveConnPtr driver)
  *
  * Returns: a reference to a virCapsPtr instance or NULL
  */
-static virCapsPtr
-bhyveDriverGetCapabilities(bhyveConnPtr driver ATTRIBUTE_NONNULL)
+static virCapsPtr ATTRIBUTE_NONNULL(1)
+bhyveDriverGetCapabilities(bhyveConnPtr driver)
 {
 
     return virObjectRef(driver->caps);