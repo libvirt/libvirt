libvirt/libvirt

commit 5c43e2e027783bf3a59ee397eaaa2a963305f414

util: storage: Remove obsolete argument virStorageFileGetMetadataInternal
As we already pass the whole structure down the call path there's no
need to return some stuff in a separate argument. Remove the argument
and tweak callers to avoid breaking semantics.

virStorageFileGetMetadataFromBuf will be refactored later along with the
storage driver.

src/util/virstoragefile.c
@@ -795,7 +795,7 @@ qcow2GetFeatures(virBitmapPtr *features,
  * information about the file and its backing store.  */
 static int ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2)
 ATTRIBUTE_NONNULL(3) ATTRIBUTE_NONNULL(4) ATTRIBUTE_NONNULL(7)
-ATTRIBUTE_NONNULL(8) ATTRIBUTE_NONNULL(9)
+ATTRIBUTE_NONNULL(8)
 virStorageFileGetMetadataInternal(const char *path,
                                   const char *canonPath,
                                   const char *directory,
@@ -803,7 +803,6 @@ virStorageFileGetMetadataInternal(const char *path,
                                   size_t len,
                                   int format,
                                   virStorageFileMetadataPtr meta,
-                                  char **backingStore,
                                   int *backingFormat,
                                   char **backingDirectory)
 {
@@ -889,10 +888,6 @@ virStorageFileGetMetadataInternal(const char *path,
                 }
             } else {
                 if (VIR_STRDUP(meta->backingStore, meta->backingStoreRaw) < 0)
-                    goto cleanup;
-
-                if (backingStore &&
-                    VIR_STRDUP(*backingStore, meta->backingStoreRaw) < 0)
                     goto cleanup;
 
                 *backingFormat = VIR_STORAGE_FILE_RAW;
@@ -1020,8 +1015,13 @@ virStorageFileGetMetadataFromBuf(const char *path,
         goto cleanup;
 
     if (virStorageFileGetMetadataInternal(path, canonPath, ".", buf, len,
-                                          format, ret, backing,
+                                          format, ret,
                                           backingFormat, NULL) < 0) {
+        virStorageFileFreeMetadata(ret);
+        ret = NULL;
+    }
+
+    if (VIR_STRDUP(*backing, ret->backingStoreRaw) < 0) {
         virStorageFileFreeMetadata(ret);
         ret = NULL;
     }
@@ -1083,7 +1083,6 @@ virStorageFileGetMetadataFromFDInternal(const char *path,
 
     ret = virStorageFileGetMetadataInternal(path, canonPath, directory,
                                             buf, len, format, meta,
-                                            NULL,
                                             backingFormat, backingDirectory);
 
     if (ret == 0) {

commit c9679edca9cc876d63f12c80d21f57aceacc97bd

conf: avoid null deref during storage probe
Commit 5c43e2e introduced a NULL deref if there is a failure
in virStorageFileGetMetadataInternal.

* src/util/virstoragefile.c (virStorageFileGetMetadataFromBuf):
Fix error handling.

Signed-off-by: Eric Blake <eblake@redhat.com>

src/util/virstoragefile.c
@@ -999,21 +999,21 @@ virStorageFileGetMetadataFromBuf(const char *path,
                                  int *backingFormat)
 {
     virStorageSourcePtr ret = NULL;
+    virStorageSourcePtr meta = NULL;
 
-    if (!(ret = virStorageFileMetadataNew(path, format)))
+    if (!(meta = virStorageFileMetadataNew(path, format)))
         return NULL;
 
-    if (virStorageFileGetMetadataInternal(ret, buf, len,
-                                          backingFormat) < 0) {
-        virStorageSourceFree(ret);
-        ret = NULL;
-    }
+    if (virStorageFileGetMetadataInternal(meta, buf, len,
+                                          backingFormat) < 0)
+        goto cleanup;
+    if (VIR_STRDUP(*backing, meta->backingStoreRaw) < 0)
+        goto cleanup;
 
-    if (VIR_STRDUP(*backing, ret->backingStoreRaw) < 0) {
-        virStorageSourceFree(ret);
-        ret = NULL;
-    }
-
+    ret = meta;
+    meta = NULL;
+ cleanup:
+    virStorageSourceFree(meta);
     return ret;
 }
 