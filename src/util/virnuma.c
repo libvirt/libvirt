libvirt/libvirt

commit 77c830d8c44695ffaec29523bf01cee2364e42ef

virnuma: Introduce virNumaGetDistances
The API gets a NUMA node and find distances to other nodes.  The
distances are returned in an array. If an item X within the array
equals to value of zero, then there's no such node as X.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

src/util/virnuma.c
@@ -217,6 +217,61 @@ virNumaGetMaxNode(void)
 
 
 /**
+ * virNumaGetDistances:
+ * @node: identifier of the requested NUMA node
+ * @distances: array of distances to sibling nodes
+ * @ndistances: size of @distances
+ *
+ * Get array of distances to sibling nodes from @node. If a
+ * distances[x] equals to zero, the node x is not enabled or
+ * doesn't exist. As a special case, if @node itself refers to
+ * disabled or nonexistent NUMA node, then @distances and
+ * @ndistances are set to NULL and zero respectively.
+ *
+ * The distances are a bit of magic. For a local node the value
+ * is 10, for remote it's typically 20 meaning that time penalty
+ * for accessing a remote node is two time bigger than when
+ * accessing a local node.
+ *
+ * Returns 0 on success, -1 otherwise.
+ */
+int
+virNumaGetDistances(int node,
+                    int **distances,
+                    int *ndistances)
+{
+    int ret = -1;
+    int max_node;
+    size_t i;
+
+    if (!numa_bitmask_isbitset(numa_nodes_ptr, node)) {
+        VIR_DEBUG("Node %d does not exist", node);
+        *distances = NULL;
+        *ndistances = 0;
+        return 0;
+    }
+
+    if ((max_node = virNumaGetMaxNode()) < 0)
+        goto cleanup;
+
+    if (VIR_ALLOC_N(*distances, max_node) < 0)
+        goto cleanup;
+
+    *ndistances = max_node + 1;
+
+    for (i = 0; i<= max_node; i++) {
+        if (!numa_bitmask_isbitset(numa_nodes_ptr, i))
+            continue;
+
+        (*distances)[i] = numa_distance(node, i);
+    }
+
+    ret = 0;
+ cleanup:
+    return ret;
+}
+
+/**
  * virNumaGetNodeMemory:
  * @node: identifier of the requested NUMA node
  * @memsize: returns the total size of memory in the NUMA node

src/libvirt_private.syms
@@ -1652,6 +1652,7 @@ virNodeSuspendGetTargetMask;
 virDomainNumatuneMemModeTypeFromString;
 virDomainNumatuneMemModeTypeToString;
 virNumaGetAutoPlacementAdvice;
+virNumaGetDistances;
 virNumaGetMaxNode;
 virNumaGetNodeMemory;
 virNumaIsAvailable;

src/util/virnuma.h
@@ -58,6 +58,9 @@ int virNumaSetupMemoryPolicy(virNumaTuneDef numatune,
 
 bool virNumaIsAvailable(void);
 int virNumaGetMaxNode(void);
+int virNumaGetDistances(int node,
+                        int **distances,
+                        int *ndistances);
 int virNumaGetNodeMemory(int node,
                          unsigned long long *memsize,
                          unsigned long long *memfree);

commit 3289edd224bc1b1bbd5cfb1813c50f11ee0204a7

virnuma: Implement virNumaGetDistances stub for non-NUMA
In case the libvirt is built without numactl support, we're
missing the virNumaGetDistances() stub so the linking fails:

  CCLD     libvirt_lxc
libvirt_lxc-nodeinfo.o: In function `virNodeCapsGetSiblingInfo':
/home/zippy/tmp/libvirt.git/src/nodeinfo.c:1763: undefined reference to `virNumaGetDistances'
collect2: error: ld returned 1 exit status
make[3]: *** [libvirt_lxc] Error 1

The issue was introduced in 77c830d8c4.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

src/util/virnuma.c
@@ -443,6 +443,17 @@ virNumaGetNodeCPUs(int node ATTRIBUTE_UNUSED,
                    _("NUMA isn't available on this host"));
     return -1;
 }
+
+int
+virNumaGetDistances(int node ATTRIBUTE_UNUSED,
+                    int **distances,
+                    int *ndistances)
+{
+    *distances = NULL;
+    *ndistances = 0;
+    VIR_DEBUG("NUMA isn't available on this host");
+    return 0;
+}
 #endif
 
 