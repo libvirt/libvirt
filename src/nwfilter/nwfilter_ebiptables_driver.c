libvirt/libvirt

commit 70571ccc98abc884ae2e7c4d2b6eba27c21b8878

Convert ebiptablesDriverProbeStateMatch to virFirewall
Conver the ebiptablesDriverProbeStateMatch initialization
check to use the virFirewall APIs for querying iptables
version.

Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

src/nwfilter/nwfilter_ebiptables_driver.c
@@ -3923,45 +3923,62 @@ ebiptablesDriverProbeCtdir(void)
         iptables_ctdir_corrected = CTDIR_STATUS_OLD;
 }
 
-static void
-ebiptablesDriverProbeStateMatch(void)
+
+static int
+ebiptablesDriverProbeStateMatchQuery(virFirewallPtr fw ATTRIBUTE_UNUSED,
+                                     const char *const *lines,
+                                     void *opaque)
 {
-    virBuffer buf = VIR_BUFFER_INITIALIZER;
-    char *cmdout = NULL, *version;
-    unsigned long thisversion;
+    unsigned long *version = opaque;
+    char *tmp;
 
-    NWFILTER_SET_IPTABLES_SHELLVAR(&buf);
-
-    virBufferAsprintf(&buf,
-                      "$IPT --version");
-
-    if (ebiptablesExecCLI(&buf, false, &cmdout) < 0) {
-        VIR_ERROR(_("Testing of iptables command failed: %s"),
-                  cmdout);
-        return;
+    if (!lines || !lines[0]) {
+        virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
+                       _("No output from iptables --version"));
+        return -1;
     }
 
     /*
      * we expect output in the format
-     * iptables v1.4.16
+     * 'iptables v1.4.16'
      */
-    if (!(version = strchr(cmdout, 'v')) ||
-        virParseVersionString(version + 1, &thisversion, true) < 0) {
-        VIR_ERROR(_("Could not determine iptables version from string %s"),
-                  cmdout);
-        goto cleanup;
+    if (!(tmp = strchr(lines[0], 'v')) ||
+        virParseVersionString(tmp + 1, version, true) < 0) {
+        virReportError(VIR_ERR_INTERNAL_ERROR,
+                       _("Cannot parse version string '%s'"),
+                       lines[0]);
+        return -1;
     }
+
+    return 0;
+}
+
+
+static int
+ebiptablesDriverProbeStateMatch(void)
+{
+    virBuffer buf = VIR_BUFFER_INITIALIZER;
+    unsigned long version;
+    virFirewallPtr fw = virFirewallNew();
+
+    NWFILTER_SET_IPTABLES_SHELLVAR(&buf);
+
+    virFirewallStartTransaction(fw, 0);
+    virFirewallAddRuleFull(fw, VIR_FIREWALL_LAYER_IPV4,
+                           false, ebiptablesDriverProbeStateMatchQuery, &version,
+                           "--version", NULL);
+
+    if (virFirewallApply(fw) < 0)
+        return -1;
 
     /*
      * since version 1.4.16 '-m state --state ...' will be converted to
      * '-m conntrack --ctstate ...'
      */
-    if (thisversion >= 1 * 1000000 + 4 * 1000 + 16)
+    if (version >= 1 * 1000000 + 4 * 1000 + 16)
         newMatchState = true;
 
- cleanup:
-    VIR_FREE(cmdout);
-    return;
+    return 0;
 }
 
 static int
@@ -4000,7 +4017,8 @@ ebiptablesDriverInit(bool privileged)
 
     if (iptables_cmd_path) {
         ebiptablesDriverProbeCtdir();
-        ebiptablesDriverProbeStateMatch();
+        if (ebiptablesDriverProbeStateMatch() < 0)
+            return -1;
     }
 
     ebiptables_driver.flags = TECHDRV_FLAG_INITIALIZED;

commit ee70839bbf76e0033611fa99c23d8920c3d403f4

nwfilter: plug memory leak with firewall
Introduced in commit 70571ccc (v1.2.4). Caught by valgrind:

==9816== 170 (32 direct, 138 indirect) bytes in 1 blocks are definitely lost in loss record 646 of 821
==9816==    at 0x4A081D4: calloc (in /usr/lib64/valgrind/vgpreload_memcheck-amd64-linux.so)
==9816==    by 0x50836FB: virAlloc (viralloc.c:144)
==9816==    by 0x50AEC2B: virFirewallNew (virfirewall.c:204)
==9816==    by 0x1E2308ED: ebiptablesDriverProbeStateMatch (nwfilter_ebiptables_driver.c:3715)
==9816==    by 0x1E2309AD: ebiptablesDriverInit (nwfilter_ebiptables_driver.c:3742)

* src/nwfilter/nwfilter_ebiptables_driver.c
(ebiptablesDriverProbeStateMatch): Properly clean up.

Signed-off-by: Eric Blake <eblake@redhat.com>

src/nwfilter/nwfilter_ebiptables_driver.c
@@ -3713,6 +3713,7 @@ ebiptablesDriverProbeStateMatch(void)
 {
     unsigned long version;
     virFirewallPtr fw = virFirewallNew();
+    int ret = -1;
 
     virFirewallStartTransaction(fw, 0);
     virFirewallAddRuleFull(fw, VIR_FIREWALL_LAYER_IPV4,
@@ -3720,7 +3721,7 @@ ebiptablesDriverProbeStateMatch(void)
                            "--version", NULL);
 
     if (virFirewallApply(fw) < 0)
-        return -1;
+        goto cleanup;
 
     /*
      * since version 1.4.16 '-m state --state ...' will be converted to
@@ -3729,7 +3730,10 @@ ebiptablesDriverProbeStateMatch(void)
     if (version >= 1 * 1000000 + 4 * 1000 + 16)
         newMatchState = true;
 
-    return 0;
+    ret = 0;
+ cleanup:
+    virFirewallFree(fw);
+    return ret;
 }
 
 static int