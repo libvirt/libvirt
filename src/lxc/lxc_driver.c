0311be9435087984c495243fbb378b7533beb99e
John Ferlan <jferlan@redhat.com>
2014-09-04 16:22:07 -0400

    lxc: Resolve Coverity FORWARD_NULL
    If we jump to cleanup before allocating 'result', then the call to
    virBlkioDeviceArrayClear() could dereference result
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>

--- src/lxc/lxc_driver.c
+++ src/lxc/lxc_driver.c
@@ -2222,8 +2222,10 @@ lxcDomainParseBlkioDeviceStr(char *blkioDeviceStr, const char *type,
                    _("unable to parse blkio device '%s' '%s'"),
                    type, blkioDeviceStr);
  cleanup:
-    virBlkioDeviceArrayClear(result, ndevices);
-    VIR_FREE(result);
+    if (result) {
+        virBlkioDeviceArrayClear(result, ndevices);
+        VIR_FREE(result);
+    }
     return -1;
 }
 
