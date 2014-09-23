f8857c8f88ece3bb0790436e42751c2a09ae4453
Michal Privoznik <mprivozn@redhat.com>
2014-09-23 11:34:06 +0200

    nodeinfo: Prefer MIN in nodeGetFreePages
    It's better to use a macro instead of if-else construct.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- src/nodeinfo.c
+++ src/nodeinfo.c
@@ -2041,9 +2041,7 @@ nodeGetFreePages(unsigned int npages,
         goto cleanup;
     }
 
-    lastCell = startCell + cellCount;
-    if (startCell + cellCount < lastCell)
-        lastCell = startCell + cellCount;
+    lastCell = MIN(lastCell, startCell + cellCount);
 
     for (cell = startCell; cell < lastCell; cell++) {
         for (i = 0; i < npages; i++) {
