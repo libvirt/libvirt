fb1e0312f4cfc2375ee94d40e5f2999cd761337d
Peter Krempa <pkrempa@redhat.com>
2014-12-08 19:25:21 +0100

    qemu: migration: Unlock vm on failed ACL check in protocol v2 APIs
    Avoid leaving the domain locked on a failed ACL check in
    qemuDomainMigratePerform() and qemuDomainMigrateFinish2().
    
    Introduced in commit abf75aea247e (Add ACL checks into the QEMU driver).
    
    (cherry picked from commit 2bdcd29c713dfedd813c89f56ae98f6f3898313d)

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -10327,8 +10327,10 @@ qemuDomainMigratePerform(virDomainPtr dom,
     if (!(vm = qemuDomObjFromDomain(dom)))
         goto cleanup;
 
-    if (virDomainMigratePerformEnsureACL(dom->conn, vm->def) < 0)
+    if (virDomainMigratePerformEnsureACL(dom->conn, vm->def) < 0) {
+        virObjectUnlock(vm);
         goto cleanup;
+    }
 
     if (flags & VIR_MIGRATE_PEER2PEER) {
         dconnuri = uri;
@@ -10374,8 +10376,10 @@ qemuDomainMigrateFinish2(virConnectPtr dconn,
         goto cleanup;
     }
 
-    if (virDomainMigrateFinish2EnsureACL(dconn, vm->def) < 0)
+    if (virDomainMigrateFinish2EnsureACL(dconn, vm->def) < 0) {
+        virObjectUnlock(vm);
         goto cleanup;
+    }
 
     /* Do not use cookies in v2 protocol, since the cookie
      * length was not sufficiently large, causing failures
