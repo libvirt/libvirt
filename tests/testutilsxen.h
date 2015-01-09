cb4befc7483eafecdacf9bc5ea5f36e2f4d5d63d
Jim Fehlig <jfehlig@suse.com>
2015-01-09 15:29:07 -0700

    Revert "tests: Tests for the xen-xl parser"
    This reverts commit 6b818d3b09f4e74ac2ea1d4020896be1e6871867.
    
    Signed-off-by: Jim Fehlig <jfehlig@suse.com>

--- tests/testutilsxen.h
+++ tests/testutilsxen.h
@@ -1,10 +1,3 @@
-#ifndef _TESTUTILSXEN_H_
-# define _TESTUTILSXEN_H_
-
-# include "capabilities.h"
+#include "capabilities.h"
 
 virCapsPtr testXenCapsInit(void);
-
-virCapsPtr testXLInitCaps(void);
-
-#endif /* _TESTUTILSXEN_H_ */
