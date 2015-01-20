a976724f9a10730e1339628482a283653efdb72c
Peter Krempa <pkrempa@redhat.com>
2015-01-20 17:01:01 +0100

    CVE-2015-0236: qemu: Check ACLs when dumping security info from save image
    The ACL check didn't check the VIR_DOMAIN_XML_SECURE flag and the
    appropriate permission for it.
    
    (cherry picked from commit 03c3c0c874c84dfa51ef17556062b095c6e1c0a3)

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -5295,7 +5295,7 @@ qemuDomainSaveImageGetXMLDesc(virConnectPtr conn, const char *path,
     if (fd < 0)
         goto cleanup;
 
-    if (virDomainSaveImageGetXMLDescEnsureACL(conn, def) < 0)
+    if (virDomainSaveImageGetXMLDescEnsureACL(conn, def, flags) < 0)
         goto cleanup;
 
     ret = qemuDomainDefFormatXML(driver, def, flags);
