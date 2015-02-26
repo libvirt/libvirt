b7d12c5c50c70427b8608bfb01ea80722284595b
Luyao Huang <lhuang@redhat.com>
2015-02-26 14:14:20 +0800

    conf: error out on invalid host id
    https://bugzilla.redhat.com/show_bug.cgi?id=1196503
    
    We already check whether the host id is valid or not, add a jump
    to forbid invalid host id.
    
    Signed-off-by: Luyao Huang <lhuang@redhat.com>
    Signed-off-by: Ján Tomko <jtomko@redhat.com>
    (cherry picked from commit 719cd2182bf06196b26204a8cf88d28001b1d79b)

--- src/conf/network_conf.c
+++ src/conf/network_conf.c
@@ -725,6 +725,7 @@ virNetworkDHCPHostDefParseXML(const char *networkName,
             virReportError(VIR_ERR_XML_ERROR,
                            _("Invalid character '%c' in id '%s' of network '%s'"),
                            *cp, id, networkName);
+            goto cleanup;
         }
     }
 
