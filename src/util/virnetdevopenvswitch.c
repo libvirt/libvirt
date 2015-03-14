libvirt/libvirt

commit f6a2f97eb9d77242f34b06487f3f052d9f1dbba4

openvswitch: Add utility functions for getting and setting Open vSwitch per-port data
Add utility functions for Open vSwitch to both save
per-port data before a live migration, and restore the
per-port data after a live migration.

Signed-off-by: Kyle Mestery <kmestery@cisco.com>

src/util/virnetdevopenvswitch.c
@@ -173,9 +173,74 @@ int virNetDevOpenvswitchRemovePort(const char *brname ATTRIBUTE_UNUSED, const ch
                              _("Unable to delete port %s from OVS"), ifname);
         goto cleanup;
     }
-    ret = 0;
 
-    cleanup:
-        virCommandFree(cmd);
-        return ret;
+    ret = 0;
+cleanup:
+    virCommandFree(cmd);
+    return ret;
+}
+
+/**
+ * virNetDevOpenvswitchGetMigrateData:
+ * @migrate: a pointer to store the data into, allocated by this function
+ * @ifname: name of the interface for which data is being migrated
+ *
+ * Allocates data to be migrated specific to Open vSwitch
+ *
+ * Returns 0 in case of success or -1 in case of failure
+ */
+int virNetDevOpenvswitchGetMigrateData(char **migrate, const char *ifname)
+{
+    virCommandPtr cmd = NULL;
+    int ret = -1;
+
+    cmd = virCommandNewArgList(OVSVSCTL, "--timeout=5", "get", "Interface",
+                               ifname, "external_ids:PortData", NULL);
+
+    virCommandSetOutputBuffer(cmd, migrate);
+
+    /* Run the command */
+    if (virCommandRun(cmd, NULL) < 0) {
+        virReportSystemError(VIR_ERR_INTERNAL_ERROR,
+                             _("Unable to run command to get OVS port data for "
+                             "interface %s"), ifname);
+        goto cleanup;
+    }
+
+    /* Wipeout the newline */
+    (*migrate)[strlen(*migrate) - 1] = '\0';
+    ret = 0;
+cleanup:
+    return ret;
+}
+
+/**
+ * virNetDevOpenvswitchSetMigrateData:
+ * @migrate: the data which was transferred during migration
+ * @ifname: the name of the interface the data is associated with
+ *
+ * Repopulates OVS per-port data on destination host
+ *
+ * Returns 0 in case of success or -1 in case of failure
+ */
+int virNetDevOpenvswitchSetMigrateData(char *migrate, const char *ifname)
+{
+    virCommandPtr cmd = NULL;
+    int ret = -1;
+
+    cmd = virCommandNewArgList(OVSVSCTL, "--timeout=5", "set",
+                               "Interface", ifname, NULL);
+    virCommandAddArgFormat(cmd, "external_ids:PortData=%s", migrate);
+
+    /* Run the command */
+    if (virCommandRun(cmd, NULL) < 0) {
+        virReportSystemError(VIR_ERR_INTERNAL_ERROR,
+                             _("Unable to run command to set OVS port data for "
+                             "interface %s"), ifname);
+        goto cleanup;
+    }
+
+    ret = 0;
+cleanup:
+    return ret;
 }

src/libvirt_private.syms
@@ -1500,7 +1500,9 @@ virNetDevMacVLanVPortProfileRegisterCallback;
 
 # virnetdevopenvswitch.h
 virNetDevOpenvswitchAddPort;
+virNetDevOpenvswitchGetMigrateData;
 virNetDevOpenvswitchRemovePort;
+virNetDevOpenvswitchSetMigrateData;
 
 
 # virnetdevtap.h

src/util/virnetdevopenvswitch.h
@@ -42,4 +42,10 @@ int virNetDevOpenvswitchAddPort(const char *brname,
 int virNetDevOpenvswitchRemovePort(const char *brname, const char *ifname)
     ATTRIBUTE_NONNULL(2) ATTRIBUTE_RETURN_CHECK;
 
+int virNetDevOpenvswitchGetMigrateData(char **migrate, const char *ifname)
+    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2) ATTRIBUTE_RETURN_CHECK;
+
+int virNetDevOpenvswitchSetMigrateData(char *migrate, const char *ifname)
+    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2) ATTRIBUTE_RETURN_CHECK;
+
 #endif /* __VIR_NETDEV_OPENVSWITCH_H__ */

commit 25df57db73adc3e610193ee1fcdd202c47ba471d

util: don't fail if no PortData is found while getting migrateData
Introduced by f6a2f97e

Problem Description:
After multiple times of migrating a domain, which has an ovs interface with no portData set,
with non-shared disk, nbd ports got overflowed.

The steps to reproduce the problem:
1 define and start a domain with its network configured as:
    <interface type='bridge'>
          <source bridge='br0'/>
          <virtualport type='openvswitch'>
          </virtualport>
          <model type='virtio'/>
          <driver name='vhost' queues='4'/>
    </interface>
2 do not set the network's portData.
3 migrate(ToURI2) it with flag 91(1011011), which means:
  VIR_MIGRATE_LIVE
  VIR_MIGRATE_PEER2PEER
  VIR_MIGRATE_PERSIST_DEST
  VIR_MIGRATE_UNDEFINE_SOURCE
  VIR_MIGRATE_NON_SHARED_DISK
4 migrate success, but we got an error log in libvirtd.log:
  error : virCommandWait:2423 : internal error: Child process (ovs-vsctl --timeout=5 get Interface
  vnet1 external_ids:PortData) unexpected exit status 1: ovs-vsctl: no key "PortData" in Interface
  record "vnet1" column external_ids
5 migrate it back, migrate it , migrate it back, .......
6 nbd port got overflowed.

The reasons for the problem is :
1 virNetDevOpenvswitchGetMigrateData() takes it as wrong if no portData is available for  the ovs
 interface of a domain. (We think it's not appropriate, as portData is just OPTIONAL)
2 in func qemuMigrationBakeCookie(), it fails in qemuMigrationCookieAddNetwork(), and returns with -1.
 qemuMigrationCookieAddNBD() is not called thereafter, and mig->nbd is still NULL.
3 However, qemuMigrationRun() just *WARN* if qemuMigrationBakeCookie() fails, migration still successes.
 cookie is NULL, it's not baked on the src side.
4 On the destination side, it would alloc a port first and then free the nbd port in COOKIE.
 But the cookie is NULL due to qemuMigrationCookieAddNetwork() failure at src side. thus the nbd port
 is not freed.

src/util/virnetdevopenvswitch.c
@@ -30,8 +30,11 @@
 #include "virerror.h"
 #include "virmacaddr.h"
 #include "virstring.h"
+#include "virlog.h"
 
 #define VIR_FROM_THIS VIR_FROM_NONE
+
+VIR_LOG_INIT("util.netdevopenvswitch");
 
 /**
  * virNetDevOpenvswitchAddPort:
@@ -206,7 +209,7 @@ int virNetDevOpenvswitchGetMigrateData(char **migrate, const char *ifname)
     virCommandPtr cmd = NULL;
     int ret = -1;
 
-    cmd = virCommandNewArgList(OVSVSCTL, "--timeout=5", "get", "Interface",
+    cmd = virCommandNewArgList(OVSVSCTL, "--timeout=5", "--if-exists", "get", "Interface",
                                ifname, "external_ids:PortData", NULL);
 
     virCommandSetOutputBuffer(cmd, migrate);
@@ -240,6 +243,11 @@ int virNetDevOpenvswitchSetMigrateData(char *migrate, const char *ifname)
 {
     virCommandPtr cmd = NULL;
     int ret = -1;
+
+    if (!migrate) {
+        VIR_DEBUG("No OVS port data for interface %s", ifname);
+        return 0;
+    }
 
     cmd = virCommandNewArgList(OVSVSCTL, "--timeout=5", "set",
                                "Interface", ifname, NULL);