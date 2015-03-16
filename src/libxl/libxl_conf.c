libvirt/libvirt

commit 4ab8cd77ad2b000f486034aa87016b262bf1e79e

libxl: add tablet/mouse input device support
Signed-off-by: Marek Marczykowski-Górecki <marmarek@invisiblethingslab.com>

src/libxl/libxl_conf.c
@@ -749,6 +749,50 @@ libxlMakeDomBuildInfo(virDomainDefPtr def,
         libxl_defbool_set(&b_info->u.hvm.vnc.enable, 0);
         libxl_defbool_set(&b_info->u.hvm.sdl.enable, 0);
 
+        if (def->ninputs) {
+            for (i = 0; i < def->ninputs; i++) {
+                if (def->inputs[i]->bus != VIR_DOMAIN_INPUT_BUS_USB) {
+                    virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                            _("libxenlight supports only USB input"));
+                    return -1;
+                }
+            }
+#ifdef LIBXL_HAVE_BUILDINFO_USBDEVICE_LIST
+            if (VIR_ALLOC_N(b_info->u.hvm.usbdevice_list, def->ninputs+1) < 0)
+                return -1;
+#else
+            if (def->ninputs > 1) {
+                virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                        _("libxenlight supports only one input device"));
+                return -1;
+            }
+#endif
+            for (i = 0; i < def->ninputs; i++) {
+                char **usbdevice;
+#ifdef LIBXL_HAVE_BUILDINFO_USBDEVICE_LIST
+                usbdevice = &b_info->u.hvm.usbdevice_list[i];
+#else
+                usbdevice = &b_info->u.hvm.usbdevice;
+#endif
+                switch (def->inputs[i]->type) {
+                    case VIR_DOMAIN_INPUT_TYPE_MOUSE:
+                        VIR_FREE(*usbdevice);
+                        if (VIR_STRDUP(*usbdevice, "mouse") < 0)
+                            return -1;
+                        break;
+                    case VIR_DOMAIN_INPUT_TYPE_TABLET:
+                        VIR_FREE(*usbdevice);
+                        if (VIR_STRDUP(*usbdevice, "tablet") < 0)
+                            return -1;
+                        break;
+                    default:
+                        virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                                _("Unknown input device type"));
+                        return -1;
+                }
+            }
+        }
+
         /*
          * The following comment and calculation were taken directly from
          * libxenlight's internal function libxl_get_required_shadow_memory():

commit a7d6b94a00c6d13430229e9ac77e82c8be578337

libxl: fix regression introduced by commit 4ab8cd77
Commit 4ab8cd77 added a check requiring input devices to have
a bus type of VIR_DOMAIN_INPUT_BUS_USB, failing to start the
domain otherwise.  But virDomainDefParseXML adds implicit mouse
and keyboard if a graphics device is configured.  See calls to
virDomainDefMaybeAddInput.

The regression is fixed by removing the check requiring USB input
devices, and skipping non-USB input devices when populating USB
'usbdevice' in libxl_domain_build_info struct.

src/libxl/libxl_conf.c
@@ -750,13 +750,6 @@ libxlMakeDomBuildInfo(virDomainDefPtr def,
         libxl_defbool_set(&b_info->u.hvm.sdl.enable, 0);
 
         if (def->ninputs) {
-            for (i = 0; i < def->ninputs; i++) {
-                if (def->inputs[i]->bus != VIR_DOMAIN_INPUT_BUS_USB) {
-                    virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
-                            _("libxenlight supports only USB input"));
-                    return -1;
-                }
-            }
 #ifdef LIBXL_HAVE_BUILDINFO_USBDEVICE_LIST
             if (VIR_ALLOC_N(b_info->u.hvm.usbdevice_list, def->ninputs+1) < 0)
                 return -1;
@@ -769,6 +762,10 @@ libxlMakeDomBuildInfo(virDomainDefPtr def,
 #endif
             for (i = 0; i < def->ninputs; i++) {
                 char **usbdevice;
+
+                if (def->inputs[i]->bus != VIR_DOMAIN_INPUT_BUS_USB)
+                    continue;
+
 #ifdef LIBXL_HAVE_BUILDINFO_USBDEVICE_LIST
                 usbdevice = &b_info->u.hvm.usbdevice_list[i];
 #else