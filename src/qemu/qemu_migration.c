188e53673901eb72d12bfdd933eba91c9cfd4e3c
Michael Chapman <mike@very.puzzling.org>
2015-03-30 13:40:59 +1100

    qemu: fix error propagation in qemuMigrationBegin
    If virCloseCallbacksSet fails, qemuMigrationBegin must return NULL to
    indicate an error occurred.
    
    Signed-off-by: Michael Chapman <mike@very.puzzling.org>
    (cherry picked from commit 77ddd0bba2379b4ba33980d8f3976f9344451cca)

--- src/qemu/qemu_migration.c
+++ src/qemu/qemu_migration.c
@@ -2745,8 +2745,10 @@ qemuMigrationBegin(virConnectPtr conn,
          * place.
          */
         if (virCloseCallbacksSet(driver->closeCallbacks, vm, conn,
-                                 qemuMigrationCleanup) < 0)
+                                 qemuMigrationCleanup) < 0) {
+            VIR_FREE(xml);
             goto endjob;
+        }
         qemuMigrationJobContinue(vm);
     } else {
         goto endjob;
