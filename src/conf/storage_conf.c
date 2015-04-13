401623bcb420794578b93b8d5a5186653eaaf222
John Ferlan <jferlan@redhat.com>
2015-04-13 17:11:53 -0400

    storage: Add check for different ports for host duplicate matching
    In virStoragePoolSourceMatchSingleHost, add a comparison for port number
    being different prior to checking the 'name' field.

--- src/conf/storage_conf.c
+++ src/conf/storage_conf.c
@@ -2412,6 +2412,9 @@ virStoragePoolSourceMatchSingleHost(virStoragePoolSourcePtr poolsrc,
     if (poolsrc->nhost != 1 && defsrc->nhost != 1)
         return false;
 
+    if (poolsrc->hosts[0].port != defsrc->hosts[0].port)
+        return false;
+
     return STREQ(poolsrc->hosts[0].name, defsrc->hosts[0].name);
 }
 
