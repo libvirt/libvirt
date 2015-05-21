ba5566e80f95618c3bb8ac327d4a1c370539166d
Laine Stump <laine@laine.org>
2015-05-21 11:26:52 -0400

    interface: allow multiple IPv4 addresses + dhcp on a single interface
    As of netcf-0.2.8, netcf supports configuring multipl IPv4 addresses,
    as well as simultaneously configuring dhcp and static IPv4 addresses,
    on a single interface. This patch updates libvirt's interface.rng to
    allow such configurations.
    
    This resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1223688

--- tests/interfacexml2xmltest.c
+++ tests/interfacexml2xmltest.c
@@ -75,6 +75,7 @@ mymain(void)
         ret = -1
 
     DO_TEST("ethernet-dhcp");
+    DO_TEST("ethernet-dhcp-and-multi-static");
     DO_TEST("ethernet-static");
     DO_TEST("ethernet-static-no-prefix");
     DO_TEST("bridge");
