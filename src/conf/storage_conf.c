ea1c7b652b2b0c6248d03d4b2ed5b3e8afbd8c1b
Peter Krempa <pkrempa@redhat.com>
2015-06-30 10:14:17 +0200

    conf: storage: Fix duplicate check for gluster pools
    The pool name has to be the same too to warrant rejecting a pool
    definition as duplicate. This regression was introduced in commit
    2184ade3a0546b915252cb3b6a5dc88e9a8d2ccf.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1236438

--- src/conf/storage_conf.c
+++ src/conf/storage_conf.c
@@ -2471,13 +2471,22 @@ virStoragePoolSourceFindDuplicate(virConnectPtr conn,
             if (STREQ(pool->def->target.path, def->target.path))
                 matchpool = pool;
             break;
-        case VIR_STORAGE_POOL_NETFS:
+
         case VIR_STORAGE_POOL_GLUSTER:
+            if (STREQ(pool->def->source.name, def->source.name) &&
+                STREQ_NULLABLE(pool->def->source.dir, def->source.dir) &&
+                virStoragePoolSourceMatchSingleHost(&pool->def->source,
+                                                    &def->source))
+                matchpool = pool;
+            break;
+
+        case VIR_STORAGE_POOL_NETFS:
             if (STREQ(pool->def->source.dir, def->source.dir) &&
                 virStoragePoolSourceMatchSingleHost(&pool->def->source,
                                                     &def->source))
                 matchpool = pool;
             break;
+
         case VIR_STORAGE_POOL_SCSI:
             if (pool->def->source.adapter.type ==
                 VIR_STORAGE_POOL_SOURCE_ADAPTER_TYPE_FC_HOST &&

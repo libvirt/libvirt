libvirt/libvirt

commit 2184ade3a0546b915252cb3b6a5dc88e9a8d2ccf

storage: Add duplicate source pool for Gluster pool def
Check the proposed pool source host XML definition against existing gluster
pools to ensure the incoming definition doesn't use the same source dir and
soure host XML definition as an existing pool.

src/conf/storage_conf.c
@@ -2464,6 +2464,7 @@ virStoragePoolSourceFindDuplicate(virConnectPtr conn,
                 matchpool = pool;
             break;
         case VIR_STORAGE_POOL_NETFS:
+        case VIR_STORAGE_POOL_GLUSTER:
             if (STREQ(pool->def->source.dir, def->source.dir) &&
                 virStoragePoolSourceMatchSingleHost(&pool->def->source,
                                                     &def->source))
@@ -2551,7 +2552,6 @@ virStoragePoolSourceFindDuplicate(virConnectPtr conn,
             break;
         case VIR_STORAGE_POOL_MPATH:
         case VIR_STORAGE_POOL_RBD:
-        case VIR_STORAGE_POOL_GLUSTER:
         case VIR_STORAGE_POOL_ZFS:
         case VIR_STORAGE_POOL_LAST:
             break;

commit ea1c7b652b2b0c6248d03d4b2ed5b3e8afbd8c1b

conf: storage: Fix duplicate check for gluster pools
The pool name has to be the same too to warrant rejecting a pool
definition as duplicate. This regression was introduced in commit
2184ade3a0546b915252cb3b6a5dc88e9a8d2ccf.

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1236438

src/conf/storage_conf.c
@@ -2471,13 +2471,22 @@ virStoragePoolSourceFindDuplicate(virConnectPtr conn,
             if (STREQ(pool->def->target.path, def->target.path))
                 matchpool = pool;
             break;
-        case VIR_STORAGE_POOL_NETFS:
+
         case VIR_STORAGE_POOL_GLUSTER:
+            if (STREQ(pool->def->source.name, def->source.name) &&
+                STREQ_NULLABLE(pool->def->source.dir, def->source.dir) &&
+                virStoragePoolSourceMatchSingleHost(&pool->def->source,
+                                                    &def->source))
+                matchpool = pool;
+            break;
+
+        case VIR_STORAGE_POOL_NETFS:
             if (STREQ(pool->def->source.dir, def->source.dir) &&
                 virStoragePoolSourceMatchSingleHost(&pool->def->source,
                                                     &def->source))
                 matchpool = pool;
             break;
+
         case VIR_STORAGE_POOL_SCSI:
             if (pool->def->source.adapter.type ==
                 VIR_STORAGE_POOL_SOURCE_ADAPTER_TYPE_FC_HOST &&