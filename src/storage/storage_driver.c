libvirt/libvirt

commit 2a31c5f030a14d85055dd8c40cf309995b09c112

storage: Introduce storagePoolUpdateAllState function
The 'checkPool' callback was originally part of the storageDriverAutostart function,
but the pools need to be checked earlier during initialization phase,
otherwise we can't start a domain which mounts a volume after the
libvirtd daemon restarted. This is because qemuProcessReconnect is called
earlier than storageDriverAutostart. Therefore the 'checkPool' logic has been
moved to storagePoolUpdateAllState which is called inside storageDriverInitialize.

We also need a valid 'conn' reference to be able to execute 'refreshPool'
during initialization phase. Though it isn't available until storageDriverAutostart
all of our storage backends do ignore 'conn' pointer, except for RBD,
but RBD doesn't support 'checkPool' callback, so it's safe to pass
conn = NULL in this case.

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1177733

src/storage/storage_driver.c
@@ -75,6 +75,65 @@ static void storageDriverUnlock(void)
 }
 
 static void
+storagePoolUpdateAllState(void)
+{
+    size_t i;
+    bool active;
+
+    for (i = 0; i < driver->pools.count; i++) {
+        virStoragePoolObjPtr pool = driver->pools.objs[i];
+        virStorageBackendPtr backend;
+
+        virStoragePoolObjLock(pool);
+        if (!virStoragePoolObjIsActive(pool)) {
+            virStoragePoolObjUnlock(pool);
+            continue;
+        }
+
+        if ((backend = virStorageBackendForType(pool->def->type)) == NULL) {
+            VIR_ERROR(_("Missing backend %d"), pool->def->type);
+            virStoragePoolObjUnlock(pool);
+            continue;
+        }
+
+        /* Backends which do not support 'checkPool' are considered
+         * inactive by default.
+         */
+        active = false;
+        if (backend->checkPool &&
+            backend->checkPool(pool, &active) < 0) {
+            virErrorPtr err = virGetLastError();
+            VIR_ERROR(_("Failed to initialize storage pool '%s': %s"),
+                      pool->def->name, err ? err->message :
+                      _("no error message found"));
+            virStoragePoolObjUnlock(pool);
+            continue;
+        }
+
+        /* We can pass NULL as connection, most backends do not use
+         * it anyway, but if they do and fail, we want to log error and
+         * continue with other pools.
+         */
+        if (active) {
+            virStoragePoolObjClearVols(pool);
+            if (backend->refreshPool(NULL, pool) < 0) {
+                virErrorPtr err = virGetLastError();
+                if (backend->stopPool)
+                    backend->stopPool(NULL, pool);
+                VIR_ERROR(_("Failed to restart storage pool '%s': %s"),
+                          pool->def->name, err ? err->message :
+                          _("no error message found"));
+                virStoragePoolObjUnlock(pool);
+                continue;
+            }
+        }
+
+        pool->active = active;
+        virStoragePoolObjUnlock(pool);
+    }
+}
+
+static void
 storageDriverAutostart(void)
 {
     size_t i;
@@ -95,23 +154,11 @@ storageDriverAutostart(void)
 
         virStoragePoolObjLock(pool);
         if ((backend = virStorageBackendForType(pool->def->type)) == NULL) {
-            VIR_ERROR(_("Missing backend %d"), pool->def->type);
             virStoragePoolObjUnlock(pool);
             continue;
         }
 
-        if (backend->checkPool &&
-            backend->checkPool(pool, &started) < 0) {
-            virErrorPtr err = virGetLastError();
-            VIR_ERROR(_("Failed to initialize storage pool '%s': %s"),
-                      pool->def->name, err ? err->message :
-                      _("no error message found"));
-            virStoragePoolObjUnlock(pool);
-            continue;
-        }
-
-        if (!started &&
-            pool->autostart &&
+        if (pool->autostart &&
             !virStoragePoolObjIsActive(pool)) {
             if (backend->startPool &&
                 backend->startPool(conn, pool) < 0) {
@@ -212,6 +259,8 @@ storageStateInitialize(bool privileged,
                                      driver->configDir,
                                      driver->autostartDir) < 0)
         goto error;
+
+    storagePoolUpdateAllState();
 
     storageDriverUnlock();
 

commit f92f31213a718e194323718cb3c667129ef1d4e8

storage: Fix regression in storagePoolUpdateAllState
Commit 2a31c5f0 introduced support for storage pool state XMLs, however
it also introduced a regression:

if (!virstoragePoolObjIsActive(pool)) {
    virStoragePoolObjUnlock(pool);
    continue;
}

The idea behind this was that since we've got state XMLs and the pool
wasn't marked as active by autostart routine (if the autostart flag had been
set earlier), the pool is inactive and we can leave it be and continue with
other pools. However, filesystem type pools like fs,dir, possibly netfs are
supposed to be active if the filesystem is mounted on the host. And this is
exactly where the regression occurs, e.g. pool type 'dir' which has been
previously destroyed and marked as !autostart gets filtered out
by the condition above.
The resolution should be simply to remove the condition completely,
all pools will get their 'active' flag updated by check callback and if
they do not support such callback, the logic doesn't change and such
pools will be inactive by default (e.g. RBD, even if a state XML exists).

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1238610

src/storage/storage_driver.c
@@ -142,11 +142,6 @@ storagePoolUpdateAllState(void)
         virStoragePoolObjPtr pool = driver->pools.objs[i];
 
         virStoragePoolObjLock(pool);
-        if (!virStoragePoolObjIsActive(pool)) {
-            virStoragePoolObjUnlock(pool);
-            continue;
-        }
-
         storagePoolUpdateState(pool);
         virStoragePoolObjUnlock(pool);
     }