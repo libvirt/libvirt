88c4c32af17adfc21b810b62b71de8b2d245fef9
Andrea Bolognani <abologna@redhat.com>
2015-08-03 17:02:39 +0200

    nodeinfo: Fix build failure when KVM headers are not available
    Compiler error:
    
      ../../src/nodeinfo.c: In function 'nodeGetThreadsPerSubcore':
      ../../src/nodeinfo.c:2393: error: label 'out' defined but not used [-Wunused-label]
      ../../src/nodeinfo.c:2352: error: unused parameter 'arch' [-Wunused-parameter]

--- src/nodeinfo.c
+++ src/nodeinfo.c
@@ -2341,6 +2341,8 @@ nodeAllocPages(unsigned int npages,
     return ret;
 }
 
+#if HAVE_LINUX_KVM_H && defined(KVM_CAP_PPC_SMT)
+
 /* Get the number of threads per subcore.
  *
  * This will be 2, 4 or 8 on POWER hosts, depending on the current
@@ -2352,8 +2354,6 @@ int
 nodeGetThreadsPerSubcore(virArch arch)
 {
     int threads_per_subcore = 0;
-
-#if HAVE_LINUX_KVM_H && defined(KVM_CAP_PPC_SMT)
     const char *kvmpath = "/dev/kvm";
     int kvmfd;
 
@@ -2388,8 +2388,19 @@ nodeGetThreadsPerSubcore(virArch arch)
 
         VIR_FORCE_CLOSE(kvmfd);
     }
-#endif /* HAVE_LINUX_KVM_H && defined(KVM_CAP_PPC_SMT) */
 
  out:
     return threads_per_subcore;
 }
+
+#else
+
+/* Fallback for nodeGetThreadsPerSubcore() used when KVM headers
+ * are not available on the system */
+int
+nodeGetThreadsPerSubcore(virArch arch ATTRIBUTE_UNUSED)
+{
+    return 0;
+}
+
+#endif /* HAVE_LINUX_KVM_H && defined(KVM_CAP_PPC_SMT) */
