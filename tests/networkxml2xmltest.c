1f24c1494a85d663fa36047922a7974a292a46a7
Martin Kletzander <mkletzan@redhat.com>
2015-08-11 15:09:29 +0200

    conf: Don't try formating non-existing addresses
    Commit a6f9af8292b6 added checking for address colisions between
    starting and ending addresses of forwarding addresses, but forgot that
    there might be no addresses set at all.
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- tests/networkxml2xmltest.c
+++ tests/networkxml2xmltest.c
@@ -99,6 +99,7 @@ mymain(void)
     DO_TEST("nat-network-dns-forward-plain");
     DO_TEST("nat-network-dns-forwarders");
     DO_TEST("nat-network-forward-nat-address");
+    DO_TEST("nat-network-forward-nat-no-address");
     DO_TEST("8021Qbh-net");
     DO_TEST("direct-net");
     DO_TEST("host-bridge-net");
