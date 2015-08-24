98242f94cd181f0257535479369054f07f951b21
John Ferlan <jferlan@redhat.com>
2015-08-24 12:38:13 -0400

    storage: Handle failure from refreshVol
    Commit id '155ca616' added the 'refreshVol' API. In an NFS root-squash
    environment it was possible that if the just created volume from XML wasn't
    properly created with the right uid/gid and/or mode, then the followup
    refreshVol will fail to open the volume in order to get the allocation/
    capacity values. This would leave the volume still on the server and
    cause a libvirtd crash because 'voldef' would be in the pool list, but
    the cleanup code would free it.
    
    (cherry picked from commit db9277a39bc364806e8d3e08a08fc128d59b7094)

--- src/storage/storage_driver.c
+++ src/storage/storage_driver.c
@@ -1864,8 +1864,12 @@ storageVolCreateXML(virStoragePoolPtr obj,
         goto cleanup;
 
     if (backend->refreshVol &&
-        backend->refreshVol(obj->conn, pool, voldef) < 0)
+        backend->refreshVol(obj->conn, pool, voldef) < 0) {
+        storageVolDeleteInternal(volobj, backend, pool, voldef,
+                                 0, false);
+        voldef = NULL;
         goto cleanup;
+    }
 
     /* Update pool metadata ignoring the disk backend since
      * it updates the pool values.
