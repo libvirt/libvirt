libvirt/libvirt

commit ba25c214f7c622c75448e760f3204844cfff50d0

libxl: open libxl log stream with libvirtd log_level
Instead of a hardcoded DEBUG log level, use the overall
daemon log level specified in libvirtd.conf when opening
a log stream with libxl. libxl is very verbose when DEBUG
log level is set, resulting in huge log files that can
potentially fill a disk. Control of libxl verbosity should
be placed in the administrator's hands.

src/libxl/libxl_conf.c
@@ -1496,6 +1496,7 @@ libxlDriverConfigNew(void)
 {
     libxlDriverConfigPtr cfg;
     char *log_file = NULL;
+    xentoollog_level log_level;
     char ebuf[1024];
     unsigned int free_mem;
 
@@ -1540,9 +1541,24 @@ libxlDriverConfigNew(void)
     }
     VIR_FREE(log_file);
 
+    switch (virLogGetDefaultPriority()) {
+    case VIR_LOG_DEBUG:
+        log_level = XTL_DEBUG;
+        break;
+    case VIR_LOG_INFO:
+        log_level = XTL_INFO;
+        break;
+    case VIR_LOG_WARN:
+        log_level = XTL_WARN;
+        break;
+    case VIR_LOG_ERROR:
+        log_level = XTL_ERROR;
+        break;
+    }
+
     cfg->logger =
         (xentoollog_logger *)xtl_createlogger_stdiostream(cfg->logger_file,
-                                      XTL_DEBUG, XTL_STDIOSTREAM_SHOW_DATE);
+                                      log_level, XTL_STDIOSTREAM_SHOW_DATE);
     if (!cfg->logger) {
         VIR_ERROR(_("cannot create logger for libxenlight, disabling driver"));
         goto error;

commit a4604eb086721542f9faa866039922c77e899d76

libxl: fix compiler error introduced by commit ba25c214
libxl/libxl_conf.c: In function 'libxlDriverConfigNew':
libxl/libxl_conf.c:1560:30: error: 'log_level' may be used uninitialized
in this function [-Werror=maybe-uninitialized]

src/libxl/libxl_conf.c
@@ -1496,7 +1496,7 @@ libxlDriverConfigNew(void)
 {
     libxlDriverConfigPtr cfg;
     char *log_file = NULL;
-    xentoollog_level log_level;
+    xentoollog_level log_level = XTL_DEBUG;
     char ebuf[1024];
     unsigned int free_mem;
 