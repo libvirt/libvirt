libvirt/libvirt

commit d6c91b3c03bf2fc9e16a097d4e4e671772eb0363

qemu: cgroup: Extract guts of qemuSetupImageCgroupInternal
They will later be reused for setting cgroup for other image backed
devices.

src/qemu/qemu_cgroup.c
@@ -52,38 +52,47 @@ static const char *const defaultDeviceACL[] = {
 #define DEVICE_PTY_MAJOR 136
 #define DEVICE_SND_MAJOR 116
 
+
 static int
-qemuSetupImageCgroupInternal(virDomainObjPtr vm,
-                             virStorageSourcePtr src,
-                             bool forceReadonly)
+qemuSetupImagePathCgroup(virDomainObjPtr vm,
+                         const char *path,
+                         bool readonly)
 {
     qemuDomainObjPrivatePtr priv = vm->privateData;
     int perms = VIR_CGROUP_DEVICE_READ;
     int ret;
 
-    if (!virCgroupHasController(priv->cgroup,
-                                VIR_CGROUP_CONTROLLER_DEVICES))
+    if (!virCgroupHasController(priv->cgroup, VIR_CGROUP_CONTROLLER_DEVICES))
         return 0;
 
+    if (readonly)
+        perms |= VIR_CGROUP_DEVICE_WRITE;
+
+    VIR_DEBUG("Allow path %s, perms: %s",
+              path, virCgroupGetDevicePermsString(perms));
+
+    ret = virCgroupAllowDevicePath(priv->cgroup, path, perms, true);
+
+    virDomainAuditCgroupPath(vm, priv->cgroup, "allow", path,
+                             virCgroupGetDevicePermsString(perms),
+                             ret == 0);
+
+    return ret;
+}
+
+
+static int
+qemuSetupImageCgroupInternal(virDomainObjPtr vm,
+                             virStorageSourcePtr src,
+                             bool forceReadonly)
+{
     if (!src->path || !virStorageSourceIsLocalStorage(src)) {
         VIR_DEBUG("Not updating cgroups for disk path '%s', type: %s",
                   NULLSTR(src->path), virStorageTypeToString(src->type));
         return 0;
     }
 
-    if (!src->readonly && !forceReadonly)
-        perms |= VIR_CGROUP_DEVICE_WRITE;
-
-    VIR_DEBUG("Allow path %s, perms: %s",
-              src->path, virCgroupGetDevicePermsString(perms));
-
-    ret = virCgroupAllowDevicePath(priv->cgroup, src->path, perms, true);
-
-    virDomainAuditCgroupPath(vm, priv->cgroup, "allow", src->path,
-                             virCgroupGetDevicePermsString(perms),
-                             ret == 0);
-
-    return ret;
+    return qemuSetupImagePathCgroup(vm, src->path, src->readonly || forceReadonly);
 }
 
 

commit 65c4c7d850010049562e27c40639aba1d0526ead

qemu: cgroup: fix cgroup permission logic
Fix logic error introduced in commit d6c91b3c which essentially broke
starting any domain.

Signed-off-by: Bjoern Walk <bwalk@linux.vnet.ibm.com>

src/qemu/qemu_cgroup.c
@@ -65,7 +65,7 @@ qemuSetupImagePathCgroup(virDomainObjPtr vm,
     if (!virCgroupHasController(priv->cgroup, VIR_CGROUP_CONTROLLER_DEVICES))
         return 0;
 
-    if (readonly)
+    if (!readonly)
         perms |= VIR_CGROUP_DEVICE_WRITE;
 
     VIR_DEBUG("Allow path %s, perms: %s",