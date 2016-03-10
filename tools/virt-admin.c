9d479dd1853aee047234caaaf97f648a396827a4
Martin Kletzander <mkletzan@redhat.com>
2016-03-10 06:34:03 +0100

    virt-admin: Don't tell everyone needlessly we're connected
    There are cases when we don't want to tell the user we are connected.
    That's for example when we first connect to the server without the
    command 'connect' itself.  That helps to clear out output of first
    command, mainly when running non-interactively.
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- tools/virt-admin.c
+++ tools/virt-admin.c
@@ -120,8 +120,6 @@ vshAdmConnect(vshControl *ctl, unsigned int flags)
 
         if (priv->wantReconnect)
             vshPrint(ctl, "%s\n", _("Reconnected to the admin server"));
-        else
-            vshPrint(ctl, "%s\n", _("Connected to the admin server"));
     }
 
     return 0;
@@ -289,6 +287,7 @@ cmdConnect(vshControl *ctl, const vshCmd *cmd)
 {
     const char *name = NULL;
     vshAdmControlPtr priv = ctl->privData;
+    bool connected = priv->conn;
 
     if (vshCommandOptStringReq(ctl, cmd, "name", &name) < 0)
         return false;
@@ -297,6 +296,8 @@ cmdConnect(vshControl *ctl, const vshCmd *cmd)
     ctl->connname = vshStrdup(ctl, name);
 
     vshAdmReconnect(ctl);
+    if (!connected)
+        vshPrint(ctl, "%s\n", _("Connected to the admin server"));
 
     return !!priv->conn;
 }
