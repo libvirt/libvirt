16b41728b59a7188f643937303e3aec4c299a980
Martin Kletzander <mkletzan@redhat.com>
2016-04-08 15:29:40 +0200

    qemu: Free priv->machineName
    Commit c3bd0019c0e3 forgot to cleanup after itself.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1325043
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- src/qemu/qemu_cgroup.c
+++ src/qemu/qemu_cgroup.c
@@ -1123,5 +1123,7 @@ qemuRemoveCgroup(virDomainObjPtr vm)
             VIR_DEBUG("Failed to terminate cgroup for %s", vm->def->name);
     }
 
+    VIR_FREE(priv->machineName);
+
     return virCgroupRemove(priv->cgroup);
 }
