99c17cb20536a0210d25d66777065b96e954b30b
Pavel Hrdina <phrdina@redhat.com>
2016-04-25 17:10:30 +0200

    genericxml2xml: add several graphics tests
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

--- tests/genericxml2xmltest.c
+++ tests/genericxml2xmltest.c
@@ -77,9 +77,16 @@ mymain(void)
 
     DO_TEST_DIFFERENT("disk-virtio");
 
+    DO_TEST_DIFFERENT("graphics-vnc-minimal");
+    DO_TEST_DIFFERENT("graphics-vnc-manual-port");
+    DO_TEST_DIFFERENT("graphics-vnc-socket");
+    DO_TEST_DIFFERENT("graphics-vnc-socket-listen");
     DO_TEST_DIFFERENT("graphics-listen-back-compat");
     DO_TEST_FULL("graphics-listen-back-compat-mismatch", 0, false,
         TEST_COMPARE_DOM_XML2XML_RESULT_FAIL_PARSE);
+    DO_TEST_DIFFERENT("graphics-vnc-listen-attr-only");
+    DO_TEST_DIFFERENT("graphics-vnc-listen-element-minimal");
+    DO_TEST_DIFFERENT("graphics-vnc-listen-element-with-address");
 
     DO_TEST_FULL("name-slash-parse", 0, false,
         TEST_COMPARE_DOM_XML2XML_RESULT_FAIL_PARSE);
