454f739f24a936aedec48c3a3ba659b374ce251a
Cole Robinson <crobinso@redhat.com>
2016-04-26 12:47:42 -0400

    conf: network: reject name containing '/'
    Trying to define a network name containing an embedded '/'
    will immediately fail when trying to write the XML to disk.
    This patch explicitly rejects names containing a '/'
    
    Besides the network bridge driver, the only other network
    implementation is a very thin one for virtualbox, which seems to
    use the network name as a host interface name, which won't
    accept '/' anyways, so I think this is fine to do unconitionally.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=787604

--- src/conf/network_conf.c
+++ src/conf/network_conf.c
@@ -2070,6 +2070,12 @@ virNetworkDefParseXML(xmlXPathContextPtr ctxt)
         goto error;
     }
 
+    if (strchr(def->name, '/')) {
+        virReportError(VIR_ERR_XML_ERROR,
+                       _("name %s cannot contain '/'"), def->name);
+        goto error;
+    }
+
     /* Extract network uuid */
     tmp = virXPathString("string(./uuid[1])", ctxt);
     if (!tmp) {
