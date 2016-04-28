1ccc7fbff34ea44e5b0cf01a359d127a4c62a695
Pavel Hrdina <phrdina@redhat.com>
2016-04-28 17:55:11 +0200

    qemu_hotplug: fix checking graphics ports
    We cannot change ports for running domain and we should error out if
    autoport is enabled.
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

--- src/qemu/qemu_hotplug.c
+++ src/qemu/qemu_hotplug.c
@@ -2666,9 +2666,8 @@ qemuDomainChangeGraphics(virQEMUDriverPtr driver,
 
     switch (dev->type) {
     case VIR_DOMAIN_GRAPHICS_TYPE_VNC:
-        if ((olddev->data.vnc.autoport != dev->data.vnc.autoport) ||
-            (!dev->data.vnc.autoport &&
-             (olddev->data.vnc.port != dev->data.vnc.port))) {
+        if (olddev->data.vnc.autoport != dev->data.vnc.autoport ||
+            olddev->data.vnc.port != dev->data.vnc.port) {
             virReportError(VIR_ERR_OPERATION_UNSUPPORTED, "%s",
                            _("cannot change port settings on vnc graphics"));
             goto cleanup;
@@ -2710,11 +2709,9 @@ qemuDomainChangeGraphics(virQEMUDriverPtr driver,
         break;
 
     case VIR_DOMAIN_GRAPHICS_TYPE_SPICE:
-        if ((olddev->data.spice.autoport != dev->data.spice.autoport) ||
-            (!dev->data.spice.autoport &&
-             (olddev->data.spice.port != dev->data.spice.port)) ||
-            (!dev->data.spice.autoport &&
-             (olddev->data.spice.tlsPort != dev->data.spice.tlsPort))) {
+        if (olddev->data.spice.autoport != dev->data.spice.autoport ||
+            olddev->data.spice.port != dev->data.spice.port ||
+            olddev->data.spice.tlsPort != dev->data.spice.tlsPort) {
             virReportError(VIR_ERR_OPERATION_UNSUPPORTED, "%s",
                            _("cannot change port settings on spice graphics"));
             goto cleanup;
