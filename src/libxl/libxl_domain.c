fccf27253cedd131c5c4720d31d96ecc68c20e59
Jim Fehlig <jfehlig@suse.com>
2016-04-28 21:08:28 -0600

    libxl: switch to using libxl_domain_create_restore from v4.4 API
    In LIBXL_API_VERSION 0x040400, the libxl_domain_create_restore API
    gained a parameter for specifying restore parameters. Switch to
    using version 0x040400, which will be useful in a subsequent commit
    to specify the Xen migration stream version when restoring.
    
    Signed-off-by: Jim Fehlig <jfehlig@suse.com>

--- src/libxl/libxl_domain.c
+++ src/libxl/libxl_domain.c
@@ -1028,6 +1028,7 @@ libxlDomainStart(libxlDriverPrivatePtr driver, virDomainObjPtr vm,
     libxlDriverConfigPtr cfg;
     virHostdevManagerPtr hostdev_mgr = driver->hostdevMgr;
     libxl_asyncprogress_how aop_console_how;
+    libxl_domain_restore_params params;
 
     libxl_domain_config_init(&d_config);
 
@@ -1115,8 +1116,11 @@ libxlDomainStart(libxlDriverPrivatePtr driver, virDomainObjPtr vm,
         ret = libxl_domain_create_new(cfg->ctx, &d_config,
                                       &domid, NULL, &aop_console_how);
     } else {
+        libxl_domain_restore_params_init(&params);
         ret = libxl_domain_create_restore(cfg->ctx, &d_config, &domid,
-                                          restore_fd, NULL, &aop_console_how);
+                                          restore_fd, &params, NULL,
+                                          &aop_console_how);
+        libxl_domain_restore_params_dispose(&params);
     }
     virObjectLock(vm);
 
