077ba95677419fb9916a7ddb09c45d24aced5dcb
Cole Robinson <crobinso@redhat.com>
2016-05-08 14:33:50 -0400

    qemu: command: escape commas in VNC socket path
    This path can be dependent on the VM libdir, which contains its name

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -1903,7 +1903,7 @@ mymain(void)
                               NONE);
 
     DO_TEST("name-escape", QEMU_CAPS_NAME_DEBUG_THREADS,
-            QEMU_CAPS_OBJECT_SECRET, QEMU_CAPS_CHARDEV);
+            QEMU_CAPS_OBJECT_SECRET, QEMU_CAPS_CHARDEV, QEMU_CAPS_VNC);
     DO_TEST("debug-threads", QEMU_CAPS_NAME_DEBUG_THREADS);
 
     DO_TEST("master-key", QEMU_CAPS_OBJECT_SECRET);
