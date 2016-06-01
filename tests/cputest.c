355163fb927004570a56279578d08576621ccc20
Jiri Denemark <jdenemar@redhat.com>
2016-06-01 17:52:55 +0200

    tests: Add CPU detection test for Intel Xeon E5-2650
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>

--- tests/cputest.c
+++ tests/cputest.c
@@ -870,6 +870,7 @@ mymain(void)
     DO_TEST_CPUID("x86", "Xeon-5110", false);
     DO_TEST_CPUID("x86", "Xeon-E3-1245", true);
     DO_TEST_CPUID("x86", "Xeon-E5-2630", true);
+    DO_TEST_CPUID("x86", "Xeon-E5-2650", true);
 
 #if WITH_QEMU && WITH_YAJL
     qemuTestDriverFree(&driver);
