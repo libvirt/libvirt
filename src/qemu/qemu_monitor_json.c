libvirt/libvirt

commit b4a5fd95f70b373601abcecdb9d87e4cdb219350

qemu: introduce vram64 attribute for QXL video device
This attribute is used to extend secondary PCI bar and expose it to the
guest as 64bit memory.  It works like this: attribute vram is there to
set size of secondary PCI bar and guest sees it as 32bit memory,
attribute vram64 can extend this secondary PCI bar.  If both attributes
are used, guest sees two memory bars, both address the same memory, with
the difference that the 32bit bar can address only the first part of the
whole memory.

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1260749

Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

src/qemu/qemu_monitor_json.c
@@ -1436,6 +1436,18 @@ qemuMonitorJSONUpdateVideoMemorySize(qemuMonitorPtr mon,
             return -1;
         }
         video->vram = prop.val.ul / 1024;
+
+        if (video->vram64 != 0) {
+            if (qemuMonitorJSONGetObjectProperty(mon, path,
+                                                 "vram64_size_mb", &prop) < 0) {
+                virReportError(VIR_ERR_INTERNAL_ERROR,
+                               _("QOM Object '%s' has no property 'vram64_size_mb'"),
+                               path);
+                return -1;
+            }
+            video->vram64 = prop.val.ul / 1024;
+        }
+
         if (qemuMonitorJSONGetObjectProperty(mon, path, "ram_size", &prop) < 0) {
             virReportError(VIR_ERR_INTERNAL_ERROR,
                            _("QOM Object '%s' has no property 'ram_size'"),
@@ -1460,6 +1472,48 @@ qemuMonitorJSONUpdateVideoMemorySize(qemuMonitorPtr mon,
         }
         video->vram = prop.val.ul * 1024;
         break;
+    case VIR_DOMAIN_VIDEO_TYPE_CIRRUS:
+    case VIR_DOMAIN_VIDEO_TYPE_XEN:
+    case VIR_DOMAIN_VIDEO_TYPE_VBOX:
+    case VIR_DOMAIN_VIDEO_TYPE_LAST:
+        break;
+    }
+
+    return 0;
+}
+
+
+/**
+ * Loads correct video vram64 size value from QEMU and update the video
+ * definition.
+ *
+ * Return 0 on success, -1 on failure and set proper error message.
+ */
+int
+qemuMonitorJSONUpdateVideoVram64Size(qemuMonitorPtr mon,
+                                     virDomainVideoDefPtr video,
+                                     char *path)
+{
+    qemuMonitorJSONObjectProperty prop = {
+        QEMU_MONITOR_OBJECT_PROPERTY_ULONG,
+        {0}
+    };
+
+    switch (video->type) {
+    case VIR_DOMAIN_VIDEO_TYPE_QXL:
+        if (video->vram64 != 0) {
+            if (qemuMonitorJSONGetObjectProperty(mon, path,
+                                                 "vram64_size_mb", &prop) < 0) {
+                virReportError(VIR_ERR_INTERNAL_ERROR,
+                               _("QOM Object '%s' has no property 'vram64_size_mb'"),
+                               path);
+                return -1;
+            }
+            video->vram64 = prop.val.ul / 1024;
+        }
+        break;
+    case VIR_DOMAIN_VIDEO_TYPE_VGA:
+    case VIR_DOMAIN_VIDEO_TYPE_VMVGA:
     case VIR_DOMAIN_VIDEO_TYPE_CIRRUS:
     case VIR_DOMAIN_VIDEO_TYPE_XEN:
     case VIR_DOMAIN_VIDEO_TYPE_VBOX:

docs/formatdomain.html.in
@@ -5200,6 +5200,8 @@ qemu-kvm -net nic,model=? /dev/null
           two as <code>vram</code>. There is also optional attribute
           <code>vgamem</code> (<span class="since">since 1.2.11</span>) to set
           the size of VGA framebuffer for fallback mode of QXL device.
+          Attribute <code>vram64</code> (<span class="since">since 1.3.2</span>)
+          extends secondary bar and makes it addressable as 64bit memory.
         </p>
       </dd>
 

docs/schemas/domaincommon.rng
@@ -2946,6 +2946,11 @@
                   <ref name="unsignedInt"/>
                 </attribute>
               </optional>
+              <optional>
+                <attribute name="vram64">
+                  <ref name="unsignedInt"/>
+                </attribute>
+              </optional>
             </group>
           </choice>
           <optional>

src/conf/domain_conf.c
@@ -11918,6 +11918,7 @@ virDomainVideoDefParseXML(xmlNodePtr node,
     char *type = NULL;
     char *heads = NULL;
     char *vram = NULL;
+    char *vram64 = NULL;
     char *ram = NULL;
     char *vgamem = NULL;
     char *primary = NULL;
@@ -11933,6 +11934,7 @@ virDomainVideoDefParseXML(xmlNodePtr node,
                 type = virXMLPropString(cur, "type");
                 ram = virXMLPropString(cur, "ram");
                 vram = virXMLPropString(cur, "vram");
+                vram64 = virXMLPropString(cur, "vram64");
                 vgamem = virXMLPropString(cur, "vgamem");
                 heads = virXMLPropString(cur, "heads");
 
@@ -11987,6 +11989,19 @@ virDomainVideoDefParseXML(xmlNodePtr node,
         def->vram = virDomainVideoDefaultRAM(dom, def->type);
     }
 
+    if (vram64) {
+        if (def->type != VIR_DOMAIN_VIDEO_TYPE_QXL) {
+            virReportError(VIR_ERR_XML_ERROR, "%s",
+                           _("vram64 attribute only supported for type of qxl"));
+            goto error;
+        }
+        if (virStrToLong_uip(vram64, NULL, 10, &def->vram64) < 0) {
+            virReportError(VIR_ERR_XML_ERROR,
+                           _("cannot parse video vram64 '%s'"), vram64);
+            goto error;
+        }
+    }
+
     if (vgamem) {
         if (def->type != VIR_DOMAIN_VIDEO_TYPE_QXL) {
             virReportError(VIR_ERR_XML_ERROR, "%s",
@@ -12013,9 +12028,11 @@ virDomainVideoDefParseXML(xmlNodePtr node,
     if (virDomainDeviceInfoParseXML(node, NULL, &def->info, flags) < 0)
         goto error;
 
+ cleanup:
     VIR_FREE(type);
     VIR_FREE(ram);
     VIR_FREE(vram);
+    VIR_FREE(vram64);
     VIR_FREE(vgamem);
     VIR_FREE(heads);
 
@@ -12023,12 +12040,8 @@ virDomainVideoDefParseXML(xmlNodePtr node,
 
  error:
     virDomainVideoDefFree(def);
-    VIR_FREE(type);
-    VIR_FREE(ram);
-    VIR_FREE(vram);
-    VIR_FREE(vgamem);
-    VIR_FREE(heads);
-    return NULL;
+    def = NULL;
+    goto cleanup;
 }
 
 static virDomainHostdevDefPtr
@@ -17041,6 +17054,13 @@ virDomainVideoDefCheckABIStability(virDomainVideoDefPtr src,
         return false;
     }
 
+    if (src->vram64 != dst->vram64) {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                       _("Target video card vram64 %u does not match source %u"),
+                       dst->vram64, src->vram64);
+        return false;
+    }
+
     if (src->vgamem != dst->vgamem) {
         virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
                        _("Target video card vgamem %u does not match source %u"),
@@ -20726,6 +20746,8 @@ virDomainVideoDefFormat(virBufferPtr buf,
         virBufferAsprintf(buf, " ram='%u'", def->ram);
     if (def->vram)
         virBufferAsprintf(buf, " vram='%u'", def->vram);
+    if (def->vram64)
+        virBufferAsprintf(buf, " vram64='%u'", def->vram64);
     if (def->vgamem)
         virBufferAsprintf(buf, " vgamem='%u'", def->vgamem);
     if (def->heads)

src/conf/domain_conf.h
@@ -1399,6 +1399,7 @@ struct _virDomainVideoDef {
     int type;
     unsigned int ram;  /* kibibytes (multiples of 1024) */
     unsigned int vram; /* kibibytes (multiples of 1024) */
+    unsigned int vram64; /* kibibytes (multiples of 1024) */
     unsigned int vgamem; /* kibibytes (multiples of 1024) */
     unsigned int heads;
     bool primary;

src/qemu/qemu_command.c
@@ -3347,6 +3347,14 @@ qemuBuildDeviceVideoStr(virDomainDefPtr def,
         }
 
         if ((video->primary &&
+             virQEMUCapsGet(qemuCaps, QEMU_CAPS_QXL_VGA_VRAM64)) ||
+            (!video->primary &&
+             virQEMUCapsGet(qemuCaps, QEMU_CAPS_QXL_VRAM64))) {
+            /* QEMU accepts mebibytes for vram64_size_mb. */
+            virBufferAsprintf(&buf, ",vram64_size_mb=%u", video->vram64 / 1024);
+        }
+
+        if ((video->primary &&
              virQEMUCapsGet(qemuCaps, QEMU_CAPS_QXL_VGA_VGAMEM)) ||
             (!video->primary &&
              virQEMUCapsGet(qemuCaps, QEMU_CAPS_QXL_VGAMEM))) {
@@ -8272,6 +8280,7 @@ qemuBuildCommandLine(virConnectPtr conn,
                     virQEMUCapsGet(qemuCaps, QEMU_CAPS_DEVICE)) {
                     unsigned int ram = def->videos[0]->ram;
                     unsigned int vram = def->videos[0]->vram;
+                    unsigned int vram64 = def->videos[0]->vram64;
                     unsigned int vgamem = def->videos[0]->vgamem;
 
                     if (vram > (UINT_MAX / 1024)) {
@@ -8296,6 +8305,12 @@ qemuBuildCommandLine(virConnectPtr conn,
                         virCommandAddArg(cmd, "-global");
                         virCommandAddArgFormat(cmd, "%s.vram_size=%u",
                                                dev, vram * 1024);
+                    }
+                    if (vram64 &&
+                        virQEMUCapsGet(qemuCaps, QEMU_CAPS_QXL_VGA_VRAM64)) {
+                        virCommandAddArg(cmd, "-global");
+                        virCommandAddArgFormat(cmd, "%s.vram64_size_mb=%u",
+                                               dev, vram64 / 1024);
                     }
                     if (vgamem &&
                         virQEMUCapsGet(qemuCaps, QEMU_CAPS_QXL_VGA_VGAMEM)) {

src/qemu/qemu_monitor.c
@@ -1154,6 +1154,41 @@ qemuMonitorUpdateVideoMemorySize(qemuMonitorPtr mon,
 }
 
 
+/**
+ * To update video vram64 size in status XML we need to load correct value from
+ * QEMU.  This is supported only with JSON monitor.
+ *
+ * Returns 0 on success, -1 on failure and sets proper error message.
+ */
+int
+qemuMonitorUpdateVideoVram64Size(qemuMonitorPtr mon,
+                                 virDomainVideoDefPtr video,
+                                 const char *videoName)
+{
+    int ret = -1;
+    char *path = NULL;
+
+    QEMU_CHECK_MONITOR(mon);
+
+    if (mon->json) {
+        ret = qemuMonitorJSONFindLinkPath(mon, videoName, &path);
+        if (ret < 0) {
+            if (ret == -2)
+                virReportError(VIR_ERR_INTERNAL_ERROR,
+                               _("Failed to find QOM Object path for "
+                                 "device '%s'"), videoName);
+            return -1;
+        }
+
+        ret = qemuMonitorJSONUpdateVideoVram64Size(mon, video, path);
+        VIR_FREE(path);
+        return ret;
+    }
+
+    return 0;
+}
+
+
 int
 qemuMonitorHMPCommandWithFd(qemuMonitorPtr mon,
                             const char *cmd,

src/qemu/qemu_monitor.h
@@ -269,6 +269,10 @@ int qemuMonitorUpdateVideoMemorySize(qemuMonitorPtr mon,
                                      virDomainVideoDefPtr video,
                                      const char *videoName)
     ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2) ATTRIBUTE_NONNULL(3);
+int qemuMonitorUpdateVideoVram64Size(qemuMonitorPtr mon,
+                                     virDomainVideoDefPtr video,
+                                     const char *videoName)
+    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2) ATTRIBUTE_NONNULL(3);
 int qemuMonitorHMPCommandWithFd(qemuMonitorPtr mon,
                                 const char *cmd,
                                 int scm_fd,

commit de0b091ae08a38b13ea62b806e28666c51da198e

QXL: fix reloading of vram64 attribute
Commit b4a5fd95 introduced vram64 attribute for QXL video device but
there were two issues.  Only function
qemuMonitorJSONUpdateVideoVram64Size should update the vram64 attribute
and also the value is in MiB, not in B.

Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

src/qemu/qemu_monitor_json.c
@@ -1483,17 +1483,6 @@ qemuMonitorJSONUpdateVideoMemorySize(qemuMonitorPtr mon,
         }
         video->vram = prop.val.ul / 1024;
 
-        if (video->vram64 != 0) {
-            if (qemuMonitorJSONGetObjectProperty(mon, path,
-                                                 "vram64_size_mb", &prop) < 0) {
-                virReportError(VIR_ERR_INTERNAL_ERROR,
-                               _("QOM Object '%s' has no property 'vram64_size_mb'"),
-                               path);
-                return -1;
-            }
-            video->vram64 = prop.val.ul / 1024;
-        }
-
         if (qemuMonitorJSONGetObjectProperty(mon, path, "ram_size", &prop) < 0) {
             virReportError(VIR_ERR_INTERNAL_ERROR,
                            _("QOM Object '%s' has no property 'ram_size'"),
@@ -1555,7 +1544,7 @@ qemuMonitorJSONUpdateVideoVram64Size(qemuMonitorPtr mon,
                                path);
                 return -1;
             }
-            video->vram64 = prop.val.ul / 1024;
+            video->vram64 = prop.val.ul * 1024;
         }
         break;
     case VIR_DOMAIN_VIDEO_TYPE_VGA: