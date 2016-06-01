f3ebce14667df388fffc510d58e7e5dfba2df38d
Jiri Denemark <jdenemar@redhat.com>
2016-06-01 17:52:54 +0200

    tests: Add CPU detection test for Intel Pentium P6100
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>

--- tests/cputest.c
+++ tests/cputest.c
@@ -865,6 +865,7 @@ mymain(void)
     DO_TEST_CPUID("x86", "Opteron-2350", true);
     DO_TEST_CPUID("x86", "Opteron-6234", true);
     DO_TEST_CPUID("x86", "Opteron-6282", false);
+    DO_TEST_CPUID("x86", "Pentium-P6100", false);
 
 #if WITH_QEMU && WITH_YAJL
     qemuTestDriverFree(&driver);
