40b54ebe1424058a5a625deed98e4ed98c690e84
Jiri Denemark <jdenemar@redhat.com>
2016-06-01 17:52:54 +0200

    tests: Add CPU detection test for AMD Phenom II X4 B95
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>

--- tests/cputest.c
+++ tests/cputest.c
@@ -866,6 +866,7 @@ mymain(void)
     DO_TEST_CPUID("x86", "Opteron-6234", true);
     DO_TEST_CPUID("x86", "Opteron-6282", false);
     DO_TEST_CPUID("x86", "Pentium-P6100", false);
+    DO_TEST_CPUID("x86", "Phenom-B95", true);
 
 #if WITH_QEMU && WITH_YAJL
     qemuTestDriverFree(&driver);
