120eaad889ecad82c1304ade09701cab154c2ef3
Jiri Denemark <jdenemar@redhat.com>
2016-06-01 17:52:52 +0200

    tests: Add CPU detection test for Intel Core i7-3740QM
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>

--- tests/cputest.c
+++ tests/cputest.c
@@ -854,6 +854,7 @@ mymain(void)
     DO_TEST_CPUID("x86", "Core-i5-6600", true);
     DO_TEST_CPUID("x86", "Core-i7-2600", true);
     DO_TEST_CPUID("x86", "Core-i7-3520M", false);
+    DO_TEST_CPUID("x86", "Core-i7-3740QM", true);
 
 #if WITH_QEMU && WITH_YAJL
     qemuTestDriverFree(&driver);
