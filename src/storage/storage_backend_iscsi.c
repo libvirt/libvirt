libvirt/libvirt

commit e81de04c1088b10997b7c13721dc4513e58a0b5d

Use virDirOpen
Switch from opendir to virDirOpen everywhere we need to report an error.

src/storage/storage_backend_iscsi.c
@@ -100,9 +100,7 @@ virStorageBackendISCSIGetHostNumber(const char *sysfs_path,
 
     virFileWaitForDevices();
 
-    sysdir = opendir(sysfs_path);
-
-    if (sysdir == NULL) {
+    if (virDirOpen(&sysdir, sysfs_path) < 0)
         virReportSystemError(errno,
                              _("Failed to opendir path '%s'"), sysfs_path);
         retval = -1;

src/storage/storage_backend_fs.c
@@ -900,12 +900,8 @@ virStorageBackendFileSystemRefresh(virConnectPtr conn ATTRIBUTE_UNUSED,
     int direrr;
     int fd = -1, ret = -1;
 
-    if (!(dir = opendir(pool->def->target.path))) {
-        virReportSystemError(errno,
-                             _("cannot open path '%s'"),
-                             pool->def->target.path);
+    if (virDirOpen(&dir, pool->def->target.path) < 0)
         goto cleanup;
-    }
 
     while ((direrr = virDirRead(dir, &ent, pool->def->target.path)) > 0) {
         int err;

src/storage/storage_backend_scsi.c
@@ -265,12 +265,8 @@ getNewStyleBlockDevice(const char *lun_path,
 
     VIR_DEBUG("Looking for block device in '%s'", block_path);
 
-    if (!(block_dir = opendir(block_path))) {
-        virReportSystemError(errno,
-                             _("Failed to opendir sysfs path '%s'"),
-                             block_path);
+    if (virDirOpen(&block_dir, block_path) < 0)
         goto cleanup;
-    }
 
     while ((direrr = virDirRead(block_dir, &block_dirent, block_path)) > 0) {
         if (VIR_STRDUP(*block_device, block_dirent->d_name) < 0)
@@ -350,12 +346,8 @@ getBlockDevice(uint32_t host,
                     host, bus, target, lun) < 0)
         goto cleanup;
 
-    if (!(lun_dir = opendir(lun_path))) {
-        virReportSystemError(errno,
-                             _("Failed to opendir sysfs path '%s'"),
-                             lun_path);
+    if (virDirOpen(&lun_dir, lun_path) < 0)
         goto cleanup;
-    }
 
     while ((direrr = virDirRead(lun_dir, &lun_dirent, lun_path)) > 0) {
         if (STREQLEN(lun_dirent->d_name, "block", 5)) {
@@ -467,13 +459,8 @@ virStorageBackendSCSIFindLUs(virStoragePoolObjPtr pool,
 
     virFileWaitForDevices();
 
-    devicedir = opendir(device_path);
-
-    if (devicedir == NULL) {
-        virReportSystemError(errno,
-                             _("Failed to opendir path '%s'"), device_path);
+    if (virDirOpen(&devicedir, device_path) < 0)
         return -1;
-    }
 
     snprintf(devicepattern, sizeof(devicepattern), "%u:%%u:%%u:%%u\n", scanhost);
 

src/util/vircgroup.c
@@ -3952,11 +3952,8 @@ int virCgroupSetOwner(virCgroupPtr cgroup,
                         cgroup->controllers[i].placement) < 0)
             goto cleanup;
 
-        if (!(dh = opendir(base))) {
-            virReportSystemError(errno,
-                                 _("Unable to open dir '%s'"), base);
+        if (virDirOpen(&dh, base) < 0)
             goto cleanup;
-        }
 
         while ((direrr = virDirRead(dh, &de, base)) > 0) {
             if (virAsprintf(&entry, "%s/%s", base, de->d_name) < 0)

src/util/virfile.c
@@ -614,11 +614,8 @@ static int virFileLoopDeviceOpenSearch(char **dev_name)
 
     VIR_DEBUG("Looking for loop devices in /dev");
 
-    if (!(dh = opendir("/dev"))) {
-        virReportSystemError(errno, "%s",
-                             _("Unable to read /dev"));
+    if (virDirOpen(&dh, "/dev") < 0)
         goto cleanup;
-    }
 
     while ((direrr = virDirRead(dh, &de, "/dev")) > 0) {
         /* Checking 'loop' prefix is insufficient, since
@@ -782,12 +779,8 @@ virFileNBDDeviceFindUnused(void)
     struct dirent *de;
     int direrr;
 
-    if (!(dh = opendir(SYSFS_BLOCK_DIR))) {
-        virReportSystemError(errno,
-                             _("Cannot read directory %s"),
-                             SYSFS_BLOCK_DIR);
+    if (virDirOpen(&dh, SYSFS_BLOCK_DIR) < 0)
         return NULL;
-    }
 
     while ((direrr = virDirRead(dh, &de, SYSFS_BLOCK_DIR)) > 0) {
         if (STRPREFIX(de->d_name, "nbd")) {
@@ -942,11 +935,8 @@ int virFileDeleteTree(const char *dir)
     if (!dir || !virFileExists(dir))
         return 0;
 
-    if (!(dh = opendir(dir))) {
-        virReportSystemError(errno, _("Cannot open dir '%s'"),
-                             dir);
+    if (virDirOpen(&dh, dir) < 0)
         return -1;
-    }
 
     while ((direrr = virDirRead(dh, &de, dir)) > 0) {
         struct stat sb;

src/util/virhostcpu.c
@@ -339,10 +339,8 @@ virHostCPUParseNode(const char *node,
     *cores = 0;
     *sockets = 0;
 
-    if (!(cpudir = opendir(node))) {
-        virReportSystemError(errno, _("cannot opendir %s"), node);
+    if (virDirOpen(&cpudir, node) < 0)
         goto cleanup;
-    }
 
     /* Keep track of the CPUs that belong to the current node */
     if (!(node_cpus_map = virBitmapNew(npresent_cpus)))

src/util/virnetdev.c
@@ -3157,12 +3157,8 @@ virNetDevRDMAFeature(const char *ifname,
     if (!virFileExists(SYSFS_INFINIBAND_DIR))
         return 0;
 
-    if (!(dirp = opendir(SYSFS_INFINIBAND_DIR))) {
-        virReportSystemError(errno,
-                             _("Failed to opendir path '%s'"),
-                             SYSFS_INFINIBAND_DIR);
+    if (virDirOpen(&dirp, SYSFS_INFINIBAND_DIR) < 0)
         return -1;
-    }
 
     if (virAsprintf(&eth_devpath, SYSFS_NET_DIR "%s/device/resource", ifname) < 0)
         goto cleanup;

src/util/virnetdevtap.c
@@ -98,13 +98,8 @@ virNetDevTapGetRealDeviceName(char *ifname ATTRIBUTE_UNUSED)
     char *devpath = NULL;
     int fd;
 
-    DIR *dirp = opendir("/dev");
-    if (dirp == NULL) {
-        virReportSystemError(errno,
-                             _("Failed to opendir path '%s'"),
-                             "/dev");
+    if (virDirOpen(&dirp, "/dev") < 0)
         return NULL;
-    }
 
     while (virDirRead(dirp, &dp, "/dev") > 0) {
         if (STRPREFIX(dp->d_name, "tap")) {

commit 290f2adf46295a8cd043135e37ea43d24c003b7f

virStorageBackendISCSIGetHostNumber: correctly use virDirOpen
Incorrect conflict resolution in my commit e81de04c1 broke this.

src/storage/storage_backend_iscsi.c
@@ -100,9 +100,7 @@ virStorageBackendISCSIGetHostNumber(const char *sysfs_path,
 
     virFileWaitForDevices();
 
-    if (virDirOpen(&sysdir, sysfs_path) < 0)
-        virReportSystemError(errno,
-                             _("Failed to opendir path '%s'"), sysfs_path);
+    if (virDirOpen(&sysdir, sysfs_path) < 0) {
         retval = -1;
         goto out;
     }