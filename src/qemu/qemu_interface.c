5f243153c578662262237dff9aaf9214b0a1f2fc
Vasiliy Tolstov <v.tolstov@selfip.ru>
2016-08-24 19:09:22 +0300

    qemu: fix ethernet network type ip/route assign
    The call to virNetDevIPInfoAddToDev() that sets up tap device IP
    addresses and routes was somehow incorrectly placed in
    qemuInterfaceStopDevice() instead of qemuInterfaceStartDevice() in
    commit fe8567f6.  This fixes that error by moving the call to
    virNetDevIPInfoAddToDev() to qemuInterfaceStartDevice().
    
    Signed-off-by: Vasiliy Tolstov <v.tolstov@selfip.ru>

--- src/qemu/qemu_interface.c
+++ src/qemu/qemu_interface.c
@@ -108,8 +108,13 @@ qemuInterfaceStartDevice(virDomainNetDefPtr net)
         break;
     }
 
-    case VIR_DOMAIN_NET_TYPE_USER:
     case VIR_DOMAIN_NET_TYPE_ETHERNET:
+        if (virNetDevIPInfoAddToDev(net->ifname, &net->hostIP) < 0)
+            goto cleanup;
+
+        break;
+
+    case VIR_DOMAIN_NET_TYPE_USER:
     case VIR_DOMAIN_NET_TYPE_VHOSTUSER:
     case VIR_DOMAIN_NET_TYPE_SERVER:
     case VIR_DOMAIN_NET_TYPE_CLIENT:
@@ -197,10 +202,6 @@ qemuInterfaceStopDevice(virDomainNetDefPtr net)
     }
 
     case VIR_DOMAIN_NET_TYPE_ETHERNET:
-        if (virNetDevIPInfoAddToDev(net->ifname, &net->hostIP) < 0)
-            goto cleanup;
-        break;
-
     case VIR_DOMAIN_NET_TYPE_USER:
     case VIR_DOMAIN_NET_TYPE_VHOSTUSER:
     case VIR_DOMAIN_NET_TYPE_SERVER:
