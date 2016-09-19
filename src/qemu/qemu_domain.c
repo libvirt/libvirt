807232203a197451a02d254dfe45f12e2173405f
Laine Stump <laine@laine.org>
2016-09-19 17:11:20 -0400

    qemu: don't force-add a dmi-to-pci-bridge just on principle
    Now the a dmi-to-pci-bridge is automatically added just as it's needed
    (when a pci-bridge is being added), we no longer have any need to
    force-add one to every single Q35 domain.

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -2298,18 +2298,6 @@ qemuDomainDefAddDefaultDevices(virDomainDefPtr def,
                                              VIR_DOMAIN_CONTROLLER_MODEL_PCIE_ROOT)) {
             goto cleanup;
         }
-        /* Add a dmi-to-pci-bridge bridge if there are no PCI controllers
-         * other than the pcie-root. This is so that there will be hot-pluggable
-         * PCI slots available.
-         *
-         * We skip this step for aarch64 mach-virt guests, where we want to
-         * be able to have a pure virtio-mmio topology
-         */
-        if (virDomainControllerFind(def, VIR_DOMAIN_CONTROLLER_TYPE_PCI, 1) < 0 &&
-            !qemuDomainMachineIsVirt(def) &&
-            !virDomainDefAddController(def, VIR_DOMAIN_CONTROLLER_TYPE_PCI, 1,
-                                       VIR_DOMAIN_CONTROLLER_MODEL_DMI_TO_PCI_BRIDGE))
-            goto cleanup;
     }
 
     if (addDefaultMemballoon && !def->memballoon) {
