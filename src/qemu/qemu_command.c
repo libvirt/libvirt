ff3112f3dc2c276a7e387ff7bb86f4fbbdf7bf2c
Martin Kletzander <mkletzan@redhat.com>
2016-09-23 11:31:30 +0200

    qemu: Only use memory-backend-file with NUMA if needed
    If this reminds you of a commit message from around a year ago, it's
    41c2aa729f0af084ede95ee9a06219a2dd5fb5df and yes, we're dealing with
    "the same thing" again.  Or f309db1f4d51009bad0d32e12efc75530b66836b and
    it's similar.
    
    There is a logic in place that if there is no real need for
    memory-backend-file, qemuBuildMemoryBackendStr() returns 0.  However
    that wasn't the case with hugepage backing.  The reason for that was
    that we abused the 'pagesize' variable for storing that information, but
    we should rather have a separate one that specifies whether we really
    need the new object for hugepage backing.  And that variable should be
    set only if this particular NUMA cell needs special treatment WRT
    hugepages.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1372153
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -3202,6 +3202,7 @@ qemuBuildMemoryBackendStr(unsigned long long size,
     int ret = -1;
     virJSONValuePtr props = NULL;
     bool nodeSpecified = virDomainNumatuneNodeSpecified(def->numa, guestNode);
+    bool needHugepage = !!pagesize;
 
     *backendProps = NULL;
     *backendType = NULL;
@@ -3224,10 +3225,10 @@ qemuBuildMemoryBackendStr(unsigned long long size,
         mode = VIR_DOMAIN_NUMATUNE_MEM_STRICT;
 
     if (pagesize == 0) {
+        bool thisHugepage = false;
+
         /* Find the huge page size we want to use */
         for (i = 0; i < def->mem.nhugepages; i++) {
-            bool thisHugepage = false;
-
             hugepage = &def->mem.hugepages[i];
 
             if (!hugepage->nodemask) {
@@ -3249,6 +3250,7 @@ qemuBuildMemoryBackendStr(unsigned long long size,
 
             if (thisHugepage) {
                 /* Hooray, we've found the page size */
+                needHugepage = true;
                 break;
             }
         }
@@ -3335,7 +3337,7 @@ qemuBuildMemoryBackendStr(unsigned long long size,
     }
 
     /* If none of the following is requested... */
-    if (!pagesize && !userNodeset && !memAccess && !nodeSpecified && !force) {
+    if (!needHugepage && !userNodeset && !memAccess && !nodeSpecified && !force) {
         /* report back that using the new backend is not necessary
          * to achieve the desired configuration */
         ret = 1;
