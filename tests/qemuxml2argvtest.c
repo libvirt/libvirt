40b6f919001565b03fa5dd394a45df470c2b7a07
John Ferlan <jferlan@redhat.com>
2016-10-17 11:31:43 -0400

    qemu: Add 'verify-peer=yes' test for chardev TCP TLS
    Missing the option to set verify-peer to yes
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -1162,6 +1162,11 @@ mymain(void)
     DO_TEST("serial-tcp-tlsx509-chardev",
             QEMU_CAPS_CHARDEV, QEMU_CAPS_NODEFCONFIG,
             QEMU_CAPS_OBJECT_TLS_CREDS_X509);
+    driver.config->chardevTLSx509verify = 1;
+    DO_TEST("serial-tcp-tlsx509-chardev-verify",
+            QEMU_CAPS_CHARDEV, QEMU_CAPS_NODEFCONFIG,
+            QEMU_CAPS_OBJECT_TLS_CREDS_X509);
+    driver.config->chardevTLSx509verify = 0;
     driver.config->chardevTLS = 0;
     VIR_FREE(driver.config->chardevTLSx509certdir);
     DO_TEST("serial-many-chardev",
