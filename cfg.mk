ac9bf9394138d0cf9a802245ad3eebf8adcb4841
Daniel P. Berrange <berrange@redhat.com>
2016-10-31 13:39:34 +0000

    docs: remove obsolete library.xen file
    The library.xen file contains a braindump of thoughts dating
    from the very first days of libvirt, when it was briefly
    called libxen. This is not useful and potentially misleading
    or confusing for people.
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

--- cfg.mk
+++ cfg.mk
@@ -1231,7 +1231,7 @@ exclude_file_name_regexp--sc_prohibit_mixed_case_abbreviations = \
   ^src/(vbox/vbox_CAPI.*.h|esx/esx_vi.(c|h)|esx/esx_storage_backend_iscsi.c)$$
 
 exclude_file_name_regexp--sc_prohibit_empty_first_line = \
-  ^(README|daemon/THREADS\.txt|src/esx/README|docs/library.xen|tests/(vmwarever|virhostcpu)data/.*)$$
+  ^(README|daemon/THREADS\.txt|src/esx/README|tests/(vmwarever|virhostcpu)data/.*)$$
 
 exclude_file_name_regexp--sc_prohibit_useless_translation = \
   ^tests/virpolkittest.c
