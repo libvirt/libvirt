e6bad6b63dfb7a4762a3b8b766510cecda327270
Jiri Denemark <jdenemar@redhat.com>
2016-11-18 10:12:10 +0100

    tests: Update capabilities for QEMU 2.6.0
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>

--- tests/domaincapstest.c
+++ tests/domaincapstest.c
@@ -417,7 +417,7 @@ mymain(void)
 
     DO_TEST_QEMU("2.6.0", "caps_2.6.0",
                  "/usr/bin/qemu-system-x86_64", NULL,
-                 "x86_64", VIR_DOMAIN_VIRT_QEMU);
+                 "x86_64", VIR_DOMAIN_VIRT_KVM);
 
     DO_TEST_QEMU("2.6.0", "caps_2.6.0-gicv2",
                  "/usr/bin/qemu-system-aarch64", NULL,
