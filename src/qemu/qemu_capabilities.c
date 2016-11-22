c271fc1f35798faf0383c24e92971bf0e5d8dfde
Eric Farman <farman@linux.vnet.ibm.com>
2016-11-21 22:58:15 -0500

    qemu: Introduce vhost-scsi capability
    Do all the stuff for the vhost-scsi capability in QEMU,
    so it's in place for our checks later.
    
    Signed-off-by: Eric Farman <farman@linux.vnet.ibm.com>
    Reviewed-by: Boris Fiuczynski <fiuczy@linux.vnet.ibm.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -352,6 +352,7 @@ VIR_ENUM_IMPL(virQEMUCaps, QEMU_CAPS_LAST,
               "ivshmem-doorbell", /* 240 */
               "query-qmp-schema",
               "gluster.debug_level",
+              "vhost-scsi",
     );
 
 
@@ -1596,6 +1597,7 @@ struct virQEMUCapsStringFlags virQEMUCapsObjectTypes[] = {
     { "intel-iommu", QEMU_CAPS_DEVICE_INTEL_IOMMU },
     { "ivshmem-plain", QEMU_CAPS_DEVICE_IVSHMEM_PLAIN },
     { "ivshmem-doorbell", QEMU_CAPS_DEVICE_IVSHMEM_DOORBELL },
+    { "vhost-scsi", QEMU_CAPS_DEVICE_VHOST_SCSI },
 };
 
 static struct virQEMUCapsStringFlags virQEMUCapsObjectPropsVirtioBalloon[] = {
