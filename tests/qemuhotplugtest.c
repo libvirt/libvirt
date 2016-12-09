7ab0080bed5ea671d244da1a7cd8856ac80e033b
Peter Krempa <pkrempa@redhat.com>
2016-12-09 16:52:16 +0100

    tests: hotplug: Test CPU hotplug with ppc64 data
    Add a positive test and few negative tests.

--- tests/qemuhotplugtest.c
+++ tests/qemuhotplugtest.c
@@ -773,6 +773,10 @@ mymain(void)
 
     DO_TEST_CPU_GROUP("x86-modern-bulk", 7, true, false);
     DO_TEST_CPU_GROUP("x86-old-bulk", 7, false, false);
+    DO_TEST_CPU_GROUP("ppc64-modern-bulk", 24, true, false);
+    DO_TEST_CPU_GROUP("ppc64-modern-bulk", 15, true, true);
+    DO_TEST_CPU_GROUP("ppc64-modern-bulk", 23, true, true);
+    DO_TEST_CPU_GROUP("ppc64-modern-bulk", 25, true, true);
 
     qemuTestDriverFree(&driver);
     return (ret == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
