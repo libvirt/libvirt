dedfa943686d07195fa5cbd3630112953f44ff2f
Collin L. Walling <walling@linux.vnet.ibm.com>
2016-12-18 14:22:23 -0500

    test-data: Qemu caps replies and xml for s390x qemu 2.7 and 2.8
    Expected Qemu replies for versions 2.7 and 2.8 from the s390x
    Qemu binary.
    
    Signed-off-by: Collin L. Walling <walling@linux.vnet.ibm.com>
    Signed-off-by: Jason J. Herne <jjherne@linux.vnet.ibm.com>

--- tests/qemucapabilitiestest.c
+++ tests/qemucapabilitiestest.c
@@ -172,6 +172,8 @@ mymain(void)
     DO_TEST("aarch64", "caps_2.6.0-gicv2");
     DO_TEST("aarch64", "caps_2.6.0-gicv3");
     DO_TEST("ppc64le", "caps_2.6.0");
+    DO_TEST("s390x", "caps_2.7.0");
+    DO_TEST("s390x", "caps_2.8.0");
 
     /*
      * Run "tests/qemucapsprobe /path/to/qemu/binary >foo.replies"
