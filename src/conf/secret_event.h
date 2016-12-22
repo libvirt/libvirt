df740caf54a331365ca4faa5c226f0dbbe0d7c5c
Daniel P. Berrange <berrange@redhat.com>
2016-12-22 15:24:43 +0000

    conf: add secret event handling
    Add helper APIs / objects for managing secret events
    
    Signed-off-by: Daniel P. Berrange <berrange@redhat.com>

---
+++ src/conf/secret_event.h
@@ -0,0 +1,61 @@
+/*
+ * secret_event.h: secret event queue processing helpers
+ *
+ * Copyright (C) 2010-2014 Red Hat, Inc.
+ * Copyright (C) 2008 VirtualIron
+ * Copyright (C) 2013 SUSE LINUX Products GmbH, Nuernberg, Germany.
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include "internal.h"
+#include "object_event.h"
+#include "object_event_private.h"
+
+#ifndef __SECRET_EVENT_H__
+# define __SECRET_EVENT_H__
+
+int
+virSecretEventStateRegisterID(virConnectPtr conn,
+                              virObjectEventStatePtr state,
+                              virSecretPtr secret,
+                              int eventID,
+                              virConnectSecretEventGenericCallback cb,
+                              void *opaque,
+                              virFreeCallback freecb,
+                              int *callbackID)
+    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2) ATTRIBUTE_NONNULL(5)
+    ATTRIBUTE_NONNULL(8);
+
+int
+virSecretEventStateRegisterClient(virConnectPtr conn,
+                                  virObjectEventStatePtr state,
+                                  virSecretPtr secret,
+                                  int eventID,
+                                  virConnectSecretEventGenericCallback cb,
+                                  void *opaque,
+                                  virFreeCallback freecb,
+                                  int *callbackID)
+    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2) ATTRIBUTE_NONNULL(5)
+    ATTRIBUTE_NONNULL(8);
+
+virObjectEventPtr
+virSecretEventLifecycleNew(const unsigned char *uuid,
+                           int usage_type,
+                           const char *usage_id,
+                           int type,
+                           int detail);
+
+#endif
