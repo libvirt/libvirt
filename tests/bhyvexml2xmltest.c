0735ddf744f95cd9f88c5f8465b1a64883710d37
Michal Privoznik <mprivozn@redhat.com>
2016-12-26 12:23:27 +0100

    bhyvexml2xmltest: Test more XMLs
    Currently, bhyvexml2xmltest does only one test. We can do better.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/bhyvexml2xmltest.c
+++ tests/bhyvexml2xmltest.c
@@ -63,7 +63,28 @@ mymain(void)
 # define DO_TEST_DIFFERENT(name) \
     DO_TEST_FULL(name, true)
 
+    DO_TEST_DIFFERENT("acpiapic");
+    DO_TEST_DIFFERENT("base");
+    DO_TEST_DIFFERENT("bhyveload-bootorder");
+    DO_TEST_DIFFERENT("bhyveload-bootorder1");
+    DO_TEST_DIFFERENT("bhyveload-bootorder2");
+    DO_TEST_DIFFERENT("bhyveload-bootorder3");
+    DO_TEST_DIFFERENT("bhyveload-bootorder4");
+    DO_TEST_DIFFERENT("bhyveload-explicitargs");
+    DO_TEST_DIFFERENT("console");
+    DO_TEST_DIFFERENT("custom-loader");
+    DO_TEST_DIFFERENT("disk-cdrom");
+    DO_TEST_DIFFERENT("disk-cdrom-grub");
+    DO_TEST_DIFFERENT("disk-virtio");
+    DO_TEST_DIFFERENT("grub-bootorder");
+    DO_TEST_DIFFERENT("grub-bootorder2");
+    DO_TEST_DIFFERENT("grub-defaults");
+    DO_TEST_DIFFERENT("localtime");
+    DO_TEST_DIFFERENT("macaddr");
     DO_TEST_DIFFERENT("metadata");
+    DO_TEST_DIFFERENT("serial");
+    DO_TEST_DIFFERENT("serial-grub");
+    DO_TEST_DIFFERENT("serial-grub-nocons");
 
     virObjectUnref(driver.caps);
     virObjectUnref(driver.xmlopt);
