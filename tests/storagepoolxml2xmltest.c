58ea49598814e26b8459891afb123989546212ba
Peter Krempa <pkrempa@redhat.com>
2017-01-27 17:02:19 +0100

    tests: storagepoolxml2xml: Remove compile conditionals
    The XML2XML test should work properly even if the storage backend is
    disabled, since it does not use it.

--- tests/storagepoolxml2xmltest.c
+++ tests/storagepoolxml2xmltest.c
@@ -97,16 +97,10 @@ mymain(void)
     DO_TEST("pool-gluster");
     DO_TEST("pool-gluster-sub");
     DO_TEST("pool-scsi-type-scsi-host-stable");
-#ifdef WITH_STORAGE_ZFS
     DO_TEST("pool-zfs");
     DO_TEST("pool-zfs-sourcedev");
-#endif
-#ifdef WITH_STORAGE_RBD
     DO_TEST("pool-rbd");
-#endif
-#ifdef WITH_STORAGE_VSTORAGE
     DO_TEST("pool-vstorage");
-#endif
 
     return ret == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
