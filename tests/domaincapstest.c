0bde051f3de02b1be25ea4a4d9f062abfa3d1397
Jiri Denemark <jdenemar@redhat.com>
2017-01-30 16:10:49 +0100

    domaincapstest: Add test data for QEMU 2.9.0
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>

--- tests/domaincapstest.c
+++ tests/domaincapstest.c
@@ -453,6 +453,14 @@ mymain(void)
                  "/usr/bin/qemu-system-x86_64", NULL,
                  "x86_64", VIR_DOMAIN_VIRT_QEMU);
 
+    DO_TEST_QEMU("2.9.0", "caps_2.9.0",
+                 "/usr/bin/qemu-system-x86_64", NULL,
+                 "x86_64", VIR_DOMAIN_VIRT_KVM);
+
+    DO_TEST_QEMU("2.9.0-tcg", "caps_2.9.0",
+                 "/usr/bin/qemu-system-x86_64", NULL,
+                 "x86_64", VIR_DOMAIN_VIRT_QEMU);
+
     DO_TEST_QEMU("2.7.0", "caps_2.7.0",
                  "/usr/bin/qemu-system-s390x", NULL,
                  "s390x", VIR_DOMAIN_VIRT_KVM);
