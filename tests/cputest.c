065564c8401d7db24057e7eaabfba7037aee4d96
Jiri Denemark <jdenemar@redhat.com>
2017-03-03 16:29:16 +0100

    cputest: New test for Intel Core i7-4510U
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>

--- tests/cputest.c
+++ tests/cputest.c
@@ -827,6 +827,7 @@ mymain(void)
     DO_TEST_CPUID(VIR_ARCH_X86_64, "Core-i7-3740QM", true);
     DO_TEST_CPUID(VIR_ARCH_X86_64, "Core-i7-3770", true);
     DO_TEST_CPUID(VIR_ARCH_X86_64, "Core-i7-4600U", true);
+    DO_TEST_CPUID(VIR_ARCH_X86_64, "Core-i7-4510U", true);
     DO_TEST_CPUID(VIR_ARCH_X86_64, "Core-i7-5600U", true);
     DO_TEST_CPUID(VIR_ARCH_X86_64, "Core2-E6850", true);
     DO_TEST_CPUID(VIR_ARCH_X86_64, "Core2-Q9500", false);
