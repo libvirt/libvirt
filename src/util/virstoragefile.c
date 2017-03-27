libvirt/libvirt

commit cbc6d53513568c9c9613b3eaae1c8a8230fd6aab

util: storage: Add variables for node names into virStorageSource
'nodeformat' should be used for strings which describe the storage
format object, and 'nodebacking' for the actual storage object itself.

src/util/virstoragefile.c
@@ -2012,6 +2012,8 @@ virStorageSourceCopy(const virStorageSource *src,
         VIR_STRDUP(ret->backingStoreRaw, src->backingStoreRaw) < 0 ||
         VIR_STRDUP(ret->snapshot, src->snapshot) < 0 ||
         VIR_STRDUP(ret->configFile, src->configFile) < 0 ||
+        VIR_STRDUP(ret->nodeformat, src->nodeformat) < 0 ||
+        VIR_STRDUP(ret->nodebacking, src->nodebacking) < 0 ||
         VIR_STRDUP(ret->compat, src->compat) < 0)
         goto error;
 
@@ -2231,6 +2233,9 @@ virStorageSourceClear(virStorageSourcePtr def)
 
     virStorageNetHostDefFree(def->nhosts, def->hosts);
     virStorageAuthDefFree(def->auth);
+
+    VIR_FREE(def->nodebacking);
+    VIR_FREE(def->nodeformat);
 
     virStorageSourceBackingStoreClear(def);
 }
@@ -3780,4 +3785,39 @@ virStorageSourceIsRelative(virStorageSourcePtr src)
     }
 
     return false;
+}
+
+
+/**
+ * virStorageSourceFindByNodeName:
+ * @top: backing chain top
+ * @nodeName: node name to find in backing chain
+ * @index: if provided the index in the backing chain
+ *
+ * Looks up the given storage source in the backing chain and returns the
+ * pointer to it. If @index is passed then it's filled by the index in the
+ * backing chain. On failure NULL is returned and no error is reported.
+ */
+virStorageSourcePtr
+virStorageSourceFindByNodeName(virStorageSourcePtr top,
+                               const char *nodeName,
+                               unsigned int *index)
+{
+    virStorageSourcePtr tmp;
+
+    if (index)
+        *index = 0;
+
+    for (tmp = top; tmp; tmp = tmp->backingStore) {
+        if (STREQ_NULLABLE(tmp->nodeformat, nodeName) ||
+            STREQ_NULLABLE(tmp->nodebacking, nodeName))
+            return tmp;
+
+        if (index)
+            (*index)++;
+    }
+
+    if (index)
+        *index = 0;
+    return NULL;
 }

src/libvirt_private.syms
@@ -2509,6 +2509,7 @@ virStorageNetProtocolTypeToString;
 virStorageSourceBackingStoreClear;
 virStorageSourceClear;
 virStorageSourceCopy;
+virStorageSourceFindByNodeName;
 virStorageSourceFree;
 virStorageSourceGetActualType;
 virStorageSourceGetSecurityLabelDef;

src/util/virstoragefile.h
@@ -276,6 +276,10 @@ struct _virStorageSource {
     /* Name of the child backing store recorded in metadata of the
      * current file.  */
     char *backingStoreRaw;
+
+    /* metadata that allows identifying given storage source */
+    char *nodeformat;  /* name of the format handler object */
+    char *nodebacking; /* name of the backing storage object */
 };
 
 
@@ -394,5 +398,11 @@ int virStorageFileCheckCompat(const char *compat);
 virStorageSourcePtr virStorageSourceNewFromBackingAbsolute(const char *path);
 
 bool virStorageSourceIsRelative(virStorageSourcePtr src);
+
+virStorageSourcePtr
+virStorageSourceFindByNodeName(virStorageSourcePtr top,
+                               const char *nodeName,
+                               unsigned int *index)
+    ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2);
 
 #endif /* __VIR_STORAGE_FILE_H__ */

commit eec3b255d26e7b38bdb0830990569fd91aee661f

Fix build with GCC's static analysis
STREQ_NULLABLE returns true if both parameters are NULL.  And that's
not what we want here.  We just want to skop comparing source nodes
that don't have that info set.  The function wouldn't make much sense
with nodeName == NULL, so we don't need to check that.  Moreover, the
function's declaration uses ATTRIBUDE_NONNULL for nodeName, which not
only means that function expects the parameter not to be NULL, but
actually tells the compiler that it can optimize out the NULL checks.
That way it could end up calling strcmp on NULL (either nodeformat or
nodebacking).  GCC figures this out if libvirt is compiled with
lv_cv_static_analysis=yes, unfortunately not everyone uses that.

Caused by cbc6d53513568c9c9613b3eaae1c8a8230fd6aab.

Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

src/util/virstoragefile.c
@@ -3809,8 +3809,8 @@ virStorageSourceFindByNodeName(virStorageSourcePtr top,
         *index = 0;
 
     for (tmp = top; tmp; tmp = tmp->backingStore) {
-        if (STREQ_NULLABLE(tmp->nodeformat, nodeName) ||
-            STREQ_NULLABLE(tmp->nodebacking, nodeName))
+        if ((tmp->nodeformat && STREQ(tmp->nodeformat, nodeName)) ||
+            (tmp->nodebacking && STREQ(tmp->nodebacking, nodeName)))
             return tmp;
 
         if (index)