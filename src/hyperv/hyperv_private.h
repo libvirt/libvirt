a993f1b404f3e6de0dd31010df30f475b6c61323
Dawid Zamirski <dzamirski@datto.com>
2017-04-04 18:26:05 -0400

    hyperv: store WMI version in hypervPrivate.
    Hyper-V 2012+ uses a new "v2" version of Msvm_* WMI classes so we will
    store that info in hypervPrivate so that it is easily accessbile in the
    driver API callbacks and handled accordingly.

--- src/hyperv/hyperv_private.h
+++ src/hyperv/hyperv_private.h
@@ -28,11 +28,17 @@
 # include "hyperv_util.h"
 # include "openwsman.h"
 
-typedef struct _hypervPrivate hypervPrivate;
+typedef enum _hypervWmiVersion hypervWmiVersion;
+enum _hypervWmiVersion {
+    HYPERV_WMI_VERSION_V1,
+    HYPERV_WMI_VERSION_V2,
+};
 
+typedef struct _hypervPrivate hypervPrivate;
 struct _hypervPrivate {
     hypervParsedUri *parsedUri;
     WsManClient *client;
+    hypervWmiVersion wmiVersion;
 };
 
 #endif /* __HYPERV_PRIVATE_H__ */
