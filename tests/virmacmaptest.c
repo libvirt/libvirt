498336836772cc222f956116e9c535c273f87422
Peter Krempa <pkrempa@redhat.com>
2017-07-26 10:02:21 +0200

    tests: Extract mock library for making hash table deterministic
    virHashNew calls virRandomBits to initialize seed for the hashing
    function. If a test uses iteration through the hash table to produce
    results they may/will be non-deterministic. Extract the mock library
    which was used for mac address mapping to be universal.
    
    Reviewed-by: Eric Blake <eblake@redhat.com>

--- tests/virmacmaptest.c
+++ tests/virmacmaptest.c
@@ -234,4 +234,4 @@ mymain(void)
     return ret == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
-VIR_TEST_MAIN_PRELOAD(mymain, abs_builddir "/.libs/virmacmapmock.so")
+VIR_TEST_MAIN_PRELOAD(mymain, abs_builddir "/.libs/virdeterministichashmock.so")
