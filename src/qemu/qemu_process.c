fda4298515cd0a069ef24235d35907bede2489c8
Peter Krempa <pkrempa@redhat.com>
2017-10-03 09:51:27 +0200

    qemu: process: document parameters for startup preparing functions
    Document mainly what flag values are passed in.

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -5285,7 +5285,11 @@ qemuProcessPrepareDomainNUMAPlacement(virDomainObjPtr vm,
 
 
 /**
- * qemuProcessPrepareDomain
+ * qemuProcessPrepareDomain:
+ * @conn: connection object (for looking up storage volumes)
+ * @driver: qemu driver
+ * @vm: domain object
+ * @flags: qemuProcessStartFlags
  *
  * This function groups all code that modifies only live XML of a domain which
  * is about to start and it's the only place to do those modifications.
@@ -5413,7 +5417,10 @@ qemuProcessPrepareDomain(virConnectPtr conn,
 
 
 /**
- * qemuProcessPrepareHost
+ * qemuProcessPrepareHost:
+ * @driver: qemu driver
+ * @vm: domain object
+ * @incoming: true if we are preparing an incoming migration
  *
  * This function groups all code that modifies host system (which also may
  * update live XML) to prepare environment for a domain which is about to start
