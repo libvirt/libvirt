86fde4d2b25f8898485b5b60014c1e9fdde52a03
Jiri Denemark <jdenemar@redhat.com>
2017-10-19 11:13:54 +0200

    bhyve: Fix build
    Commit v3.8.0-95-gfd885a06a dropped nmodels parameter from several APIs
    in src/cpu/cpu.h, but failed to update all callers.
    
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>

--- src/bhyve/bhyve_driver.c
+++ src/bhyve/bhyve_driver.c
@@ -1433,7 +1433,7 @@ bhyveConnectBaselineCPU(virConnectPtr conn,
     if (!(cpus = virCPUDefListParse(xmlCPUs, ncpus, VIR_CPU_TYPE_HOST)))
         goto cleanup;
 
-    if (!(cpu = cpuBaseline(cpus, ncpus, NULL, 0,
+    if (!(cpu = cpuBaseline(cpus, ncpus, NULL,
                             !!(flags & VIR_CONNECT_BASELINE_CPU_MIGRATABLE))))
         goto cleanup;
 
