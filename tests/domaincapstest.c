61ea70341a668738a4fb524a65fa1a80705b2bab
Andrea Bolognani <abologna@redhat.com>
2017-11-06 12:40:24 +0100

    tests: Rename ppc64le caps to ppc64
    The architecture itself is called ppc64, and it can run both in big
    endian and little endian mode - the latter is known as ppc64le.
    
    From the (virtual) hardware point of view, ppc64 is a more accurate
    name so it should be used here.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: John Ferlan <jferlan@redhat.com>

--- tests/domaincapstest.c
+++ tests/domaincapstest.c
@@ -460,7 +460,7 @@ mymain(void)
 
     DO_TEST_QEMU("2.6.0", "caps_2.6.0",
                  "/usr/bin/qemu-system-ppc64", NULL,
-                 "ppc64le", VIR_DOMAIN_VIRT_KVM);
+                 "ppc64", VIR_DOMAIN_VIRT_KVM);
 
     DO_TEST_QEMU("2.8.0", "caps_2.8.0",
                  "/usr/bin/qemu-system-x86_64", NULL,
