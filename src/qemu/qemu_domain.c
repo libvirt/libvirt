b9b9195f15c94ec0e1d26d0def7387b32126f09b
Andrea Bolognani <abologna@redhat.com>
2018-02-20 15:59:25 +0100

    qemu: Remove old qemuDomainDeviceDefValidateControllerPCI()
    We've implemented all existing checks, and more, in the new
    function, so we can finally drop the old one.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Laine Stump <laine@laine.org>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -4267,25 +4267,6 @@ qemuDomainDeviceDefValidateControllerSCSI(const virDomainControllerDef *controll
 }
 
 
-static int
-qemuDomainDeviceDefValidateControllerPCIOld(const virDomainControllerDef *controller,
-                                            const virDomainDef *def,
-                                            virQEMUCapsPtr qemuCaps ATTRIBUTE_UNUSED)
-{
-    /* skip pcie-root */
-    if (controller->model == VIR_DOMAIN_CONTROLLER_MODEL_PCIE_ROOT)
-        return 0;
-
-    /* Skip pci-root, except for pSeries guests (which actually
-     * support more than one PCI Host Bridge per guest) */
-    if (!qemuDomainIsPSeries(def) &&
-        controller->model == VIR_DOMAIN_CONTROLLER_MODEL_PCI_ROOT)
-        return 0;
-
-    return 0;
-}
-
-
 /**
  * virDomainControllerPCIModelNameToQEMUCaps:
  * @modelName: model name
@@ -4770,7 +4751,7 @@ qemuDomainDeviceDefValidateControllerPCI(const virDomainControllerDef *cont,
         return -1;
     }
 
-    return qemuDomainDeviceDefValidateControllerPCIOld(cont, def, qemuCaps);
+    return 0;
 }
 
 
