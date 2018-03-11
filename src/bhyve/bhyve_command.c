de3fe191f5eab6702a8f83da091af4e8e8098d47
Roman Bogorodskiy <bogorodskiy@gmail.com>
2018-03-11 20:00:08 +0400

    bhyve: fix crash on missing interface model
    The bhyve driver crashes in bhyveBuildNetArgStr() when
    network interface model is not defined. As it has to be provided
    explicitly, add a check to report an error if it's missing.
    
    Signed-off-by: Roman Bogorodskiy <bogorodskiy@gmail.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- src/bhyve/bhyve_command.c
+++ src/bhyve/bhyve_command.c
@@ -56,6 +56,12 @@ bhyveBuildNetArgStr(virConnectPtr conn,
     int ret = -1;
     virDomainNetType actualType = virDomainNetGetActualType(net);
 
+    if (net->model == NULL) {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                       _("NIC model must be specified"));
+        return -1;
+    }
+
     if (STREQ(net->model, "virtio")) {
         if (VIR_STRDUP(nic_model, "virtio-net") < 0)
             return -1;
