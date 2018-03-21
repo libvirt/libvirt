116fcab069054c443f3361f18c4dc20ce9707b4a
Andrea Bolognani <abologna@redhat.com>
2018-03-21 18:05:05 +0100

    tests: Add capabilities data for QEMU 2.12
    The QEMU binary is compiled from the v2.12.0-rc0 tag.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: John Ferlan <jferlan@redhat.com>

--- tests/qemucapabilitiestest.c
+++ tests/qemucapabilitiestest.c
@@ -177,18 +177,23 @@ mymain(void)
     DO_TEST("x86_64", "caps_2.8.0");
     DO_TEST("x86_64", "caps_2.9.0");
     DO_TEST("x86_64", "caps_2.10.0");
+    DO_TEST("x86_64", "caps_2.12.0");
     DO_TEST("aarch64", "caps_2.6.0-gicv2");
     DO_TEST("aarch64", "caps_2.6.0-gicv3");
     DO_TEST("aarch64", "caps_2.10.0-gicv2");
     DO_TEST("aarch64", "caps_2.10.0-gicv3");
+    DO_TEST("aarch64", "caps_2.12.0-gicv2");
+    DO_TEST("aarch64", "caps_2.12.0-gicv3");
     DO_TEST("ppc64", "caps_2.6.0");
     DO_TEST("ppc64", "caps_2.9.0");
     DO_TEST("ppc64", "caps_2.10.0");
+    DO_TEST("ppc64", "caps_2.12.0");
     DO_TEST("s390x", "caps_2.7.0");
     DO_TEST("s390x", "caps_2.8.0");
     DO_TEST("s390x", "caps_2.9.0");
     DO_TEST("s390x", "caps_2.10.0");
     DO_TEST("s390x", "caps_2.11.0");
+    DO_TEST("s390x", "caps_2.12.0");
 
     /*
      * Run "tests/qemucapsprobe /path/to/qemu/binary >foo.replies"
