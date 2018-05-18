47a1ca6e2c358a7a3c6749b718dd6853ba81d34c
Daniel P. Berrangé <berrange@redhat.com>
2018-05-18 16:12:29 +0100

    qemu: clarify message about block peek format restriction
    Include the actual disk format in the error message to help the user see
    what is wrong.
    
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- src/qemu/qemu_driver.c
+++ src/qemu/qemu_driver.c
@@ -11544,8 +11544,9 @@ qemuDomainBlockPeek(virDomainPtr dom,
     }
 
     if (disk->src->format != VIR_STORAGE_FILE_RAW) {
-        virReportError(VIR_ERR_OPERATION_UNSUPPORTED, "%s",
-                       _("peeking is supported only for RAW disks"));
+        virReportError(VIR_ERR_OPERATION_UNSUPPORTED,
+                       _("peeking is only supported for disk with 'raw' format not '%s'"),
+                       virStorageFileFormatTypeToString(disk->src->format));
         goto cleanup;
     }
 
