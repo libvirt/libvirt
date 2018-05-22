41f88886198e231285cc813f8c0687c8ec5c9488
Peter Krempa <pkrempa@redhat.com>
2018-05-22 14:15:36 +0200

    qemu: monitor: Remove dead code from text monitor
    Remove all unused functions and cleanup headers.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/qemu/qemu_monitor_text.h
+++ src/qemu/qemu_monitor_text.h
@@ -29,117 +29,7 @@
 
 # include "qemu_monitor.h"
 
-int qemuMonitorTextIOProcess(qemuMonitorPtr mon,
-                             const char *data,
-                             size_t len,
-                             qemuMonitorMessagePtr msg);
-
-int qemuMonitorTextCommandWithFd(qemuMonitorPtr mon,
-                                 const char *cmd,
-                                 int scm_fd,
-                                 char **reply);
-
-int qemuMonitorTextStartCPUs(qemuMonitorPtr mon);
-int qemuMonitorTextStopCPUs(qemuMonitorPtr mon);
-int qemuMonitorTextGetStatus(qemuMonitorPtr mon,
-                             bool *running,
-                             virDomainPausedReason *reason);
-
-int qemuMonitorTextSystemPowerdown(qemuMonitorPtr mon);
-int qemuMonitorTextSystemReset(qemuMonitorPtr mon);
-
-int qemuMonitorTextQueryCPUs(qemuMonitorPtr mon,
-                             struct qemuMonitorQueryCpusEntry **entries,
-                             size_t *nentries);
-int qemuMonitorTextGetVirtType(qemuMonitorPtr mon,
-                               virDomainVirtType *virtType);
-int qemuMonitorTextGetBalloonInfo(qemuMonitorPtr mon,
-                                  unsigned long long *currmem);
-int qemuMonitorTextGetMemoryStats(qemuMonitorPtr mon,
-                                  virDomainMemoryStatPtr stats,
-                                  unsigned int nr_stats);
-int qemuMonitorTextGetBlockInfo(qemuMonitorPtr mon,
-                                virHashTablePtr table);
-
-int qemuMonitorTextGetAllBlockStatsInfo(qemuMonitorPtr mon,
-                                        virHashTablePtr hash);
-int qemuMonitorTextBlockResize(qemuMonitorPtr mon,
-                               const char *device,
-                               unsigned long long size);
-int qemuMonitorTextSetVNCPassword(qemuMonitorPtr mon,
-                                  const char *password);
-int qemuMonitorTextSetPassword(qemuMonitorPtr mon,
-                               const char *protocol,
-                               const char *password,
-                               const char *action_if_connected);
-int qemuMonitorTextExpirePassword(qemuMonitorPtr mon,
-                                  const char *protocol,
-                                  const char *expire_time);
-int qemuMonitorTextSetBalloon(qemuMonitorPtr mon,
-                              unsigned long long newmem);
 int qemuMonitorTextSetCPU(qemuMonitorPtr mon, int cpu, bool online);
-
-int qemuMonitorTextEjectMedia(qemuMonitorPtr mon,
-                              const char *dev_name,
-                              bool force);
-int qemuMonitorTextChangeMedia(qemuMonitorPtr mon,
-                               const char *dev_name,
-                               const char *newmedia,
-                               const char *format);
-
-
-int qemuMonitorTextSaveVirtualMemory(qemuMonitorPtr mon,
-                                     unsigned long long offset,
-                                     size_t length,
-                                     const char *path);
-int qemuMonitorTextSavePhysicalMemory(qemuMonitorPtr mon,
-                                      unsigned long long offset,
-                                      size_t length,
-                                      const char *path);
-
-int qemuMonitorTextSetMigrationSpeed(qemuMonitorPtr mon,
-                                     unsigned long bandwidth);
-
-int qemuMonitorTextSetMigrationDowntime(qemuMonitorPtr mon,
-                                        unsigned long long downtime);
-
-int qemuMonitorTextGetMigrationStats(qemuMonitorPtr mon,
-                                     qemuMonitorMigrationStatsPtr stats);
-
-int qemuMonitorTextMigrate(qemuMonitorPtr mon,
-                           unsigned int flags,
-                           const char *uri);
-
-int qemuMonitorTextMigrateCancel(qemuMonitorPtr mon);
-
-int qemuMonitorTextGraphicsRelocate(qemuMonitorPtr mon,
-                                    int type,
-                                    const char *hostname,
-                                    int port,
-                                    int tlsPort,
-                                    const char *tlsSubject);
-
-int qemuMonitorTextSendFileHandle(qemuMonitorPtr mon,
-                                  const char *fdname,
-                                  int fd);
-
-int qemuMonitorTextCloseFileHandle(qemuMonitorPtr mon,
-                                   const char *fdname);
-
-int qemuMonitorTextAddNetdev(qemuMonitorPtr mon,
-                             const char *netdevstr);
-
-int qemuMonitorTextRemoveNetdev(qemuMonitorPtr mon,
-                                const char *alias);
-
-int qemuMonitorTextGetChardevInfo(qemuMonitorPtr mon,
-                                  virHashTablePtr info);
-
-int qemuMonitorTextAddDevice(qemuMonitorPtr mon,
-                             const char *devicestr);
-
-int qemuMonitorTextDelDevice(qemuMonitorPtr mon,
-                             const char *devalias);
 
 int qemuMonitorTextAddDrive(qemuMonitorPtr mon,
                              const char *drivestr);
@@ -147,41 +37,8 @@ int qemuMonitorTextAddDrive(qemuMonitorPtr mon,
 int qemuMonitorTextDriveDel(qemuMonitorPtr mon,
                              const char *drivestr);
 
-int qemuMonitorTextSetDrivePassphrase(qemuMonitorPtr mon,
-                                      const char *alias,
-                                      const char *passphrase);
-
 int qemuMonitorTextCreateSnapshot(qemuMonitorPtr mon, const char *name);
 int qemuMonitorTextLoadSnapshot(qemuMonitorPtr mon, const char *name);
 int qemuMonitorTextDeleteSnapshot(qemuMonitorPtr mon, const char *name);
-
-int qemuMonitorTextArbitraryCommand(qemuMonitorPtr mon, const char *cmd,
-                                    char **reply);
-
-int qemuMonitorTextInjectNMI(qemuMonitorPtr mon);
-
-int qemuMonitorTextSendKey(qemuMonitorPtr mon,
-                           unsigned int holdtime,
-                           unsigned int *keycodes,
-                           unsigned int nkeycodes);
-
-int qemuMonitorTextScreendump(qemuMonitorPtr mon, const char *file);
-
-int qemuMonitorTextSetLink(qemuMonitorPtr mon,
-                           const char *name,
-                           virDomainNetInterfaceLinkState state);
-
-int qemuMonitorTextOpenGraphics(qemuMonitorPtr mon,
-                                const char *protocol,
-                                const char *fdname,
-                                bool skipauth);
-
-int qemuMonitorTextSetBlockIoThrottle(qemuMonitorPtr mon,
-                                      const char *device,
-                                      virDomainBlockIoTuneInfoPtr info);
-
-int qemuMonitorTextGetBlockIoThrottle(qemuMonitorPtr mon,
-                                      const char *device,
-                                      virDomainBlockIoTuneInfoPtr reply);
 
 #endif /* QEMU_MONITOR_TEXT_H */
