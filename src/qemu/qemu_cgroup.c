a9caa981592c5609631949d3a77e02b0e8493b48
Brijesh Singh <brijesh.singh@amd.com>
2018-06-08 09:40:57 -0500

    qemu/cgroup: Add /dev/sev in shared devices list
    QEMU uses /dev/sev device while creating the SEV guest, lets add /dev/sev
    in the list of devices allowed to be accessed by the QEMU.
    
    Signed-off-by: Brijesh Singh <brijesh.singh@amd.com>
    Reviewed-by: Erik Skultety <eskultet@redhat.com>

--- src/qemu/qemu_cgroup.c
+++ src/qemu/qemu_cgroup.c
@@ -48,7 +48,7 @@ const char *const defaultDeviceACL[] = {
     "/dev/null", "/dev/full", "/dev/zero",
     "/dev/random", "/dev/urandom",
     "/dev/ptmx", "/dev/kvm", "/dev/kqemu",
-    "/dev/rtc", "/dev/hpet",
+    "/dev/rtc", "/dev/hpet", "/dev/sev",
     NULL,
 };
 #define DEVICE_PTY_MAJOR 136
