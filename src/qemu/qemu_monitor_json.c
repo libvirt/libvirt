78f47a6395c0f71ca47fb5e328052db054508ddf
Peter Krempa <pkrempa@redhat.com>
2018-08-01 09:15:22 +0200

    qemu: monitor: Fix incrementing of 'nstats' in qemuMonitorJSONBlockStatsCollectData
    commit 8d9ca6cdb3a5 refactored qemuMonitorJSONBlockStatsCollectData so
    that the number of stats is passed back via a pointer. The commit failed
    to fix the macro which increments the number of stats to increment the
    actual pointee.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>

--- src/qemu/qemu_monitor_json.c
+++ src/qemu/qemu_monitor_json.c
@@ -2299,7 +2299,7 @@ qemuMonitorJSONBlockStatsCollectData(virJSONValuePtr dev,
 
 #define QEMU_MONITOR_BLOCK_STAT_GET(NAME, VAR, MANDATORY) \
     if (MANDATORY || virJSONValueObjectHasKey(stats, NAME)) { \
-        nstats++; \
+        (*nstats)++; \
         if (virJSONValueObjectGetNumberLong(stats, NAME, &VAR) < 0) { \
             virReportError(VIR_ERR_INTERNAL_ERROR, \
                            _("cannot read %s statistic"), NAME); \
