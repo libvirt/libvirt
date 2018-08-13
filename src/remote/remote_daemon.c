9ed59012d3ccdf8365f5e8119959017849bc89fd
Ján Tomko <jtomko@redhat.com>
2018-08-13 13:38:38 +0200

    Revert "remote: daemon: Make sure that JSON symbols are properly loaded at startup"
    This reverts commit 3251fc9c9b9639c3fec3181530599415523d671a.
    
    Jansson cannot parse QEMU's quirky JSON.
    Revert back to yajl.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1614569
    
    Signed-off-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- src/remote/remote_daemon.c
+++ src/remote/remote_daemon.c
@@ -59,7 +59,6 @@
 #include "virutil.h"
 #include "virgettext.h"
 #include "util/virnetdevopenvswitch.h"
-#include "virjsoncompat.h"
 
 #include "driver.h"
 
@@ -1183,9 +1182,6 @@ int main(int argc, char **argv) {
         VIR_ERROR(_("Can't initialize logging"));
         exit(EXIT_FAILURE);
     }
-
-    if (virJSONInitialize() < 0)
-        exit(EXIT_FAILURE);
 
     daemonSetupNetDevOpenvswitch(config);
 
