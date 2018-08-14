libvirt/libvirt

commit f7d0663d491eddf576166d1ab78668b3e7019ac8

util: netlink: define cleanup function using VIR_DEFINE_AUTOPTR_FUNC
Using the new VIR_DEFINE_AUTOPTR_FUNC macro defined in
src/util/viralloc.h, define a new wrapper around an existing
cleanup function which will be called when a variable declared
with VIR_AUTOPTR macro goes out of scope. Also, drop the redundant
viralloc.h include, since that has moved from the source module into
the header.

This commit also typedefs virNetlinkMsg to struct nl_msg type for use
with the cleanup macros.

When a variable of type virNetlinkMsg * is declared using VIR_AUTOPTR,
the function nlmsg_free will be run automatically on it when it
goes out of scope.

Signed-off-by: Sukrit Bhatnagar <skrtbhtngr@gmail.com>
Reviewed-by: Erik Skultety <eskultet@redhat.com>

src/util/virnetlink.h
@@ -22,6 +22,7 @@
 
 # include "internal.h"
 # include "virmacaddr.h"
+# include "viralloc.h"
 
 # if defined(__linux__) && defined(HAVE_LIBNL)
 
@@ -43,6 +44,8 @@ struct nlattr;
 struct nlmsghdr;
 
 # endif /* __linux__ */
+
+typedef struct nl_msg virNetlinkMsg;
 
 int virNetlinkStartup(void);
 void virNetlinkShutdown(void);
@@ -122,5 +125,7 @@ int virNetlinkEventAddClient(virNetlinkEventHandleCallback handleCB,
  */
 int virNetlinkEventRemoveClient(int watch, const virMacAddr *macaddr,
                                 unsigned int protocol);
+
+VIR_DEFINE_AUTOPTR_FUNC(virNetlinkMsg, nlmsg_free)
 
 #endif /* __VIR_NETLINK_H__ */

src/util/virnetlink.c
@@ -38,7 +38,6 @@
 #include "virnetlink.h"
 #include "virnetdev.h"
 #include "virlog.h"
-#include "viralloc.h"
 #include "virthread.h"
 #include "virmacaddr.h"
 #include "virerror.h"

commit ff7bd6a245d4ec2594765175cfb419bdc0635715

Fix build on non-linux platforms after another VIR_AUTOPTR patches
Caused by commit f7d0663d49. The problem is missing libnl library on
these platforms, so the VIR_DEFINE_AUTOPTR_FUNC has to be compiled in
conditionally.

Signed-off-by: Erik Skultety <eskultet@redhat.com>

src/util/virnetlink.h
@@ -36,6 +36,9 @@
 #   undef inline
 #  endif
 
+typedef struct nl_msg virNetlinkMsg;
+VIR_DEFINE_AUTOPTR_FUNC(virNetlinkMsg, nlmsg_free)
+
 # else
 
 struct nl_msg;
@@ -44,8 +47,6 @@ struct nlattr;
 struct nlmsghdr;
 
 # endif /* __linux__ */
-
-typedef struct nl_msg virNetlinkMsg;
 
 int virNetlinkStartup(void);
 void virNetlinkShutdown(void);
@@ -125,7 +126,5 @@ int virNetlinkEventAddClient(virNetlinkEventHandleCallback handleCB,
  */
 int virNetlinkEventRemoveClient(int watch, const virMacAddr *macaddr,
                                 unsigned int protocol);
-
-VIR_DEFINE_AUTOPTR_FUNC(virNetlinkMsg, nlmsg_free)
 
 #endif /* __VIR_NETLINK_H__ */