6c50cef8a3e4e3407fb42a713a353e42ae3f2bc6
Erik Skultety <eskultet@redhat.com>
2018-08-15 14:00:23 +0200

    tests: sev: Test launch-security with specific QEMU version
    In order to test SEV we need real QEMU capabilities. Ideally, this would
    be tested with -latest capabilities, however, our capabilities are
    currently tied to Intel HW, even the 2.12.0 containing SEV were edited by
    hand, so we can only use that one for now, as splitting the capabilities
    according to the vendor is a refactor for another day. The need for real
    capabilities comes from the extended SEV platform data (PDH, cbitpos,
    etc.) we'll need to cache/parse.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>
    Acked-by: Peter Krempa <pkrempa@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -2972,9 +2972,7 @@ mymain(void)
     DO_TEST_CAPS_ARCH_LATEST("vhost-vsock-ccw", "s390x");
     DO_TEST_CAPS_ARCH_LATEST("vhost-vsock-ccw-auto", "s390x");
 
-    DO_TEST("launch-security-sev",
-            QEMU_CAPS_KVM,
-            QEMU_CAPS_SEV_GUEST);
+    DO_TEST_CAPS_VER("launch-security-sev", "2.12.0");
 
     if (getenv("LIBVIRT_SKIP_CLEANUP") == NULL)
         virFileDeleteTree(fakerootdir);
