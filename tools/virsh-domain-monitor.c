2c4df6c68bce64d5b97f8dc18bb64576746555c1
John Ferlan <jferlan@redhat.com>
2018-10-03 15:04:14 -0400

    virsh: Add ability to display IOThread stats
    Add an --iothread qualifier to domstats and an explanation in
    the man page. Describe the values in as generic terms as possible
    allowing each hypervisor to provide a specific algorithm to utilize
    the values as it sees fit.
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>
    ACKed-by: Michal Privoznik <mprivozn@redhat.com>

--- tools/virsh-domain-monitor.c
+++ tools/virsh-domain-monitor.c
@@ -2067,6 +2067,10 @@ static const vshCmdOptDef opts_domstats[] = {
      .type = VSH_OT_BOOL,
      .help = N_("report domain perf event statistics"),
     },
+    {.name = "iothread",
+     .type = VSH_OT_BOOL,
+     .help = N_("report domain IOThread information"),
+    },
     {.name = "list-active",
      .type = VSH_OT_BOOL,
      .help = N_("list only active domains"),
@@ -2179,6 +2183,9 @@ cmdDomstats(vshControl *ctl, const vshCmd *cmd)
 
     if (vshCommandOptBool(cmd, "perf"))
         stats |= VIR_DOMAIN_STATS_PERF;
+
+    if (vshCommandOptBool(cmd, "iothread"))
+        stats |= VIR_DOMAIN_STATS_IOTHREAD;
 
     if (vshCommandOptBool(cmd, "list-active"))
         flags |= VIR_CONNECT_GET_ALL_DOMAINS_STATS_ACTIVE;
