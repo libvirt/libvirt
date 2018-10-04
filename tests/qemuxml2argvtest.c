ccad7b58887b46fc8cd0d3636479b53628babb2a
Ján Tomko <jtomko@redhat.com>
2018-10-04 19:27:17 +0200

    tests: add virtio-rng-egd-unix
    Test RNG devices connected to EGD via UNIX sockets.
    
    Signed-off-by: Ján Tomko <jtomko@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -2095,6 +2095,8 @@ mymain(void)
     DO_TEST("virtio-rng-egd",
             QEMU_CAPS_DEVICE_VIRTIO_RNG,
             QEMU_CAPS_OBJECT_RNG_EGD);
+    DO_TEST_CAPS_VER("virtio-rng-egd-unix", "2.5.0");
+    DO_TEST_CAPS_LATEST("virtio-rng-egd-unix");
     DO_TEST("virtio-rng-multiple",
             QEMU_CAPS_DEVICE_VIRTIO_RNG,
             QEMU_CAPS_OBJECT_RNG_EGD,
