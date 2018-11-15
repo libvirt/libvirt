0a75de221984b971cb841c6918e6746eb22b1547
Erik Skultety <eskultet@redhat.com>
2018-11-15 11:37:00 +0100

    conf: gfx: Add egl-headless as a member to virDomainGraphicsDef struct
    Since we need to specify the rendernode option onto QEMU cmdline, we
    need this union member to retain consistency in how we build the
    cmdline.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/conf/domain_conf.h
+++ src/conf/domain_conf.h
@@ -1659,6 +1659,9 @@ struct _virDomainGraphicsDef {
             virTristateBool gl;
             char *rendernode;
         } spice;
+        struct {
+            char *rendernode;
+        } egl_headless;
     } data;
     /* nListens, listens, and *port are only useful if type is vnc,
      * rdp, or spice. They've been extracted from the union only to
