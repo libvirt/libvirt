53ca6fa4eacb9e519710dacf237997b813f892d2
Erik Skultety <eskultet@redhat.com>
2018-11-22 12:05:06 +0100

    qemu: domain: egl-headless: Add the DRI device into the namespace
    Just like for SPICE, we need to put the DRI device into the namespace,
    otherwise it will be left out from the DAC relabeling process.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/qemu/qemu_domain.c
+++ src/qemu/qemu_domain.c
@@ -11875,11 +11875,9 @@ qemuDomainSetupGraphics(virQEMUDriverConfigPtr cfg ATTRIBUTE_UNUSED,
                         virDomainGraphicsDefPtr gfx,
                         const struct qemuDomainCreateDeviceData *data)
 {
-    const char *rendernode = gfx->data.spice.rendernode;
+    const char *rendernode = virDomainGraphicsGetRenderNode(gfx);
 
-    if (gfx->type != VIR_DOMAIN_GRAPHICS_TYPE_SPICE ||
-        gfx->data.spice.gl != VIR_TRISTATE_BOOL_YES ||
-        !rendernode)
+    if (!rendernode)
         return 0;
 
     return qemuDomainCreateDevice(rendernode, data, false);
