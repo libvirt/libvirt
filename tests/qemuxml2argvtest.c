900289b767fd671cfb0ce39087f088fd1287f4ac
Luyao Zhong <luyao.zhong@intel.com>
2018-12-20 17:14:46 +0800

    tests: Use DO_TEST_CAPS_LATEST for nvdimm qemuxml2argv
    Deprecate DO_TEST to do nvdimm qemuxml2argvdata tests, because
    DO_TEST_CAPS_LATEST is a better choice. The DO_TEST needs
    to specify all qemu capabilities and is not easy for scaling.
    
    Signed-off-by: Luyao Zhong <luyao.zhong@intel.com>
    Reviewed-by: John Ferlan <jferlan@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -2755,15 +2755,9 @@ mymain(void)
     DO_TEST("memory-hotplug-ppc64-nonuma", QEMU_CAPS_KVM, QEMU_CAPS_DEVICE_PC_DIMM, QEMU_CAPS_NUMA,
             QEMU_CAPS_DEVICE_SPAPR_PCI_HOST_BRIDGE,
             QEMU_CAPS_OBJECT_MEMORY_RAM, QEMU_CAPS_OBJECT_MEMORY_FILE);
-    DO_TEST("memory-hotplug-nvdimm",
-            QEMU_CAPS_DEVICE_NVDIMM,
-            QEMU_CAPS_NUMA, QEMU_CAPS_OBJECT_MEMORY_RAM, QEMU_CAPS_OBJECT_MEMORY_FILE);
-    DO_TEST("memory-hotplug-nvdimm-access",
-            QEMU_CAPS_DEVICE_NVDIMM,
-            QEMU_CAPS_NUMA, QEMU_CAPS_OBJECT_MEMORY_RAM, QEMU_CAPS_OBJECT_MEMORY_FILE);
-    DO_TEST("memory-hotplug-nvdimm-label",
-            QEMU_CAPS_DEVICE_NVDIMM,
-            QEMU_CAPS_NUMA, QEMU_CAPS_OBJECT_MEMORY_RAM, QEMU_CAPS_OBJECT_MEMORY_FILE);
+    DO_TEST_CAPS_LATEST("memory-hotplug-nvdimm");
+    DO_TEST_CAPS_LATEST("memory-hotplug-nvdimm-access");
+    DO_TEST_CAPS_LATEST("memory-hotplug-nvdimm-label");
 
     DO_TEST("machine-aeskeywrap-on-caps",
             QEMU_CAPS_AES_KEY_WRAP,
