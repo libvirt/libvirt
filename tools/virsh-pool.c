a3dbaa364721ae7bc7b8ae700091bf05392818f7
John Ferlan <jferlan@redhat.com>
2019-01-11 14:48:31 -0500

    virsh: Add source-protocol-ver for pool commands
    Allow the addition of the <protocol ver='n'/> to the provided XML.
    
    Signed-off-by: John Ferlan <jferlan@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- tools/virsh-pool.c
+++ tools/virsh-pool.c
@@ -136,6 +136,10 @@
     {.name = "adapter-parent-fabric-wwn", \
      .type = VSH_OT_STRING, \
      .help = N_("adapter parent scsi_hostN fabric_wwn to be used for underlying vHBA storage") \
+    }, \
+    {.name = "source-protocol-ver", \
+     .type = VSH_OT_STRING, \
+     .help = N_("nfsvers value for NFS pool mount option") \
     }
 
 virStoragePoolPtr
@@ -319,7 +323,7 @@ virshBuildPoolXML(vshControl *ctl,
                *secretUsage = NULL, *adapterName = NULL, *adapterParent = NULL,
                *adapterWwnn = NULL, *adapterWwpn = NULL, *secretUUID = NULL,
                *adapterParentWwnn = NULL, *adapterParentWwpn = NULL,
-               *adapterParentFabricWwn = NULL;
+               *adapterParentFabricWwn = NULL, *protoVer = NULL;
     virBuffer buf = VIR_BUFFER_INITIALIZER;
 
     VSH_EXCLUSIVE_OPTIONS("secret-usage", "secret-uuid");
@@ -345,7 +349,8 @@ virshBuildPoolXML(vshControl *ctl,
         vshCommandOptStringReq(ctl, cmd, "adapter-parent", &adapterParent) < 0 ||
         vshCommandOptStringReq(ctl, cmd, "adapter-parent-wwnn", &adapterParentWwnn) < 0 ||
         vshCommandOptStringReq(ctl, cmd, "adapter-parent-wwpn", &adapterParentWwpn) < 0 ||
-        vshCommandOptStringReq(ctl, cmd, "adapter-parent-fabric-wwn", &adapterParentFabricWwn) < 0)
+        vshCommandOptStringReq(ctl, cmd, "adapter-parent-fabric-wwn", &adapterParentFabricWwn) < 0 ||
+        vshCommandOptStringReq(ctl, cmd, "source-protocol-ver", &protoVer) < 0)
         goto cleanup;
 
     virBufferAsprintf(&buf, "<pool type='%s'>\n", type);
@@ -393,6 +398,9 @@ virshBuildPoolXML(vshControl *ctl,
             virBufferAsprintf(&buf, "<format type='%s'/>\n", srcFormat);
         if (srcName)
             virBufferAsprintf(&buf, "<name>%s</name>\n", srcName);
+
+        if (protoVer)
+            virBufferAsprintf(&buf, "<protocol ver='%s'/>\n", protoVer);
 
         virBufferAdjustIndent(&buf, -2);
         virBufferAddLit(&buf, "</source>\n");
