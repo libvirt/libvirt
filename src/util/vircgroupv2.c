884479b42b857dc3dce6b3bd1dec4d8e971dee3c
Pavel Hrdina <phrdina@redhat.com>
2019-01-11 14:56:48 +0100

    vircgroup: introduce virCgroupV2DenyAllDevices
    If we want to deny all devices we just need to replace any existing
    program with new program with empty map.
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/util/vircgroupv2.c
+++ src/util/vircgroupv2.c
@@ -1824,6 +1824,16 @@ virCgroupV2AllowAllDevices(virCgroupPtr group,
 }
 
 
+static int
+virCgroupV2DenyAllDevices(virCgroupPtr group)
+{
+    if (virCgroupV2DevicesDetectProg(group) < 0)
+        return -1;
+
+    return virCgroupV2DevicesCreateProg(group);
+}
+
+
 virCgroupBackend virCgroupV2Backend = {
     .type = VIR_CGROUP_BACKEND_TYPE_V2,
 
@@ -1876,6 +1886,7 @@ virCgroupBackend virCgroupV2Backend = {
     .allowDevice = virCgroupV2AllowDevice,
     .denyDevice = virCgroupV2DenyDevice,
     .allowAllDevices = virCgroupV2AllowAllDevices,
+    .denyAllDevices = virCgroupV2DenyAllDevices,
 
     .setCpuShares = virCgroupV2SetCpuShares,
     .getCpuShares = virCgroupV2GetCpuShares,
