98c128fd996eb7003a883562f2bb100a84a939e8
Peter Krempa <pkrempa@redhat.com>
2019-01-28 11:25:12 +0100

    tests: qemuxml2argv: Use real caps when auto-generating SCSI controller type
    Using an old strict set of capabilities is not of much use if a code
    path would select a more modern controller by accident.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -1167,8 +1167,8 @@ mymain(void)
                     QEMU_CAPS_DEVICE_USB_STORAGE);
     DO_TEST("disk-scsi-device",
             QEMU_CAPS_SCSI_LSI);
-    DO_TEST("disk-scsi-device-auto",
-            QEMU_CAPS_SCSI_LSI);
+    DO_TEST_CAPS_VER("disk-scsi-device-auto", "1.5.3");
+    DO_TEST_CAPS_LATEST("disk-scsi-device-auto");
     DO_TEST("disk-scsi-disk-split",
             QEMU_CAPS_SCSI_CD, QEMU_CAPS_SCSI_LSI, QEMU_CAPS_VIRTIO_SCSI);
     DO_TEST("disk-scsi-disk-wwn",
