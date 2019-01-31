bca2346641b12c0daf8b7b9822da30a1ed12210e
Andrea Bolognani <abologna@redhat.com>
2019-01-31 14:02:47 +0100

    tests: Update qemucaps2xml for QEMU 4.0.0 on x86_64
    Commit fb0d0d6c5492 added capabilities data and updated
    qemucapabilitiestest but forgot to update qemucaps2xmltest
    at the same time.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- tests/qemucaps2xmltest.c
+++ tests/qemucaps2xmltest.c
@@ -198,6 +198,7 @@ mymain(void)
     DO_TEST("x86_64", "caps_2.12.0");
     DO_TEST("x86_64", "caps_3.0.0");
     DO_TEST("x86_64", "caps_3.1.0");
+    DO_TEST("x86_64", "caps_4.0.0");
     DO_TEST("aarch64", "caps_2.6.0");
     DO_TEST("aarch64", "caps_2.10.0");
     DO_TEST("aarch64", "caps_2.12.0");
