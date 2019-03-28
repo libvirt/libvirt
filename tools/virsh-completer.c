libvirt/libvirt

commit f81f8b62bd17d4b5e91a5cc770e2b93338b0d150

virsh: Introduce virshStorageVolNameCompleter
This one is a bit simpler since virStoragePoolListAllVolumes()
has no flags yet.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: John Ferlan <jferlan@redhat.com>

tools/virsh-completer.c
@@ -24,6 +24,7 @@
 
 #include "virsh-completer.h"
 #include "virsh.h"
+#include "virsh-pool.h"
 #include "virsh-util.h"
 #include "internal.h"
 #include "viralloc.h"
@@ -193,5 +194,56 @@ virshStoragePoolNameCompleter(vshControl *ctl,
     for (i = 0; i < npools; i++)
         VIR_FREE(ret[i]);
     VIR_FREE(ret);
+    return NULL;
+}
+
+
+char **
+virshStorageVolNameCompleter(vshControl *ctl,
+                             const vshCmd *cmd,
+                             unsigned int flags)
+{
+    virshControlPtr priv = ctl->privData;
+    virStoragePoolPtr pool = NULL;
+    virStorageVolPtr *vols = NULL;
+    int nvols = 0;
+    size_t i = 0;
+    char **ret = NULL;
+
+    virCheckFlags(0, NULL);
+
+    if (!priv->conn || virConnectIsAlive(priv->conn) <= 0)
+        return NULL;
+
+    if (!(pool = virshCommandOptPool(ctl, cmd, "pool", NULL)))
+        return false;
+
+    if ((nvols = virStoragePoolListAllVolumes(pool, &vols, flags)) < 0)
+        goto error;
+
+    if (VIR_ALLOC_N(ret, nvols + 1) < 0)
+        goto error;
+
+    for (i = 0; i < nvols; i++) {
+        const char *name = virStorageVolGetName(vols[i]);
+
+        if (VIR_STRDUP(ret[i], name) < 0)
+            goto error;
+
+        virStorageVolFree(vols[i]);
+    }
+    VIR_FREE(vols);
+    virStoragePoolFree(pool);
+
+    return ret;
+
+ error:
+    for (; i < nvols; i++)
+        virStorageVolFree(vols[i]);
+    VIR_FREE(vols);
+    for (i = 0; i < nvols; i++)
+        VIR_FREE(ret[i]);
+    VIR_FREE(ret);
+    virStoragePoolFree(pool);
     return NULL;
 }

tools/virsh-completer.h
@@ -42,4 +42,8 @@ char ** virshStoragePoolNameCompleter(vshControl *ctl,
                                       const vshCmd *cmd,
                                       unsigned int flags);
 
+char ** virshStorageVolNameCompleter(vshControl *ctl,
+                                     const vshCmd *cmd,
+                                     unsigned int flags);
+
 #endif

tools/virsh-volume.c
@@ -63,7 +63,8 @@
     {.name = "vol", \
      .type = VSH_OT_DATA, \
      .flags = VSH_OFLAG_REQ, \
-     .help = N_("vol name, key or path") \
+     .help = N_("vol name, key or path"), \
+     .completer = virshStorageVolNameCompleter, \
     }
 
 virStorageVolPtr

commit 24b092c404a9d44a7eae6b0b17b2af3df8ecf601

virsh: Don't infloop on snapshot/storage_vol failure
Most of our completers used the pattern:
if ((nITEM = virITEMListAll()) < 0)
    return NULL;

but the virDomainSnapshot and virStorageVolume completers were instead
using goto error. If the ListAll fails with -1, the cleanup label was
running a loop of 'size_t i < int nITEM', which is an extreme waste of
CPU cycles. Broken since their introduction in v4.1.

Fixes: f81f8b62
Fixes: 4cb4b649
Reported-by: Ján Tomko <jtomko@redhat.com>
Signed-off-by: Eric Blake <eblake@redhat.com>

tools/virsh-completer.c
@@ -278,6 +278,7 @@ virshStorageVolNameCompleter(vshControl *ctl,
     virshControlPtr priv = ctl->privData;
     virStoragePoolPtr pool = NULL;
     virStorageVolPtr *vols = NULL;
+    int rc;
     int nvols = 0;
     size_t i = 0;
     char **ret = NULL;
@@ -290,8 +291,9 @@ virshStorageVolNameCompleter(vshControl *ctl,
     if (!(pool = virshCommandOptPool(ctl, cmd, "pool", NULL)))
         return NULL;
 
-    if ((nvols = virStoragePoolListAllVolumes(pool, &vols, flags)) < 0)
+    if ((rc = virStoragePoolListAllVolumes(pool, &vols, flags)) < 0)
         goto error;
+    nvols = rc;
 
     if (VIR_ALLOC_N(ret, nvols + 1) < 0)
         goto error;
@@ -631,6 +633,7 @@ virshSnapshotNameCompleter(vshControl *ctl,
     virshControlPtr priv = ctl->privData;
     virDomainPtr dom = NULL;
     virDomainSnapshotPtr *snapshots = NULL;
+    int rc;
     int nsnapshots = 0;
     size_t i = 0;
     char **ret = NULL;
@@ -643,8 +646,9 @@ virshSnapshotNameCompleter(vshControl *ctl,
     if (!(dom = virshCommandOptDomain(ctl, cmd, NULL)))
         return NULL;
 
-    if ((nsnapshots = virDomainListAllSnapshots(dom, &snapshots, flags)) < 0)
+    if ((rc = virDomainListAllSnapshots(dom, &snapshots, flags)) < 0)
         goto error;
+    nsnapshots = rc;
 
     if (VIR_ALLOC_N(ret, nsnapshots + 1) < 0)
         goto error;