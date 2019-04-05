8d6ab7976fa691763fc05a154f2bab865d435b00
Jiri Denemark <jdenemar@redhat.com>
2019-04-05 11:33:32 +0200

    cpu_x86: Do not cache microcode version
    The microcode version checks are used to invalidate cached CPU data we
    get from QEMU. To minimize /proc/cpuinfo parsing the microcode version
    was only read when libvirtd started and cached for the daemon's
    lifetime. However, the CPU microcode can change anytime (updating the
    microcode package can automatically upload it to the CPU) and we need to
    stop caching it to avoid using stale CPU model data.
    
    Signed-off-by: Jiri Denemark <jdenemar@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    (cherry picked from commit be46f613261d3b655a1f15afd635087e68a9c39b)

--- src/cpu/cpu_x86.c
+++ src/cpu/cpu_x86.c
@@ -154,7 +154,6 @@ struct _virCPUx86Map {
 };
 
 static virCPUx86MapPtr cpuMap;
-static unsigned int microcodeVersion;
 
 int virCPUx86DriverOnceInit(void);
 VIR_ONCE_GLOBAL_INIT(virCPUx86Driver);
@@ -1413,8 +1412,6 @@ virCPUx86DriverOnceInit(void)
     if (!(cpuMap = virCPUx86LoadMap()))
         return -1;
 
-    microcodeVersion = virHostCPUGetMicrocodeVersion();
-
     return 0;
 }
 
@@ -2454,7 +2451,7 @@ virCPUx86GetHost(virCPUDefPtr cpu,
         goto cleanup;
 
     ret = x86DecodeCPUData(cpu, cpuData, models);
-    cpu->microcodeVersion = microcodeVersion;
+    cpu->microcodeVersion = virHostCPUGetMicrocodeVersion();
 
  cleanup:
     virCPUx86DataFree(cpuData);
