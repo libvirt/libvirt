83f046458ea47bcdef707932847f72c3c1a226e7
Ján Tomko <jtomko@redhat.com>
2019-08-06 18:22:24 +0200

    qemu: pass private data to qemuBuildFilesystemCommandLine
    This will be used by a future patch.
    
    Signed-off-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- src/qemu/qemu_command.c
+++ src/qemu/qemu_command.c
@@ -2672,7 +2672,8 @@ qemuBuildFSDevCommandLine(virCommandPtr cmd,
 static int
 qemuBuildFilesystemCommandLine(virCommandPtr cmd,
                                const virDomainDef *def,
-                               virQEMUCapsPtr qemuCaps)
+                               virQEMUCapsPtr qemuCaps,
+                               qemuDomainObjPrivatePtr priv G_GNUC_UNUSED)
 {
     size_t i;
 
@@ -9860,7 +9861,7 @@ qemuBuildCommandLine(virQEMUDriverPtr driver,
     if (qemuBuildDisksCommandLine(cmd, def, qemuCaps) < 0)
         return NULL;
 
-    if (qemuBuildFilesystemCommandLine(cmd, def, qemuCaps) < 0)
+    if (qemuBuildFilesystemCommandLine(cmd, def, qemuCaps, priv) < 0)
         return NULL;
 
     if (qemuBuildNetCommandLine(driver, vm, logManager, secManager, cmd,
