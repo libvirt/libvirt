2ce3274ea5f8f4f21547ceb98f236812c4f72d94
Andrea Bolognani <abologna@redhat.com>
2019-08-15 15:24:50 +0200

    ci: Introduce $(CI_PREPARE_SCRIPT)
    This script is run before $(CI_BUILD_SCRIPT) and can be used
    to tweak the environment as necessary before the build starts.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

---
+++ ci/prepare.sh
@@ -0,0 +1,9 @@
+# This script is used to prepare the environment that will be used
+# to build libvirt inside the container.
+#
+# You can customize it to your liking, or alternatively use a
+# completely different script by passing
+#
+#  CI_PREPARE_SCRIPT=/path/to/your/prepare/script
+#
+# to make.
