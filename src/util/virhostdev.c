2e7225ea8c6dc32a3bc2df79515ec894f59cf138
Michal Privoznik <mprivozn@redhat.com>
2019-08-19 11:47:19 +0200

    virhostdev: Disable legacy kvm assignment
    The KVM assignment is going to be removed shortly. Don't let the
    hostdev module configure it.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>
    Tested-by: Daniel Henrique Barboza <danielhb413@gmail.com>

--- src/util/virhostdev.c
+++ src/util/virhostdev.c
@@ -246,12 +246,16 @@ virHostdevGetPCIHostDevice(const virDomainHostdevDef *hostdev,
 
     virPCIDeviceSetManaged(actual, hostdev->managed);
 
-    if (pcisrc->backend == VIR_DOMAIN_HOSTDEV_PCI_BACKEND_VFIO)
+    if (pcisrc->backend == VIR_DOMAIN_HOSTDEV_PCI_BACKEND_VFIO) {
         virPCIDeviceSetStubDriver(actual, VIR_PCI_STUB_DRIVER_VFIO);
-    else if (pcisrc->backend == VIR_DOMAIN_HOSTDEV_PCI_BACKEND_XEN)
+    } else if (pcisrc->backend == VIR_DOMAIN_HOSTDEV_PCI_BACKEND_XEN) {
         virPCIDeviceSetStubDriver(actual, VIR_PCI_STUB_DRIVER_XEN);
-    else
-        virPCIDeviceSetStubDriver(actual, VIR_PCI_STUB_DRIVER_KVM);
+    } else {
+        virReportError(VIR_ERR_CONFIG_UNSUPPORTED,
+                       _("pci backend driver '%s' is not supported"),
+                       virDomainHostdevSubsysPCIBackendTypeToString(pcisrc->backend));
+        return -1;
+    }
 
     VIR_STEAL_PTR(*pci, actual);
     return 0;
