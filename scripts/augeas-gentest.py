17bbdef5cb8aaa9dd8b6231824412fbd3c3ee74e
Daniel P. Berrangé <berrange@redhat.com>
2019-08-30 13:22:54 +0100

    build-aux: rewrite augeas test generator in Python
    As part of an goal to eliminate Perl from libvirt build tools,
    rewrite the augeas-gentest.pl tool in Python.
    
    This was a straight conversion, manually going line-by-line to
    change the syntax from Perl to Python. Thus the overall structure
    of the file and approach is the same.
    
    The use of $(AUG_GENTEST) as a dependancy in the makefiles needed
    to be fixed, because this was assumed to be the filename of the
    script, but is in fact a full shell command line.
    
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

---
+++ scripts/augeas-gentest.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2012-2019 Red Hat, Inc.
+#
+# augeas-gentest.py: Generate an augeas test file, from an
+#                    example config file + test file template
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+
+import re
+import sys
+
+if len(sys.argv) != 3:
+    print("syntax: %s CONFIG TEMPLATE" % sys.argv[0], file=sys.stderr)
+    sys.exit(1)
+
+config = sys.argv[1]
+template = sys.argv[2]
+
+
+def expand_config(config):
+    with open(config) as fh:
+        group = False
+        for line in fh:
+            if re.search(r'''^#\w''', line) is not None:
+                line = line[1:]
+                line = line.replace('"', '\\"')
+                print(line, end='')
+                if re.search(r'''\[\s$''', line):
+                    group = True
+            elif group:
+                line = line.replace('"', '\\"')
+
+                if re.search(r'''#\s*\]''', line):
+                    group = False
+
+                if line[0] == '#':
+                    line = line[1:]
+                    print(line, end='')
+
+
+def expand_template(template, config):
+    with open(template) as fh:
+        for line in fh:
+            if '@CONFIG@' in line:
+                print('   let conf = "', end='')
+                expand_config(config)
+                print('"')
+            else:
+                print(line, end='')
+
+
+expand_template(template, config)
