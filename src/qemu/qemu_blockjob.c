7f99d8a739c01a280ca0c173ecd6051a92f0c897
Michal Privoznik <mprivozn@redhat.com>
2019-08-30 15:07:48 +0200

    qemu_blockjob: Print image path on failed security metadata move too
    When a block job is completed, the security image metadata are
    moved to the new image. If this fails an warning is printed, but
    the message contains only domain name and lacks image paths. Put
    them both into the warning message.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    ACKed-by: Peter Krempa <pkrempa@redhat.com>

--- src/qemu/qemu_blockjob.c
+++ src/qemu/qemu_blockjob.c
@@ -646,8 +646,14 @@ qemuBlockJobEventProcessLegacyCompleted(virQEMUDriverPtr driver,
         virDomainLockImageDetach(driver->lockManager, vm, disk->src);
 
         /* Move secret driver metadata */
-        if (qemuSecurityMoveImageMetadata(driver, vm, disk->src, disk->mirror) < 0)
-            VIR_WARN("Unable to move disk metadata on vm %s", vm->def->name);
+        if (qemuSecurityMoveImageMetadata(driver, vm, disk->src, disk->mirror) < 0) {
+            VIR_WARN("Unable to move disk metadata on "
+                     "vm %s from %s to %s (disk target %s)",
+                     vm->def->name,
+                     NULLSTR(disk->src->path),
+                     NULLSTR(disk->mirror->path),
+                     disk->dst);
+        }
 
         virObjectUnref(disk->src);
         disk->src = disk->mirror;
