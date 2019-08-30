d30a1ad04431c245ee606d7825e8a1f3d8e691c8
Daniel P. Berrangé <berrange@redhat.com>
2019-08-30 13:22:54 +0100

    src: rewrite symfile library checker in Python
    As part of a goal to eliminate Perl from libvirt build tools,
    rewrite the check-symfile.pl tool in Python.
    
    This was a straight conversion, manually going line-by-line to
    change the syntax from Perl to Python. Thus the overall structure
    of the file and approach is the same.
    
    Tested-by: Cole Robinson <crobinso@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

---
+++ scripts/check-symfile.py
@@ -0,0 +1,80 @@
+#!/usr/bin/env python
+#
+# Copyright (C) 2012-2019 Red Hat, Inc.
+#
+# This library is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This library is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this library.  If not, see
+# <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+
+import re
+import subprocess
+import sys
+
+if len(sys.argv) < 3:
+    print("syntax: %s SYMFILE ELFLIB(S)" % sys.argv[0], file=sys.stderr)
+
+symfile = sys.argv[1]
+elflibs = sys.argv[2:]
+
+wantsyms = {}
+gotsyms = {}
+
+ret = 0
+
+with open(symfile, "r") as fh:
+    for line in fh:
+        line = line.strip()
+        if line.find("{") != -1:
+            continue
+        if line.find("}") != -1:
+            continue
+        if line in ["global:", "local:"]:
+            continue
+        if line == "":
+            continue
+        if line[0] == '#':
+            continue
+        if line.find("*") != -1:
+            continue
+
+        line = line.strip(";")
+
+        if line in wantsyms:
+            print("Symbol $1 is listed twice", file=sys.stderr)
+            ret = 1
+        else:
+            wantsyms[line] = True
+
+for elflib in elflibs:
+    nm = subprocess.Popen(["nm", elflib], shell=False,
+                          stdout=subprocess.PIPE).stdout
+
+    for line in nm:
+        line = line.decode("utf-8")
+        symmatch = re.search(r'''^\S+\s(?:[TBD])\s(\S+)\s*$''', line)
+        if symmatch is None:
+            continue
+
+        gotsyms[symmatch.group(1)] = True
+
+
+for sym in wantsyms.keys():
+    if sym in gotsyms:
+        continue
+
+    print("Expected symbol '%s' is not in ELF library" % sym, file=sys.stderr)
+    ret = 1
+
+sys.exit(ret)
