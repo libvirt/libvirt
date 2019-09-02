149bbc52e22287eca95923f9ab05567d8727272d
Julio Faracco <jcfaracco@gmail.com>
2019-09-02 14:00:27 -0300

    util: Set backing file name for LOOP_GET_STATUS64 queries.
    This is an issue for LXC loop devices when you are trying to get loop
    devices info using `ioctl`. Modern apps uses `/sys/dev/block` to grab
    information about devices, but if you use the method mention you won't
    be able to retrive the associated file with that loop device. See
    example below from cryptsetup sources:
    
        static char *_ioctl_backing_file(const char *loop)
        {
            struct loop_info64 lo64 = {0};
            int loop_fd;
    
            loop_fd = open(loop, O_RDONLY);
            if (loop_fd < 0)
                return NULL;
    
            if (ioctl(loop_fd, LOOP_GET_STATUS64, &lo64) < 0) {
                close(loop_fd);
                return NULL;
            }
    
            lo64.lo_file_name[LO_NAME_SIZE-2] = '*';
            lo64.lo_file_name[LO_NAME_SIZE-1] = 0;
    
            close(loop_fd);
            return strdup((char*)lo64.lo_file_name);
        }
    
    It will return an empty string because lo_file_name was not set.
    Function `virFileLoopDeviceOpenSearch()` is using `ioctl` to query data,
    but it is not checking `lo_file_name` field.
    
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Signed-off-by: Julio Faracco <jcfaracco@gmail.com>

--- src/util/virfile.c
+++ src/util/virfile.c
@@ -781,6 +781,14 @@ int virFileLoopDeviceAssociate(const char *file,
     memset(&lo, 0, sizeof(lo));
     lo.lo_flags = LO_FLAGS_AUTOCLEAR;
 
+    /* Set backing file name for LOOP_GET_STATUS64 queries */
+    if (virStrncpy((char *) lo.lo_file_name, file,
+                   strlen(file), LO_NAME_SIZE) < 0) {
+        virReportSystemError(errno,
+                             _("Unable to set backing file %s"), file);
+        goto cleanup;
+    }
+
     if ((fsfd = open(file, O_RDWR)) < 0) {
         virReportSystemError(errno,
                              _("Unable to open %s"), file);
