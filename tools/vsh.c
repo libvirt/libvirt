libvirt/libvirt

commit c937c1d23db00cf0f33091f9d30f7ac33d9d6aa4

tools: prefer g_strdup to vshStrdup
Remove all the uses of vshStrdup in favor of GLib's g_strdup.

Signed-off-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>

tools/vsh.c
@@ -176,7 +176,7 @@ int
 vshStringToArray(const char *str,
                  char ***array)
 {
-    char *str_copied = vshStrdup(NULL, str);
+    char *str_copied = g_strdup(str);
     char *str_tok = NULL;
     char *tmp;
     unsigned int nstr_tokens = 0;
@@ -214,10 +214,10 @@ vshStringToArray(const char *str,
             continue;
         }
         *tmp++ = '\0';
-        arr[nstr_tokens++] = vshStrdup(NULL, str_tok);
+        arr[nstr_tokens++] = g_strdup(str_tok);
         str_tok = tmp;
     }
-    arr[nstr_tokens++] = vshStrdup(NULL, str_tok);
+    arr[nstr_tokens++] = g_strdup(str_tok);
 
     *array = arr;
     VIR_FREE(str_copied);
@@ -1451,7 +1451,7 @@ vshCommandParse(vshControl *ctl, vshCommandParser *parser, vshCmd **partial)
                 /* aliases need to be resolved to the actual commands */
                 if (cmd->flags & VSH_CMD_FLAG_ALIAS) {
                     VIR_FREE(tkdata);
-                    tkdata = vshStrdup(ctl, cmd->alias);
+                    tkdata = g_strdup(cmd->alias);
                     cmd = vshCmddefSearch(tkdata);
                 }
                 if (vshCmddefOptParse(cmd, &opts_need_arg,
@@ -1472,7 +1472,7 @@ vshCommandParse(vshControl *ctl, vshCommandParser *parser, vshCmd **partial)
 
                 if (optstr) {
                     *optstr = '\0'; /* convert the '=' to '\0' */
-                    optstr = vshStrdup(ctl, optstr + 1);
+                    optstr = g_strdup(optstr + 1);
                 }
                 /* Special case 'help' to ignore all spurious options */
                 if (!(opt = vshCmddefGetOption(ctl, cmd, tkdata + 2,
@@ -1582,7 +1582,7 @@ vshCommandParse(vshControl *ctl, vshCommandParser *parser, vshCmd **partial)
                 vshCommandOptFree(first);
                 first = vshMalloc(ctl, sizeof(vshCmdOpt));
                 first->def = help->opts;
-                first->data = vshStrdup(ctl, cmd->name);
+                first->data = g_strdup(cmd->name);
                 first->next = NULL;
 
                 cmd = help;
@@ -1686,7 +1686,7 @@ vshCommandStringGetArg(vshControl *ctl, vshCommandParser *parser, char **res,
     bool double_quote = false;
     int sz = 0;
     char *p = parser->pos;
-    char *q = vshStrdup(ctl, p);
+    char *q = g_strdup(p);
 
     *res = q;
 
@@ -1834,11 +1834,11 @@ vshGetTypedParamValue(vshControl *ctl, virTypedParameterPtr item)
         break;
 
     case VIR_TYPED_PARAM_BOOLEAN:
-        str = vshStrdup(ctl, item->value.b ? _("yes") : _("no"));
+        str = g_strdup(item->value.b ? _("yes") : _("no"));
         break;
 
     case VIR_TYPED_PARAM_STRING:
-        str = vshStrdup(ctl, item->value.s);
+        str = g_strdup(item->value.s);
         break;
 
     default:
@@ -2668,7 +2668,7 @@ vshReadlineCommandGenerator(const char *text)
                         virStringListFree(ret);
                         return NULL;
                     }
-                    ret[ret_size] = vshStrdup(NULL, name);
+                    ret[ret_size] = g_strdup(name);
                     ret_size++;
                     /* Terminate the string list properly. */
                     ret[ret_size] = NULL;
@@ -2820,7 +2820,7 @@ vshReadlineParse(const char *text, int state)
     char *ret = NULL;
 
     if (!state) {
-        char *buf = vshStrdup(NULL, rl_line_buffer);
+        char *buf = g_strdup(rl_line_buffer);
 
         vshCommandFree(partial);
         partial = NULL;
@@ -2882,7 +2882,7 @@ vshReadlineParse(const char *text, int state)
     }
 
     if (list) {
-        ret = vshStrdup(NULL, list[list_index]);
+        ret = g_strdup(list[list_index]);
         list_index++;
     }
 
@@ -3055,7 +3055,7 @@ vshReadline(vshControl *ctl, const char *prompt)
     if (len > 0 && r[len-1] == '\n')
         r[len-1] = '\0';
 
-    return vshStrdup(ctl, r);
+    return g_strdup(r);
 }
 
 #endif /* !WITH_READLINE */
@@ -3095,7 +3095,7 @@ vshInitDebug(vshControl *ctl)
         /* log file not set from cmdline */
         debugEnv = getenv(env);
         if (debugEnv && *debugEnv) {
-            ctl->logfile = vshStrdup(ctl, debugEnv);
+            ctl->logfile = g_strdup(debugEnv);
             vshOpenLogFile(ctl);
         }
         VIR_FREE(env);
@@ -3345,7 +3345,7 @@ cmdEcho(vshControl *ctl, const vshCmd *cmd)
             }
             str = virBufferContentAndReset(&xmlbuf);
         } else {
-            str = vshStrdup(ctl, arg);
+            str = g_strdup(arg);
         }
 
         if (shell)

tools/virsh-checkpoint.c
@@ -127,7 +127,7 @@ cmdCheckpointCreate(vshControl *ctl,
     if (vshCommandOptStringReq(ctl, cmd, "xmlfile", &from) < 0)
         goto cleanup;
     if (!from) {
-        buffer = vshStrdup(ctl, "<domaincheckpoint/>");
+        buffer = g_strdup("<domaincheckpoint/>");
     } else {
         if (virFileReadAll(from, VSH_MAX_XML_FILE, &buffer) < 0) {
             vshSaveLibvirtError();
@@ -426,7 +426,7 @@ virshGetCheckpointParent(vshControl *ctl,
     parent = virDomainCheckpointGetParent(checkpoint, 0);
     if (parent) {
         /* API works, and virDomainCheckpointGetName will succeed */
-        *parent_name = vshStrdup(ctl, virDomainCheckpointGetName(parent));
+        *parent_name = g_strdup(virDomainCheckpointGetName(parent));
         ret = 0;
     } else if (last_error->code == VIR_ERR_NO_DOMAIN_CHECKPOINT) {
         /* API works, and we found a root with no parent */

tools/virsh-domain-monitor.c
@@ -73,7 +73,7 @@ virshGetDomainDescription(vshControl *ctl, virDomainPtr dom, bool title,
         int errCode = virGetLastErrorCode();
 
         if (errCode == VIR_ERR_NO_DOMAIN_METADATA) {
-            desc = vshStrdup(ctl, "");
+            desc = g_strdup("");
             vshResetLibvirtError();
             return desc;
         }
@@ -92,7 +92,7 @@ virshGetDomainDescription(vshControl *ctl, virDomainPtr dom, bool title,
         desc = virXPathString("string(./description[1])", ctxt);
 
     if (!desc)
-        desc = vshStrdup(ctl, "");
+        desc = g_strdup("");
 
  cleanup:
     xmlXPathFreeContext(ctxt);
@@ -2419,7 +2419,7 @@ cmdDomIfAddr(vshControl *ctl, const vshCmd *cmd)
             ip_addr_str = virBufferContentAndReset(&buf);
 
             if (!ip_addr_str)
-                ip_addr_str = vshStrdup(ctl, "");
+                ip_addr_str = g_strdup("");
 
             /* Don't repeat interface name */
             if (full || !j)

tools/virsh-domain.c
@@ -5085,7 +5085,7 @@ cmdSchedInfoUpdate(vshControl *ctl, const vshCmd *cmd,
     int rv;
 
     while ((opt = vshCommandOptArgv(ctl, cmd, opt))) {
-        set_field = vshStrdup(ctl, opt->data);
+        set_field = g_strdup(opt->data);
         if (!(set_val = strchr(set_field, '='))) {
             vshError(ctl, "%s", _("Invalid syntax for --set, "
                                   "expecting name=value"));
@@ -11481,7 +11481,7 @@ cmdDomDisplay(vshControl *ctl, const vshCmd *cmd)
                 VIR_FREE(listen_addr);
 
                 if (uri) {
-                    listen_addr = vshStrdup(ctl, uri->server);
+                    listen_addr = g_strdup(uri->server);
                     virURIFree(uri);
                 }
             }

tools/virsh-nodedev.c
@@ -477,13 +477,12 @@ cmdNodeListDevices(vshControl *ctl, const vshCmd *cmd G_GNUC_UNUSED)
         struct virshNodeList arrays = { names, parents };
 
         for (i = 0; i < list->ndevices; i++)
-            names[i] = vshStrdup(ctl, virNodeDeviceGetName(list->devices[i]));
+            names[i] = g_strdup(virNodeDeviceGetName(list->devices[i]));
 
         for (i = 0; i < list->ndevices; i++) {
             virNodeDevicePtr dev = list->devices[i];
             if (STRNEQ(names[i], "computer")) {
-                const char *parent = virNodeDeviceGetParent(dev);
-                parents[i] = parent ? vshStrdup(ctl, parent) : NULL;
+                parents[i] = g_strdup(virNodeDeviceGetParent(dev));
             } else {
                 parents[i] = NULL;
             }

tools/virsh-pool.c
@@ -1247,10 +1247,9 @@ cmdPoolList(vshControl *ctl, const vshCmd *cmd G_GNUC_UNUSED)
 
         /* Retrieve the autostart status of the pool */
         if (virStoragePoolGetAutostart(list->pools[i], &autostart) < 0)
-            poolInfoTexts[i].autostart = vshStrdup(ctl, _("no autostart"));
+            poolInfoTexts[i].autostart = g_strdup(_("no autostart"));
         else
-            poolInfoTexts[i].autostart = vshStrdup(ctl, autostart ?
-                                                    _("yes") : _("no"));
+            poolInfoTexts[i].autostart = g_strdup(autostart ? _("yes") : _("no"));
 
         /* Retrieve the persistence status of the pool */
         if (details) {
@@ -1258,28 +1257,27 @@ cmdPoolList(vshControl *ctl, const vshCmd *cmd G_GNUC_UNUSED)
             vshDebug(ctl, VSH_ERR_DEBUG, "Persistent flag value: %d\n",
                      persistent);
             if (persistent < 0)
-                poolInfoTexts[i].persistent = vshStrdup(ctl, _("unknown"));
+                poolInfoTexts[i].persistent = g_strdup(_("unknown"));
             else
-                poolInfoTexts[i].persistent = vshStrdup(ctl, persistent ?
-                                                         _("yes") : _("no"));
+                poolInfoTexts[i].persistent = g_strdup(persistent ? _("yes") : _("no"));
         }
 
         /* Collect further extended information about the pool */
         if (virStoragePoolGetInfo(list->pools[i], &info) != 0) {
             /* Something went wrong retrieving pool info, cope with it */
             vshError(ctl, "%s", _("Could not retrieve pool information"));
-            poolInfoTexts[i].state = vshStrdup(ctl, _("unknown"));
+            poolInfoTexts[i].state = g_strdup(_("unknown"));
             if (details) {
-                poolInfoTexts[i].capacity = vshStrdup(ctl, _("unknown"));
-                poolInfoTexts[i].allocation = vshStrdup(ctl, _("unknown"));
-                poolInfoTexts[i].available = vshStrdup(ctl, _("unknown"));
+                poolInfoTexts[i].capacity = g_strdup(_("unknown"));
+                poolInfoTexts[i].allocation = g_strdup(_("unknown"));
+                poolInfoTexts[i].available = g_strdup(_("unknown"));
             }
         } else {
             /* Decide which state string to display */
             if (details) {
                 const char *state = virshStoragePoolStateToString(info.state);
 
-                poolInfoTexts[i].state = vshStrdup(ctl, state);
+                poolInfoTexts[i].state = g_strdup(state);
 
                 /* Create the pool size related strings */
                 if (info.state == VIR_STORAGE_POOL_RUNNING ||
@@ -1303,17 +1301,17 @@ cmdPoolList(vshControl *ctl, const vshCmd *cmd G_GNUC_UNUSED)
                         goto cleanup;
                 } else {
                     /* Capacity related information isn't available */
-                    poolInfoTexts[i].capacity = vshStrdup(ctl, _("-"));
-                    poolInfoTexts[i].allocation = vshStrdup(ctl, _("-"));
-                    poolInfoTexts[i].available = vshStrdup(ctl, _("-"));
+                    poolInfoTexts[i].capacity = g_strdup(_("-"));
+                    poolInfoTexts[i].allocation = g_strdup(_("-"));
+                    poolInfoTexts[i].available = g_strdup(_("-"));
                 }
             } else {
                 /* --details option was not specified, only active/inactive
                  * state strings are used */
                 if (virStoragePoolIsActive(list->pools[i]))
-                    poolInfoTexts[i].state = vshStrdup(ctl, _("active"));
+                    poolInfoTexts[i].state = g_strdup(_("active"));
                 else
-                    poolInfoTexts[i].state = vshStrdup(ctl, _("inactive"));
+                    poolInfoTexts[i].state = g_strdup(_("inactive"));
            }
         }
     }

tools/virsh-snapshot.c
@@ -197,7 +197,7 @@ cmdSnapshotCreate(vshControl *ctl, const vshCmd *cmd)
     if (vshCommandOptStringReq(ctl, cmd, "xmlfile", &from) < 0)
         goto cleanup;
     if (!from) {
-        buffer = vshStrdup(ctl, "<domainsnapshot/>");
+        buffer = g_strdup("<domainsnapshot/>");
     } else {
         if (virFileReadAll(from, VSH_MAX_XML_FILE, &buffer) < 0) {
             vshSaveLibvirtError();
@@ -755,7 +755,7 @@ virshGetSnapshotParent(vshControl *ctl, virDomainSnapshotPtr snapshot,
         parent = virDomainSnapshotGetParent(snapshot, 0);
         if (parent) {
             /* API works, and virDomainSnapshotGetName will succeed */
-            *parent_name = vshStrdup(ctl, virDomainSnapshotGetName(parent));
+            *parent_name = g_strdup(virDomainSnapshotGetName(parent));
             ret = 0;
             goto cleanup;
         }
@@ -1236,7 +1236,7 @@ virshSnapshotListCollect(vshControl *ctl, virDomainPtr dom,
                                                        count - 1, flags);
             if (count >= 0) {
                 count++;
-                names[0] = vshStrdup(ctl, fromname);
+                names[0] = g_strdup(fromname);
             }
         } else {
             count = virDomainSnapshotListChildrenNames(from, names,

tools/virsh-volume.c
@@ -1413,22 +1413,21 @@ cmdVolList(vshControl *ctl, const vshCmd *cmd G_GNUC_UNUSED)
         /* Retrieve the volume path */
         if ((volInfoTexts[i].path = virStorageVolGetPath(vol)) == NULL) {
             /* Something went wrong retrieving a volume path, cope with it */
-            volInfoTexts[i].path = vshStrdup(ctl, _("unknown"));
+            volInfoTexts[i].path = g_strdup(_("unknown"));
         }
 
         /* If requested, retrieve volume type and sizing information */
         if (details) {
             if (virStorageVolGetInfo(vol, &volumeInfo) != 0) {
                 /* Something went wrong retrieving volume info, cope with it */
-                volInfoTexts[i].allocation = vshStrdup(ctl, _("unknown"));
-                volInfoTexts[i].capacity = vshStrdup(ctl, _("unknown"));
-                volInfoTexts[i].type = vshStrdup(ctl, _("unknown"));
+                volInfoTexts[i].allocation = g_strdup(_("unknown"));
+                volInfoTexts[i].capacity = g_strdup(_("unknown"));
+                volInfoTexts[i].type = g_strdup(_("unknown"));
             } else {
                 /* Convert the returned volume info into output strings */
 
                 /* Volume type */
-                volInfoTexts[i].type = vshStrdup(ctl,
-                                                 virshVolumeTypeToString(volumeInfo.type));
+                volInfoTexts[i].type = g_strdup(virshVolumeTypeToString(volumeInfo.type));
 
                 val = vshPrettyCapacity(volumeInfo.capacity, &unit);
                 if (virAsprintf(&volInfoTexts[i].capacity,

commit fbc5a15372f2347f1d6504e6bb9bdfa92a30782e

vsh: mark ctl as unused in vshReadline
My commit removed the last use in the version for platforms
without readline.

Fixes: c937c1d23db00cf0f33091f9d30f7ac33d9d6aa4

Signed-off-by: Ján Tomko <jtomko@redhat.com>

tools/vsh.c
@@ -3028,7 +3028,8 @@ vshReadlineDeinit(vshControl *ctl G_GNUC_UNUSED)
 }
 
 char *
-vshReadline(vshControl *ctl, const char *prompt)
+vshReadline(vshControl *ctl G_GNUC_UNUSED,
+            const char *prompt)
 {
     char line[1024];
     char *r;