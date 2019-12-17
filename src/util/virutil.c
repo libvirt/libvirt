e59b946ce473395d8c484af43aadc0056a640a6f
Fabiano Fidêncio <fidencio@redhat.com>
2019-12-17 16:31:11 +0100

    util: Rewrite virGetUserConfigDirectory() using g_get_user_config_dir()
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    Reviewed-by: Cole Robinson <crobinso@redhat.com>

--- src/util/virutil.c
+++ src/util/virutil.c
@@ -586,6 +586,16 @@ virGetUserDirectory(void)
 }
 
 
+char *virGetUserConfigDirectory(void)
+{
+#ifdef WIN32
+    return g_strdup(g_get_user_config_dir());
+#else
+    return g_build_filename(g_get_user_config_dir(), "libvirt", NULL);
+#endif
+}
+
+
 #ifdef HAVE_GETPWUID_R
 /* Look up fields from the user database for the given user.  On
  * error, set errno, report the error if not instructed otherwise via @quiet,
@@ -752,11 +762,6 @@ static char *virGetXDGDirectory(const char *xdgenvname, const char *xdgdefdir)
 
     VIR_FREE(home);
     return ret;
-}
-
-char *virGetUserConfigDirectory(void)
-{
-    return virGetXDGDirectory("XDG_CONFIG_HOME", ".config");
 }
 
 char *virGetUserCacheDirectory(void)
@@ -1188,21 +1193,6 @@ virGetUserShell(uid_t uid G_GNUC_UNUSED)
 }
 
 char *
-virGetUserConfigDirectory(void)
-{
-    char *ret;
-    if (virGetWin32SpecialFolder(CSIDL_LOCAL_APPDATA, &ret) < 0)
-        return NULL;
-
-    if (!ret) {
-        virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
-                       _("Unable to determine config directory"));
-        return NULL;
-    }
-    return ret;
-}
-
-char *
 virGetUserCacheDirectory(void)
 {
     char *ret;
@@ -1238,15 +1228,6 @@ virGetUserShell(uid_t uid G_GNUC_UNUSED)
 {
     virReportError(VIR_ERR_INTERNAL_ERROR,
                    "%s", _("virGetUserShell is not available"));
-
-    return NULL;
-}
-
-char *
-virGetUserConfigDirectory(void)
-{
-    virReportError(VIR_ERR_INTERNAL_ERROR,
-                   "%s", _("virGetUserConfigDirectory is not available"));
 
     return NULL;
 }
