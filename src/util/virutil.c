520e626e7e6cba32cd214a3cf93e8e0aaf08d4f0
Fabiano Fidêncio <fidencio@redhat.com>
2019-12-17 16:34:28 +0100

    util: Rewrite virGetUserCacheDirectory() using g_get_user_cache_dir()
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    Reviewed-by: Cole Robinson <crobinso@redhat.com>

--- src/util/virutil.c
+++ src/util/virutil.c
@@ -596,6 +596,16 @@ char *virGetUserConfigDirectory(void)
 }
 
 
+char *virGetUserCacheDirectory(void)
+{
+#ifdef WIN32
+    return g_strdup(g_get_user_cache_dir());
+#else
+    return g_build_filename(g_get_user_cache_dir(), "libvirt", NULL);
+#endif
+}
+
+
 #ifdef HAVE_GETPWUID_R
 /* Look up fields from the user database for the given user.  On
  * error, set errno, report the error if not instructed otherwise via @quiet,
@@ -745,29 +755,6 @@ char *virGetUserShell(uid_t uid)
     return ret;
 }
 
-
-static char *virGetXDGDirectory(const char *xdgenvname, const char *xdgdefdir)
-{
-    const char *path = getenv(xdgenvname);
-    char *ret = NULL;
-    char *home = NULL;
-
-    if (path && path[0]) {
-        ret = g_strdup_printf("%s/libvirt", path);
-    } else {
-        home = virGetUserDirectory();
-        if (home)
-            ret = g_strdup_printf("%s/%s/libvirt", home, xdgdefdir);
-    }
-
-    VIR_FREE(home);
-    return ret;
-}
-
-char *virGetUserCacheDirectory(void)
-{
-    return virGetXDGDirectory("XDG_CACHE_HOME", ".cache");
-}
 
 char *virGetUserRuntimeDirectory(void)
 {
@@ -1193,21 +1180,6 @@ virGetUserShell(uid_t uid G_GNUC_UNUSED)
 }
 
 char *
-virGetUserCacheDirectory(void)
-{
-    char *ret;
-    if (virGetWin32SpecialFolder(CSIDL_INTERNET_CACHE, &ret) < 0)
-        return NULL;
-
-    if (!ret) {
-        virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
-                       _("Unable to determine config directory"));
-        return NULL;
-    }
-    return ret;
-}
-
-char *
 virGetUserRuntimeDirectory(void)
 {
     return virGetUserCacheDirectory();
@@ -1228,15 +1200,6 @@ virGetUserShell(uid_t uid G_GNUC_UNUSED)
 {
     virReportError(VIR_ERR_INTERNAL_ERROR,
                    "%s", _("virGetUserShell is not available"));
-
-    return NULL;
-}
-
-char *
-virGetUserCacheDirectory(void)
-{
-    virReportError(VIR_ERR_INTERNAL_ERROR,
-                   "%s", _("virGetUserCacheDirectory is not available"));
 
     return NULL;
 }
