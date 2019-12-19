53655ba2c9cbc2213684c504f558d65b822223bb
Fabiano Fidêncio <fidencio@redhat.com>
2019-12-19 10:37:20 +0100

    locking: Use g_autofree in virLockDaemonExecRestartStatePath()
    Together with the change, let's also simplify the function and get rid
    of the goto.
    
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/locking/lock_daemon.c
+++ src/locking/lock_daemon.c
@@ -820,29 +820,23 @@ virLockDaemonExecRestartStatePath(bool privileged,
     if (privileged) {
         *state_file = g_strdup(RUNSTATEDIR "/virtlockd-restart-exec.json");
     } else {
-        char *rundir = NULL;
+        g_autofree char *rundir = NULL;
         mode_t old_umask;
 
         if (!(rundir = virGetUserRuntimeDirectory()))
-            goto error;
+            return -1;
 
         old_umask = umask(077);
         if (virFileMakePath(rundir) < 0) {
             umask(old_umask);
-            VIR_FREE(rundir);
-            goto error;
+            return -1;
         }
         umask(old_umask);
 
         *state_file = g_strdup_printf("%s/virtlockd-restart-exec.json", rundir);
-
-        VIR_FREE(rundir);
     }
 
     return 0;
-
- error:
-    return -1;
 }
 
 
