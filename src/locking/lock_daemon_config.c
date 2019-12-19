9dbbf056a83f26ad097379ffdd4d8c409f01b252
Fabiano Fidêncio <fidencio@redhat.com>
2019-12-19 09:42:57 +0100

    locking: Use g_autofree in virLockDaemonConfigFilePath()
    Signed-off-by: Fabiano Fidêncio <fidencio@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/locking/lock_daemon_config.c
+++ src/locking/lock_daemon_config.c
@@ -41,13 +41,12 @@ virLockDaemonConfigFilePath(bool privileged, char **configfile)
     if (privileged) {
         *configfile = g_strdup(SYSCONFDIR "/libvirt/virtlockd.conf");
     } else {
-        char *configdir = NULL;
+        g_autofree char *configdir = NULL;
 
         if (!(configdir = virGetUserConfigDirectory()))
             goto error;
 
         *configfile = g_strdup_printf("%s/virtlockd.conf", configdir);
-        VIR_FREE(configdir);
     }
 
     return 0;
