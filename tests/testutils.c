879e9db194865d70b7d66b1087a5d6903e902eab
Ján Tomko <jtomko@redhat.com>
2020-02-09 01:26:08 +0100

    testutils: check return value of g_setenv
    The function returns gboolean.
    Compare against the FALSE value from GLib.
    
    Signed-off-by: Ján Tomko <jtomko@redhat.com>
    Fixes: 2c3353242337bb50fe5abc9454fd5fc98236d4ef
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- tests/testutils.c
+++ tests/testutils.c
@@ -836,7 +836,7 @@ virTestSetEnvPath(void)
     }
 
     if (new_path &&
-        g_setenv("PATH", new_path, TRUE) < 0)
+        g_setenv("PATH", new_path, TRUE) == FALSE)
         goto cleanup;
 
     ret = 0;
