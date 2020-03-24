libvirt/libvirt

commit 93af79fba3fd75a8df6b7ca608719dd97f9511a0

util: remove unneeded cleanup labels
Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/util/virhostcpu.c
@@ -773,7 +773,6 @@ virHostCPUGetStatsLinux(FILE *procstat,
                         virNodeCPUStatsPtr params,
                         int *nparams)
 {
-    int ret = -1;
     char line[1024];
     unsigned long long usr, ni, sys, idle, iowait;
     unsigned long long irq, softirq, steal, guest, guest_nice;
@@ -782,15 +781,14 @@ virHostCPUGetStatsLinux(FILE *procstat,
     if ((*nparams) == 0) {
         /* Current number of cpu stats supported by linux */
         *nparams = LINUX_NB_CPU_STATS;
-        ret = 0;
-        goto cleanup;
+        return 0;
     }
 
     if ((*nparams) != LINUX_NB_CPU_STATS) {
         virReportInvalidArg(*nparams,
                             _("nparams in %s must be equal to %d"),
                             __FUNCTION__, LINUX_NB_CPU_STATS);
-        goto cleanup;
+        return -1;
     }
 
     if (cpuNum == VIR_NODE_CPU_STATS_ALL_CPUS) {
@@ -813,22 +811,21 @@ virHostCPUGetStatsLinux(FILE *procstat,
 
             if (virHostCPUStatsAssign(&params[0], VIR_NODE_CPU_STATS_KERNEL,
                                       (sys + irq + softirq) * TICK_TO_NSEC) < 0)
-                goto cleanup;
+                return -1;
 
             if (virHostCPUStatsAssign(&params[1], VIR_NODE_CPU_STATS_USER,
                                       (usr + ni) * TICK_TO_NSEC) < 0)
-                goto cleanup;
+                return -1;
 
             if (virHostCPUStatsAssign(&params[2], VIR_NODE_CPU_STATS_IDLE,
                                       idle * TICK_TO_NSEC) < 0)
-                goto cleanup;
+                return -1;
 
             if (virHostCPUStatsAssign(&params[3], VIR_NODE_CPU_STATS_IOWAIT,
                                       iowait * TICK_TO_NSEC) < 0)
-                goto cleanup;
+                return -1;
 
-            ret = 0;
-            goto cleanup;
+            return 0;
         }
     }
 
@@ -836,8 +833,7 @@ virHostCPUGetStatsLinux(FILE *procstat,
                         _("Invalid cpuNum in %s"),
                         __FUNCTION__);
 
- cleanup:
-    return ret;
+    return 0;
 }
 
 

src/util/vircgroupv1.c
@@ -696,7 +696,6 @@ virCgroupV1AddTask(virCgroupPtr group,
                    pid_t pid,
                    unsigned int flags)
 {
-    int ret = -1;
     size_t i;
 
     for (i = 0; i < VIR_CGROUP_CONTROLLER_LAST; i++) {
@@ -712,12 +711,10 @@ virCgroupV1AddTask(virCgroupPtr group,
             continue;
 
         if (virCgroupSetValueI64(group, i, "tasks", pid) < 0)
-            goto cleanup;
+            return -1;
     }
 
-    ret = 0;
- cleanup:
-    return ret;
+    return 0;
 }
 
 
@@ -1818,18 +1815,13 @@ static int
 virCgroupV1AllowAllDevices(virCgroupPtr group,
                            int perms)
 {
-    int ret = -1;
-
     if (virCgroupV1AllowDevice(group, 'b', -1, -1, perms) < 0)
-        goto cleanup;
+        return -1;
 
     if (virCgroupV1AllowDevice(group, 'c', -1, -1, perms) < 0)
-        goto cleanup;
+        return -1;
 
-    ret = 0;
-
- cleanup:
-    return ret;
+    return 0;
 }
 
 

src/util/vircommand.c
@@ -417,8 +417,6 @@ virCommandHandshakeChild(virCommandPtr cmd)
 static int
 virExecCommon(virCommandPtr cmd, gid_t *groups, int ngroups)
 {
-    int ret = -1;
-
     if (cmd->uid != (uid_t)-1 || cmd->gid != (gid_t)-1 ||
         cmd->capabilities || (cmd->flags & VIR_EXEC_CLEAR_CAPS)) {
         VIR_DEBUG("Setting child uid:gid to %d:%d with caps %llx",
@@ -426,7 +424,7 @@ virExecCommon(virCommandPtr cmd, gid_t *groups, int ngroups)
         if (virSetUIDGIDWithCaps(cmd->uid, cmd->gid, groups, ngroups,
                                  cmd->capabilities,
                                  !!(cmd->flags & VIR_EXEC_CLEAR_CAPS)) < 0)
-            goto cleanup;
+            return -1;
     }
 
     if (cmd->pwd) {
@@ -434,13 +432,10 @@ virExecCommon(virCommandPtr cmd, gid_t *groups, int ngroups)
         if (chdir(cmd->pwd) < 0) {
             virReportSystemError(errno,
                                  _("Unable to change to %s"), cmd->pwd);
-            goto cleanup;
+            return -1;
         }
     }
-    ret = 0;
-
- cleanup:
-    return ret;
+    return 0;
 }
 
 # ifdef __linux__

src/util/virdbus.c
@@ -1191,7 +1191,6 @@ int virDBusMessageDecodeArgs(DBusMessage* msg,
                              va_list args)
 {
     DBusMessageIter iter;
-    int ret = -1;
 
     if (!dbus_message_iter_init(msg, &iter)) {
         if (*types != '\0') {
@@ -1199,15 +1198,12 @@ int virDBusMessageDecodeArgs(DBusMessage* msg,
                            _("No args present for signature %s"),
                            types);
         } else {
-            ret = 0;
+            return 0;
         }
-        goto cleanup;
+        return -1;
     }
 
-    ret = virDBusMessageIterDecode(&iter, types, args);
-
- cleanup:
-    return ret;
+    return virDBusMessageIterDecode(&iter, types, args);
 }
 
 
@@ -1396,25 +1392,21 @@ int virDBusCreateMethodV(DBusMessage **call,
                          const char *types,
                          va_list args)
 {
-    int ret = -1;
-
     if (!(*call = dbus_message_new_method_call(destination,
                                                path,
                                                iface,
                                                member))) {
         virReportOOMError();
-        goto cleanup;
+        return -1;
     }
 
     if (virDBusMessageEncodeArgs(*call, types, args) < 0) {
         virDBusMessageUnref(*call);
         *call = NULL;
-        goto cleanup;
+        return -1;
     }
 
-    ret = 0;
- cleanup:
-    return ret;
+    return 0;
 }
 
 
@@ -1468,22 +1460,18 @@ int virDBusCreateReplyV(DBusMessage **reply,
                         const char *types,
                         va_list args)
 {
-    int ret = -1;
-
     if (!(*reply = dbus_message_new(DBUS_MESSAGE_TYPE_METHOD_RETURN))) {
         virReportOOMError();
-        goto cleanup;
+        return -1;
     }
 
     if (virDBusMessageEncodeArgs(*reply, types, args) < 0) {
         virDBusMessageUnref(*reply);
         *reply = NULL;
-        goto cleanup;
+        return -1;
     }
 
-    ret = 0;
- cleanup:
-    return ret;
+    return 0;
 }
 
 

src/util/virfile.c
@@ -3598,27 +3598,25 @@ int
 virFileGetHugepageSize(const char *path,
                        unsigned long long *size)
 {
-    int ret = -1;
     struct statfs fs;
 
     if (statfs(path, &fs) < 0) {
         virReportSystemError(errno,
                              _("cannot determine filesystem for '%s'"),
                              path);
-        goto cleanup;
+        return -1;
     }
 
     if (fs.f_type != HUGETLBFS_MAGIC) {
         virReportError(VIR_ERR_INTERNAL_ERROR,
                        _("not a hugetlbfs mount: '%s'"),
                        path);
-        goto cleanup;
+        return -1;
     }
 
     *size = fs.f_bsize / 1024; /* we are storing size in KiB */
-    ret = 0;
- cleanup:
-    return ret;
+
+    return 0;
 }
 
 # define PROC_MEMINFO "/proc/meminfo"
@@ -3790,12 +3788,11 @@ virFileSetupDev(const char *path,
 {
     const unsigned long mount_flags = MS_NOSUID;
     const char *mount_fs = "tmpfs";
-    int ret = -1;
 
     if (virFileMakePath(path) < 0) {
         virReportSystemError(errno,
                              _("Failed to make path %s"), path);
-        goto cleanup;
+        return -1;
     }
 
     VIR_DEBUG("Mount devfs on %s type=tmpfs flags=0x%lx, opts=%s",
@@ -3804,12 +3801,10 @@ virFileSetupDev(const char *path,
         virReportSystemError(errno,
                              _("Failed to mount devfs on %s type %s (%s)"),
                              path, mount_fs, mount_options);
-        goto cleanup;
+        return -1;
     }
 
-    ret = 0;
- cleanup:
-    return ret;
+    return 0;
 }
 
 

src/util/virhash.c
@@ -646,15 +646,13 @@ virHashForEach(virHashTablePtr table, virHashIterator iter, void *data)
             ret = iter(entry->payload, entry->name, data);
 
             if (ret < 0)
-                goto cleanup;
+                return ret;
 
             entry = next;
         }
     }
 
-    ret = 0;
- cleanup:
-    return ret;
+    return 0;
 }
 
 

src/util/virhostdev.c
@@ -80,7 +80,6 @@ struct virHostdevIsPCINodeDeviceUsedData {
 static int virHostdevIsPCINodeDeviceUsed(virPCIDeviceAddressPtr devAddr, void *opaque)
 {
     virPCIDevicePtr actual;
-    int ret = -1;
     struct virHostdevIsPCINodeDeviceUsedData *helperData = opaque;
 
     actual = virPCIDeviceListFindByIDs(helperData->mgr->activePCIHostdevs,
@@ -106,12 +105,10 @@ static int virHostdevIsPCINodeDeviceUsed(virPCIDeviceAddressPtr devAddr, void *o
             virReportError(VIR_ERR_OPERATION_INVALID,
                            _("PCI device %s is in use"),
                            virPCIDeviceGetName(actual));
-        goto cleanup;
+        return -1;
     }
  iommu_owner:
-    ret = 0;
- cleanup:
-    return ret;
+    return 0;
 }
 
 static int virHostdevManagerOnceInit(void)

src/util/virhostmem.c
@@ -142,7 +142,6 @@ virHostMemGetStatsLinux(FILE *meminfo,
                         virNodeMemoryStatsPtr params,
                         int *nparams)
 {
-    int ret = -1;
     size_t i = 0, j = 0, k = 0;
     int found = 0;
     int nr_param;
@@ -169,15 +168,14 @@ virHostMemGetStatsLinux(FILE *meminfo,
     if ((*nparams) == 0) {
         /* Current number of memory stats supported by linux */
         *nparams = nr_param;
-        ret = 0;
-        goto cleanup;
+        return 0;
     }
 
     if ((*nparams) != nr_param) {
         virReportInvalidArg(nparams,
                             _("nparams in %s must be %d"),
                             __FUNCTION__, nr_param);
-        goto cleanup;
+        return -1;
     }
 
     while (fgets(line, sizeof(line), meminfo) != NULL) {
@@ -200,7 +198,7 @@ virHostMemGetStatsLinux(FILE *meminfo,
                 if (p == NULL) {
                     virReportError(VIR_ERR_INTERNAL_ERROR,
                                    "%s", _("no prefix found"));
-                    goto cleanup;
+                    return -1;
                 }
                 p++;
             }
@@ -219,7 +217,7 @@ virHostMemGetStatsLinux(FILE *meminfo,
                 if (virStrcpyStatic(param->field, convp->field) < 0) {
                     virReportError(VIR_ERR_INTERNAL_ERROR,
                                    "%s", _("Field kernel memory too long for destination"));
-                    goto cleanup;
+                    return -1;
                 }
                 param->value = val;
                 found++;
@@ -233,13 +231,10 @@ virHostMemGetStatsLinux(FILE *meminfo,
     if (found == 0) {
         virReportError(VIR_ERR_INTERNAL_ERROR,
                        "%s", _("no available memory line found"));
-        goto cleanup;
+        return -1;
     }
 
-    ret = 0;
-
- cleanup:
-    return ret;
+    return 0;
 }
 #endif
 
@@ -611,14 +606,12 @@ static int
 virHostMemGetInfoFake(unsigned long long *mem,
                       unsigned long long *freeMem)
 {
-    int ret = -1;
-
     if (mem) {
         double total = physmem_total();
         if (!total) {
             virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                            _("Cannot determine free memory"));
-            goto cleanup;
+            return -1;
         }
 
         *mem = (unsigned long long) total;
@@ -630,15 +623,13 @@ virHostMemGetInfoFake(unsigned long long *mem,
         if (!avail) {
             virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                            _("Cannot determine free memory"));
-            goto cleanup;
+            return -1;
         }
 
         *freeMem = (unsigned long long) avail;
     }
 
-    ret = 0;
- cleanup:
-    return ret;
+    return 0;
 }
 
 
@@ -649,7 +640,6 @@ virHostMemGetCellsFree(unsigned long long *freeMems,
 {
     unsigned long long mem;
     int n, lastCell, numCells;
-    int ret = -1;
     int maxCell;
 
     if (!virNumaIsAvailable())
@@ -663,7 +653,7 @@ virHostMemGetCellsFree(unsigned long long *freeMems,
         virReportError(VIR_ERR_INTERNAL_ERROR,
                        _("start cell %d out of range (0-%d)"),
                        startCell, maxCell);
-        goto cleanup;
+        return -1;
     }
     lastCell = startCell + maxCells - 1;
     if (lastCell > maxCell)
@@ -674,10 +664,7 @@ virHostMemGetCellsFree(unsigned long long *freeMems,
 
         freeMems[numCells++] = mem;
     }
-    ret = numCells;
-
- cleanup:
-    return ret;
+    return numCells;
 }
 
 int
@@ -725,7 +712,6 @@ virHostMemGetFreePages(unsigned int npages,
                        unsigned int cellCount,
                        unsigned long long *counts)
 {
-    int ret = -1;
     int cell, lastCell;
     size_t i, ncounts = 0;
 
@@ -736,7 +722,7 @@ virHostMemGetFreePages(unsigned int npages,
         virReportError(VIR_ERR_INTERNAL_ERROR,
                        _("start cell %d out of range (0-%d)"),
                        startCell, lastCell);
-        goto cleanup;
+        return -1;
     }
 
     lastCell = MIN(lastCell, startCell + (int) cellCount - 1);
@@ -747,7 +733,7 @@ virHostMemGetFreePages(unsigned int npages,
             unsigned long long page_free;
 
             if (virNumaGetPageInfo(cell, page_size, 0, NULL, &page_free) < 0)
-                goto cleanup;
+                return -1;
 
             counts[ncounts++] = page_free;
         }
@@ -756,12 +742,10 @@ virHostMemGetFreePages(unsigned int npages,
     if (!ncounts) {
         virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                        _("no suitable info found"));
-        goto cleanup;
+        return -1;
     }
 
-    ret = ncounts;
- cleanup:
-    return ret;
+    return ncounts;
 }
 
 int
@@ -772,7 +756,6 @@ virHostMemAllocPages(unsigned int npages,
                      unsigned int cellCount,
                      bool add)
 {
-    int ret = -1;
     int cell, lastCell;
     size_t i, ncounts = 0;
 
@@ -783,7 +766,7 @@ virHostMemAllocPages(unsigned int npages,
         virReportError(VIR_ERR_INTERNAL_ERROR,
                        _("start cell %d out of range (0-%d)"),
                        startCell, lastCell);
-        goto cleanup;
+        return -1;
     }
 
     lastCell = MIN(lastCell, startCell + (int) cellCount - 1);
@@ -794,13 +777,11 @@ virHostMemAllocPages(unsigned int npages,
             unsigned long long page_count = pageCounts[i];
 
             if (virNumaSetPagePoolSize(cell, page_size, page_count, add) < 0)
-                goto cleanup;
+                return -1;
 
             ncounts++;
         }
     }
 
-    ret = ncounts;
- cleanup:
-    return ret;
+    return ncounts;
 }

commit 75a4ec42f70b5324f95d7ffbbfbf7457620735e4

util: virhostcpu: Fail when fetching CPU Stats for invalid cpu
virHostCPUGetStatsLinux walks through every cpu in /proc/stat until it
finds cpu%cpuNum that matches with the requested cpu.
If none is found it logs the error but it should return -1, instead of 0.
Otherwise virsh nodecpustats --cpu <invalid cpu number> and API bindings
don't fail properly, printing a blank line instead of an error message.

This patch also includes an additional test for virhostcputest to avoid
this regression to happen again in the future.

Fixes: 93af79fba3fd75a8df6b7ca608719dd97f9511a0
Reported-by: Satheesh Rajendran <satheera@in.ibm.com>
Signed-off-by: Mauro S. M. Rodrigues <maurosr@linux.vnet.ibm.com>
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>
Tested-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

src/util/virhostcpu.c
@@ -852,7 +852,7 @@ virHostCPUGetStatsLinux(FILE *procstat,
                         _("Invalid cpuNum in %s"),
                         __FUNCTION__);
 
-    return 0;
+    return -1;
 }
 
 

tests/virhostcputest.c
@@ -196,6 +196,7 @@ linuxTestHostCPU(const void *opaque)
 struct nodeCPUStatsData {
     const char *name;
     int ncpus;
+    bool shouldFail;
 };
 
 static int
@@ -214,6 +215,19 @@ linuxTestNodeCPUStats(const void *data)
     result = linuxCPUStatsCompareFiles(cpustatfile,
                                        testData->ncpus,
                                        outfile);
+    if (result < 0) {
+        if (testData->shouldFail) {
+            /* Expected error */
+            result = 0;
+        }
+    } else {
+        if (testData->shouldFail) {
+            fprintf(stderr, "Expected a failure, got success");
+            result = -1;
+        }
+    }
+
+
     VIR_FREE(cpustatfile);
     VIR_FREE(outfile);
     return result;
@@ -258,14 +272,15 @@ mymain(void)
         if (virTestRun(nodeData[i].testName, linuxTestHostCPU, &nodeData[i]) != 0)
             ret = -1;
 
-# define DO_TEST_CPU_STATS(name, ncpus) \
+# define DO_TEST_CPU_STATS(name, ncpus, shouldFail) \
     do { \
-        static struct nodeCPUStatsData data = { name, ncpus }; \
+        static struct nodeCPUStatsData data = { name, ncpus, shouldFail}; \
         if (virTestRun("CPU stats " name, linuxTestNodeCPUStats, &data) < 0) \
             ret = -1; \
     } while (0)
 
-    DO_TEST_CPU_STATS("24cpu", 24);
+    DO_TEST_CPU_STATS("24cpu", 24, false);
+    DO_TEST_CPU_STATS("24cpu", 25, true);
 
     return ret == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }