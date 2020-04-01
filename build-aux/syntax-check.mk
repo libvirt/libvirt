b659b5f93ae14644fa8441eeeec9c9e0fbde40d1
Peter Krempa <pkrempa@redhat.com>
2020-04-01 12:24:34 +0200

    syntax-check: Exempt tests/qemucapabilitiesdata from 'sc_prohibit_devname'
    QEMU uses the 'devname' string in the QAPI schema so a bump would
    trigger this check. Exempt all of the capabilities data from the check.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- build-aux/syntax-check.mk
+++ build-aux/syntax-check.mk
@@ -2102,7 +2102,7 @@ exclude_file_name_regexp--sc_prohibit_useless_translation = \
   ^tests/virpolkittest.c
 
 exclude_file_name_regexp--sc_prohibit_devname = \
-  ^(tools/virsh.pod|build-aux/syntax-check\.mk|docs/.*)$$
+  ^(tools/virsh.pod|build-aux/syntax-check\.mk|docs/.*|tests/qemucapabilitiesdata/.*)$$
 
 exclude_file_name_regexp--sc_prohibit_virXXXFree = \
   ^(docs/|tests/|examples/|tools/|build-aux/syntax-check\.mk|src/test/test_driver.c|src/libvirt_public.syms|include/libvirt/libvirt-(domain|network|nodedev|storage|stream|secret|nwfilter|interface|domain-snapshot).h|src/libvirt-(domain|qemu|network|nodedev|storage|stream|secret|nwfilter|interface|domain-snapshot).c|src/qemu/qemu_shim.c$$)
