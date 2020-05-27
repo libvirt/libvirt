c2f15f1b1869c1732e529967d1851582409290fb
Michal Privoznik <mprivozn@redhat.com>
2020-05-27 11:00:07 +0200

    qemu: Introduce QEMU_CAPS_NUMA_HMAT capability
    This capability tracks whether QEMU is capable of defining HMAT
    ACPI table for the guest.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>

--- src/qemu/qemu_capabilities.c
+++ src/qemu/qemu_capabilities.c
@@ -595,6 +595,7 @@ VIR_ENUM_IMPL(virQEMUCaps,
               "migration-param.xbzrle-cache-size",
               "intel-iommu.aw-bits",
               "spapr-tpm-proxy",
+              "numa.hmat",
     );
 
 
@@ -1522,6 +1523,7 @@ static struct virQEMUCapsStringFlags virQEMUCapsQMPSchemaQueries[] = {
     { "migrate-set-parameters/arg-type/max-bandwidth", QEMU_CAPS_MIGRATION_PARAM_BANDWIDTH },
     { "migrate-set-parameters/arg-type/downtime-limit", QEMU_CAPS_MIGRATION_PARAM_DOWNTIME },
     { "migrate-set-parameters/arg-type/xbzrle-cache-size", QEMU_CAPS_MIGRATION_PARAM_XBZRLE_CACHE_SIZE },
+    { "set-numa-node/arg-type/+hmat-lb", QEMU_CAPS_NUMA_HMAT },
 };
 
 typedef struct _virQEMUCapsObjectTypeProps virQEMUCapsObjectTypeProps;
