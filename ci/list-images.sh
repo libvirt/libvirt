2250a0b56f12e30d67210fd49d2123014bc73d2c
Andrea Bolognani <abologna@redhat.com>
2020-06-02 17:28:58 +0200

    ci: Update build system integration
    The ci-* targets need to know where our container images are stored
    and how they are called to work, so now that we use the GitLab
    container registry instead of Quay some changes are necessary.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- ci/list-images.sh
+++ ci/list-images.sh
@@ -1,26 +1,14 @@
 #!/bin/sh
 
-engine="$1"
-prefix="$2"
+prefix="${1##registry.gitlab.com/}"
 
-do_podman() {
-    # Podman freaks out if the search term ends with a dash, which ours
-    # by default does, so let's strip it. The repository name is the
-    # second field in the output, and it already starts with the registry
-    podman search --limit 100 "${prefix%-}" | while read _ repo _; do
-        echo "$repo"
-    done
+PROJECT_ID=192693
+
+all_repos() {
+  curl -s "https://gitlab.com/api/v4/projects/$PROJECT_ID/registry/repositories?per_page=100" \
+    | tr , '\n' | grep '"path":' | sed 's,"path":",,g;s,"$,,g'
 }
 
-do_docker() {
-    # Docker doesn't include the registry name in the output, so we have
-    # to add it. The repository name is the first field in the output
-    registry="${prefix%%/*}"
-    docker search --limit 100 "$prefix" | while read repo _; do
-        echo "$registry/$repo"
-    done
-}
-
-"do_$engine" | grep "^$prefix" | sed "s,^$prefix,,g" | while read repo; do
+all_repos | grep "^$prefix" | sed "s,^$prefix,,g" | while read repo; do
     echo "    $repo"
 done | sort -u
