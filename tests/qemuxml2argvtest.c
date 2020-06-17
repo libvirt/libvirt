079305296204384775d3f138061c5b458ee67859
Peter Krempa <pkrempa@redhat.com>
2020-06-17 15:42:54 +0200

    qemuxml2argvtest: hostdev-scsi-virtio-scsi: Modernize to qemu-4.1
    Modernize the current state to the pre-blockdev version of qemu to
    minimize changes. Later patch will add a 'latest' case too.
    
    Additionally this removes duplicated call of the same test.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -2659,14 +2659,9 @@ mymain(void)
     DO_TEST_CAPS_VER("hostdev-scsi-lsi", "2.8.0");
     DO_TEST_CAPS_VER("hostdev-scsi-lsi", "4.1.0");
     DO_TEST_CAPS_LATEST("hostdev-scsi-lsi");
+    DO_TEST_CAPS_VER("hostdev-scsi-virtio-scsi", "4.1.0");
 
-    DO_TEST("hostdev-scsi-virtio-scsi",
-            QEMU_CAPS_VIRTIO_SCSI,
-            QEMU_CAPS_SCSI_LSI);
     DO_TEST("hostdev-scsi-readonly",
-            QEMU_CAPS_VIRTIO_SCSI,
-            QEMU_CAPS_SCSI_LSI);
-    DO_TEST("hostdev-scsi-virtio-scsi",
             QEMU_CAPS_VIRTIO_SCSI,
             QEMU_CAPS_SCSI_LSI);
     DO_TEST("hostdev-scsi-virtio-iscsi",
