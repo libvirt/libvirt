1a231590d3469989272f1dc35165dda38ad5d0bb
Pavel Hrdina <phrdina@redhat.com>
2020-06-17 21:41:28 +0200

    meson: move content from config-post.h to config.h
    With meson we generate meson-config.h into the build directory and
    include it in config.h so there is no need to have separate
    config-post.h file.
    
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Neal Gompa <ngompa13@gmail.com>

--- config.h
+++ config.h
@@ -1,1 +1,42 @@
 #include <meson-config.h>
+
+#ifndef __GNUC__
+# error "Libvirt requires GCC >= 4.8, or CLang"
+#endif
+
+/*
+ * Define __GNUC_PREREQ to a sane default if it isn't yet defined.
+ * This is done here so that it's included as early as possible;
+ */
+#ifndef __GNUC_PREREQ
+# define __GNUC_PREREQ(maj, min) \
+    ((__GNUC__ << 16) + __GNUC_MINOR__ >= ((maj) << 16) + (min))
+#endif
+
+#if defined(__clang_major__) && defined(__clang_minor__)
+# ifdef __apple_build_version__
+#  if __clang_major__ < 5 || (__clang_major__ == 5 && __clang_minor__ < 1)
+#   error You need at least XCode Clang v5.1 to compile QEMU
+#  endif
+# else
+#  if __clang_major__ < 3 || (__clang_major__ == 3 && __clang_minor__ < 4)
+#   error You need at least Clang v3.4 to compile QEMU
+#  endif
+# endif
+#elif defined(__GNUC__) && defined(__GNUC_MINOR__)
+# if __GNUC__ < 4 || (__GNUC__ == 4 && __GNUC_MINOR__ < 8)
+#  error You need at least GCC v4.8 to compile QEMU
+# endif
+#else
+# error You either need at least GCC 4.8 or Clang 3.4 or XCode Clang 5.1 to compile libvirt
+#endif
+
+/* Ask for warnings for anything that was marked deprecated in
+ * the defined version, or before. It is a candidate for rewrite.
+ */
+#define GLIB_VERSION_MIN_REQUIRED GLIB_VERSION_2_48
+
+/* Ask for warnings if code tries to use function that did not
+ * exist in the defined version. These risk breaking builds
+ */
+#define GLIB_VERSION_MAX_ALLOWED GLIB_VERSION_2_48
