aef2c5ea6f04e765170565a77a0fdc5fd6a3ea47
Peter Krempa <pkrempa@redhat.com>
2020-06-17 15:51:57 +0200

    qemuxml2argvtest: hostdev-scsi-virtio-scsi: Add "latest" caps version
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -2660,6 +2660,7 @@ mymain(void)
     DO_TEST_CAPS_VER("hostdev-scsi-lsi", "4.1.0");
     DO_TEST_CAPS_LATEST("hostdev-scsi-lsi");
     DO_TEST_CAPS_VER("hostdev-scsi-virtio-scsi", "4.1.0");
+    DO_TEST_CAPS_LATEST("hostdev-scsi-virtio-scsi");
 
     DO_TEST("hostdev-scsi-readonly",
             QEMU_CAPS_VIRTIO_SCSI,
