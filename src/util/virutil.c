852ee1950aee5f31c9656b30c5fe9124f734c38c
Laine Stump <laine@redhat.com>
2020-06-18 22:33:28 -0400

    util: remove OOM error log from virGetHostnameImpl()
    The strings allocated in virGetHostnameImpl() are all allocated via
    g_strdup(), which will exit on OOM anyway, so the call to
    virReportOOMError() is redundant, and removing it allows slight
    modification to the code, in particular the cleanup label can be
    eliminated.
    
    Signed-off-by: Laine Stump <laine@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/util/virutil.c
+++ src/util/virutil.c
@@ -503,8 +503,7 @@ virGetHostnameImpl(bool quiet)
          * string as-is; it's up to callers to check whether "localhost"
          * is allowed.
          */
-        result = g_strdup(hostname);
-        goto cleanup;
+        return g_strdup(hostname);
     }
 
     /* otherwise, it's a shortened, non-localhost, hostname.  Attempt to
@@ -519,8 +518,7 @@ virGetHostnameImpl(bool quiet)
         if (!quiet)
             VIR_WARN("getaddrinfo failed for '%s': %s",
                      hostname, gai_strerror(r));
-        result = g_strdup(hostname);
-        goto cleanup;
+        return g_strdup(hostname);
     }
 
     /* Tell static analyzers about getaddrinfo semantics.  */
@@ -538,10 +536,6 @@ virGetHostnameImpl(bool quiet)
         result = g_strdup(info->ai_canonname);
 
     freeaddrinfo(info);
-
- cleanup:
-    if (!result)
-        virReportOOMError();
     return result;
 }
 
