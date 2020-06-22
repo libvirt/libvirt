f57a8cd3df0167d72b87fdd868a287608a741b73
Han Han <hhan@redhat.com>
2020-06-22 10:51:10 +0800

    virsh: Fix typo of metadata
    Signed-off-by: Han Han <hhan@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tools/virsh-domain.c
+++ tools/virsh-domain.c
@@ -13559,7 +13559,7 @@ virshEventMetadataChangePrint(virConnectPtr conn G_GNUC_UNUSED,
 {
     virBuffer buf = VIR_BUFFER_INITIALIZER;
 
-    virBufferAsprintf(&buf, _("event 'metdata-change' for domain %s: %s %s\n"),
+    virBufferAsprintf(&buf, _("event 'metadata-change' for domain %s: %s %s\n"),
                       virDomainGetName(dom),
                       UNKNOWNSTR(virshEventMetadataChangeTypeTypeToString(type)),
                       NULLSTR(nsuri));
