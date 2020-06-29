c98eac9c59a01d69726cd167b1d016d5ac826d99
Pavel Hrdina <phrdina@redhat.com>
2020-06-29 22:14:13 +0200

    meson: docs: generate docs timestamp
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
    Reviewed-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Neal Gompa <ngompa13@gmail.com>

---
+++ scripts/meson-timestamp.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+import os
+
+from datetime import datetime, timezone
+
+timestamp = os.environ.get('SOURCE_DATE_EPOCH', None)
+timeformat = '%c %Z'
+
+if timestamp:
+    print(datetime.fromtimestamp(int(timestamp), tz=timezone.utc).strftime(timeformat))
+else:
+    print(datetime.now(tz=timezone.utc).strftime(timeformat))
