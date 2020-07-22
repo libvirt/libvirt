libvirt/libvirt

commit 4c0398b5284d14c55eca51095673b6fadbbd85fb

qemu_process: fix starting VMs if machine group has limited cpuset.cpus
Commit <f136b83139c63f20de0df3285d9e82df2fb97bfc> reworked process
affinity setting but did not take cgroups into account which introduced
an issue when starting VM with custom cpuset.cpus for the whole machine
group.

If the machine group is limited to some pCPUs libvirt should not try to
set a VM to run on all pCPUs as it will result in permission denied when
writing to cpuset.cpus.

To fix this the affinity has to be set separately from cgroups cpuset.

Resolves: <https://bugzilla.redhat.com/show_bug.cgi?id=1746517>

Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/qemu/qemu_process.c
@@ -2622,6 +2622,7 @@ qemuProcessSetupPid(virDomainObjPtr vm,
     virDomainNumatuneMemMode mem_mode;
     virCgroupPtr cgroup = NULL;
     virBitmapPtr use_cpumask = NULL;
+    virBitmapPtr afinity_cpumask = NULL;
     g_autoptr(virBitmap) hostcpumap = NULL;
     char *mem_mask = NULL;
     int ret = -1;
@@ -2647,7 +2648,7 @@ qemuProcessSetupPid(virDomainObjPtr vm,
          * its config file */
         if (qemuProcessGetAllCpuAffinity(&hostcpumap) < 0)
             goto cleanup;
-        use_cpumask = hostcpumap;
+        afinity_cpumask = hostcpumap;
     }
 
     /*
@@ -2688,8 +2689,11 @@ qemuProcessSetupPid(virDomainObjPtr vm,
 
     }
 
+    if (!afinity_cpumask)
+        afinity_cpumask = use_cpumask;
+
     /* Setup legacy affinity. */
-    if (use_cpumask && virProcessSetAffinity(pid, use_cpumask) < 0)
+    if (afinity_cpumask && virProcessSetAffinity(pid, afinity_cpumask) < 0)
         goto cleanup;
 
     /* Set scheduler type and priority, but not for the main thread. */

commit 6c7ba7b496374c5d7d4fb925f38415f087234641

qemu: Fix affinity typo
Fixes: 4c0398b5284d14c55eca51095673b6fadbbd85fb
Signed-off-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_process.c
@@ -2653,7 +2653,7 @@ qemuProcessSetupPid(virDomainObjPtr vm,
     virDomainNumatuneMemMode mem_mode;
     virCgroupPtr cgroup = NULL;
     virBitmapPtr use_cpumask = NULL;
-    virBitmapPtr afinity_cpumask = NULL;
+    virBitmapPtr affinity_cpumask = NULL;
     g_autoptr(virBitmap) hostcpumap = NULL;
     g_autofree char *mem_mask = NULL;
     int ret = -1;
@@ -2679,7 +2679,7 @@ qemuProcessSetupPid(virDomainObjPtr vm,
          * its config file */
         if (qemuProcessGetAllCpuAffinity(&hostcpumap) < 0)
             goto cleanup;
-        afinity_cpumask = hostcpumap;
+        affinity_cpumask = hostcpumap;
     }
 
     /*
@@ -2720,11 +2720,11 @@ qemuProcessSetupPid(virDomainObjPtr vm,
 
     }
 
-    if (!afinity_cpumask)
-        afinity_cpumask = use_cpumask;
+    if (!affinity_cpumask)
+        affinity_cpumask = use_cpumask;
 
     /* Setup legacy affinity. */
-    if (afinity_cpumask && virProcessSetAffinity(pid, afinity_cpumask) < 0)
+    if (affinity_cpumask && virProcessSetAffinity(pid, affinity_cpumask) < 0)
         goto cleanup;
 
     /* Set scheduler type and priority, but not for the main thread. */