c957791859411fba5cea59719386017b4bdfebf2
Pavel Hrdina <phrdina@redhat.com>
2020-07-23 13:00:08 +0200

    meson: src/network: install default network xml
    Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
    Reviewed-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Neal Gompa <ngompa13@gmail.com>

---
+++ scripts/meson-install-symlink.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+destdir = os.environ.get('DESTDIR', os.sep)
+dirname = sys.argv[1]
+target = sys.argv[2]
+link = sys.argv[3]
+
+workdir = os.path.join(destdir, dirname.strip(os.sep))
+
+os.makedirs(workdir, exist_ok=True)
+os.chdir(workdir)
+os.symlink(target, link)
