bde9e2c6c018f79d8e3b0bd30ea7aa80dcc9e435
Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
2020-09-24 15:54:24 +0300

    logging: fix endless loop on EOF
    On EOF condition we always have POLLHUP event and read returns
    0 thus we never break loop in virLogHandlerDomainLogFileDrain.
    
    Signed-off-by: Nikolay Shirokovskiy <nshirokovskiy@virtuozzo.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- src/logging/log_handler.c
+++ src/logging/log_handler.c
@@ -461,6 +461,8 @@ virLogHandlerDomainLogFileDrain(virLogHandlerLogFilePtr file)
             if (errno == EINTR)
                 continue;
             return;
+        } else if (len == 0) {
+            return;
         }
 
         if (virRotatingFileWriterAppend(file->file, buf, len) != len)
