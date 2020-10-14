917d8a539041e6708b0fb97b42463976c6965b2e
Peter Krempa <pkrempa@redhat.com>
2020-10-14 11:36:02 +0200

    qemumonitorjsontest: Allow deprecation of 'nbd-server-add' QMP command
    qemu is going to deprecate this command in the next release. Allow this
    as later patches will implement the use of replacement.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemumonitorjsontest.c
+++ tests/qemumonitorjsontest.c
@@ -3276,7 +3276,7 @@ mymain(void)
     DO_TEST_GEN(qemuMonitorJSONDrivePivot);
     DO_TEST_GEN(qemuMonitorJSONScreendump);
     DO_TEST_GEN(qemuMonitorJSONOpenGraphics);
-    DO_TEST_GEN(qemuMonitorJSONNBDServerAdd);
+    DO_TEST_GEN_DEPRECATED(qemuMonitorJSONNBDServerAdd, false);
     DO_TEST_GEN(qemuMonitorJSONDetachCharDev);
     DO_TEST_GEN(qemuMonitorJSONBlockdevTrayOpen);
     DO_TEST_GEN(qemuMonitorJSONBlockdevTrayClose);
