8e9598dcad7e6ac0540704ea6f77c2ecceaf4fd7
Barrett Schonefeld <bschoney@utexas.edu>
2020-11-23 16:09:35 -0600

    util: log: convert pointers to use g_autofree
    Signed-off-by: Barrett Schonefeld <bschoney@utexas.edu>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Signed-off-by: Ján Tomko <jtomko@redhat.com>

--- src/util/virlog.c
+++ src/util/virlog.c
@@ -505,8 +505,8 @@ virLogVMessage(virLogSourcePtr source,
                va_list vargs)
 {
     static bool logInitMessageStderr = true;
-    char *str = NULL;
-    char *msg = NULL;
+    g_autofree char *str = NULL;
+    g_autofree char *msg = NULL;
     char timestamp[VIR_TIME_STRING_BUFLEN];
     size_t i;
     int saved_errno = errno;
@@ -603,8 +603,6 @@ virLogVMessage(virLogSourcePtr source,
     virLogUnlock();
 
  cleanup:
-    VIR_FREE(str);
-    VIR_FREE(msg);
     errno = saved_errno;
 }
 
