cafbc6d1d26ceb7de4bad56c18964ba085feeffa
Daniel P. Berrangé <berrange@redhat.com>
2020-12-08 09:35:15 +0000

    util: add missing FSF copyright statement
    We previous added code for passing FDs which was explicitly derived from
    gnulib's passfd code:
    
      commit 17460825f3c78e1635f2beb0165c5a19e3b09f7d
      Author: Daniel P. Berrangé <berrange@redhat.com>
      Date:   Fri Jan 17 11:57:17 2020 +0000
    
        src: implement APIs for passing FDs over UNIX sockets
    
        This is a simplified variant of gnulib's passfd module
        without the portability code that we do not require.
    
    while the license was unchanged, we mistakenly failed to copy the FSF
    copyright header which is required by the license terms.
    
    Reported-by: Bruno Haible <bruno@clisp.org>
    Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

--- src/util/virsocket.c
+++ src/util/virsocket.c
@@ -1,5 +1,6 @@
 /*
  * Copyright (C) 2020 Red Hat, Inc.
+ * Copyright (C) 2011-2020 Free Software Foundation, Inc.
  *
  * This library is free software; you can redistribute it and/or
  * modify it under the terms of the GNU Lesser General Public
