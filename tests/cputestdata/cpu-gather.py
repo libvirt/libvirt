d2009088448f22566e44722fe4cf2675411c6a65
Tim Wiederhake <twiederh@redhat.com>
2020-12-15 17:24:50 +0100

    cpu-gather: Allow overwriting model name
    Some hardware, e.g. exotic platforms or pre-production hardware, may
    report wrong or random data for the cpu model name. As the name of
    the created files is derived from that name, this may lead to issues.
    
    Signed-off-by: Tim Wiederhake <twiederh@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/cputestdata/cpu-gather.py
+++ tests/cputestdata/cpu-gather.py
@@ -1,20 +1,33 @@
 #!/usr/bin/env python3
 
+import argparse
 import os
 import subprocess
 
 
-def gather_name():
+def gather_name(args):
+    if args.name:
+        return args.name
+
     with open("/proc/cpuinfo", "rt") as f:
         for line in f.readlines():
             if line.startswith("model name"):
                 return line.split(":", 2)[1].strip()
 
-    exit("Error: '/proc/cpuinfo' does not contain a model name.")
+    exit("Error: '/proc/cpuinfo' does not contain a model name.\n"
+         "Use '--model' to set a model name.")
 
 
 def main():
-    name = gather_name()
+    parser = argparse.ArgumentParser(description="Gather cpu test data")
+    parser.add_argument(
+        "--name",
+        help="CPU model name. "
+        "If unset, model name is read from '/proc/cpuinfo'.")
+
+    args = parser.parse_args()
+
+    name = gather_name(args)
     print("model name\t: {}".format(name))
 
     print(end="", flush=True)
