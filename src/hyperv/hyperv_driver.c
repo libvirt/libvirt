8a0c80ab23165816a63c5fa182e6d7649be0ecbd
Matt Coleman <mcoleman@datto.com>
2021-01-21 13:51:12 -0500

    hyperv: use g_autoptr for Msvm_ComputerSystem in hypervConnectNumOfDefinedDomains
    Signed-off-by: Matt Coleman <matt@datto.com>
    Reviewed-by: Laine Stump <laine@redhat.com>

--- src/hyperv/hyperv_driver.c
+++ src/hyperv/hyperv_driver.c
@@ -2291,26 +2291,20 @@ hypervConnectListDefinedDomains(virConnectPtr conn, char **const names, int maxn
 static int
 hypervConnectNumOfDefinedDomains(virConnectPtr conn)
 {
-    bool success = false;
     hypervPrivate *priv = conn->privateData;
-    Msvm_ComputerSystem *computerSystemList = NULL;
+    g_autoptr(Msvm_ComputerSystem) computerSystemList = NULL;
     Msvm_ComputerSystem *computerSystem = NULL;
     int count = 0;
 
     if (hypervGetInactiveVirtualSystemList(priv, &computerSystemList) < 0)
-        goto cleanup;
+        return -1;
 
     for (computerSystem = computerSystemList; computerSystem != NULL;
          computerSystem = computerSystem->next) {
         ++count;
     }
 
-    success = true;
-
- cleanup:
-    hypervFreeObject((hypervObject *)computerSystemList);
-
-    return success ? count : -1;
+    return count;
 }
 
 
