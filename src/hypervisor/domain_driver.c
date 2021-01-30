fc71d914287c22af2adb1fd6e11000d2a543917b
Daniel Henrique Barboza <danielhb413@gmail.com>
2021-01-30 13:57:04 -0300

    domain_driver.c: use g_auto* in virDomainDriverNodeDeviceReAttach()
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Signed-off-by: Daniel Henrique Barboza <danielhb413@gmail.com>

--- src/hypervisor/domain_driver.c
+++ src/hypervisor/domain_driver.c
@@ -420,50 +420,42 @@ int
 virDomainDriverNodeDeviceReAttach(virNodeDevicePtr dev,
                                   virHostdevManagerPtr hostdevMgr)
 {
-    virPCIDevicePtr pci = NULL;
+    g_autoptr(virPCIDevice) pci = NULL;
     virPCIDeviceAddress devAddr;
-    int ret = -1;
-    virNodeDeviceDefPtr def = NULL;
+    g_autoptr(virNodeDeviceDef) def = NULL;
     g_autofree char *xml = NULL;
-    virConnectPtr nodeconn = NULL;
-    virNodeDevicePtr nodedev = NULL;
+    g_autoptr(virConnect) nodeconn = NULL;
+    g_autoptr(virNodeDevice) nodedev = NULL;
 
     if (!(nodeconn = virGetConnectNodeDev()))
-        goto cleanup;
+        return -1;
 
     /* 'dev' is associated with virConnectPtr, so for split
      * daemons, we need to get a copy that is associated with
      * the virnodedevd daemon. */
     if (!(nodedev = virNodeDeviceLookupByName(
               nodeconn, virNodeDeviceGetName(dev))))
-        goto cleanup;
+        return -1;
 
     xml = virNodeDeviceGetXMLDesc(nodedev, 0);
     if (!xml)
-        goto cleanup;
+        return -1;
 
     def = virNodeDeviceDefParseString(xml, EXISTING_DEVICE, NULL);
     if (!def)
-        goto cleanup;
+        return -1;
 
     /* ACL check must happen against original 'dev',
      * not the new 'nodedev' we acquired */
     if (virNodeDeviceReAttachEnsureACL(dev->conn, def) < 0)
-        goto cleanup;
+        return -1;
 
     if (virDomainDriverNodeDeviceGetPCIInfo(def, &devAddr) < 0)
-        goto cleanup;
+        return -1;
 
     pci = virPCIDeviceNew(&devAddr);
     if (!pci)
-        goto cleanup;
+        return -1;
 
-    ret = virHostdevPCINodeDeviceReAttach(hostdevMgr, pci);
-
-    virPCIDeviceFree(pci);
- cleanup:
-    virNodeDeviceDefFree(def);
-    virObjectUnref(nodedev);
-    virObjectUnref(nodeconn);
-    return ret;
+    return virHostdevPCINodeDeviceReAttach(hostdevMgr, pci);
 }
