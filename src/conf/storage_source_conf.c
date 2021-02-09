676e56e515bb58bc0ee69d26537f6850c740be81
Michal Privoznik <mprivozn@redhat.com>
2021-02-09 20:16:03 +0100

    virStorageSourceClear: Unref @vhostuser
    The @vhostuser member of virStorageSource structure is allocated
    during parsing in virDomainDiskSourceVHostUserParse() but never
    freed leading to a memleak. Since the member is an object it has
    to be unrefed instead of g_free()-d.
    
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

--- src/conf/storage_source_conf.c
+++ src/conf/storage_source_conf.c
@@ -1125,6 +1125,7 @@ virStorageSourceClear(virStorageSourcePtr def)
     virStorageEncryptionFree(def->encryption);
     virStoragePRDefFree(def->pr);
     virStorageSourceNVMeDefFree(def->nvme);
+    virObjectUnref(def->vhostuser);
     virStorageSourceSeclabelsClear(def);
     virStoragePermsFree(def->perms);
     VIR_FREE(def->timestamps);
