33122ed16fd6f7191df9d5f8f5698eb0f6297530
Peter Krempa <pkrempa@redhat.com>
2021-03-02 12:12:11 +0100

    sanlock: Use virStrcpy instead of virStrncpy
    We want a (possibly truncated) copy of the full source string so
    virStrcpy is a better fit.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/locking/lock_driver_sanlock.c
+++ src/locking/lock_driver_sanlock.c
@@ -924,9 +924,7 @@ static int virLockManagerSanlockAcquire(virLockManagerPtr lock,
 
     /* sanlock doesn't use owner_name for anything, so it's safe to take just
      * the first SANLK_NAME_LEN - 1 characters from vm_name */
-    ignore_value(virStrncpy(opt->owner_name, priv->vm_name,
-                            MIN(strlen(priv->vm_name), SANLK_NAME_LEN - 1),
-                            SANLK_NAME_LEN));
+    virStrcpy(opt->owner_name, priv->vm_name, SANLK_NAME_LEN);
 
     if (state && STRNEQ(state, "")) {
         if ((rv = sanlock_state_to_args((char *)state,
