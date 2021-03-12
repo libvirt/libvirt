909c07350b59decfa2c79b8ce4f29a6c76b7154a
Peter Krempa <pkrempa@redhat.com>
2021-03-12 17:42:14 +0100

    qemuxml2argvtest: Add DO_CAPS_LATEST variant of 'numatune-memnode'
    The test has interesting config of the memory backend object. Preserve
    the 5.2 output too since it's prior to JSONification.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -2133,6 +2133,8 @@ mymain(void)
             QEMU_CAPS_NUMA,
             QEMU_CAPS_OBJECT_MEMORY_RAM);
     DO_TEST_PARSE_ERROR("numatune-memnode", NONE);
+    DO_TEST_CAPS_VER("numatune-memnode", "5.2.0");
+    DO_TEST_CAPS_LATEST("numatune-memnode");
 
     DO_TEST("numatune-memnode-no-memory",
             QEMU_CAPS_NUMA,
