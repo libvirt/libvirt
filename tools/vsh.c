a131b8b517a88685ddb5f1b17c4d1a16b9a97b61
Ján Tomko <jtomko@redhat.com>
2021-04-06 14:23:31 +0200

    vsh: fix memory leak in vshCommandParse
    One of the error branches used a plain free where vshCommandFree
    was required.
    
    https://bugzilla.redhat.com/show_bug.cgi?id=1943415
    
    Signed-off-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Erik Skultety <eskultet@redhat.com>

--- tools/vsh.c
+++ tools/vsh.c
@@ -1530,7 +1530,7 @@ vshCommandParse(vshControl *ctl, vshCommandParser *parser, vshCmd **partial)
 
             if (!partial &&
                 vshCommandCheckOpts(ctl, c, opts_required, opts_seen) < 0) {
-                VIR_FREE(c);
+                vshCommandFree(c);
                 goto syntaxError;
             }
 
