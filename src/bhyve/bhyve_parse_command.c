libvirt/libvirt

commit ad80bba90a3d783d03559de7184a90ef6414942d

bhyveParsePCIFbuf: Use g_strsplit instead of virStringSplitCount
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/bhyve/bhyve_parse_command.c
@@ -558,10 +558,8 @@ bhyveParsePCIFbuf(virDomainDefPtr def,
 
     virDomainVideoDefPtr video = NULL;
     virDomainGraphicsDefPtr graphics = NULL;
-    char **params = NULL;
-    char *param = NULL, *separator = NULL;
-    size_t nparams = 0;
-    size_t i = 0;
+    g_auto(GStrv) **params = NULL;
+    GStrv next;
 
     if (!(video = virDomainVideoDefNew(xmlopt)))
         goto cleanup;
@@ -579,11 +577,11 @@ bhyveParsePCIFbuf(virDomainDefPtr def,
     if (!config)
         goto error;
 
-    if (!(params = virStringSplitCount(config, ",", 0, &nparams)))
+    if (!(params = g_strsplit(config, ",", 0)))
         goto error;
 
-    for (i = 0; i < nparams; i++) {
-        param = params[i];
+    for (next = params; *next; next++) {
+        char *param = *next;
         if (!video->driver)
             video->driver = g_new0(virDomainVideoDriverDef, 1);
 
@@ -649,13 +647,11 @@ bhyveParsePCIFbuf(virDomainDefPtr def,
     if (VIR_APPEND_ELEMENT(def->graphics, def->ngraphics, graphics) < 0)
         goto error;
 
-    g_strfreev(params);
     return 0;
 
  error:
     virDomainVideoDefFree(video);
     virDomainGraphicsDefFree(graphics);
-    g_strfreev(params);
     return -1;
 }
 

commit 88e9f304028b3893ec8544cb1ea6e8a44542265f

bhyve: Fix declaration of 'params' in 'bhyveParsePCIFbuf'
In commit ad80bba90a3 I mistakenly didn't delete '**' from the
variable declaration when converting it to 'GStrv' and deleted the
'separator' variable since it was declared on the same line as a
different variable.

Fixes: ad80bba90a3
Signed-off-by: Peter Krempa <pkrempa@redhat.com>

src/bhyve/bhyve_parse_command.c
@@ -558,7 +558,7 @@ bhyveParsePCIFbuf(virDomainDefPtr def,
 
     virDomainVideoDefPtr video = NULL;
     virDomainGraphicsDefPtr graphics = NULL;
-    g_auto(GStrv) **params = NULL;
+    g_auto(GStrv) params = NULL;
     GStrv next;
 
     if (!(video = virDomainVideoDefNew(xmlopt)))
@@ -582,6 +582,7 @@ bhyveParsePCIFbuf(virDomainDefPtr def,
 
     for (next = params; *next; next++) {
         char *param = *next;
+        char *separator;
         if (!video->driver)
             video->driver = g_new0(virDomainVideoDriverDef, 1);
 