libvirt/libvirt

commit 8fe30b2167b5b56461b11dbf02aca83030070caf

log: actually do substring matches with fnmatch
Historically we matched log filters with strstr(), and when switching to
fnmatch in cbb0fd3cfdc287f6f4653ef1f04a7cfb2ea51b27, it was stated that
we would continue to match substrings, with "foo" being equivalent to
"*foo*". Unfortuntely I forget to provide the code to actually make that
happen. This fixes it to prepend and append "*". We don't bother to
check if the pattern already has a leading/trailing '*', because
"**foo**" will match the same as "*foo*".

Reviewed-by: Erik Skultety <eskultet@redhat.com>
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

src/util/virlog.c
@@ -1409,6 +1409,7 @@ virLogFilterNew(const char *match,
 {
     virLogFilterPtr ret = NULL;
     char *mdup = NULL;
+    size_t mlen = strlen(match);
 
     virCheckFlags(VIR_LOG_STACK_TRACE, NULL);
 
@@ -1418,8 +1419,15 @@ virLogFilterNew(const char *match,
         return NULL;
     }
 
-    if (VIR_STRDUP_QUIET(mdup, match) < 0)
+    /* We must treat 'foo' as equiv to '*foo*' for fnmatch
+     * todo substring matches, so add 2 extra bytes
+     */
+    if (VIR_ALLOC_N_QUIET(mdup, mlen + 3) < 0)
         return NULL;
+
+    mdup[0] = '*';
+    memcpy(mdup + 1, match, mlen);
+    mdup[mlen + 1] = '*';
 
     if (VIR_ALLOC_QUIET(ret) < 0) {
         VIR_FREE(mdup);

commit e7a999364e59bce07eea54e386ae821e8ff6df9f

virlog: Remove stray "todo" in comment
Fixes: 8fe30b2167b5b56461b11dbf02aca83030070caf
Signed-off-by: Tim Wiederhake <twiederh@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>
Signed-off-by: Ján Tomko <jtomko@redhat.com>

src/util/virlog.c
@@ -1315,7 +1315,7 @@ virLogFilterNew(const char *match,
     ret->priority = priority;
 
     /* We must treat 'foo' as equiv to '*foo*' for g_pattern_match
-     * todo substring matches, so add 2 extra bytes
+     * substring matches, so add 2 extra bytes
      */
     ret->match = g_new0(char, mlen + 3);
     ret->match[0] = '*';