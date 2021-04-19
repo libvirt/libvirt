libvirt/libvirt

commit caf71b64fe6989116316b966fda119cd3e47f485

qemu: block: Add JSON props generator for ssh storage backing

src/qemu/qemu_block.c
@@ -900,6 +900,38 @@ qemuBlockStorageSourceGetSheepdogProps(virStorageSourcePtr src)
     return ret;
 }
 
+
+static virJSONValuePtr
+qemuBlockStorageSourceGetSshProps(virStorageSourcePtr src)
+{
+    virJSONValuePtr serverprops;
+    virJSONValuePtr ret = NULL;
+    const char *username = NULL;
+
+    if (src->nhosts != 1) {
+        virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
+                       _("sheepdog protocol accepts only one host"));
+        return NULL;
+    }
+
+    serverprops = qemuBlockStorageSourceBuildJSONInetSocketAddress(&src->hosts[0]);
+    if (!serverprops)
+        return NULL;
+
+    if (src->auth)
+        username = src->auth->username;
+
+    ignore_value(virJSONValueObjectCreate(&ret,
+                                          "s:driver", "ssh",
+                                          "s:path", src->path,
+                                          "a:server", serverprops,
+                                          "S:user", username,
+                                          NULL));
+
+    return ret;
+}
+
+
 /**
  * qemuBlockStorageSourceGetBackendProps:
  * @src: disk source
@@ -970,6 +1002,10 @@ qemuBlockStorageSourceGetBackendProps(virStorageSourcePtr src)
             break;
 
         case VIR_STORAGE_NET_PROTOCOL_SSH:
+            if (!(fileprops = qemuBlockStorageSourceGetSshProps(src)))
+                return NULL;
+            break;
+
         case VIR_STORAGE_NET_PROTOCOL_NONE:
         case VIR_STORAGE_NET_PROTOCOL_LAST:
             break;

commit 48472a3a3d3d02234cc76a19f7467ea36b534102

qemu_block: fix copy&paste typo in SSH protocol
Fixes: caf71b64fe6989116316b966fda119cd3e47f485
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_block.c
@@ -965,7 +965,7 @@ qemuBlockStorageSourceGetSshProps(virStorageSource *src)
 
     if (src->nhosts != 1) {
         virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
-                       _("sheepdog protocol accepts only one host"));
+                       _("ssh protocol accepts only one host"));
         return NULL;
     }
 