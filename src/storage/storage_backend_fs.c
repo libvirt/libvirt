libvirt/libvirt

commit 16c69e7aaed4cabfd8e8c19cc326854d4c480437

storage: use virFindFileInPath to validate presence of mkfs
Future patch will remove MKFS define as we will no longer check it
during compilation.

Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

src/storage/storage_backend_fs.c
@@ -397,13 +397,20 @@ virStorageBackendFileSystemCheck(virStoragePoolObj *pool,
     return 0;
 }
 
-/* some platforms don't support mkfs */
-#ifdef MKFS
 static int
 virStorageBackendExecuteMKFS(const char *device,
                              const char *format)
 {
     g_autoptr(virCommand) cmd = NULL;
+    g_autofree char *mkfs = virFindFileInPath(MKFS);
+
+    if (!mkfs) {
+        virReportError(VIR_ERR_INTERNAL_ERROR,
+                       _("mkfs is not available on this platform: "
+                         "Failed to make filesystem of type '%s' on device '%s'"),
+                       format, device);
+        return -1;
+    }
 
     cmd = virCommandNewArgList(MKFS, "-t", format, NULL);
 
@@ -426,19 +433,6 @@ virStorageBackendExecuteMKFS(const char *device,
 
     return 0;
 }
-#else /* #ifdef MKFS */
-static int
-virStorageBackendExecuteMKFS(const char *device G_GNUC_UNUSED,
-                             const char *format G_GNUC_UNUSED)
-{
-    virReportError(VIR_ERR_INTERNAL_ERROR,
-                   _("mkfs is not supported on this platform: "
-                     "Failed to make filesystem of "
-                     "type '%s' on device '%s'"),
-                   format, device);
-    return -1;
-}
-#endif /* #ifdef MKFS */
 
 static int
 virStorageBackendMakeFileSystem(virStoragePoolObj *pool,

commit 9c81d1ec11b05a5d5caec2baaca577df6d9f3ea7

storage_backend_fs: use MKFS ony if WITH_STORAGE_FS is defined
The code in storage_backend_fs is used for storage_dir and storage_fs
drivers so some parts need to be guarded by checking for
WITH_STORAGE_FS.

Fixes: 16c69e7aaed4cabfd8e8c19cc326854d4c480437
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/storage/storage_backend_fs.c
@@ -402,7 +402,11 @@ virStorageBackendExecuteMKFS(const char *device,
                              const char *format)
 {
     g_autoptr(virCommand) cmd = NULL;
-    g_autofree char *mkfs = virFindFileInPath(MKFS);
+    g_autofree char *mkfs = NULL;
+
+#if WITH_STORAGE_FS
+    mkfs = virFindFileInPath(MKFS);
+#endif /* WITH_STORAGE_FS */
 
     if (!mkfs) {
         virReportError(VIR_ERR_INTERNAL_ERROR,
@@ -412,7 +416,7 @@ virStorageBackendExecuteMKFS(const char *device,
         return -1;
     }
 
-    cmd = virCommandNewArgList(MKFS, "-t", format, NULL);
+    cmd = virCommandNewArgList(mkfs, "-t", format, NULL);
 
     /* use the force, otherwise mkfs.xfs won't overwrite existing fs.
      * Similarly mkfs.ext2, mkfs.ext3, and mkfs.ext4 require supplying -F