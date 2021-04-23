libvirt/libvirt

commit ab5d2776c925ec45eb54ec5432f5645cebb80c85

virxml: Add virXMLPropEnum
Convenience function to return the value of an enum XML attribute.

Signed-off-by: Tim Wiederhake <twiederh@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

src/util/virxml.c
@@ -760,6 +760,54 @@ virXMLPropUInt(xmlNodePtr node,
 
 
 /**
+ * virXMLPropEnum:
+ * @node: XML dom node pointer
+ * @name: Name of the property (attribute) to get
+ * @strToInt: Conversion function to turn enum name to value. Expected to
+ *            return negative value on failure.
+ * @flags: Bitwise or of virXMLPropFlags
+ * @result: The returned value
+ *
+ * Convenience function to return value of an enum attribute.
+ *
+ * Returns 1 in case of success in which case @result is set,
+ *         or 0 if the attribute is not present,
+ *         or -1 and reports an error on failure.
+ */
+int
+virXMLPropEnum(xmlNodePtr node,
+               const char* name,
+               int (*strToInt)(const char*),
+               virXMLPropFlags flags,
+               unsigned int *result)
+{
+    g_autofree char *tmp = NULL;
+    int ret;
+
+    if (!(tmp = virXMLPropString(node, name))) {
+        if (!(flags & VIR_XML_PROP_REQUIRED))
+            return 0;
+
+        virReportError(VIR_ERR_XML_ERROR,
+                       _("Missing required attribute '%s' in element '%s'"),
+                       name, node->name);
+        return -1;
+    }
+
+    ret = strToInt(tmp);
+    if (ret < 0 ||
+        ((flags & VIR_XML_PROP_NONZERO) && (ret == 0))) {
+        virReportError(VIR_ERR_XML_ERROR,
+                       _("Invalid value for attribute '%s' in element '%s': '%s'."),
+                       name, node->name, NULLSTR(tmp));
+        return -1;
+    }
+
+    *result = ret;
+    return 0;
+}
+
+/**
  * virXPathBoolean:
  * @xpath: the XPath string to evaluate
  * @ctxt: an XPath context

src/libvirt_private.syms
@@ -3548,6 +3548,7 @@ virXMLNodeSanitizeNamespaces;
 virXMLNodeToString;
 virXMLParseHelper;
 virXMLPickShellSafeComment;
+virXMLPropEnum;
 virXMLPropInt;
 virXMLPropString;
 virXMLPropStringLimit;

src/util/virxml.h
@@ -135,6 +135,15 @@ virXMLPropUInt(xmlNodePtr node,
                unsigned int *result)
     ATTRIBUTE_NONNULL(0) ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(4);
 
+int
+virXMLPropEnum(xmlNodePtr node,
+               const char* name,
+               int (*strToInt)(const char*),
+               virXMLPropFlags flags,
+               unsigned int *result)
+    ATTRIBUTE_NONNULL(0) ATTRIBUTE_NONNULL(1) ATTRIBUTE_NONNULL(2)
+    ATTRIBUTE_NONNULL(4);
+
 /* Internal function; prefer the macros below.  */
 xmlDocPtr
 virXMLParseHelper(int domcode,

commit c02c301130a80a4368ac4dedf9759411d95d7fae

virXMLPropEnum: Fix return value
Function incorrectly returns 0 when property was successfully read.

Fixes: ab5d2776c925ec45eb54ec5432f5645cebb80c85
Signed-off-by: Tim Wiederhake <twiederh@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>
Signed-off-by: Ján Tomko <jtomko@redhat.com>

src/util/virxml.c
@@ -856,7 +856,7 @@ virXMLPropEnum(xmlNodePtr node,
     }
 
     *result = ret;
-    return 0;
+    return 1;
 }
 
 /**