libvirt/libvirt

commit 65c2901906b5e2d707f9184ff3719a5375bab427

virfile: Simplify virFindFileInPath() with g_find_program_in_path()
Signed-off-by: Luke Yue <lukedyue@gmail.com>
Signed-off-by: Martin Kletzander <mkletzan@redhat.com>

src/util/virfile.c
@@ -1662,54 +1662,17 @@ virFileIsLink(const char *linkpath)
 char *
 virFindFileInPath(const char *file)
 {
-    const char *origpath = NULL;
-    g_auto(GStrv) paths = NULL;
-    char **pathiter;
-
+    g_autofree char *path = NULL;
     if (file == NULL)
         return NULL;
 
-    /* if we are passed an absolute path (starting with /), return a
-     * copy of that path, after validating that it is executable
+    path = g_find_program_in_path(file);
+
+    /* Workaround for a bug in g_find_program_in_path() not returning absolute
+     * path as documented.  This has been fixed in
+     * https://gitlab.gnome.org/GNOME/glib/-/merge_requests/2127
      */
-    if (g_path_is_absolute(file)) {
-        if (!virFileIsExecutable(file))
-            return NULL;
-
-        return g_strdup(file);
-    }
-
-    /* If we are passed an anchored path (containing a /), then there
-     * is no path search - it must exist in the current directory
-     */
-    if (strchr(file, '/')) {
-        char *abspath = NULL;
-
-        if (!virFileIsExecutable(file))
-            return NULL;
-
-        return g_canonicalize_filename(file, NULL);
-    }
-
-    /* copy PATH env so we can tweak it */
-    origpath = getenv("PATH");
-    if (!origpath)
-        origpath = "/bin:/usr/bin";
-
-    /* for each path segment, append the file to search for and test for
-     * it. return it if found.
-     */
-
-    if (!(paths = g_strsplit(origpath, ":", 0)))
-        return NULL;
-
-    for (pathiter = paths; *pathiter; pathiter++) {
-        g_autofree char *fullpath = g_build_filename(*pathiter, file, NULL);
-        if (virFileIsExecutable(fullpath))
-            return g_steal_pointer(&fullpath);
-    }
-
-    return NULL;
+    return g_canonicalize_filename(path, NULL);
 }
 
 

commit 9a51edebf88556823e05c3710ec378c976ce3386

virFindFileInPath: Don't pass NULL to g_canonicalize_filename()
If given file is not found in $PATH then g_find_program_in_path()
returns NULL. However, g_canonicalize_filename() does not accept
NULL as input.

Fixes: 65c2901906b5e2d707f9184ff3719a5375bab427
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

src/util/virfile.c
@@ -1667,6 +1667,8 @@ virFindFileInPath(const char *file)
         return NULL;
 
     path = g_find_program_in_path(file);
+    if (!path)
+        return NULL;
 
     /* Workaround for a bug in g_find_program_in_path() not returning absolute
      * path as documented.  This has been fixed in