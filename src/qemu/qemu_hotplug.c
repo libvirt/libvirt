libvirt/libvirt

commit 7b8bec45601b6570f6a7413e94d291986d2663f1

qemu_hotplug.c: add net devices to the domain list earlier
An upcoming patch will be checking if the addition of a new net device
requires adjusting the domain locked memory limit, which must be done
prior to sending the command to qemu to add the new device. But
qemuDomainAdjustMaxMemLock() checks all (and only) the devices that
are currently in the domain definition, and currently we are adding
new net devices to the domain definition only at the very end of the
hotplug operation, after qemu has already executed the device_add
command.

In order for the upcoming patch to work, this patch changes
qemuDomainAttachNetDevice() to add the device to the domain nets list
at an earlier time. It can't be added until after PCI address and
alias name have been determined (because both of those examine
existing devices in the domain to figure out a unique value for the
new device), but must be done before making the qemu monitor call.

Since the device has been added to the list earlier, we need to
potentially remove it on failure. This is done by replacing the
existing call to virDomainNetRemoveHostdev() (which checks if this is
a hostdev net device, and if so removes it from the hostdevs list,
since it could have already been added to that list) with a call to
the new virDomainNetRemoveByObj(), which looks for the device on both
nets and hostdevs lists, and removes it where it finds it.

Signed-off-by: Laine Stump <laine@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/qemu/qemu_hotplug.c
@@ -1203,9 +1203,6 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
     g_autoptr(virConnect) conn = NULL;
     virErrorPtr save_err = NULL;
 
-    /* preallocate new slot for device */
-    VIR_REALLOC_N(vm->def->nets, vm->def->nnets + 1);
-
     /* If appropriate, grab a physical device from the configured
      * network's pool of devices, or resolve bridge device name
      * to the one defined in the network definition.
@@ -1255,6 +1252,18 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
     }
 
     releaseaddr = true;
+
+    /* We've completed all examinations of the full domain definition
+     * that require the new device to *not* be present (e.g. PCI
+     * address allocation and alias name assignment) so it is now safe
+     * to add the new device to the domain's nets list (in order for
+     * it to be in place for checks that *do* need it present in the
+     * domain definition, e.g. checking if we need to adjust the
+     * locked memory limit). This means we will need to remove it if
+     * there is a failure.
+     */
+    if (VIR_APPEND_ELEMENT_COPY(vm->def->nets, vm->def->nnets, net) < 0)
+        goto cleanup;
 
     switch (actualType) {
     case VIR_DOMAIN_NET_TYPE_BRIDGE:
@@ -1512,9 +1521,7 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
     ret = 0;
 
  cleanup:
-    if (!ret) {
-        vm->def->nets[vm->def->nnets++] = net;
-    } else {
+    if (ret < 0) {
         virErrorPreserveLast(&save_err);
         if (releaseaddr)
             qemuDomainReleaseDeviceAddress(vm, &net->info);
@@ -1536,7 +1543,11 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
             qemuDomainNetDeviceVportRemove(net);
         }
 
-        virDomainNetRemoveHostdev(vm->def, net);
+        /* we had potentially pre-added the device to the domain
+         * device lists, if so we need to remove it (from def->nets
+         * and/or def->hostdevs) on failure
+         */
+        virDomainNetRemoveByObj(vm->def, net);
 
         if (net->type == VIR_DOMAIN_NET_TYPE_NETWORK) {
             if (conn)

commit 54b602019d7dfa94a6c52ef7aa3abdfaa93ed233

qemu_hotplug: don't forget to add hostdev interfaces to the interface list
Originally qemuDomainAttachNetDevice() would wait until the cleanup at
the very end of the function to add newly hotplugged interfaces to the
domain's nets list. commit 7b8bec4560 modified it to add the new
interface to the nets list earlier (but not all the way at the
beginning of the function either, because there are some operations
(PCI address assignment in particular) that need the new device to not
yet be visible in the domaindef).

But hostdev interfaces short-circuit past most of the body of
qemuDomainAttachNetDevice() (since none of it applies to hostdev
interfaces). In the past that was okay, but since the line that adds
the new interface to the domaindef's nets list is in that "most of the
body", after that commit hotplugged hostdev interfaces are no longer
being properly added to the domaindef nets list, so they don't show up
in the status XML or the virsh domiflist output.

It really *is* important to add interfaces to the nets list earlier,
so we can't revert commit 7b8bec4560, and we also can't move the
insert to common code *earlier* in the function, so instead this patch
duplicates the VIR_APPEND_ELEMENT_COPY() just before the code path for
hostdev interfaces jumps to cleanup.

Resolves: https://bugzilla.redhat.com/1972468
Fixes: 7b8bec45601b6570f6a7413e94d291986d2663f1
Signed-off-by: Laine Stump <laine@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/qemu/qemu_hotplug.c
@@ -1230,10 +1230,21 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
         /* This is really a "smart hostdev", so it should be attached
          * as a hostdev (the hostdev code will reach over into the
          * netdev-specific code as appropriate), then also added to
-         * the nets list (see cleanup:) if successful.
+         * the nets list if successful.
          */
-        ret = qemuDomainAttachHostDevice(driver, vm,
-                                         virDomainNetGetActualHostdev(net));
+        if (qemuDomainAttachHostDevice(driver, vm,
+                                       virDomainNetGetActualHostdev(net)) < 0) {
+            goto cleanup;
+        }
+        if (VIR_APPEND_ELEMENT_COPY(vm->def->nets, vm->def->nnets, net) < 0)
+            goto cleanup;
+
+        /* the rest of the setup doesn't apply to hostdev interfaces, so
+         * we can skip straight to the cleanup (nothing there applies to
+         * hostdev interfaces either, but it might in the future, so we
+         * may as well be consistent)
+         */
+        ret = 0;
         goto cleanup;
     }
 