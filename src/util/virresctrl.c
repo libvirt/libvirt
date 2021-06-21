libvirt/libvirt

commit 7d2fd6ef0163a939adb7ce0f0fad3b7654c340de

Do not check return value of VIR_EXPAND_N
Signed-off-by: Jiri Denemark <jdenemar@redhat.com>
Reviewed-by: Erik Skultety <eskultet@redhat.com>

src/util/virresctrl.c
@@ -608,10 +608,9 @@ virResctrlGetCacheInfo(virResctrlInfoPtr resctrl,
         if (rv < 0)
             goto cleanup;
 
-        if (resctrl->nlevels <= level &&
+        if (resctrl->nlevels <= level)
             VIR_EXPAND_N(resctrl->levels, resctrl->nlevels,
-                         level - resctrl->nlevels + 1) < 0)
-            goto cleanup;
+                         level - resctrl->nlevels + 1);
 
         if (!resctrl->levels[level]) {
             virResctrlInfoPerTypePtr *types = NULL;
@@ -932,8 +931,7 @@ virResctrlInfoGetCache(virResctrlInfoPtr resctrl,
             i_type->max_cache_id++;
         }
 
-        if (VIR_EXPAND_N(*controls, *ncontrols, 1) < 0)
-            goto error;
+        VIR_EXPAND_N(*controls, *ncontrols, 1);
         (*controls)[*ncontrols - 1] = g_new0(virResctrlInfoPerCache, 1);
         memcpy((*controls)[*ncontrols - 1], &i_type->control, sizeof(i_type->control));
     }
@@ -1098,9 +1096,8 @@ virResctrlAllocGetType(virResctrlAllocPtr alloc,
 {
     virResctrlAllocPerLevelPtr a_level = NULL;
 
-    if (alloc->nlevels <= level &&
-        VIR_EXPAND_N(alloc->levels, alloc->nlevels, level - alloc->nlevels + 1) < 0)
-        return NULL;
+    if (alloc->nlevels <= level)
+        VIR_EXPAND_N(alloc->levels, alloc->nlevels, level - alloc->nlevels + 1);
 
     if (!alloc->levels[level]) {
         virResctrlAllocPerTypePtr *types = NULL;
@@ -1132,10 +1129,9 @@ virResctrlAllocUpdateMask(virResctrlAllocPtr alloc,
     if (!a_type)
         return -1;
 
-    if (a_type->nmasks <= cache &&
+    if (a_type->nmasks <= cache)
         VIR_EXPAND_N(a_type->masks, a_type->nmasks,
-                     cache - a_type->nmasks + 1) < 0)
-        return -1;
+                     cache - a_type->nmasks + 1);
 
     if (!a_type->masks[cache])
         a_type->masks[cache] = virBitmapNewCopy(mask);
@@ -1156,10 +1152,9 @@ virResctrlAllocUpdateSize(virResctrlAllocPtr alloc,
     if (!a_type)
         return -1;
 
-    if (a_type->nsizes <= cache &&
+    if (a_type->nsizes <= cache)
         VIR_EXPAND_N(a_type->sizes, a_type->nsizes,
-                     cache - a_type->nsizes + 1) < 0)
-        return -1;
+                     cache - a_type->nsizes + 1);
 
     if (!a_type->sizes[cache])
         a_type->sizes[cache] = g_new0(unsigned long long, 1);
@@ -1316,10 +1311,9 @@ virResctrlAllocSetMemoryBandwidth(virResctrlAllocPtr alloc,
         alloc->mem_bw = mem_bw;
     }
 
-    if (mem_bw->nbandwidths <= id &&
+    if (mem_bw->nbandwidths <= id)
         VIR_EXPAND_N(mem_bw->bandwidths, mem_bw->nbandwidths,
-                     id - mem_bw->nbandwidths + 1) < 0)
-        return -1;
+                     id - mem_bw->nbandwidths + 1);
 
     if (mem_bw->bandwidths[id]) {
         virReportError(VIR_ERR_XML_ERROR,
@@ -1469,9 +1463,9 @@ virResctrlAllocParseProcessMemoryBandwidth(virResctrlInfoPtr resctrl,
                          "memory bandwidth node '%u'"), id);
         return -1;
     }
-    if (alloc->mem_bw->nbandwidths <= id &&
+    if (alloc->mem_bw->nbandwidths <= id) {
         VIR_EXPAND_N(alloc->mem_bw->bandwidths, alloc->mem_bw->nbandwidths,
-                     id - alloc->mem_bw->nbandwidths + 1) < 0) {
+                     id - alloc->mem_bw->nbandwidths + 1);
         return -1;
     }
     if (!alloc->mem_bw->bandwidths[id])
@@ -1856,9 +1850,8 @@ virResctrlAllocNewFromInfo(virResctrlInfoPtr info)
     if (info->membw_info) {
         ret->mem_bw = g_new0(virResctrlAllocMemBW, 1);
 
-        if (VIR_EXPAND_N(ret->mem_bw->bandwidths, ret->mem_bw->nbandwidths,
-                         info->membw_info->max_id + 1) < 0)
-            goto error;
+        VIR_EXPAND_N(ret->mem_bw->bandwidths, ret->mem_bw->nbandwidths,
+                     info->membw_info->max_id + 1);
 
         for (i = 0; i < ret->mem_bw->nbandwidths; i++) {
             ret->mem_bw->bandwidths[i] = g_new0(unsigned int, 1);
@@ -2146,10 +2139,9 @@ virResctrlAllocCopyMemBW(virResctrlAllocPtr dst,
 
     dst_bw = dst->mem_bw;
 
-    if (src_bw->nbandwidths > dst_bw->nbandwidths &&
+    if (src_bw->nbandwidths > dst_bw->nbandwidths)
         VIR_EXPAND_N(dst_bw->bandwidths, dst_bw->nbandwidths,
-                     src_bw->nbandwidths - dst_bw->nbandwidths) < 0)
-        return -1;
+                     src_bw->nbandwidths - dst_bw->nbandwidths);
 
     for (i = 0; i < src_bw->nbandwidths; i++) {
         if (dst_bw->bandwidths[i])

src/access/viraccessdriverstack.c
@@ -40,8 +40,7 @@ int virAccessDriverStackAppend(virAccessManagerPtr manager,
 {
     virAccessDriverStackPrivatePtr priv = virAccessManagerGetPrivateData(manager);
 
-    if (VIR_EXPAND_N(priv->managers, priv->managersLen, 1) < 0)
-        return -1;
+    VIR_EXPAND_N(priv->managers, priv->managersLen, 1);
 
     priv->managers[priv->managersLen-1] = child;
 

src/conf/backup_conf.c
@@ -554,8 +554,7 @@ virDomainBackupAlignDisks(virDomainBackupDefPtr def,
         backup_all = true;
 
     ndisks = def->ndisks;
-    if (VIR_EXPAND_N(def->disks, def->ndisks, dom->ndisks - def->ndisks) < 0)
-        return -1;
+    VIR_EXPAND_N(def->disks, def->ndisks, dom->ndisks - def->ndisks);
 
     for (i = 0; i < dom->ndisks; i++) {
         virDomainBackupDiskDefPtr backupdisk = NULL;

src/conf/capabilities.c
@@ -580,9 +580,7 @@ virCapabilitiesHostSecModelAddBaseLabel(virCapsHostSecModelPtr secmodel,
     if (type == NULL || label == NULL)
         return -1;
 
-    if (VIR_EXPAND_N(secmodel->labels, secmodel->nlabels, 1) < 0)
-        return -1;
-
+    VIR_EXPAND_N(secmodel->labels, secmodel->nlabels, 1);
     secmodel->labels[secmodel->nlabels - 1].type = g_strdup(type);
     secmodel->labels[secmodel->nlabels - 1].label = g_strdup(label);
 

src/conf/domain_addr.c
@@ -728,8 +728,7 @@ virDomainPCIAddressSetGrow(virDomainPCIAddressSetPtr addrs,
 
     i = addrs->nbuses;
 
-    if (VIR_EXPAND_N(addrs->buses, addrs->nbuses, add) < 0)
-        return -1;
+    VIR_EXPAND_N(addrs->buses, addrs->nbuses, add);
 
     if (needDMIToPCIBridge) {
         /* first of the new buses is dmi-to-pci-bridge, the
@@ -1985,8 +1984,7 @@ virDomainUSBAddressSetAddController(virDomainUSBAddressSetPtr addrs,
         return 0;
 
     if (addrs->nbuses <= cont->idx) {
-        if (VIR_EXPAND_N(addrs->buses, addrs->nbuses, cont->idx - addrs->nbuses + 1) < 0)
-            goto cleanup;
+        VIR_EXPAND_N(addrs->buses, addrs->nbuses, cont->idx - addrs->nbuses + 1);
     } else if (addrs->buses[cont->idx]) {
         virReportError(VIR_ERR_XML_ERROR,
                        _("Duplicate USB controllers with index %u"),

src/conf/domain_conf.c
@@ -2001,8 +2001,7 @@ virDomainDefSetVcpusMax(virDomainDefPtr def,
         return 0;
 
     if (def->maxvcpus < maxvcpus) {
-        if (VIR_EXPAND_N(def->vcpus, def->maxvcpus, maxvcpus - def->maxvcpus) < 0)
-            return -1;
+        VIR_EXPAND_N(def->vcpus, def->maxvcpus, maxvcpus - def->maxvcpus);
 
         for (i = oldmax; i < def->maxvcpus; i++) {
             if (!(def->vcpus[i] = virDomainVcpuDefNew(xmlopt)))

src/conf/nwfilter_conf.c
@@ -381,11 +381,7 @@ virNWFilterRuleDefAddVar(virNWFilterRuleDefPtr nwf,
             }
     }
 
-    if (VIR_EXPAND_N(nwf->varAccess, nwf->nVarAccess, 1) < 0) {
-        virNWFilterVarAccessFree(varAccess);
-        return -1;
-    }
-
+    VIR_EXPAND_N(nwf->varAccess, nwf->nVarAccess, 1);
     nwf->varAccess[nwf->nVarAccess - 1] = varAccess;
     item->varAccess = varAccess;
 

src/conf/nwfilter_params.c
@@ -221,9 +221,7 @@ virNWFilterVarValueAddValue(virNWFilterVarValuePtr val, char *value)
         break;
 
     case NWFILTER_VALUE_TYPE_ARRAY:
-        if (VIR_EXPAND_N(val->u.array.values,
-                         val->u.array.nValues, 1) < 0)
-            return -1;
+        VIR_EXPAND_N(val->u.array.values, val->u.array.nValues, 1);
         val->u.array.values[val->u.array.nValues - 1] = value;
         rc = 0;
         break;
@@ -374,9 +372,7 @@ virNWFilterVarCombIterAddVariable(virNWFilterVarCombIterEntryPtr cie,
         }
     }
 
-    if (VIR_EXPAND_N(cie->varNames, cie->nVarNames, 1) < 0)
-        return -1;
-
+    VIR_EXPAND_N(cie->varNames, cie->nVarNames, 1);
     cie->varNames[cie->nVarNames - 1] = varName;
 
     return 0;

commit 36d6da4ebf9b648ca378d93a7cad696f045a5073

virresctrl: fix starting VMs with cputune.memorytune specified
When removing check for return value of VIR_EXPAND_N this place was
incorrectly modified causing failure to start a VM with cputune
memorytune configured with useless error message:

    error: Failed to start domain 'vm1'
    error: An error occurred, but the cause is unknown

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=1973094
Fixes: 7d2fd6ef0163a939adb7ce0f0fad3b7654c340de
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/util/virresctrl.c
@@ -1437,7 +1437,6 @@ virResctrlAllocParseProcessMemoryBandwidth(virResctrlInfo *resctrl,
     if (alloc->mem_bw->nbandwidths <= id) {
         VIR_EXPAND_N(alloc->mem_bw->bandwidths, alloc->mem_bw->nbandwidths,
                      id - alloc->mem_bw->nbandwidths + 1);
-        return -1;
     }
     if (!alloc->mem_bw->bandwidths[id])
         alloc->mem_bw->bandwidths[id] = g_new0(unsigned int, 1);