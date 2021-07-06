953c7e024a7d8f3b0cd1f5d6702cddcdac820e24
Tim Wiederhake <twiederh@redhat.com>
2021-07-06 13:43:28 +0200

    virNWFilterCreateVarsFrom: Remove superfluous `goto`s
    Signed-off-by: Tim Wiederhake <twiederh@redhat.com>
    Reviewed-by: Peter Krempa <pkrempa@redhat.com>

--- src/nwfilter/nwfilter_gentech_driver.c
+++ src/nwfilter/nwfilter_gentech_driver.c
@@ -202,15 +202,12 @@ virNWFilterCreateVarsFrom(GHashTable *vars1,
     g_autoptr(GHashTable) res = virHashNew(virNWFilterVarValueHashFree);
 
     if (virNWFilterHashTablePutAll(vars1, res) < 0)
-        goto error;
+        return NULL;
 
     if (virNWFilterHashTablePutAll(vars2, res) < 0)
-        goto error;
+        return NULL;
 
     return g_steal_pointer(&res);
-
- error:
-    return NULL;
 }
 
 
