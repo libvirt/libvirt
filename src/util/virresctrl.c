libvirt/libvirt

commit d8a354954aba9cd45ab0317915a0a2be27c04767

Use 'virBitmapNewCopy' instead of 'virBitmapCopy'
There are only 3 places using the function. Two can use virBitmapNewCopy
directly. In case of the qemu capabilities code we need to free the old
bitmap first.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/util/virresctrl.c
@@ -1158,9 +1158,9 @@ virResctrlAllocUpdateMask(virResctrlAllocPtr alloc,
         return -1;
 
     if (!a_type->masks[cache])
-        a_type->masks[cache] = virBitmapNew(virBitmapSize(mask));
+        a_type->masks[cache] = virBitmapNewCopy(mask);
 
-    return virBitmapCopy(a_type->masks[cache], mask);
+    return 0;
 }
 
 

src/conf/capabilities.c
@@ -1589,8 +1589,7 @@ virCapabilitiesHostNUMAInitFake(virCapsHostNUMAPtr caps)
                         cpus[cid].die_id = 0;
                         cpus[cid].socket_id = s;
                         cpus[cid].core_id = c;
-                        cpus[cid].siblings = virBitmapNew(ncpus);
-                        virBitmapCopy(cpus[cid].siblings, siblings);
+                        cpus[cid].siblings = virBitmapNewCopy(siblings);
                         cid++;
                     }
 

src/qemu/qemu_capabilities.c
@@ -1932,7 +1932,8 @@ virQEMUCapsPtr virQEMUCapsNewCopy(virQEMUCapsPtr qemuCaps)
 
     ret->ctime = qemuCaps->ctime;
 
-    virBitmapCopy(ret->flags, qemuCaps->flags);
+    virBitmapFree(ret->flags);
+    ret->flags = virBitmapNewCopy(qemuCaps->flags);
 
     ret->version = qemuCaps->version;
     ret->kvmVersion = qemuCaps->kvmVersion;

commit a9c7da6126750da3b4e6357080a1ee66ed8d5b57

virresctrl: Fix updating the mask for a cache resource
In 'virResctrlAllocUpdateMask', mask is updated only if 'previous mask' is NULL.

By default, the bitmask for a cache resource for a VM is initialized with
'default-resctrl-group' bitmask. So the 'previous mask' would not be NULL and
mask won't get updated if cachetune is configured for a VM. This causes libvirt
to use same bitmask as 'default-resctrl-group' bitmask for a cache resource for
a VM. This patch fixes the issue.

Fixes: d8a354954aba9cd45ab0317915a0a2be27c04767

Signed-off-by: Vinayak Kale <vkale@nvidia.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Daniel Henrique Barboza <danielhb413@gmail.com>
Signed-off-by: Ján Tomko <jtomko@redhat.com>

src/util/virresctrl.c
@@ -1104,8 +1104,10 @@ virResctrlAllocUpdateMask(virResctrlAlloc *alloc,
         VIR_EXPAND_N(a_type->masks, a_type->nmasks,
                      cache - a_type->nmasks + 1);
 
-    if (!a_type->masks[cache])
-        a_type->masks[cache] = virBitmapNewCopy(mask);
+    if (a_type->masks[cache])
+        virBitmapFree(a_type->masks[cache]);
+
+    a_type->masks[cache] = virBitmapNewCopy(mask);
 
     return 0;
 }