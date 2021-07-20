b0b5820efee7313b9471013821f2ec434bd87853
Kristina Hanicova <khanicov@redhat.com>
2021-07-20 14:58:09 +0200

    genericxml2xmltest: Modify cachetune test to include id
    I changed DO_TEST_DIFFERENT to DO_TEST, which allows us to remove
    the duplicate out file. I also added id attribute for domain
    element in order to parse it as a live XML ('cachetune id' is in
    the output of only live XMLs). Lastly I added id of cachetune to
    test its output value.
    
    Signed-off-by: Kristina Hanicova <khanicov@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/genericxml2xmltest.c
+++ tests/genericxml2xmltest.c
@@ -214,7 +214,7 @@ mymain(void)
 
     DO_TEST("cachetune-small");
     DO_TEST("cachetune-cdp");
-    DO_TEST_DIFFERENT("cachetune");
+    DO_TEST("cachetune");
     DO_TEST_DIFFERENT("cachetune-extra-tunes");
     DO_TEST_FULL("cachetune-colliding-allocs", false, true,
                  TEST_COMPARE_DOM_XML2XML_RESULT_FAIL_PARSE);
