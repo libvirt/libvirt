libvirt/libvirt

commit e9b534905f4fb03d8f31d007a0d1aa1c911e2a2c

nodedev: handle mdevs from multiple parents
Due to a rather unfortunate misunderstanding, we were parsing the list
of defined devices from mdevctl incorrectly. Since my primary
development machine only has a single device capable of mdevs, I
apparently neglected to test multiple parent devices and made some
assumptions based on reading the mdevctl code. These assumptions turned
out to be incorrect, so the parsing failed when devices from more than
one parent device were returned.

The details: mdevctl returns an array of objects representing the
defined devices. But instead of an array of multiple objects (with each
object representing a parent device), the array always contains only a
single object. That object has a separate property for each parent
device.

Signed-off-by: Jonathon Jongsma <jjongsma@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/node_device/node_device_driver.c
@@ -1056,6 +1056,7 @@ nodeDeviceParseMdevctlJSON(const char *jsonstring,
     size_t noutdevs = 0;
     size_t i;
     size_t j;
+    virJSONValue *obj;
 
     json_devicelist = virJSONValueFromString(jsonstring);
 
@@ -1065,31 +1066,33 @@ nodeDeviceParseMdevctlJSON(const char *jsonstring,
         goto error;
     }
 
-    n = virJSONValueArraySize(json_devicelist);
+    /* mdevctl list --dumpjson produces an output that is an array that
+     * contains only a single object which contains a property for each parent
+     * device */
+    if (virJSONValueArraySize(json_devicelist) != 1) {
+        virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
+                       _("Unexpected format for mdevctl response"));
+        goto error;
+    }
 
+    obj = virJSONValueArrayGet(json_devicelist, 0);
+
+    if (!virJSONValueIsObject(obj)) {
+        virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
+                       _("device list is not an object"));
+        goto error;
+    }
+
+    n = virJSONValueObjectKeysNumber(obj);
     for (i = 0; i < n; i++) {
-        virJSONValue *obj = virJSONValueArrayGet(json_devicelist, i);
         const char *parent;
         virJSONValue *child_array;
         int nchildren;
 
-        if (!virJSONValueIsObject(obj)) {
-            virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
-                           _("Parent device is not an object"));
-            goto error;
-        }
-
-        /* mdevctl returns an array of objects.  Each object is a parent device
-         * object containing a single key-value pair which maps from the name
-         * of the parent device to an array of child devices */
-        if (virJSONValueObjectKeysNumber(obj) != 1) {
-            virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
-                           _("Unexpected format for parent device object"));
-            goto error;
-        }
-
-        parent = virJSONValueObjectGetKey(obj, 0);
-        child_array = virJSONValueObjectGetValue(obj, 0);
+        /* The key of each object property is the name of a parent device
+         * which maps to an array of child devices */
+        parent = virJSONValueObjectGetKey(obj, i);
+        child_array = virJSONValueObjectGetValue(obj, i);
 
         if (!virJSONValueIsArray(child_array)) {
             virReportError(VIR_ERR_INTERNAL_ERROR, "%s",

commit 19b088cfb95daab724da8a45df229708192e6250

nodedev: fix internal error when no defined mdevs exist
Commit e9b534905f4 introduced an error when parsing an empty list
returned from mdevctl.

This occurs e.g. if nodedev-undefine is used to undefine the last
defined mdev which causes the following error messages

 libvirtd[33143]: internal error: Unexpected format for mdevctl response
 libvirtd[33143]: internal error: failed to query mdevs from mdevctl:
 libvirtd[33143]: mdevctl failed to updated mediated devices

Signed-off-by: Boris Fiuczynski <fiuczy@linux.ibm.com>
Reviewed-by: Shalini Chellathurai Saroja <shalini@linux.ibm.com>
Reviewed-by: Martin Kletzander <mkletzan@redhat.com>
Reviewed-by: Jonathon Jongsma <jjongsma@redhat.com>

src/node_device/node_device_driver.c
@@ -1139,6 +1139,12 @@ nodeDeviceParseMdevctlJSON(const char *jsonstring,
         goto error;
     }
 
+    if (virJSONValueArraySize(json_devicelist) == 0) {
+        VIR_DEBUG("mdevctl has no defined mediated devices");
+        *devs = NULL;
+        return 0;
+    }
+
     /* mdevctl list --dumpjson produces an output that is an array that
      * contains only a single object which contains a property for each parent
      * device */

tests/nodedevmdevctltest.c
@@ -360,6 +360,7 @@ mymain(void)
 
     DO_TEST_LIST_DEFINED();
 
+    DO_TEST_PARSE_JSON("mdevctl-list-empty");
     DO_TEST_PARSE_JSON("mdevctl-list-multiple");
 
     DO_TEST_DEFINE("mdev_d069d019_36ea_4111_8f0a_8c9a70e21366");