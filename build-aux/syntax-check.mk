33761790925765c696d831cb43c03e446770ec4c
Ján Tomko <jtomko@redhat.com>
2021-09-03 16:39:36 +0200

    Revert "meson: avoid bogus warnings from clang and g_autoptr"
    Commit 345996c6208b281233074362a8d81295e2e711d4 disabled the
    -Wunused-but-set-variable warning on CLang, beacuse it warned
    on variables that were unread, but we relied on the side effects
    of their destructors.
    
    Reinstate the warning now that all the occurrences have been fixed.
    
    Signed-off-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- build-aux/syntax-check.mk
+++ build-aux/syntax-check.mk
@@ -1589,7 +1589,7 @@ exclude_file_name_regexp--sc_prohibit_canonicalize_file_name = \
   ^(build-aux/syntax-check\.mk|tests/virfilemock\.c)$$
 
 exclude_file_name_regexp--sc_prohibit_raw_allocation = \
-  ^(meson\.build|docs/advanced-tests\.rst|src/util/viralloc\.[ch]|examples/.*|tests/(securityselinuxhelper|(vircgroup|nss)mock|commandhelper)\.c|tools/wireshark/src/packet-libvirt\.c|tools/nss/libvirt_nss(_leases|_macs)?\.c)$$
+  ^(docs/advanced-tests\.rst|src/util/viralloc\.[ch]|examples/.*|tests/(securityselinuxhelper|(vircgroup|nss)mock|commandhelper)\.c|tools/wireshark/src/packet-libvirt\.c|tools/nss/libvirt_nss(_leases|_macs)?\.c)$$
 
 exclude_file_name_regexp--sc_prohibit_readlink = \
   ^src/(util/virutil|lxc/lxc_container)\.c$$
