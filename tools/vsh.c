libvirt/libvirt

commit 80f70c74a72aff1727636a1d9f0c7766fbcb9f5b

vsh: Add completer for '--command' of 'help' command
Wrap 'vshReadlineCommandGenerator' into a function with proper prototype
to provide a completer for the help command.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

tools/vsh.c
@@ -3026,9 +3026,20 @@ vshDeinit(vshControl *ctl)
  * Generic commands available to use by any client
  * -----------------------------------------------
  */
+
+static char **
+vshCompleteHelpCommand(vshControl *ctl G_GNUC_UNUSED,
+                       const vshCmd *cmd G_GNUC_UNUSED,
+                       unsigned int completerflags G_GNUC_UNUSED)
+{
+    return vshReadlineCommandGenerator();
+}
+
+
 const vshCmdOptDef opts_help[] = {
     {.name = "command",
      .type = VSH_OT_STRING,
+     .completer = vshCompleteHelpCommand,
      .help = N_("Prints global help, command specific help, or help for a group of related commands")
     },
     {.name = NULL}

commit 9d26af5e5b431eab364a0b36152cc51897ef641c

vsh: Move 'vshReadlineOptionsGenerator' out of '#if WITH_READLINE'
It's now also used in vshCompleteHelpCommand which is outside of the
conditionally compiled code.

Fixes: 80f70c74a7
Signed-off-by: Peter Krempa <pkrempa@redhat.com>

tools/vsh.c
@@ -2543,17 +2543,12 @@ vshTreePrint(vshControl *ctl, vshTreeLookup lookup, void *opaque,
     return ret;
 }
 
-#if WITH_READLINE
-
-/* -----------------
- * Readline stuff
- * -----------------
- */
 
 /**
  * vshReadlineCommandGenerator:
  *
- * Generator function for command completion.
+ * Generator function for command completion. Used also for completing the
+ * '--command' option of the 'help' command.
  *
  * Returns a string list of all commands, or NULL on failure.
  */
@@ -2589,6 +2584,15 @@ vshReadlineCommandGenerator(void)
 
     return g_steal_pointer(&ret);
 }
+
+
+#if WITH_READLINE
+
+/* -----------------
+ * Readline stuff
+ * -----------------
+ */
+
 
 static char **
 vshReadlineOptionsGenerator(const vshCmdDef *cmd,