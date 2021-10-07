e0156069846ab61e04c4505b9b41ac8fdf3e3598
Andrea Bolognani <abologna@redhat.com>
2021-10-07 19:44:26 +0200

    tests: Add test for virtio-iommu address
    virtio-iommu needs to be an integrated device, and our address
    assignment code will make sure that is the case. If the user has
    provided an explicit address, however, we should make sure any
    addresses pointing to a different bus are rejected.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -3253,6 +3253,7 @@ mymain(void)
     DO_TEST_CAPS_LATEST_PARSE_ERROR("virtio-iommu-wrong-machine");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("virtio-iommu-no-acpi");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("virtio-iommu-invalid-address-type");
+    DO_TEST_CAPS_LATEST_PARSE_ERROR("virtio-iommu-invalid-address");
 
     DO_TEST("cpu-hotplug-startup", QEMU_CAPS_QUERY_HOTPLUGGABLE_CPUS);
     DO_TEST_PARSE_ERROR("cpu-hotplug-granularity",
