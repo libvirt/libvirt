a0ec7165e3bbc416478740f6d2e8fef2ece18602
Praveen K Paladugu <prapal@linux.microsoft.com>
2021-10-07 21:49:37 +0000

    tools: add virt-host-validate-ch for ch driver
    Signed-off-by: Wei-Chen Chen <weicche@microsoft.com>
    Signed-off-by: Praveen K Paladugu <prapal@linux.microsoft.com>
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

---
+++ tools/virt-host-validate-ch.c
@@ -0,0 +1,84 @@
+/*
+ * virt-host-validate-ch.c: Sanity check a CH hypervisor host
+ *
+ * Copyright Microsoft Corp. 2020-2021
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library.  If not, see
+ * <http://www.gnu.org/licenses/>.
+ */
+
+#include <config.h>
+
+#include "virarch.h"
+#include "virbitmap.h"
+#include "virt-host-validate-ch.h"
+#include "virt-host-validate-common.h"
+
+int virHostValidateCh(void)
+{
+    int ret = 0;
+    virBitmap *flags;
+    bool hasHwVirt = false;
+    bool hasVirtFlag = false;
+    virArch arch = virArchFromHost();
+    const char *kvmhint =
+        _("Check that CPU and firmware supports virtualization "
+          "and kvm module is loaded");
+
+    if (!(flags = virHostValidateGetCPUFlags()))
+        return -1;
+
+    /* Cloud-Hypervisor only supports x86_64 and aarch64 */
+    switch ((int)arch) {
+    case VIR_ARCH_X86_64:
+        hasVirtFlag = true;
+        kvmhint = _("Check that the 'kvm-intel' or 'kvm-amd' modules are "
+                    "loaded & the BIOS has enabled virtualization");
+        if (virBitmapIsBitSet(flags, VIR_HOST_VALIDATE_CPU_FLAG_SVM) ||
+            virBitmapIsBitSet(flags, VIR_HOST_VALIDATE_CPU_FLAG_VMX))
+            hasHwVirt = true;
+        break;
+    case VIR_ARCH_AARCH64:
+        hasVirtFlag = true;
+        hasHwVirt = true;
+        break;
+    default:
+        hasHwVirt = false;
+        break;
+    }
+
+    if (hasVirtFlag) {
+        virHostMsgCheck("CH", "%s", _("for hardware virtualization"));
+        if (hasHwVirt) {
+            virHostMsgPass();
+        } else {
+            virHostMsgFail(VIR_HOST_VALIDATE_FAIL,
+                           _("Only emulated CPUs are available, performance will be "
+                             "significantly limited"));
+            ret = -1;
+        }
+    }
+
+    if (hasHwVirt || !hasVirtFlag) {
+        if (virHostValidateDeviceExists("CH", "/dev/kvm", VIR_HOST_VALIDATE_FAIL,
+                                        kvmhint) < 0)
+            ret = -1;
+        else if (virHostValidateDeviceAccessible("CH", "/dev/kvm", VIR_HOST_VALIDATE_FAIL,
+                                                 _("Check /dev/kvm is world writable or you are in "
+                                                   "a group that is allowed to access it")) < 0)
+            ret = -1;
+    }
+
+    return ret;
+}
--- tools/virt-host-validate.c
+++ tools/virt-host-validate.c
@@ -40,6 +40,9 @@
 #if WITH_BHYVE
 # include "virt-host-validate-bhyve.h"
 #endif
+#if WITH_CH
+# include "virt-host-validate-ch.h"
+#endif
 
 static void
 show_help(FILE *out, const char *argv0)
@@ -53,6 +56,7 @@ show_help(FILE *out, const char *argv0)
               "   - qemu\n"
               "   - lxc\n"
               "   - bhyve\n"
+              "   - ch\n"
               "\n"
               " Options:\n"
               "   -h, --help     Display command line help\n"
@@ -142,6 +146,14 @@ main(int argc, char **argv)
     if (!hvname || STREQ(hvname, "bhyve")) {
         usedHvname = true;
         if (virHostValidateBhyve() < 0)
+            ret = EXIT_FAILURE;
+    }
+#endif
+
+#if WITH_CH
+    if (!hvname || STREQ(hvname, "ch")) {
+        usedHvname = true;
+        if (virHostValidateCh() < 0)
             ret = EXIT_FAILURE;
     }
 #endif
