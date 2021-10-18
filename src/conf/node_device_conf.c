8eb08e0fc5fe42efd2b0a4226f2b71f183ba193b
Peter Krempa <pkrempa@redhat.com>
2021-10-18 09:54:08 +0200

    virNodeDeviceDefParse: Don't call post-parse callbacks with NULL def
    When parsing of the node device XML fails we'd still call the post-parse
    and validation callbacks which makes no sense. Additionally the
    callbacks were expecting a non-NULL pointer which leads to a crash.
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2014139
    Fixes: d5ae634ba28
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Erik Skultety <eskultet@redhat.com>

--- src/conf/node_device_conf.c
+++ src/conf/node_device_conf.c
@@ -2177,10 +2177,10 @@ virNodeDeviceDefParse(const char *str,
     g_autoptr(xmlDoc) xml = NULL;
     g_autoptr(virNodeDeviceDef) def = NULL;
 
-    if ((xml = virXMLParse(filename, str, _("(node_device_definition)"), NULL, false))) {
-        def = virNodeDeviceDefParseNode(xml, xmlDocGetRootElement(xml),
-                                        create, virt_type);
-    }
+    if (!(xml = virXMLParse(filename, str, _("(node_device_definition)"), NULL, false)) ||
+        !(def = virNodeDeviceDefParseNode(xml, xmlDocGetRootElement(xml),
+                                          create, virt_type)))
+        return NULL;
 
     if (parserCallbacks) {
         int ret = 0;
