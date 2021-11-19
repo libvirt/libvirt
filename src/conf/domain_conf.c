libvirt/libvirt

commit 114e3b423210d316b3326816fd2c33335b1167fe

qemu: match alias when looking for proper <interface> to detach.
Previously we only checked MAC address and PCI address (or CCW
address). This is not enough information in cases where PCI address
isn't provided and multiple interfaces have the same MAC address (for
example, a virtio + hostdev "teaming" pair - their MAC addresses are
always the same).

Resolves: https://bugzilla.redhat.com/1926190
Signed-off-by: Laine Stump <laine@redhat.com>
Reviewed-by: Martin Kletzander <mkletzan@redhat.com>

src/conf/domain_conf.c
@@ -16431,6 +16431,11 @@ virDomainNetFindIdx(virDomainDefPtr def, virDomainNetDefPtr net)
                                             &net->info.addr.ccw))
             continue;
 
+        if (net->info.alias &&
+            STRNEQ_NULLABLE(def->nets[i]->info.alias, net->info.alias)) {
+            continue;
+        }
+
         if (matchidx >= 0) {
             /* there were multiple matches on mac address, and no
              * qualifying guest-side PCI/CCW address was given, so we must

commit 28bb7266a05d5e07e532e66c17eac77130d369d7

conf: match by network alias only if aliases are assigned
Commit 114e3b423210d316b3326816fd2c33335b1167fe added matching by
aliases for interfaces with same MAC address.
( https://bugzilla.redhat.com/show_bug.cgi?id=1926190 )

However, unless the domain is using user aliases (prefixed ua-),
there are no aliases in the persistent definition.

Only match by MAC/PCI/other addresses in that case, to fix update-device
with --persistent flag (AFFECT_LIVE | AFFECT_CONFIG).

https://bugzilla.redhat.com/show_bug.cgi?id=2024098

Fixes: 114e3b423210d316b3326816fd2c33335b1167fe
Signed-off-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Martin Kletzander <mkletzan@redhat.com>

src/conf/domain_conf.c
@@ -15770,8 +15770,8 @@ virDomainNetFindIdx(virDomainDef *def, virDomainNetDef *net)
                                             &net->info.addr.ccw))
             continue;
 
-        if (net->info.alias &&
-            STRNEQ_NULLABLE(def->nets[i]->info.alias, net->info.alias)) {
+        if (net->info.alias && def->nets[i]->info.alias &&
+            STRNEQ(def->nets[i]->info.alias, net->info.alias)) {
             continue;
         }
 