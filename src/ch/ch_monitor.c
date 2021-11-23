libvirt/libvirt

commit 93accefd9eca1bc3d7e923a979ab2d1b8a312ff7

ch_monitor: Add pty json builder function
Add function to build the the json structure to configure a PTY in
cloud-hypervisor.

The devices themselves still aren't allowed in configurations yet
though.

Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
Signed-off-by: William Douglas <william.douglas@intel.com>

src/ch/ch_monitor.c
@@ -90,6 +90,30 @@ virCHMonitorBuildCPUJson(virJSONValue *content, virDomainDef *vmdef)
 }
 
 static int
+virCHMonitorBuildPTYJson(virJSONValue *content, virDomainDef *vmdef)
+{
+    virJSONValue *ptys = virJSONValueNewObject();
+
+    if (vmdef->nconsoles) {
+        g_autoptr(virJSONValue) pty = virJSONValueNewObject();
+        if (virJSONValueObjectAppendString(pty, "mode", "Pty") < 0)
+            return -1;
+        if (virJSONValueObjectAppend(content, "console", &pty) < 0)
+            return -1;
+    }
+
+    if (vmdef->nserials) {
+        g_autoptr(virJSONValue) pty = virJSONValueNewObject();
+        if (virJSONValueObjectAppendString(ptys, "mode", "Pty") < 0)
+            return -1;
+        if (virJSONValueObjectAppend(content, "serial", &pty) < 0)
+            return -1;
+    }
+
+    return 0;
+}
+
+static int
 virCHMonitorBuildKernelRelatedJson(virJSONValue *content, virDomainDef *vmdef)
 {
     virJSONValue *kernel = virJSONValueNewObject();
@@ -369,6 +393,9 @@ virCHMonitorBuildVMJson(virDomainDef *vmdef, char **jsonstr)
                        _("VM is not defined"));
         goto cleanup;
     }
+
+    if (virCHMonitorBuildPTYJson(content, vmdef) < 0)
+        goto cleanup;
 
     if (virCHMonitorBuildCPUJson(content, vmdef) < 0)
         goto cleanup;

commit 096412f1ba5480879539e17e65f6c6c66f493e85

ch: fix logic in virCHMonitorBuildPtyJson
There is a leftover 'ptys' variable, which we only assign
to and one assignment to 'content', where we add an empty
'pty' object.

Remove 'ptys'.

Fixes: 93accefd9eca1bc3d7e923a979ab2d1b8a312ff7
Signed-off-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Kristína Hanicová <khanicov@redhat.com>

src/ch/ch_monitor.c
@@ -88,8 +88,6 @@ virCHMonitorBuildCPUJson(virJSONValue *content, virDomainDef *vmdef)
 static int
 virCHMonitorBuildPTYJson(virJSONValue *content, virDomainDef *vmdef)
 {
-    virJSONValue *ptys = virJSONValueNewObject();
-
     if (vmdef->nconsoles) {
         g_autoptr(virJSONValue) pty = virJSONValueNewObject();
         if (virJSONValueObjectAppendString(pty, "mode", "Pty") < 0)
@@ -100,7 +98,7 @@ virCHMonitorBuildPTYJson(virJSONValue *content, virDomainDef *vmdef)
 
     if (vmdef->nserials) {
         g_autoptr(virJSONValue) pty = virJSONValueNewObject();
-        if (virJSONValueObjectAppendString(ptys, "mode", "Pty") < 0)
+        if (virJSONValueObjectAppendString(pty, "mode", "Pty") < 0)
             return -1;
         if (virJSONValueObjectAppend(content, "serial", &pty) < 0)
             return -1;