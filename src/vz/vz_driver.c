libvirt/libvirt

commit c18d9e23fafabcfbb80481e0705931036b8e7331

lib: Don't check for virCapabilitiesAddGuest() retval
The virCapabilitiesAddGuest() function can't fail. It aborts on
OOM. Therefore, there's no need to check for its return value.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/vz/vz_driver.c
@@ -83,8 +83,8 @@ vzCapsAddGuestDomain(virCaps *caps,
 {
     virCapsGuest *guest;
 
-    if ((guest = virCapabilitiesAddGuest(caps, ostype, arch, emulator,
-                                         NULL, 0, NULL)) == NULL)
+    guest = virCapabilitiesAddGuest(caps, ostype, arch,
+                                    emulator, NULL, 0, NULL);
         return -1;
 
 

src/bhyve/bhyve_capabilities.c
@@ -50,11 +50,9 @@ virBhyveCapsBuild(void)
                                    false, false)) == NULL)
         return NULL;
 
-    if ((guest = virCapabilitiesAddGuest(caps, VIR_DOMAIN_OSTYPE_HVM,
-                                         VIR_ARCH_X86_64,
-                                         "bhyve",
-                                         NULL, 0, NULL)) == NULL)
-        goto error;
+    guest = virCapabilitiesAddGuest(caps, VIR_DOMAIN_OSTYPE_HVM,
+                                    VIR_ARCH_X86_64, "bhyve",
+                                    NULL, 0, NULL);
 
     if (virCapabilitiesAddGuestDomain(guest, VIR_DOMAIN_VIRT_BHYVE,
                                       NULL, NULL, 0, NULL) == NULL)

src/ch/ch_conf.c
@@ -65,14 +65,8 @@ virCaps *virCHDriverCapsInit(void)
     if (virCapabilitiesInitCaches(caps) < 0)
         goto cleanup;
 
-    if ((guest = virCapabilitiesAddGuest(caps,
-                                         VIR_DOMAIN_OSTYPE_HVM,
-                                         caps->host.arch,
-                                         NULL,
-                                         NULL,
-                                         0,
-                                         NULL)) == NULL)
-        goto cleanup;
+    guest = virCapabilitiesAddGuest(caps, VIR_DOMAIN_OSTYPE_HVM,
+                                    caps->host.arch, NULL, NULL, 0, NULL);
 
     if (virCapabilitiesAddGuestDomain(guest,
                                       VIR_DOMAIN_VIRT_KVM,

src/esx/esx_driver.c
@@ -567,9 +567,6 @@ esxCapsInit(esxPrivate *priv)
                                     NULL, NULL, 0,
                                     NULL);
 
-    if (!guest)
-        goto failure;
-
     if (!virCapabilitiesAddGuestDomain(guest, VIR_DOMAIN_VIRT_VMWARE, NULL, NULL, 0, NULL))
         goto failure;
 
@@ -579,9 +576,6 @@ esxCapsInit(esxPrivate *priv)
                                         VIR_ARCH_X86_64,
                                         NULL, NULL,
                                         0, NULL);
-
-        if (!guest)
-            goto failure;
 
         if (!virCapabilitiesAddGuestDomain(guest, VIR_DOMAIN_VIRT_VMWARE, NULL, NULL, 0, NULL))
             goto failure;

src/hyperv/hyperv_driver.c
@@ -276,8 +276,6 @@ hypervCapsInit(hypervPrivate *priv)
     /* i686 caps */
     guest = virCapabilitiesAddGuest(caps, VIR_DOMAIN_OSTYPE_HVM, VIR_ARCH_I686,
                                     NULL, NULL, 0, NULL);
-    if (!guest)
-        goto error;
 
     if (!virCapabilitiesAddGuestDomain(guest, VIR_DOMAIN_VIRT_HYPERV, NULL, NULL, 0, NULL))
         goto error;
@@ -285,8 +283,6 @@ hypervCapsInit(hypervPrivate *priv)
     /* x86_64 caps */
     guest = virCapabilitiesAddGuest(caps, VIR_DOMAIN_OSTYPE_HVM, VIR_ARCH_X86_64,
                                     NULL, NULL, 0, NULL);
-    if (!guest)
-        goto error;
 
     if (!virCapabilitiesAddGuestDomain(guest, VIR_DOMAIN_VIRT_HYPERV, NULL, NULL, 0, NULL))
         goto error;

src/libxl/libxl_capabilities.c
@@ -473,20 +473,17 @@ libxlCapsInitGuests(libxl_ctx *ctx, virCaps *caps)
         if ((machines = virCapabilitiesAllocMachines(xen_machines, 1)) == NULL)
             return -1;
 
-        if ((guest = virCapabilitiesAddGuest(caps,
-                                             guest_archs[i].hvm ? VIR_DOMAIN_OSTYPE_HVM :
-                                                (guest_archs[i].pvh ? VIR_DOMAIN_OSTYPE_XENPVH :
-                                                 VIR_DOMAIN_OSTYPE_XEN),
-                                             guest_archs[i].arch,
-                                             LIBXL_EXECBIN_DIR "/qemu-system-i386",
-                                             (guest_archs[i].hvm ?
-                                              LIBXL_FIRMWARE_DIR "/hvmloader" :
-                                              NULL),
-                                             1,
-                                             machines)) == NULL) {
-            virCapabilitiesFreeMachines(machines, 1);
-            return -1;
-        }
+        guest = virCapabilitiesAddGuest(caps,
+                                        guest_archs[i].hvm ? VIR_DOMAIN_OSTYPE_HVM :
+                                        (guest_archs[i].pvh ? VIR_DOMAIN_OSTYPE_XENPVH :
+                                         VIR_DOMAIN_OSTYPE_XEN),
+                                        guest_archs[i].arch,
+                                        LIBXL_EXECBIN_DIR "/qemu-system-i386",
+                                        (guest_archs[i].hvm ?
+                                         LIBXL_FIRMWARE_DIR "/hvmloader" :
+                                         NULL),
+                                        1,
+                                        machines);
         machines = NULL;
 
         if (virCapabilitiesAddGuestDomain(guest,

src/lxc/lxc_conf.c
@@ -97,14 +97,8 @@ virCaps *virLXCDriverCapsInit(virLXCDriver *driver)
                                          LIBEXECDIR)))
         goto error;
 
-    if ((guest = virCapabilitiesAddGuest(caps,
-                                         VIR_DOMAIN_OSTYPE_EXE,
-                                         caps->host.arch,
-                                         lxc_path,
-                                         NULL,
-                                         0,
-                                         NULL)) == NULL)
-        goto error;
+    guest = virCapabilitiesAddGuest(caps, VIR_DOMAIN_OSTYPE_EXE,
+                                    caps->host.arch, lxc_path, NULL, 0, NULL);
 
     if (virCapabilitiesAddGuestDomain(guest,
                                       VIR_DOMAIN_VIRT_LXC,
@@ -116,14 +110,8 @@ virCaps *virLXCDriverCapsInit(virLXCDriver *driver)
 
     /* On 64-bit hosts, we can use personality() to request a 32bit process */
     if ((altArch = lxcContainerGetAlt32bitArch(caps->host.arch)) != VIR_ARCH_NONE) {
-        if ((guest = virCapabilitiesAddGuest(caps,
-                                             VIR_DOMAIN_OSTYPE_EXE,
-                                             altArch,
-                                             lxc_path,
-                                             NULL,
-                                             0,
-                                             NULL)) == NULL)
-            goto error;
+        guest = virCapabilitiesAddGuest(caps, VIR_DOMAIN_OSTYPE_EXE,
+                                        altArch, lxc_path, NULL, 0, NULL);
 
         if (virCapabilitiesAddGuestDomain(guest,
                                           VIR_DOMAIN_VIRT_LXC,

src/openvz/openvz_conf.c
@@ -156,14 +156,8 @@ virCaps *openvzCapsInit(void)
     if (virCapabilitiesInitCaches(caps) < 0)
         return NULL;
 
-    if ((guest = virCapabilitiesAddGuest(caps,
-                                         VIR_DOMAIN_OSTYPE_EXE,
-                                         caps->host.arch,
-                                         NULL,
-                                         NULL,
-                                         0,
-                                         NULL)) == NULL)
-        return NULL;
+    guest = virCapabilitiesAddGuest(caps, VIR_DOMAIN_OSTYPE_EXE,
+                                    caps->host.arch, NULL, NULL, 0, NULL);
 
     if (virCapabilitiesAddGuestDomain(guest,
                                       VIR_DOMAIN_VIRT_OPENVZ,

commit ac3e9f5efcd0c4a67ef9b3a9a15c3f75a121e2d4

vz: fix vzCapsAddGuestDomain
There is a stray 'return -1' executed on all code paths.

Fixes: c18d9e23fafabcfbb80481e0705931036b8e7331
Signed-off-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/vz/vz_driver.c
@@ -85,8 +85,6 @@ vzCapsAddGuestDomain(virCaps *caps,
 
     guest = virCapabilitiesAddGuest(caps, ostype, arch,
                                     emulator, NULL, 0, NULL);
-        return -1;
-
 
     virCapabilitiesAddGuestDomain(guest, virt_type, NULL, NULL, 0, NULL);
 