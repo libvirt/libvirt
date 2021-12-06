libvirt/libvirt

commit 795e9e05c3b6b9ef3abe6f6078a6373a136ec23b

virPCIGetVirtualFunctions: Fetch also network interface name if needed
'virNetDevGetVirtualFunctions' calls 'virPCIGetVirtualFunctions' and
then re-iterates the returned list to fetch the interface names for the
returned virtual functions.

If we move the fetching of the interface name into
virPCIGetVirtualFunctions we can simplify the code and remove a bunch of
impossible error states.

To accomplish this the function is renamed to
'virPCIGetVirtualFunctionsFull' while keeping a wrapper with original
name and if the physical port ID is passed the interface name is fetched
too without the need to re-convert the address into a sysfs link.

For now 'virNetDevGetVirtualFunctions' still converts the returned data
into two lists.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/util/virnetdev.c
@@ -1227,62 +1227,30 @@ virNetDevGetVirtualFunctions(const char *pfname,
                              virPCIDeviceAddress ***virt_fns,
                              size_t *n_vfname)
 {
-    int ret = -1;
     size_t i;
     g_autofree char *pf_sysfs_device_link = NULL;
     g_autofree char *pfPhysPortID = NULL;
     g_autoptr(virPCIVirtualFunctionList) vfs = NULL;
 
-    *virt_fns = NULL;
-    *n_vfname = 0;
-
     if (virNetDevGetPhysPortID(pfname, &pfPhysPortID) < 0)
-        goto cleanup;
+        return -1;
 
     if (virNetDevSysfsFile(&pf_sysfs_device_link, pfname, "device") < 0)
-        goto cleanup;
+        return -1;
 
-    if (virPCIGetVirtualFunctions(pf_sysfs_device_link, &vfs) < 0)
-        goto cleanup;
+    if (virPCIGetVirtualFunctionsFull(pf_sysfs_device_link, &vfs, pfPhysPortID) < 0)
+        return -1;
 
     *vfname = g_new0(char *, vfs->nfunctions);
     *virt_fns = g_new0(virPCIDeviceAddress *, vfs->nfunctions);
     *n_vfname = vfs->nfunctions;
 
     for (i = 0; i < *n_vfname; i++) {
-        g_autofree char *pci_sysfs_device_link = NULL;
-
         virt_fns[i] = g_steal_pointer(&vfs->functions[i].addr);
-
-        if (virPCIDeviceAddressGetSysfsFile((*virt_fns)[i],
-                                            &pci_sysfs_device_link) < 0) {
-            virReportSystemError(ENOSYS, "%s",
-                                 _("Failed to get PCI SYSFS file"));
-            goto cleanup;
-        }
-
-        if (virPCIGetNetName(pci_sysfs_device_link, 0,
-                             pfPhysPortID, &((*vfname)[i])) < 0) {
-            goto cleanup;
-        }
-
-        if (!(*vfname)[i])
-            VIR_INFO("VF does not have an interface name");
+        vfname[i] = g_steal_pointer(&vfs->functions[i].ifname);
     }
 
-    ret = 0;
-
- cleanup:
-    if (ret < 0) {
-        virStringListFreeCount(*vfname, *n_vfname);
-
-        for (i = 0; i < *n_vfname; i++)
-            VIR_FREE((*virt_fns)[i]);
-        VIR_FREE(*virt_fns);
-        *vfname = NULL;
-        *n_vfname = 0;
-    }
-    return ret;
+    return 0;
 }
 
 /**

src/util/virpci.c
@@ -2257,9 +2257,18 @@ virPCIVirtualFunctionListFree(virPCIVirtualFunctionList *list)
 
     for (i = 0; i < list->nfunctions; i++) {
         g_free(list->functions[i].addr);
+        g_free(list->functions[i].ifname);
     }
 
     g_free(list);
+}
+
+
+int
+virPCIGetVirtualFunctions(const char *sysfs_path,
+                          virPCIVirtualFunctionList **vfs)
+{
+    return virPCIGetVirtualFunctionsFull(sysfs_path, vfs, NULL);
 }
 
 
@@ -2332,12 +2341,20 @@ virPCIGetPhysicalFunction(const char *vf_sysfs_path,
 }
 
 
-/*
- * Returns virtual functions of a physical function
+/**
+ * virPCIGetVirtualFunctionsFull:
+ * @sysfs_path: path to physical function sysfs entry
+ * @vfs: filled with the virtual function data
+ * @pfPhysPortID: Optional physical port id. If provided the network interface
+ *                name of the VFs is queried too.
+ *
+ *
+ * Returns virtual functions of a physical function.
  */
 int
-virPCIGetVirtualFunctions(const char *sysfs_path,
-                          virPCIVirtualFunctionList **vfs)
+virPCIGetVirtualFunctionsFull(const char *sysfs_path,
+                              virPCIVirtualFunctionList **vfs,
+                              const char *pfPhysPortID)
 {
     g_autofree char *totalvfs_file = NULL;
     g_autofree char *totalvfs_str = NULL;
@@ -2363,7 +2380,7 @@ virPCIGetVirtualFunctions(const char *sysfs_path,
 
     do {
         g_autofree char *device_link = NULL;
-        struct virPCIVirtualFunction fnc = { NULL };
+        struct virPCIVirtualFunction fnc = { NULL, NULL };
 
         /* look for virtfn%d links until one isn't found */
         device_link = g_strdup_printf("%s/virtfn%zu", sysfs_path, list->nfunctions);
@@ -2376,6 +2393,13 @@ virPCIGetVirtualFunctions(const char *sysfs_path,
                            _("Failed to get SRIOV function from device link '%s'"),
                            device_link);
             return -1;
+        }
+
+        if (pfPhysPortID) {
+            if (virPCIGetNetName(device_link, 0, pfPhysPortID, &fnc.ifname) < 0) {
+                g_free(fnc.addr);
+                return -1;
+            }
         }
 
         VIR_APPEND_ELEMENT(list->functions, list->nfunctions, fnc);
@@ -2636,8 +2660,9 @@ virPCIGetPhysicalFunction(const char *vf_sysfs_path G_GNUC_UNUSED,
 }
 
 int
-virPCIGetVirtualFunctions(const char *sysfs_path G_GNUC_UNUSED,
-                          virPCIVirtualFunctionList **vfs G_GNUC_UNUSED)
+virPCIGetVirtualFunctionsFull(const char *sysfs_path G_GNUC_UNUSED,
+                              virPCIVirtualFunctionList **vfs G_GNUC_UNUSED,
+                              const char *pfPhysPortID G_GNUC_UNUSED)
 {
     virReportError(VIR_ERR_INTERNAL_ERROR, "%s", _(unsupported));
     return -1;

src/util/virpci.h
@@ -214,6 +214,7 @@ int virPCIGetPhysicalFunction(const char *vf_sysfs_path,
 
 struct virPCIVirtualFunction {
     virPCIDeviceAddress *addr;
+    char *ifname;
 };
 
 struct _virPCIVirtualFunctionList {
@@ -226,6 +227,9 @@ typedef struct _virPCIVirtualFunctionList virPCIVirtualFunctionList;
 void virPCIVirtualFunctionListFree(virPCIVirtualFunctionList *list);
 G_DEFINE_AUTOPTR_CLEANUP_FUNC(virPCIVirtualFunctionList, virPCIVirtualFunctionListFree);
 
+int virPCIGetVirtualFunctionsFull(const char *sysfs_path,
+                                  virPCIVirtualFunctionList **vfs,
+                                  const char *pfPhysPortID);
 int virPCIGetVirtualFunctions(const char *sysfs_path,
                               virPCIVirtualFunctionList **vfs);
 

src/libvirt_private.syms
@@ -3012,6 +3012,7 @@ virPCIGetPhysicalFunction;
 virPCIGetVirtualFunctionIndex;
 virPCIGetVirtualFunctionInfo;
 virPCIGetVirtualFunctions;
+virPCIGetVirtualFunctionsFull;
 virPCIHeaderTypeFromString;
 virPCIHeaderTypeToString;
 virPCIIsVirtualFunction;

commit 71345f91d721a865ae7f578b802c3640179f03d9

util: fix erroneous requirement for phys_port_id to get ifname of a VF
Commit 795e9e05c3 (libvirt-7.7.0) refactored the code in virpci.c and
virnetdev.c that gathered lists of the Virtual Functions (VF) of an
SRIOV Physical Function (PF) to simplify the code.

Unfortunately the simplification made the assumption, in the new
function virPCIGetVirtualFunctionsFull(), that a VF's netdev
interface name should only be retrieved if the PF had a valid
phys_port_id. That is an incorrect assumption - only a small handful
of (now previous-generation) Mellanox SRIOV cards actually use
phys_port_id (this is for an odd design where there are multiple
physical network ports on a single PCI address); all other SRIOV cards
(including new Mellanox cards) have a file in sysfs called
phys_port_id, but it can't be read, and so the pfPhysPortID string is
NULL.

The result of this logic error is that virtual networks that are a
pool of VFs to be used for macvtap connections will be unable to
start, giving an errror like this:

 VF 0 of SRIOV PF enp130s0f0 couldn't be added to the interface pool because it isn't bound to a network driver - possibly in use elsewhere

This error message is misinformed - the caller of
virNetDevGetVirtualFunctionsFull() only *thinks* that the VF isn't
bound to a network driver because it doesn't see a netdev name for the
VF in the list. But that's only because
virNetDevGetVirtualFunctionsFull() didn't even try to get the names!

We do need a way for virPCIGetVirtualFunctionsFull() to sometimes
retrieve the netdev names and sometimes not. One way of doing that
would be to send down the netdev name of the PF whenever we also want
to know the netdev names of the VFs, but send a NULL when we
don't. This can conveniently be done by just *replacing* pfPhysPortID
in the arglist with pfNetDevName - pfPhysPortID is determined by
simply calling virNetDevGetPhysPortID(pfNetDevName) so we can just
make that call down in virPCIGetVirtualFunctionsFull() (when needed).

This solves the regression introduced by commit 795e9e05c3, and also

src/util/virnetdev.c
@@ -1223,15 +1223,11 @@ virNetDevGetVirtualFunctions(const char *pfname,
                              virPCIVirtualFunctionList **vfs)
 {
     g_autofree char *pf_sysfs_device_link = NULL;
-    g_autofree char *pfPhysPortID = NULL;
-
-    if (virNetDevGetPhysPortID(pfname, &pfPhysPortID) < 0)
-        return -1;
 
     if (virNetDevSysfsFile(&pf_sysfs_device_link, pfname, "device") < 0)
         return -1;
 
-    if (virPCIGetVirtualFunctionsFull(pf_sysfs_device_link, vfs, pfPhysPortID) < 0)
+    if (virPCIGetVirtualFunctionsFull(pf_sysfs_device_link, vfs, pfname) < 0)
         return -1;
 
     return 0;

src/util/virpci.c
@@ -2340,8 +2340,8 @@ virPCIGetPhysicalFunction(const char *vf_sysfs_path,
  * virPCIGetVirtualFunctionsFull:
  * @sysfs_path: path to physical function sysfs entry
  * @vfs: filled with the virtual function data
- * @pfPhysPortID: Optional physical port id. If provided the network interface
- *                name of the VFs is queried too.
+ * @pfNetDevName: Optional netdev name of this PF. If provided, the netdev
+ *                names of the VFs are queried too.
  *
  *
  * Returns virtual functions of a physical function.
@@ -2349,7 +2349,7 @@ virPCIGetPhysicalFunction(const char *vf_sysfs_path,
 int
 virPCIGetVirtualFunctionsFull(const char *sysfs_path,
                               virPCIVirtualFunctionList **vfs,
-                              const char *pfPhysPortID)
+                              const char *pfNetDevName)
 {
     g_autofree char *totalvfs_file = NULL;
     g_autofree char *totalvfs_str = NULL;
@@ -2390,8 +2390,12 @@ virPCIGetVirtualFunctionsFull(const char *sysfs_path,
             return -1;
         }
 
-        if (pfPhysPortID) {
-            if (virPCIGetNetName(device_link, 0, pfPhysPortID, &fnc.ifname) < 0) {
+        if (pfNetDevName) {
+            g_autofree char *pfPhysPortID = NULL;
+
+            if (virNetDevGetPhysPortID(pfNetDevName, &pfPhysPortID) < 0 ||
+                virPCIGetNetName(device_link, 0, pfPhysPortID, &fnc.ifname) < 0) {
+
                 g_free(fnc.addr);
                 return -1;
             }
@@ -2712,7 +2716,7 @@ virPCIGetPhysicalFunction(const char *vf_sysfs_path G_GNUC_UNUSED,
 int
 virPCIGetVirtualFunctionsFull(const char *sysfs_path G_GNUC_UNUSED,
                               virPCIVirtualFunctionList **vfs G_GNUC_UNUSED,
-                              const char *pfPhysPortID G_GNUC_UNUSED)
+                              const char *pfNetDevName G_GNUC_UNUSED)
 {
     virReportError(VIR_ERR_INTERNAL_ERROR, "%s", _(unsupported));
     return -1;

src/util/virpci.h
@@ -230,7 +230,7 @@ G_DEFINE_AUTOPTR_CLEANUP_FUNC(virPCIVirtualFunctionList, virPCIVirtualFunctionLi
 
 int virPCIGetVirtualFunctionsFull(const char *sysfs_path,
                                   virPCIVirtualFunctionList **vfs,
-                                  const char *pfPhysPortID);
+                                  const char *pfNetDevName);
 int virPCIGetVirtualFunctions(const char *sysfs_path,
                               virPCIVirtualFunctionList **vfs);
 