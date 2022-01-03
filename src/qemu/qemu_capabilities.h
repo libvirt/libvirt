bd3d00babc9b9b51bf2ee3ee39fafe479b8f8ae3
Peter Krempa <pkrempa@redhat.com>
2022-01-03 15:50:49 +0100

    qemu: Revert to using non-JSON commandline for -device
    When -device is configured via JSON a bug [1] is triggered in qemu were
    the DEVICE_DELETED event for the removal of the device frontend is no
    longer delivered to libvirt. Without the DEVICE_DELETED event we don't
    remove the corresponding entries in the VM XML.
    
    Until qemu will be fixed we must stop using the JSON syntax for -device.
    
    This patch removes the detection of the capability. The capability is
    used only during startup of a fresh VM so we don't need to consider any
    compaitibility steps for existing VMs.
    
    For users who wish to use 'libvirt-7.9' and 'libvirt-7.10' with
    'qemu-6.2' there are two possible workarounds:
    
     - filter out the 'device.json' qemu capability '/etc/libvirt/qemu.conf':
    
       capability_filters = [ "device.json" ]
    
     - filter out the 'device.json' qemu capability via qemu namespace XML:
    
       <domain type='kvm' xmlns:qemu='http://libvirt.org/schemas/domain/qemu/1.0'>
         [...]
         <qemu:capabilities>
           <qemu:del capability='device.json'/>
         </qemu:capabilities>
       </domain>
    
    We must never again use the same capability name as we are now
    instructing users to filter it as a workaround so once qemu is fixed
    we'll need to pick a new capability value for it.
    
    [1] https://bugzilla.redhat.com/show_bug.cgi?id=2036669
    
    Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2035237
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Ani Sinha <ani@anisinha.ca>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/qemu/qemu_capabilities.h
+++ src/qemu/qemu_capabilities.h
@@ -628,7 +628,7 @@ typedef enum { /* virQEMUCapsFlags grouping marker for syntax-check */
     QEMU_CAPS_CHARDEV_JSON, /* -chardev accepts JSON */
 
     /* 415 */
-    QEMU_CAPS_DEVICE_JSON, /* -device accepts JSON */
+    QEMU_CAPS_DEVICE_JSON, /* -device accepts JSON (must not be used - users are filtering the capbility) */
     QEMU_CAPS_QUERY_DIRTY_RATE, /* accepts query-dirty-rate */
     QEMU_CAPS_RBD_ENCRYPTION, /* Ceph RBD encryption support */
     QEMU_CAPS_SEV_GUEST_KERNEL_HASHES, /* sev-guest.kernel-hashes= */
