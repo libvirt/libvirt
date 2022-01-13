libvirt/libvirt

commit f4aae9726df2bc910281ae5041ad3cfd9ee54a81

qemu_snapshot: create: move snapshot redefine to separate function
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_snapshot.c
@@ -1714,6 +1714,48 @@ qemuSnapshotCreateWriteMetadata(virDomainObj *vm,
 }
 
 
+static virDomainSnapshotPtr
+qemuSnapshotRedefine(virDomainObj *vm,
+                     virDomainPtr domain,
+                     virDomainSnapshotDef *def,
+                     virQEMUDriver *driver,
+                     virQEMUDriverConfig *cfg,
+                     unsigned int flags)
+{
+    virDomainMomentObj *snap = NULL;
+    virDomainSnapshotPtr ret = NULL;
+
+    if (virDomainSnapshotRedefinePrep(vm, &def, &snap,
+                                      driver->xmlopt,
+                                      flags) < 0)
+        return NULL;
+
+    if (!snap) {
+        if (!(snap = virDomainSnapshotAssignDef(vm->snapshots, def)))
+            return NULL;
+    }
+    /* XXX Should we validate that the redefined snapshot even
+     * makes sense, such as checking that qemu-img recognizes the
+     * snapshot name in at least one of the domain's disks?  */
+
+    if (flags & VIR_DOMAIN_SNAPSHOT_CREATE_CURRENT)
+        qemuSnapshotSetCurrent(vm, snap);
+
+    if (qemuSnapshotCreateWriteMetadata(vm, snap, driver, cfg) < 0)
+        goto error;
+
+    ret = virGetDomainSnapshot(domain, snap->def->name);
+    if (!ret)
+        goto error;
+
+    return ret;
+
+ error:
+    virDomainSnapshotObjListRemove(vm->snapshots, snap);
+    return NULL;
+}
+
+
 virDomainSnapshotPtr
 qemuSnapshotCreateXML(virDomainPtr domain,
                       virDomainObj *vm,
@@ -1771,15 +1813,8 @@ qemuSnapshotCreateXML(virDomainPtr domain,
     qemuDomainObjSetAsyncJobMask(vm, QEMU_JOB_NONE);
 
     if (redefine) {
-        if (virDomainSnapshotRedefinePrep(vm, &def, &snap,
-                                          driver->xmlopt,
-                                          flags) < 0)
-            goto endjob;
-
-        if (!snap) {
-            if (!(snap = virDomainSnapshotAssignDef(vm->snapshots, def)))
-                goto endjob;
-        }
+        snapshot = qemuSnapshotRedefine(vm, domain, def, driver, cfg, flags);
+        goto endjob;
     } else {
         virDomainMomentObj *current = NULL;
 
@@ -1801,11 +1836,7 @@ qemuSnapshotCreateXML(virDomainPtr domain,
     }
 
     /* actually do the snapshot */
-    if (redefine) {
-        /* XXX Should we validate that the redefined snapshot even
-         * makes sense, such as checking that qemu-img recognizes the
-         * snapshot name in at least one of the domain's disks?  */
-    } else if (virDomainObjIsActive(vm)) {
+    if (virDomainObjIsActive(vm)) {
         if (flags & VIR_DOMAIN_SNAPSHOT_CREATE_DISK_ONLY ||
             virDomainSnapshotObjGetDef(snap)->memory == VIR_DOMAIN_SNAPSHOT_LOCATION_EXTERNAL) {
             /* external full system or disk snapshot */
@@ -1839,7 +1870,7 @@ qemuSnapshotCreateXML(virDomainPtr domain,
         goto endjob;
 
     if (!(flags & VIR_DOMAIN_SNAPSHOT_CREATE_NO_METADATA)) {
-        if (!redefine || (flags & VIR_DOMAIN_SNAPSHOT_CREATE_CURRENT))
+        if ((flags & VIR_DOMAIN_SNAPSHOT_CREATE_CURRENT))
             qemuSnapshotSetCurrent(vm, snap);
 
         if (qemuSnapshotCreateWriteMetadata(vm, snap, driver, cfg) < 0) {

commit 59e0d130fed50a9833a32654171fc71d7337a6f2

qemuSnapshotRedefine: Fix use of snapshot definition after free
Commit f4aae9726df factored out the snapshot redefinition code into a
separate function, but didn't account for the fact that the code is
consuming the reference to the snapshot definition and by moving the
code away the caller (qemuSnapshotCreateXML) now frees the definition
which didn't happen before as we cleared the pointer.

Fix it by increasing the reference locally. Later patches will refactor
the code so that it's more obvious what's happening.

Fixes: f4aae9726df
Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2039651
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_snapshot.c
@@ -1709,13 +1709,14 @@ qemuSnapshotCreateWriteMetadata(virDomainObj *vm,
 static virDomainSnapshotPtr
 qemuSnapshotRedefine(virDomainObj *vm,
                      virDomainPtr domain,
-                     virDomainSnapshotDef *snapdef,
+                     virDomainSnapshotDef *snapdeftmp,
                      virQEMUDriver *driver,
                      virQEMUDriverConfig *cfg,
                      unsigned int flags)
 {
     virDomainMomentObj *snap = NULL;
     virDomainSnapshotPtr ret = NULL;
+    g_autoptr(virDomainSnapshotDef) snapdef = virObjectRef(snapdeftmp);
 
     if (virDomainSnapshotRedefinePrep(vm, &snapdef, &snap,
                                       driver->xmlopt,
@@ -1725,6 +1726,7 @@ qemuSnapshotRedefine(virDomainObj *vm,
     if (!snap) {
         if (!(snap = virDomainSnapshotAssignDef(vm->snapshots, snapdef)))
             return NULL;
+        snapdef = NULL;
     }
     /* XXX Should we validate that the redefined snapshot even
      * makes sense, such as checking that qemu-img recognizes the