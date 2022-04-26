libvirt/libvirt

commit 034432e47b72cff45c905feadf7165af8bb50b7e

docstring: typedef: libvirt:: Add 'Since version' metadata
Either create or append to existing docstring, the version (git tag)
that a given typedef was introduced in the format:

    Since: v1.2.3

Signed-off-by: Victor Toso <victortoso@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

include/libvirt/libvirt-storage.h
@@ -32,6 +32,9 @@
  * virStoragePool:
  *
  * a virStoragePool is a private structure representing a storage pool
+ *
+ * Since: v0.4.1
+ *
  */
 typedef struct _virStoragePool virStoragePool;
 
@@ -40,10 +43,19 @@ typedef struct _virStoragePool virStoragePool;
  *
  * a virStoragePoolPtr is pointer to a virStoragePool private structure, this is the
  * type used to reference a storage pool in the API.
+ *
+ * Since: v0.4.1
+ *
  */
 typedef virStoragePool *virStoragePoolPtr;
 
 
+/**
+ * virStoragePoolState:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef enum {
     VIR_STORAGE_POOL_INACTIVE = 0, /* Not running */
     VIR_STORAGE_POOL_BUILDING = 1, /* Initializing pool, not available */
@@ -56,6 +68,12 @@ typedef enum {
 # endif
 } virStoragePoolState;
 
+/**
+ * virStoragePoolBuildFlags:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef enum {
     VIR_STORAGE_POOL_BUILD_NEW  = 0,   /* Regular build from scratch */
     VIR_STORAGE_POOL_BUILD_REPAIR = (1 << 0), /* Repair / reinitialize */
@@ -64,11 +82,23 @@ typedef enum {
     VIR_STORAGE_POOL_BUILD_OVERWRITE = (1 << 3),  /* Overwrite data */
 } virStoragePoolBuildFlags;
 
+/**
+ * virStoragePoolDeleteFlags:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef enum {
     VIR_STORAGE_POOL_DELETE_NORMAL = 0, /* Delete metadata only    (fast) */
     VIR_STORAGE_POOL_DELETE_ZEROED = 1 << 0,  /* Clear all data to zeros (slow) */
 } virStoragePoolDeleteFlags;
 
+/**
+ * virStoragePoolCreateFlags:
+ *
+ * Since: v1.3.1
+ *
+ */
 typedef enum {
     /* Create the pool but do not perform pool build */
     VIR_STORAGE_POOL_CREATE_NORMAL = 0,
@@ -87,6 +117,12 @@ typedef enum {
     VIR_STORAGE_POOL_CREATE_WITH_BUILD_NO_OVERWRITE = 1 << 2,
 } virStoragePoolCreateFlags;
 
+/**
+ * virStoragePoolInfo:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef struct _virStoragePoolInfo virStoragePoolInfo;
 
 struct _virStoragePoolInfo {
@@ -96,6 +132,12 @@ struct _virStoragePoolInfo {
     unsigned long long available;  /* Remaining free space bytes */
 };
 
+/**
+ * virStoragePoolInfoPtr:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef virStoragePoolInfo *virStoragePoolInfoPtr;
 
 
@@ -103,6 +145,9 @@ typedef virStoragePoolInfo *virStoragePoolInfoPtr;
  * virStorageVol:
  *
  * a virStorageVol is a private structure representing a storage volume
+ *
+ * Since: v0.4.1
+ *
  */
 typedef struct _virStorageVol virStorageVol;
 
@@ -111,10 +156,19 @@ typedef struct _virStorageVol virStorageVol;
  *
  * a virStorageVolPtr is pointer to a virStorageVol private structure, this is the
  * type used to reference a storage volume in the API.
+ *
+ * Since: v0.4.1
+ *
  */
 typedef virStorageVol *virStorageVolPtr;
 
 
+/**
+ * virStorageVolType:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef enum {
     VIR_STORAGE_VOL_FILE = 0,     /* Regular file based volumes */
     VIR_STORAGE_VOL_BLOCK = 1,    /* Block based volumes */
@@ -129,12 +183,24 @@ typedef enum {
 # endif
 } virStorageVolType;
 
+/**
+ * virStorageVolDeleteFlags:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef enum {
     VIR_STORAGE_VOL_DELETE_NORMAL = 0, /* Delete metadata only    (fast) */
     VIR_STORAGE_VOL_DELETE_ZEROED = 1 << 0,  /* Clear all data to zeros (slow) */
     VIR_STORAGE_VOL_DELETE_WITH_SNAPSHOTS = 1 << 1, /* Force removal of volume, even if in use */
 } virStorageVolDeleteFlags;
 
+/**
+ * virStorageVolWipeAlgorithm:
+ *
+ * Since: v0.9.10
+ *
+ */
 typedef enum {
     VIR_STORAGE_VOL_WIPE_ALG_ZERO = 0, /* 1-pass, all zeroes */
     VIR_STORAGE_VOL_WIPE_ALG_NNSA = 1, /* 4-pass  NNSA Policy Letter
@@ -167,6 +233,12 @@ typedef enum {
 # endif
 } virStorageVolWipeAlgorithm;
 
+/**
+ * virStorageVolInfoFlags:
+ *
+ * Since: v3.0.0
+ *
+ */
 typedef enum {
     VIR_STORAGE_VOL_USE_ALLOCATION = 0,
 
@@ -175,6 +247,12 @@ typedef enum {
 
 } virStorageVolInfoFlags;
 
+/**
+ * virStorageVolInfo:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef struct _virStorageVolInfo virStorageVolInfo;
 
 struct _virStorageVolInfo {
@@ -183,8 +261,21 @@ struct _virStorageVolInfo {
     unsigned long long allocation; /* Current allocation bytes */
 };
 
+
+/**
+ * virStorageVolInfoPtr:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef virStorageVolInfo *virStorageVolInfoPtr;
 
+/**
+ * virStorageXMLFlags:
+ *
+ * Since: v0.9.13
+ *
+ */
 typedef enum {
     VIR_STORAGE_XML_INACTIVE    = (1 << 0), /* dump inactive pool/volume information */
 } virStorageXMLFlags;
@@ -220,6 +311,9 @@ int                     virConnectListDefinedStoragePools(virConnectPtr conn,
  * Flags used to tune pools returned by virConnectListAllStoragePools().
  * Note that these flags come in groups; if all bits from a group are 0,
  * then that group is not used to filter results.
+ *
+ * Since: v1.0.0
+ *
  */
 typedef enum {
     VIR_CONNECT_LIST_STORAGE_POOLS_INACTIVE      = 1 << 0,
@@ -271,7 +365,12 @@ virStoragePoolPtr       virStoragePoolLookupByUUIDString(virConnectPtr conn,
 virStoragePoolPtr       virStoragePoolLookupByVolume    (virStorageVolPtr vol);
 virStoragePoolPtr       virStoragePoolLookupByTargetPath(virConnectPtr conn,
                                                          const char *path);
-
+/**
+ * virStoragePoolDefineFlags:
+ *
+ * Since: v7.7.0
+ *
+ */
 typedef enum {
     VIR_STORAGE_POOL_DEFINE_VALIDATE = 1 << 0, /* Validate the XML document against schema */
 } virStoragePoolDefineFlags;
@@ -345,6 +444,12 @@ virStorageVolPtr        virStorageVolLookupByPath       (virConnectPtr conn,
 const char*             virStorageVolGetName            (virStorageVolPtr vol);
 const char*             virStorageVolGetKey             (virStorageVolPtr vol);
 
+/**
+ * virStorageVolCreateFlags:
+ *
+ * Since: v1.0.1
+ *
+ */
 typedef enum {
     VIR_STORAGE_VOL_CREATE_PREALLOC_METADATA = 1 << 0,
     VIR_STORAGE_VOL_CREATE_REFLINK = 1 << 1, /* perform a btrfs lightweight copy */
@@ -357,7 +462,12 @@ virStorageVolPtr        virStorageVolCreateXMLFrom      (virStoragePoolPtr pool,
                                                          const char *xmldesc,
                                                          virStorageVolPtr clonevol,
                                                          unsigned int flags);
-
+/**
+ * virStorageVolDownloadFlags:
+ *
+ * Since: v3.4.0
+ *
+ */
 typedef enum {
     VIR_STORAGE_VOL_DOWNLOAD_SPARSE_STREAM = 1 << 0, /* Use sparse stream */
 } virStorageVolDownloadFlags;
@@ -367,6 +477,13 @@ int                     virStorageVolDownload           (virStorageVolPtr vol,
                                                          unsigned long long offset,
                                                          unsigned long long length,
                                                          unsigned int flags);
+
+/**
+ * virStorageVolUploadFlags:
+ *
+ * Since: v3.4.0
+ *
+ */
 typedef enum {
     VIR_STORAGE_VOL_UPLOAD_SPARSE_STREAM = 1 << 0,  /* Use sparse stream */
 } virStorageVolUploadFlags;
@@ -396,6 +513,12 @@ char *                  virStorageVolGetXMLDesc         (virStorageVolPtr pool,
 
 char *                  virStorageVolGetPath            (virStorageVolPtr vol);
 
+/**
+ * virStorageVolResizeFlags:
+ *
+ * Since: v0.9.10
+ *
+ */
 typedef enum {
     VIR_STORAGE_VOL_RESIZE_ALLOCATE = 1 << 0, /* force allocation of new size */
     VIR_STORAGE_VOL_RESIZE_DELTA    = 1 << 1, /* size is relative to current */
@@ -425,6 +548,9 @@ int virStoragePoolIsPersistent(virStoragePoolPtr pool);
  * An enumeration of supported eventId parameters for
  * virConnectStoragePoolEventRegisterAny(). Each event id determines which
  * signature of callback function will be used.
+ *
+ * Since: v2.0.0
+ *
  */
 typedef enum {
     VIR_STORAGE_POOL_EVENT_ID_LIFECYCLE = 0, /* virConnectStoragePoolEventLifecycleCallback */
@@ -472,6 +598,9 @@ int virConnectStoragePoolEventDeregisterAny(virConnectPtr conn,
  *
  * a virStoragePoolEventLifecycleType is emitted during storage pool
  * lifecycle events
+ *
+ * Since: v2.0.0
+ *
  */
 typedef enum {
     VIR_STORAGE_POOL_EVENT_DEFINED = 0,

include/libvirt/libvirt-domain-checkpoint.h
@@ -34,6 +34,9 @@
  * a domain.  A checkpoint is useful for tracking which portions of the
  * domain disks have been altered since a point in time, but by itself does
  * not allow reverting back to that point in time.
+ *
+ * Since: v5.2.0
+ *
  */
 typedef struct _virDomainCheckpoint virDomainCheckpoint;
 
@@ -43,6 +46,9 @@ typedef struct _virDomainCheckpoint virDomainCheckpoint;
  * A virDomainCheckpointPtr is pointer to a virDomainCheckpoint
  * private structure, and is the type used to reference a domain
  * checkpoint in the API.
+ *
+ * Since: v5.2.0
+ *
  */
 typedef virDomainCheckpoint *virDomainCheckpointPtr;
 
@@ -50,6 +56,12 @@ const char *virDomainCheckpointGetName(virDomainCheckpointPtr checkpoint);
 virDomainPtr virDomainCheckpointGetDomain(virDomainCheckpointPtr checkpoint);
 virConnectPtr virDomainCheckpointGetConnect(virDomainCheckpointPtr checkpoint);
 
+/**
+ * virDomainCheckpointCreateFlags:
+ *
+ * Since: v5.6.0
+ *
+ */
 typedef enum {
     VIR_DOMAIN_CHECKPOINT_CREATE_REDEFINE    = (1 << 0), /* Restore or alter
                                                             metadata */
@@ -65,7 +77,12 @@ typedef enum {
 virDomainCheckpointPtr virDomainCheckpointCreateXML(virDomainPtr domain,
                                                     const char *xmlDesc,
                                                     unsigned int flags);
-
+/**
+ * virDomainCheckpointXMLFlags:
+ *
+ * Since: v5.6.0
+ *
+ */
 typedef enum {
     VIR_DOMAIN_CHECKPOINT_XML_SECURE    = (1 << 0), /* Include sensitive data */
     VIR_DOMAIN_CHECKPOINT_XML_NO_DOMAIN = (1 << 1), /* Suppress <domain>
@@ -86,7 +103,11 @@ char *virDomainCheckpointGetXMLDesc(virDomainCheckpointPtr checkpoint,
  * flag (1<<0) depends on which function it is passed to; but serves
  * to toggle the per-call default of whether the listing is shallow or
  * recursive.  Remaining bits come in groups; if all bits from a group
- * are 0, then that group is not used to filter results.  */
+ * are 0, then that group is not used to filter results.
+ *
+ * Since: v5.6.0
+ *
+ */
 typedef enum {
     VIR_DOMAIN_CHECKPOINT_LIST_ROOTS       = (1 << 0), /* Filter by checkpoints
                                                           with no parents, when
@@ -123,7 +144,14 @@ virDomainCheckpointPtr virDomainCheckpointLookupByName(virDomainPtr domain,
 virDomainCheckpointPtr virDomainCheckpointGetParent(virDomainCheckpointPtr checkpoint,
                                                     unsigned int flags);
 
-/* Delete a checkpoint */
+/**
+ * virDomainCheckpointDeleteFlags:
+ *
+ * Delete a checkpoint
+ *
+ * Since: v5.6.0
+ *
+ */
 typedef enum {
     VIR_DOMAIN_CHECKPOINT_DELETE_CHILDREN      = (1 << 0), /* Also delete children */
     VIR_DOMAIN_CHECKPOINT_DELETE_METADATA_ONLY = (1 << 1), /* Delete just metadata */

include/libvirt/libvirt-domain-snapshot.h
@@ -34,6 +34,9 @@
  * a domain.  A snapshot captures the state of the domain at a point in
  * time, with the intent that the guest can be reverted back to that
  * state at a later time.
+ *
+ * Since: v0.8.0
+ *
  */
 typedef struct _virDomainSnapshot virDomainSnapshot;
 
@@ -42,6 +45,9 @@ typedef struct _virDomainSnapshot virDomainSnapshot;
  *
  * A virDomainSnapshotPtr is pointer to a virDomainSnapshot private structure,
  * and is the type used to reference a domain snapshot in the API.
+ *
+ * Since: v0.8.0
+ *
  */
 typedef virDomainSnapshot *virDomainSnapshotPtr;
 
@@ -49,6 +55,12 @@ const char *virDomainSnapshotGetName(virDomainSnapshotPtr snapshot);
 virDomainPtr virDomainSnapshotGetDomain(virDomainSnapshotPtr snapshot);
 virConnectPtr virDomainSnapshotGetConnect(virDomainSnapshotPtr snapshot);
 
+/**
+ * virDomainSnapshotCreateFlags:
+ *
+ * Since: v0.9.5
+ *
+ */
 typedef enum {
     VIR_DOMAIN_SNAPSHOT_CREATE_REDEFINE    = (1 << 0), /* Restore or alter
                                                           metadata */
@@ -79,7 +91,12 @@ typedef enum {
 virDomainSnapshotPtr virDomainSnapshotCreateXML(virDomainPtr domain,
                                                 const char *xmlDesc,
                                                 unsigned int flags);
-
+/**
+ * virDomainSnapshotXMLFlags:
+ *
+ * Since: v5.1.0
+ *
+ */
 typedef enum {
     VIR_DOMAIN_SNAPSHOT_XML_SECURE         = VIR_DOMAIN_XML_SECURE, /* dump security sensitive information too */
 } virDomainSnapshotXMLFlags;
@@ -98,7 +115,11 @@ char *virDomainSnapshotGetXMLDesc(virDomainSnapshotPtr snapshot,
  * of flag (1<<0) depends on which function it is passed to; but serves
  * to toggle the per-call default of whether the listing is shallow or
  * recursive.  Remaining bits come in groups; if all bits from a group are
- * 0, then that group is not used to filter results.  */
+ * 0, then that group is not used to filter results.
+ *
+ * Since: v0.9.5
+ *
+ */
 typedef enum {
     VIR_DOMAIN_SNAPSHOT_LIST_ROOTS       = (1 << 0), /* Filter by snapshots
                                                         with no parents, when
@@ -194,6 +215,12 @@ int virDomainSnapshotIsCurrent(virDomainSnapshotPtr snapshot,
 int virDomainSnapshotHasMetadata(virDomainSnapshotPtr snapshot,
                                  unsigned int flags);
 
+/**
+ * virDomainSnapshotRevertFlags:
+ *
+ * Since: v0.9.5
+ *
+ */
 typedef enum {
     VIR_DOMAIN_SNAPSHOT_REVERT_RUNNING = 1 << 0, /* Run after revert */
     VIR_DOMAIN_SNAPSHOT_REVERT_PAUSED  = 1 << 1, /* Pause after revert */
@@ -208,7 +235,14 @@ typedef enum {
 int virDomainRevertToSnapshot(virDomainSnapshotPtr snapshot,
                               unsigned int flags);
 
-/* Delete a snapshot */
+/**
+ * virDomainSnapshotDeleteFlags:
+ *
+ * Delete a snapshot
+ *
+ * Since: v0.8.0
+ *
+ */
 typedef enum {
     VIR_DOMAIN_SNAPSHOT_DELETE_CHILDREN      = (1 << 0), /* Also delete children */
     VIR_DOMAIN_SNAPSHOT_DELETE_METADATA_ONLY = (1 << 1), /* Delete just metadata */

include/libvirt/libvirt-event.h
@@ -34,6 +34,9 @@
  * a virEventHandleType is used similar to POLLxxx FD events, but is specific
  * to libvirt. A client app must translate to, and from POLL events when using
  * this construct.
+ *
+ * Since: v0.5.0
+ *
  */
 typedef enum {
     VIR_EVENT_HANDLE_READABLE  = (1 << 0),

include/libvirt/libvirt-host.h
@@ -33,6 +33,9 @@
  *
  * a virConnect is a private structure representing a connection to
  * the Hypervisor.
+ *
+ * Since: v0.0.1
+ *
  */
 typedef struct _virConnect virConnect;
 
@@ -41,6 +44,9 @@ typedef struct _virConnect virConnect;
  *
  * a virConnectPtr is pointer to a virConnect private structure, this is the
  * type used to reference a connection to the Hypervisor in the API.
+ *
+ * Since: v0.0.1
+ *
  */
 typedef virConnect *virConnectPtr;
 
@@ -49,6 +55,9 @@ typedef virConnect *virConnectPtr;
  *
  * Flags to indicate which system-wide sleep state the host must be
  * transitioned to.
+ *
+ * Since: v0.9.8
+ *
  */
 typedef enum {
     VIR_NODE_SUSPEND_TARGET_MEM     = 0,
@@ -64,6 +73,9 @@ typedef enum {
  * virStream:
  *
  * a virStream is a private structure representing a data stream.
+ *
+ * Since: v0.7.2
+ *
  */
 typedef struct _virStream virStream;
 
@@ -72,6 +84,9 @@ typedef struct _virStream virStream;
  *
  * a virStreamPtr is pointer to a virStream private structure, this is the
  * type used to reference a data stream in the API.
+ *
+ * Since: v0.7.2
+ *
  */
 typedef virStream *virStreamPtr;
 
@@ -91,6 +106,9 @@ typedef virStream *virStreamPtr;
  * a virSecurityLabel is a structure filled by virDomainGetSecurityLabel(),
  * providing the security label and associated attributes for the specified
  * domain.
+ *
+ * Since: v0.6.1
+ *
  */
 typedef struct _virSecurityLabel virSecurityLabel;
 
@@ -103,6 +121,9 @@ struct _virSecurityLabel {
  * virSecurityLabelPtr:
  *
  * a virSecurityLabelPtr is a pointer to a virSecurityLabel.
+ *
+ * Since: v0.6.1
+ *
  */
 typedef virSecurityLabel *virSecurityLabelPtr;
 
@@ -130,6 +151,9 @@ typedef virSecurityLabel *virSecurityLabelPtr;
  * a virSecurityModel is a structure filled by virNodeGetSecurityModel(),
  * providing the per-hypervisor security model and DOI attributes for the
  * specified domain.
+ *
+ * Since: v0.6.1
+ *
  */
 typedef struct _virSecurityModel virSecurityModel;
 
@@ -142,6 +166,9 @@ struct _virSecurityModel {
  * virSecurityModelPtr:
  *
  * a virSecurityModelPtr is a pointer to a virSecurityModel.
+ *
+ * Since: v0.6.1
+ *
  */
 typedef virSecurityModel *virSecurityModelPtr;
 
@@ -149,7 +176,7 @@ typedef virSecurityModel *virSecurityModelPtr;
 /* data types related to virNodePtr */
 
 /**
- * virNodeInfoPtr:
+ * virNodeInfo:
  *
  * a virNodeInfo is a structure filled by virNodeGetInfo() and providing
  * the information for the Node.
@@ -157,8 +184,9 @@ typedef virSecurityModel *virSecurityModelPtr;
  * Note that the information in this struct is not guaranteed to be an
  * accurate relection of the system hardware. See the virNodeGetInfo()
  * API documentation for further guidance.
+ *
+ * Since: v0.1.0
  */
-
 typedef struct _virNodeInfo virNodeInfo;
 
 struct _virNodeInfo {
@@ -188,9 +216,12 @@ struct _virNodeInfo {
 # define VIR_NODE_CPU_STATS_FIELD_LENGTH 80
 
 /**
- * VIR_NODE_CPU_STATS_ALL_CPUS:
+ * virNodeGetCPUStatsAllCPUs:
  *
  * Value for specifying request for the total CPU time/utilization
+ *
+ * Since: v0.9.8
+ *
  */
 typedef enum {
     VIR_NODE_CPU_STATS_ALL_CPUS = -1,
@@ -262,6 +293,9 @@ typedef enum {
  *
  * a virNodeCPUStats is a structure filled by virNodeGetCPUStats()
  * providing information about the CPU stats of the node.
+ *
+ * Since: v0.9.3
+ *
  */
 typedef struct _virNodeCPUStats virNodeCPUStats;
 
@@ -280,9 +314,12 @@ struct _virNodeCPUStats {
 # define VIR_NODE_MEMORY_STATS_FIELD_LENGTH 80
 
 /**
- * VIR_NODE_MEMORY_STATS_ALL_CELLS:
+ * virNodeGetMemoryStatsAllCells:
  *
  * Value for specifying request for the total memory of all cells.
+ *
+ * Since: v0.9.8
+ *
  */
 typedef enum {
     VIR_NODE_MEMORY_STATS_ALL_CELLS = -1,
@@ -334,6 +371,9 @@ typedef enum {
  *
  * a virNodeMemoryStats is a structure filled by virNodeGetMemoryStats()
  * providing information about the memory of the node.
+ *
+ * Since: v0.9.3
+ *
  */
 typedef struct _virNodeMemoryStats virNodeMemoryStats;
 
@@ -463,6 +503,9 @@ int virNodeGetCPUMap(virConnectPtr conn,
  * virNodeInfoPtr:
  *
  * a virNodeInfoPtr is a pointer to a virNodeInfo structure.
+ *
+ * Since: v0.1.0
+ *
  */
 
 typedef virNodeInfo *virNodeInfoPtr;
@@ -471,6 +514,9 @@ typedef virNodeInfo *virNodeInfoPtr;
  * virNodeCPUStatsPtr:
  *
  * a virNodeCPUStatsPtr is a pointer to a virNodeCPUStats structure.
+ *
+ * Since: v0.9.3
+ *
  */
 
 typedef virNodeCPUStats *virNodeCPUStatsPtr;
@@ -479,6 +525,9 @@ typedef virNodeCPUStats *virNodeCPUStatsPtr;
  * virNodeMemoryStatsPtr:
  *
  * a virNodeMemoryStatsPtr is a pointer to a virNodeMemoryStats structure.
+ *
+ * Since: v0.9.3
+ *
  */
 
 typedef virNodeMemoryStats *virNodeMemoryStatsPtr;
@@ -554,16 +603,24 @@ int virNodeGetSEVInfo (virConnectPtr conn,
                        unsigned int flags);
 
 /**
- * virConnectFlags
+ * virConnectFlags:
  *
  * Flags when opening a connection to a hypervisor
+ *
+ * Since: v0.4.1
+ *
  */
 typedef enum {
     VIR_CONNECT_RO         = (1 << 0),  /* A readonly connection */
     VIR_CONNECT_NO_ALIASES = (1 << 1),  /* Don't try to resolve URI aliases */
 } virConnectFlags;
 
-
+/**
+ * virConnectCredentialType:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef enum {
     VIR_CRED_USERNAME = 1,     /* Identity to act as */
     VIR_CRED_AUTHNAME = 2,     /* Identify to authorize as */
@@ -589,7 +646,20 @@ struct _virConnectCredential {
     unsigned int resultlen; /* Length of the result */
 };
 
+/**
+ * virConnectCredential:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef struct _virConnectCredential virConnectCredential;
+
+/**
+ * virConnectCredentialPtr:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef virConnectCredential *virConnectCredentialPtr;
 
 
@@ -618,8 +688,20 @@ struct _virConnectAuth {
     void *cbdata;
 };
 
-
+/**
+ * virConnectAuth:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef struct _virConnectAuth virConnectAuth;
+
+/**
+ * virConnectAuthPtr:
+ *
+ * Since: v0.4.1
+ *
+ */
 typedef virConnectAuth *virConnectAuthPtr;
 
 /*
@@ -850,6 +932,12 @@ int virConnectIsAlive(virConnectPtr conn);
  * CPU specification API
  */
 
+/**
+ * virCPUCompareResult:
+ *
+ * Since: v0.7.5
+ *
+ */
 typedef enum {
     VIR_CPU_COMPARE_ERROR           = -1,
     VIR_CPU_COMPARE_INCOMPATIBLE    = 0,
@@ -861,6 +949,12 @@ typedef enum {
 # endif
 } virCPUCompareResult;
 
+/**
+ * virConnectCompareCPUFlags:
+ *
+ * Since: v1.2.6
+ *
+ */
 typedef enum {
     VIR_CONNECT_COMPARE_CPU_FAIL_INCOMPATIBLE = (1 << 0), /* treat incompatible
                                                              CPUs as failure */
@@ -885,9 +979,12 @@ int virConnectGetCPUModelNames(virConnectPtr conn,
                                unsigned int flags);
 
 /**
- * virConnectBaselineCPUFlags
+ * virConnectBaselineCPUFlags:
  *
  * Flags when getting XML description of a computed CPU
+ *
+ * Since: v1.1.2
+ *
  */
 typedef enum {
     VIR_CONNECT_BASELINE_CPU_EXPAND_FEATURES  = (1 << 0),  /* show all features */
@@ -915,7 +1012,12 @@ int virNodeGetFreePages(virConnectPtr conn,
                         unsigned int cellcount,
                         unsigned long long *counts,
                         unsigned int flags);
-
+/**
+ * virNodeAllocPagesFlags:
+ *
+ * Since: v1.2.9
+ *
+ */
 typedef enum {
     VIR_NODE_ALLOC_PAGES_ADD = 0, /* Add @pageCounts to the pages pool. This
                                      can be used only to size up the pool. */

include/libvirt/libvirt-interface.h
@@ -31,6 +31,9 @@
  * virInterface:
  *
  * a virInterface is a private structure representing a virtual interface.
+ *
+ * Since: v0.6.4
+ *
  */
 typedef struct _virInterface virInterface;
 
@@ -39,6 +42,9 @@ typedef struct _virInterface virInterface;
  *
  * a virInterfacePtr is pointer to a virInterface private structure, this is the
  * type used to reference a virtual interface in the API.
+ *
+ * Since: v0.6.4
+ *
  */
 typedef virInterface *virInterfacePtr;
 
@@ -53,10 +59,13 @@ int                     virConnectNumOfDefinedInterfaces (virConnectPtr conn);
 int                     virConnectListDefinedInterfaces  (virConnectPtr conn,
                                                           char **const names,
                                                           int maxnames);
-/*
- * virConnectListAllInterfaces:
+/**
+ * virConnectListAllInterfacesFlags:
  *
  * Flags used to filter the returned interfaces.
+ *
+ * Since: v0.10.2
+ *
  */
 typedef enum {
     VIR_CONNECT_LIST_INTERFACES_INACTIVE      = 1 << 0,
@@ -75,10 +84,22 @@ virInterfacePtr         virInterfaceLookupByMACString (virConnectPtr conn,
 const char*             virInterfaceGetName       (virInterfacePtr iface);
 const char*             virInterfaceGetMACString  (virInterfacePtr iface);
 
+/**
+ * virInterfaceXMLFlags:
+ *
+ * Since: v0.7.3
+ *
+ */
 typedef enum {
     VIR_INTERFACE_XML_INACTIVE = 1 << 0 /* dump inactive interface information */
 } virInterfaceXMLFlags;
 
+/**
+ * virInterfaceDefineFlags:
+ *
+ * Since: v7.7.0
+ *
+ */
 typedef enum {
     VIR_INTERFACE_DEFINE_VALIDATE = 1 << 0, /* Validate the XML document against schema */
 } virInterfaceDefineFlags;

commit 469f3467e1c5772208fd7e3e75bfc29ca2bda667

docstring: typedef: fix a few version
Fixes: 034432e47b72cff45c905feadf7165af8bb50b7e
Signed-off-by: Victor Toso <victortoso@redhat.com>
Reviewed-by: Andrea Bolognani <abologna@redhat.com>

include/libvirt/libvirt-domain.h
@@ -1755,7 +1755,7 @@ int     virDomainGetMemoryParameters(virDomainPtr domain,
  *
  * Memory size modification flags.
  *
- * Since: v1.0.0
+ * Since: v0.9.0
  */
 typedef enum {
     /* See virDomainModificationImpact for these flags.  */
@@ -2264,7 +2264,7 @@ typedef virVcpuInfo *virVcpuInfoPtr;
  *
  * Flags for controlling virtual CPU hot-plugging.
  *
- * Since: v1.0.0
+ * Since: v0.8.5
  */
 typedef enum {
     /* See virDomainModificationImpact for these flags.  */
@@ -2511,7 +2511,7 @@ int                     virDomainGetVcpus       (virDomainPtr domain,
 /**
  * virDomainDeviceModifyFlags:
  *
- * Since: v1.0.0
+ * Since: v0.7.7
  */
 typedef enum {
     /* See virDomainModificationImpact for these flags.  */

include/libvirt/libvirt-storage.h
@@ -292,7 +292,7 @@ int                     virConnectListDefinedStoragePools(virConnectPtr conn,
  * Note that these flags come in groups; if all bits from a group are 0,
  * then that group is not used to filter results.
  *
- * Since: v1.0.0
+ * Since: v0.10.2
  */
 typedef enum {
     VIR_CONNECT_LIST_STORAGE_POOLS_INACTIVE      = 1 << 0,