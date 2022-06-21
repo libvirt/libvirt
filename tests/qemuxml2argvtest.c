9248434536f2f02b4f6a571cf43cdb1e4f4e3e1c
Peter Krempa <pkrempa@redhat.com>
2022-06-21 16:31:30 +0200

    qemuxml2argvtest: Remove 'qemu-3.1' versions of virtio-(non-)transitional tests
    Starting from qemu-4.0 a new device model name is used instead of the
    'disable_*' props. Since we are going to bump to qemu-4.2 as minimum
    this test can be removed.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -3326,9 +3326,6 @@ mymain(void)
     DO_TEST("riscv64-virt-pci",
             QEMU_CAPS_OBJECT_GPEX);
 
-    /* Older version checks disable-legacy usage */
-    DO_TEST_CAPS_VER("virtio-transitional", "3.1.0");
-    DO_TEST_CAPS_VER("virtio-non-transitional", "3.1.0");
     DO_TEST_CAPS_LATEST("virtio-transitional");
     DO_TEST_CAPS_LATEST("virtio-non-transitional");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("virtio-transitional-not-supported");
