e398a6aba421aae49cc4f07303308ed4cf23f354
Paolo Bonzini <pbonzini@redhat.com>
2022-08-08 17:15:03 +0200

    tests: drop "-fast" from query-cpus-fast tests
    All supported versions of QEMU include the query-cpus-fast QMP command.
    In preparation for dropping support for the old "query-cpus" commands,
    remove the "-fast" suffix from both x86-full-fast and s390-fast.
    
    Signed-off-by: Paolo Bonzini <pbonzini@redhat.com>
    Reviewed-by: Peter Krempa <pkrempa@redhat.com>

--- tests/qemumonitorjsontest.c
+++ tests/qemumonitorjsontest.c
@@ -3115,8 +3115,8 @@ mymain(void)
     DO_TEST_CPU_DATA("ecx");
 
     DO_TEST_CPU_INFO("x86-basic-pluggable", 8);
+    DO_TEST_CPU_INFO_FAST("x86-full", 11);
     DO_TEST_CPU_INFO("x86-node-full", 8);
-    DO_TEST_CPU_INFO_FAST("x86-full-fast", 11);
     DO_TEST_CPU_INFO_FAST("x86-dies", 16);
 
     DO_TEST_CPU_INFO("ppc64-basic", 24);
@@ -3125,7 +3125,7 @@ mymain(void)
     DO_TEST_CPU_INFO("ppc64-hotplug-4", 24);
     DO_TEST_CPU_INFO("ppc64-no-threads", 16);
 
-    DO_TEST_CPU_INFO_FAST("s390-fast", 2);
+    DO_TEST_CPU_INFO_FAST("s390", 2);
 
 #define DO_TEST_BLOCK_NODE_DETECT(testname) \
     do { \
