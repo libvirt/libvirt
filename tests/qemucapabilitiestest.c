92cc65783ec2e5a0ea6bf339d70b905241e4f52b
Andrea Bolognani <abologna@redhat.com>
2022-08-09 18:10:27 +0200

    tests: Use domaincapsmock in qemucapabilitiestest
    This doesn't change anything at the moment, but is necessary
    for the upcoming fix.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Christophe de Dinechin <dinechin@redhat.com>
    Tested-by: Christophe de Dinechin <dinechin@redhat.com>

--- tests/qemucapabilitiestest.c
+++ tests/qemucapabilitiestest.c
@@ -230,4 +230,5 @@ mymain(void)
     return (data.ret == 0) ? EXIT_SUCCESS : EXIT_FAILURE;
 }
 
-VIR_TEST_MAIN(mymain)
+VIR_TEST_MAIN_PRELOAD(mymain,
+                      VIR_TEST_MOCK("domaincaps"))
