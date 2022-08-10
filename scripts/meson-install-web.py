03ba5f68b8fbbf4707e7da88d2dde99b96e306d4
Andrea Bolognani <abologna@redhat.com>
2022-08-10 18:06:24 +0200

    scripts: Add $DESTDIR support to meson-install-web.py
    meson already supports $DESTDIR natively, but in this case
    we're using a custom script and so we have to do some extra
    work ourselves.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

--- scripts/meson-install-web.py
+++ scripts/meson-install-web.py
@@ -4,7 +4,11 @@ import os
 import shutil
 import sys
 
+destdir = os.environ.get('DESTDIR', os.sep)
+
 for desc in sys.argv[1:]:
     inst = desc.split(':')
-    os.makedirs(inst[1], exist_ok=True)
-    shutil.copy(inst[0], inst[1])
+    src = inst[0]
+    dst = os.path.join(destdir, inst[1].strip(os.sep))
+    os.makedirs(dst, exist_ok=True)
+    shutil.copy(src, dst)
