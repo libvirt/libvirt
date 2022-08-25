efb3ca87d534d510b19cdfe38574a0fa37c6477a
Ján Tomko <jtomko@redhat.com>
2022-08-25 17:29:48 +0200

    qemu: assume QEMU_CAPS_VIRTIO_SCSI_IOTHREAD
    All the supported QEMU versions should have iothread support
    on the virtio-scsi controllers if they are compiled in.
    
    Signed-off-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Peter Krempa <pkrempa@redhat.com>

--- src/qemu/qemu_process.c
+++ src/qemu/qemu_process.c
@@ -5301,27 +5301,11 @@ static int
 qemuProcessStartValidateIOThreads(virDomainObj *vm,
                                   virQEMUCaps *qemuCaps)
 {
-    size_t i;
-
     if (vm->def->niothreadids > 0 &&
         !virQEMUCapsGet(qemuCaps, QEMU_CAPS_OBJECT_IOTHREAD)) {
         virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
                        _("IOThreads not supported for this QEMU"));
         return -1;
-    }
-
-    for (i = 0; i < vm->def->ncontrollers; i++) {
-        virDomainControllerDef *cont = vm->def->controllers[i];
-
-        if (cont->type == VIR_DOMAIN_CONTROLLER_TYPE_SCSI &&
-            cont->model == VIR_DOMAIN_CONTROLLER_MODEL_SCSI_VIRTIO_SCSI &&
-            cont->iothread > 0 &&
-            !virQEMUCapsGet(qemuCaps, QEMU_CAPS_VIRTIO_SCSI_IOTHREAD)) {
-            virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
-                           _("IOThreads for virtio-scsi not supported for "
-                             "this QEMU"));
-            return -1;
-        }
     }
 
     return 0;
