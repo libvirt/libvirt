50704f6c25dc279bbf0ee3d0b61cdaae031ff8ee
Peter Krempa <pkrempa@redhat.com>
2022-08-25 10:52:21 +0200

    qemudomainsnapshotxml2xmltest: Add test case for a snapshot with 'inactiveDomain' element
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- tests/qemudomainsnapshotxml2xmltest.c
+++ tests/qemudomainsnapshotxml2xmltest.c
@@ -160,6 +160,8 @@ mymain(void)
     DO_TEST_OUT("external_vm_redefine", "c7a5fdbd-edaf-9455-926a-d65c16db1809",
                 0);
 
+    DO_TEST_OUT("memory-snapshot-inactivedomain", "14beef2c-8cae-4ea8-bf55-e48fe0cd4b73", 0);
+
     DO_TEST_INOUT("empty", "9d37b878-a7cc-9f9a-b78f-49b3abad25a8",
                   1386166249, 0);
     DO_TEST_INOUT("noparent", "9d37b878-a7cc-9f9a-b78f-49b3abad25a8",
