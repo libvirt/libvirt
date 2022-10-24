libvirt/libvirt

commit a0f37232b9c4296ca16955cc625f75eb848ace39

cgroup/LXC: Do not condition availability of v2 by controllers
systemd in hybrid mode uses v1 hierarchies for controllers and v2 for
process tracking.

The LXC code uses virCgroupAddMachineProcess() to move processes into
appropriate cgroup by manipulating cgroupfs directly. (Note, despite
libvirt also supports talking to systemd directly via
org.freedesktop.machine1 API.)

If this path is taken, libvirt/lxc must convince systemd that processes
really belong to new cgroup, i.e. also the tracking v2 hierarchy must
undergo migration too.

The current check would evaluate v2 backend as unavailable with hybrid
mode (because there are no available controllers). Simplify the
condition and consider the mounted cgroup2 as sufficient to touch v2
hierarchy.

This consequently creates an issue with binding the V2 mount. In hybrid
mode the V2 filesystem may be mounted upon the V1 filesystem. By reversing
the order in which backends are mounted in virCgroupBindMount this problem
is circumvented.

Resolves: https://gitlab.com/libvirt/libvirt/-/issues/182
Signed-off-by: Eric van Blokland <mail@ericvanblokland.nl>
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/util/vircgroupv2.c
@@ -75,22 +75,10 @@ virCgroupV2Available(void)
         if (STRNEQ(entry.mnt_type, "cgroup2"))
             continue;
 
-        /* Systemd uses cgroup v2 for process tracking but no controller is
-         * available. We should consider this configuration as cgroup v2 is
-         * not available. */
-        contFile = g_strdup_printf("%s/cgroup.controllers", entry.mnt_dir);
-
-        if (virFileReadAll(contFile, 1024 * 1024, &contStr) < 0)
-            goto cleanup;
-
-        if (STREQ(contStr, ""))
-            continue;
-
         ret = true;
         break;
     }
 
- cleanup:
     VIR_FORCE_FCLOSE(mounts);
     return ret;
 }

src/util/vircgroup.c
@@ -2921,10 +2921,12 @@ int
 virCgroupBindMount(virCgroup *group, const char *oldroot,
                    const char *mountopts)
 {
-    size_t i;
+    ssize_t i;
     virCgroup *parent = virCgroupGetNested(group);
 
-    for (i = 0; i < VIR_CGROUP_BACKEND_TYPE_LAST; i++) {
+    /* In hybrid environments, V2 may be mounted over V1.
+     * Mount the backends in reverse order. */
+    for (i = VIR_CGROUP_BACKEND_TYPE_LAST - 1; i >= 0; i--) {
         if (parent->backends[i] &&
             parent->backends[i]->bindMount(parent, oldroot, mountopts) < 0) {
             return -1;

commit e49313b54ed2a149c71f9073659222742ff3ffb0

vircgroup: Remove unused variables in virCgroupV2Available
After recent commit 'contFile' and 'contStr' became unused breaking
build with clang:

../../../libvirt/src/util/vircgroupv2.c:72:26: error: unused variable 'contFile' [-Werror,-Wunused-variable]
        g_autofree char *contFile = NULL;
                         ^
../../../libvirt/src/util/vircgroupv2.c:73:26: error: unused variable 'contStr' [-Werror,-Wunused-variable]
        g_autofree char *contStr = NULL;
                         ^

Fixes: a0f37232b9c4296ca16955cc625f75eb848ace39
Signed-off-by: Peter Krempa <pkrempa@redhat.com>

src/util/vircgroupv2.c
@@ -69,9 +69,6 @@ virCgroupV2Available(void)
         return false;
 
     while (getmntent_r(mounts, &entry, buf, sizeof(buf)) != NULL) {
-        g_autofree char *contFile = NULL;
-        g_autofree char *contStr = NULL;
-
         if (STRNEQ(entry.mnt_type, "cgroup2"))
             continue;
 