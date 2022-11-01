a63d439b6390136a88ef9538aaf9ae16f464a59a
Han Han <hhan@redhat.com>
2022-11-01 10:45:08 +0800

    include: Fix the introduced version of VIR_DOMAIN_STATS_VM
    Fixes: 8c9e3dae14
    
    Signed-off-by: Han Han <hhan@redhat.com>
    Reviewed-by: Erik Skultety <eskultet@redhat.com>

--- include/libvirt/libvirt-domain.h
+++ include/libvirt/libvirt-domain.h
@@ -2710,7 +2710,7 @@ typedef enum {
     VIR_DOMAIN_STATS_IOTHREAD = (1 << 7), /* return iothread poll info (Since: 4.10.0) */
     VIR_DOMAIN_STATS_MEMORY = (1 << 8), /* return domain memory info (Since: 6.0.0) */
     VIR_DOMAIN_STATS_DIRTYRATE = (1 << 9), /* return domain dirty rate info (Since: 7.2.0) */
-    VIR_DOMAIN_STATS_VM = (1 << 10), /* return vm info (Since: 8.6.0) */
+    VIR_DOMAIN_STATS_VM = (1 << 10), /* return vm info (Since: 8.9.0) */
 } virDomainStatsTypes;
 
 /**
