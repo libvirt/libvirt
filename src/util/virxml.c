libvirt/libvirt

commit 65eaf58335f0eca789c74726b84910fb48c4cece

util: xml: Introduce virXMLPropStringRequired
Similarly to virXMLPropString it extracts a string but reports an error
similar to the newer virXMLProp helpers if the attribute is not present.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/util/virxml.c
@@ -456,6 +456,33 @@ virXMLPropString(xmlNodePtr node,
 
 
 /**
+ * virXMLPropStringRequired:
+ * @node: XML dom node pointer
+ * @name: Name of the property (attribute) to get
+ *
+ * Convenience function to return copy of an mandatoryu attribute value of an
+ * XML node.
+ *
+ * Returns the property (attribute) value as string or NULL and if the attribute
+ * is not present (libvirt error reported).
+ * The caller is responsible for freeing the returned buffer.
+ */
+char *
+virXMLPropStringRequired(xmlNodePtr node,
+                         const char *name)
+{
+    char *ret = virXMLPropString(node, name);
+
+     if (!(*ret))
+         virReportError(VIR_ERR_XML_ERROR,
+                        _("Missing required attribute '%s' in element '%s'"),
+                        name, node->name);
+
+     return ret;
+}
+
+
+/**
  * virXMLNodeContentString:
  * @node: XML dom node pointer
  *

src/libvirt_private.syms
@@ -3683,6 +3683,7 @@ virXMLPropEnum;
 virXMLPropEnumDefault;
 virXMLPropInt;
 virXMLPropString;
+virXMLPropStringRequired;
 virXMLPropTristateBool;
 virXMLPropTristateBoolAllowDefault;
 virXMLPropTristateSwitch;

src/util/virxml.h
@@ -97,6 +97,10 @@ char *
 virXMLPropString(xmlNodePtr node,
                  const char *name);
 char *
+virXMLPropStringRequired(xmlNodePtr node,
+                         const char *name);
+
+char *
 virXMLNodeContentString(xmlNodePtr node);
 
 int

commit b9315159f5fc50fc54bb0a573b42f21bda23e0ce

Fix null pointer dereference in virXMLPropStringRequired
Fixes: 65eaf58335f

Signed-off-by: Gedalya <gedalya@gedalya.net>

src/util/virxml.c
@@ -320,7 +320,7 @@ virXMLPropStringRequired(xmlNodePtr node,
 {
     char *ret = virXMLPropString(node, name);
 
-     if (!(*ret))
+     if (!ret)
          virReportError(VIR_ERR_XML_ERROR,
                         _("Missing required attribute '%s' in element '%s'"),
                         name, node->name);