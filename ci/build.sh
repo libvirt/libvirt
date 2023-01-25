b845c2fd3b820d95a43d8e7c05083dd3852056f4
Erik Skultety <eskultet@redhat.com>
2023-01-25 13:22:49 +0100

    ci: build.sh: Use 'meson setup' explicitly
    Even though 'setup' is assumed when no other command is given, we're
    being explicit in our GitLab recipes, so do the same for the local
    build.sh script too.
    
    Signed-off-by: Erik Skultety <eskultet@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- ci/build.sh
+++ ci/build.sh
@@ -8,7 +8,7 @@ export VIR_TEST_DEBUG=1
 # populated at build time from the Dockerfile. A typical use case would
 # be to pass options to trigger cross-compilation
 
-meson build --werror $MESON_OPTS $CI_MESON_ARGS || \
+meson setup build --werror $MESON_OPTS $CI_MESON_ARGS || \
 (cat build/meson-logs/meson-log.txt && exit 1)
 
 ninja -C build $CI_NINJA_ARGS
