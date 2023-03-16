66e5c43a033f20268af2e59869b2ec561c335273
Andrea Bolognani <abologna@redhat.com>
2023-03-16 19:10:29 +0100

    tests: Add firmware-auto-efi-loader-path
    This is identical to the existing
    firmware-auto-efi-loader-path-nonstandard test case, but uses
    a standard firmware path.
    
    Right now the two test cases behave identically, but that's
    going to change in the future.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxml2argvtest.c
+++ tests/qemuxml2argvtest.c
@@ -1116,6 +1116,7 @@ mymain(void)
     DO_TEST_CAPS_LATEST("firmware-auto-efi-nvram");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-loader-secure");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("firmware-auto-efi-loader-insecure");
+    DO_TEST_CAPS_LATEST_PARSE_ERROR("firmware-auto-efi-loader-path");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("firmware-auto-efi-loader-path-nonstandard");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-secboot");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-no-secboot");
