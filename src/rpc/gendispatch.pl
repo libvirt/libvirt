libvirt/libvirt

commit 778c3004609ede0a9df4cf3e01c031047530efb7

rpc: use VIR_LOCK_GUARD in remote client code
Using VIR_LOCK_GUARD helps to simplify the control flow
logic.

Reviewed-by: Ján Tomko <jtomko@redhat.com>
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

src/rpc/gendispatch.pl
@@ -1818,11 +1818,10 @@ elsif ($mode eq "client") {
             }
         }
 
-        print "\n";
         if ($structprefix eq "admin") {
-            print "    virObjectLock(priv);\n";
+            print "    VIR_LOCK_GUARD lock = virObjectLockGuard(&priv);\n";
         } else {
-            print "    remoteDriverLock(priv);\n";
+            print "    VIR_LOCK_GUARD lock = remoteDriverLock(priv);\n";
         }
 
         if ($call->{streamflag} ne "none") {
@@ -2019,12 +2018,6 @@ elsif ($mode eq "client") {
         print "done:\n";
 
         print join("\n", @free_list);
-
-        if ($structprefix eq "admin") {
-            print "    virObjectUnlock(priv);\n";
-        } else {
-            print "    remoteDriverUnlock(priv);\n";
-        }
 
         print "    return rv;\n";
         print "}\n";

commit 50f0e8e7aa32c307e976a09387bccd5e40a285d9

rpc: fix typo in admin code generation
An extra '&' introduced a crash.

https://bugzilla.redhat.com/show_bug.cgi?id=2178866

Fixes: 778c3004609ede0a9df4cf3e01c031047530efb7
Signed-off-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

src/rpc/gendispatch.pl
@@ -1812,7 +1812,7 @@ elsif ($mode eq "client") {
         }
 
         if ($structprefix eq "admin") {
-            print "    VIR_LOCK_GUARD lock = virObjectLockGuard(&priv);\n";
+            print "    VIR_LOCK_GUARD lock = virObjectLockGuard(priv);\n";
         } else {
             print "    VIR_LOCK_GUARD lock = remoteDriverLock(priv);\n";
         }