0cbd00525d6ea79b15e9047dbe2bf5e6a2a2f468
Peter Krempa <pkrempa@redhat.com>
2023-03-23 15:21:37 +0100

    ci: Add OpenSUSE leap 15.4 job
    As a precursor to dropping the EOL OpenSUSE 15.3 job add first the
    definitions for the replacement version.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

---
+++ ci/buildenv/opensuse-leap-154.sh
@@ -0,0 +1,96 @@
+# THIS FILE WAS AUTO-GENERATED
+#
+#  $ lcitool manifest ci/manifest.yml
+#
+# https://gitlab.com/libvirt/libvirt-ci
+
+function install_buildenv() {
+    zypper update -y
+    zypper install -y \
+           audit-devel \
+           augeas \
+           augeas-lenses \
+           bash-completion \
+           ca-certificates \
+           ccache \
+           clang \
+           codespell \
+           cpp \
+           cppi \
+           cyrus-sasl-devel \
+           device-mapper-devel \
+           diffutils \
+           dwarves \
+           ebtables \
+           fuse-devel \
+           gcc \
+           gettext-runtime \
+           git \
+           glib2-devel \
+           glibc-devel \
+           glibc-locale \
+           glusterfs-devel \
+           grep \
+           iproute2 \
+           iptables \
+           kmod \
+           libacl-devel \
+           libapparmor-devel \
+           libattr-devel \
+           libblkid-devel \
+           libcap-ng-devel \
+           libcurl-devel \
+           libgnutls-devel \
+           libiscsi-devel \
+           libnl3-devel \
+           libnuma-devel \
+           libpcap-devel \
+           libpciaccess-devel \
+           librbd-devel \
+           libselinux-devel \
+           libssh-devel \
+           libssh2-devel \
+           libtirpc-devel \
+           libudev-devel \
+           libwsman-devel \
+           libxml2 \
+           libxml2-devel \
+           libxslt \
+           libyajl-devel \
+           lvm2 \
+           make \
+           meson \
+           nfs-utils \
+           ninja \
+           numad \
+           open-iscsi \
+           parted-devel \
+           perl-base \
+           pkgconfig \
+           polkit \
+           python3-base \
+           python3-docutils \
+           python3-flake8 \
+           qemu-tools \
+           readline-devel \
+           rpcgen \
+           rpm-build \
+           sanlock-devel \
+           scrub \
+           sed \
+           systemd-rpm-macros \
+           systemtap-sdt-devel \
+           wireshark-devel \
+           xen-devel
+    rpm -qa | sort > /packages.txt
+    mkdir -p /usr/libexec/ccache-wrappers
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/cc
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/clang
+    ln -s /usr/bin/ccache /usr/libexec/ccache-wrappers/gcc
+}
+
+export CCACHE_WRAPPERSDIR="/usr/libexec/ccache-wrappers"
+export LANG="en_US.UTF-8"
+export MAKE="/usr/bin/make"
+export NINJA="/usr/bin/ninja"
+export PYTHON="/usr/bin/python3"
