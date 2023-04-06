libvirt/libvirt

commit 2c13a2a7c9c368ea81eccd4ba12d9cf34bdd331b

virt-host-validate: Detect SMMU support on ARMs
In vir-host-validate we do two checks related to IOMMU:

  1) hardware support, and
  2) kernel support.

While users are usually interested in the latter, the former also
makes sense. And for the former (hardware support) we have this
huge if-else block for nearly every architecture, except ARM.

Now, IOMMU is called SMMU in ARM world, and while there's
certainly a definitive way of detecting SMMU support (e.g. via
dumping some registers in asm), we can work around this - just
like we do for Intel and AMD - and check for an ACPI table
presence.

In ARM world, there's I/O Remapping Table (IORT) which describes
SMMU capabilities on given host and is exposed in sysfs
(regardless of arm_smmu module).

Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2178885
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

tools/virt-host-validate-common.c
@@ -388,6 +388,15 @@ int virHostValidateIOMMU(const char *hvname,
             return VIR_HOST_VALIDATE_FAILURE(VIR_HOST_VALIDATE_NOTE);
         }
         virHostMsgPass();
+    } else if (ARCH_IS_ARM(arch)) {
+        if (access("/sys/firmware/acpi/tables/IORT", F_OK) == 0) {
+            virHostMsgPass();
+        } else {
+            virHostMsgFail(level,
+                           "No ACPI IORT table found, IOMMU not "
+                           "supported by this hardware platform");
+            return VIR_HOST_VALIDATE_FAILURE(level);
+        }
     } else {
         virHostMsgFail(level,
                        "Unknown if this platform has IOMMU support");

commit d335f49c70b3db959e6c11ab9ccbbf5de3079965

virt-host-validate: Detect SMMU presence on ARMs by parsing IORT table
In my previous commit v9.2.0-rc1~3 I've made virt-host-validate
to report host IOMMU check pass if IORT table is present. This is
not sufficient though, because IORT describes much more than just
IOMMU (well, it's called SMMU in ARM world). In fact, this can be
seen in previous commit which adds test cases: there are tables
(IORT_virt_aarch64) which does not contain any SMMU records.

But after previous commits, we can parse the table so switch to
that.

Fixes: 2c13a2a7c9c368ea81eccd4ba12d9cf34bdd331b
Resolves: https://bugzilla.redhat.com/show_bug.cgi?id=2178885
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Andrea Bolognani <abologna@redhat.com>

tools/virt-host-validate-common.c
@@ -26,6 +26,7 @@
 #include <sys/utsname.h>
 #include <sys/stat.h>
 
+#include "viracpi.h"
 #include "viralloc.h"
 #include "vircgroup.h"
 #include "virfile.h"
@@ -389,13 +390,24 @@ int virHostValidateIOMMU(const char *hvname,
         }
         virHostMsgPass();
     } else if (ARCH_IS_ARM(arch)) {
-        if (access("/sys/firmware/acpi/tables/IORT", F_OK) == 0) {
-            virHostMsgPass();
-        } else {
+        if (access("/sys/firmware/acpi/tables/IORT", F_OK) != 0) {
             virHostMsgFail(level,
                            "No ACPI IORT table found, IOMMU not "
                            "supported by this hardware platform");
             return VIR_HOST_VALIDATE_FAILURE(level);
+        } else {
+            rc = virAcpiHasSMMU();
+            if (rc < 0) {
+                virHostMsgFail(level,
+                               "Failed to parse ACPI IORT table");
+                return VIR_HOST_VALIDATE_FAILURE(level);
+            } else if (rc == 0) {
+                virHostMsgFail(level,
+                               "No SMMU found");
+                return VIR_HOST_VALIDATE_FAILURE(level);
+            } else {
+                virHostMsgPass();
+            }
         }
     } else {
         virHostMsgFail(level,