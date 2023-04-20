libvirt/libvirt

commit 9dadc7302920f9fca0057c655d03c2b0206b9a70

caps: drop requiredSourceElements from storage pool capabilities
Capabilities should not duplicate data that are obvious from our
documentation and will not change with different QEMU binaries
or the way how we compile libvirt.

Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

src/conf/storage_conf.c
@@ -366,7 +366,7 @@ virStoragePoolOptionsFormatPool(virBufferPtr buf,
     if (!(poolOptions = virStoragePoolOptionsForPoolType(type)))
         return -1;
 
-    if (!poolOptions->formatToString && !poolOptions->flags)
+    if (!poolOptions->formatToString)
         return 0;
 
     virBufferAddLit(buf, "<poolOptions>\n");
@@ -384,29 +384,6 @@ virStoragePoolOptionsFormatPool(virBufferPtr buf,
         for (i = 0; i < poolOptions->lastFormat; i++)
             virBufferAsprintf(buf, "<value>%s</value>\n",
                               (poolOptions->formatToString)(i));
-
-        virBufferAdjustIndent(buf, -2);
-        virBufferAddLit(buf, "</enum>\n");
-    }
-
-    if (poolOptions->flags) {
-        virBufferAddLit(buf, "<enum name='requiredSourceElements'>\n");
-        virBufferAdjustIndent(buf, 2);
-
-        if (poolOptions->flags & VIR_STORAGE_POOL_SOURCE_HOST)
-            virBufferAddLit(buf, "<value>host</value>\n");
-        if (poolOptions->flags & VIR_STORAGE_POOL_SOURCE_DEVICE)
-            virBufferAddLit(buf, "<value>device</value>\n");
-        if (poolOptions->flags & VIR_STORAGE_POOL_SOURCE_DIR)
-            virBufferAddLit(buf, "<value>dir</value>\n");
-        if (poolOptions->flags & VIR_STORAGE_POOL_SOURCE_ADAPTER)
-            virBufferAddLit(buf, "<value>adapter</value>\n");
-        if (poolOptions->flags & VIR_STORAGE_POOL_SOURCE_NAME)
-            virBufferAddLit(buf, "<value>name</value>\n");
-        if (poolOptions->flags & VIR_STORAGE_POOL_SOURCE_INITIATOR_IQN)
-            virBufferAddLit(buf, "<value>initiator</value>\n");
-        if (poolOptions->flags & VIR_STORAGE_POOL_SOURCE_NETWORK)
-            virBufferAddLit(buf, "<value>network</value>\n");
 
         virBufferAdjustIndent(buf, -2);
         virBufferAddLit(buf, "</enum>\n");

docs/formatstoragecaps.html.in
@@ -56,9 +56,6 @@
         &lt;value&gt;ext2&lt;/value&gt;
         ...
       &lt;/enum&gt;
-      &lt;enum name='requiredSourceElements'&gt;
-        &lt;value&gt;device&lt;/value&gt;
-      &lt;/enum&gt;
     &lt;/poolOptions&gt;
     &lt;volOptions&gt;
       &lt;defaultFormat type='raw'&lt;/&gt;
@@ -91,10 +88,6 @@
             <dt><code>sourceFormatType</code></dt>
             <dd>Lists all the possible <code>poolOptions</code> source
             pool format types.
-            </dd>
-            <dt><code>requiredSourceElements</code></dt>
-            <dd>Lists all the required <code>poolOptions</code> source
-            subelements required for a valid source pool element.
             </dd>
             <dt><code>targetFormatType</code></dt>
             <dd>Lists all the possible <code>volOptions</code> target volume

tests/storagepoolcapsschemadata/poolcaps-fs.xml
@@ -41,9 +41,6 @@
         <value>xfs</value>
         <value>ocfs2</value>
       </enum>
-      <enum name='requiredSourceElements'>
-        <value>device</value>
-      </enum>
     </poolOptions>
     <volOptions>
       <defaultFormat type='raw'/>
@@ -77,10 +74,6 @@
         <value>glusterfs</value>
         <value>cifs</value>
       </enum>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>dir</value>
-      </enum>
     </poolOptions>
     <volOptions>
       <defaultFormat type='raw'/>
@@ -112,10 +105,6 @@
         <value>unknown</value>
         <value>lvm2</value>
       </enum>
-      <enum name='requiredSourceElements'>
-        <value>device</value>
-        <value>name</value>
-      </enum>
     </poolOptions>
   </pool>
   <pool type='disk' supported='no'>
@@ -131,9 +120,6 @@
         <value>pc98</value>
         <value>sun</value>
         <value>lvm2</value>
-      </enum>
-      <enum name='requiredSourceElements'>
-        <value>device</value>
       </enum>
     </poolOptions>
     <volOptions>
@@ -151,60 +137,18 @@
     </volOptions>
   </pool>
   <pool type='iscsi' supported='no'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>device</value>
-        <value>initiator</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='iscsi-direct' supported='no'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>device</value>
-        <value>initiator</value>
-        <value>network</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='scsi' supported='no'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>adapter</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='mpath' supported='no'>
   </pool>
   <pool type='rbd' supported='no'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>name</value>
-        <value>network</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='sheepdog' supported='no'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>name</value>
-        <value>network</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='gluster' supported='no'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>dir</value>
-        <value>name</value>
-        <value>network</value>
-      </enum>
-    </poolOptions>
     <volOptions>
       <defaultFormat type='raw'/>
       <enum name='targetFormatType'>
@@ -229,19 +173,8 @@
     </volOptions>
   </pool>
   <pool type='zfs' supported='no'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>device</value>
-        <value>name</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='vstorage' supported='no'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>name</value>
-      </enum>
-    </poolOptions>
     <volOptions>
       <defaultFormat type='raw'/>
       <enum name='targetFormatType'>

tests/storagepoolcapsschemadata/poolcaps-full.xml
@@ -41,9 +41,6 @@
         <value>xfs</value>
         <value>ocfs2</value>
       </enum>
-      <enum name='requiredSourceElements'>
-        <value>device</value>
-      </enum>
     </poolOptions>
     <volOptions>
       <defaultFormat type='raw'/>
@@ -77,10 +74,6 @@
         <value>glusterfs</value>
         <value>cifs</value>
       </enum>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>dir</value>
-      </enum>
     </poolOptions>
     <volOptions>
       <defaultFormat type='raw'/>
@@ -112,10 +105,6 @@
         <value>unknown</value>
         <value>lvm2</value>
       </enum>
-      <enum name='requiredSourceElements'>
-        <value>device</value>
-        <value>name</value>
-      </enum>
     </poolOptions>
   </pool>
   <pool type='disk' supported='yes'>
@@ -131,9 +120,6 @@
         <value>pc98</value>
         <value>sun</value>
         <value>lvm2</value>
-      </enum>
-      <enum name='requiredSourceElements'>
-        <value>device</value>
       </enum>
     </poolOptions>
     <volOptions>
@@ -151,60 +137,18 @@
     </volOptions>
   </pool>
   <pool type='iscsi' supported='yes'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>device</value>
-        <value>initiator</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='iscsi-direct' supported='yes'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>device</value>
-        <value>initiator</value>
-        <value>network</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='scsi' supported='yes'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>adapter</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='mpath' supported='yes'>
   </pool>
   <pool type='rbd' supported='yes'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>name</value>
-        <value>network</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='sheepdog' supported='yes'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>name</value>
-        <value>network</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='gluster' supported='yes'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>host</value>
-        <value>dir</value>
-        <value>name</value>
-        <value>network</value>
-      </enum>
-    </poolOptions>
     <volOptions>
       <defaultFormat type='raw'/>
       <enum name='targetFormatType'>
@@ -229,19 +173,8 @@
     </volOptions>
   </pool>
   <pool type='zfs' supported='yes'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>device</value>
-        <value>name</value>
-      </enum>
-    </poolOptions>
   </pool>
   <pool type='vstorage' supported='yes'>
-    <poolOptions>
-      <enum name='requiredSourceElements'>
-        <value>name</value>
-      </enum>
-    </poolOptions>
     <volOptions>
       <defaultFormat type='raw'/>
       <enum name='targetFormatType'>

commit d063389f10707a6694fb98a69f403a63e4d22245

conf: storage: remove redundant condition
We exit early if poolOptions->formatToString is false.

Fixes: 9dadc7302920f9fca0057c655d03c2b0206b9a70
Signed-off-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

src/conf/storage_conf.c
@@ -364,6 +364,7 @@ virStoragePoolOptionsFormatPool(virBuffer *buf,
                                 int type)
 {
     virStoragePoolOptions *poolOptions;
+    size_t i;
 
     if (!(poolOptions = virStoragePoolOptionsForPoolType(type)))
         return -1;
@@ -374,22 +375,17 @@ virStoragePoolOptionsFormatPool(virBuffer *buf,
     virBufferAddLit(buf, "<poolOptions>\n");
     virBufferAdjustIndent(buf, 2);
 
-    if (poolOptions->formatToString) {
-        size_t i;
+    virBufferAsprintf(buf, "<defaultFormat type='%s'/>\n",
+                      (poolOptions->formatToString)(poolOptions->defaultFormat));
 
-        virBufferAsprintf(buf, "<defaultFormat type='%s'/>\n",
-                          (poolOptions->formatToString)(poolOptions->defaultFormat));
+    virBufferAddLit(buf, "<enum name='sourceFormatType'>\n");
+    virBufferAdjustIndent(buf, 2);
 
-        virBufferAddLit(buf, "<enum name='sourceFormatType'>\n");
-        virBufferAdjustIndent(buf, 2);
+    for (i = 0; i < poolOptions->lastFormat; i++)
+        virBufferAsprintf(buf, "<value>%s</value>\n", (poolOptions->formatToString)(i));
 
-        for (i = 0; i < poolOptions->lastFormat; i++)
-            virBufferAsprintf(buf, "<value>%s</value>\n",
-                              (poolOptions->formatToString)(i));
-
-        virBufferAdjustIndent(buf, -2);
-        virBufferAddLit(buf, "</enum>\n");
-    }
+    virBufferAdjustIndent(buf, -2);
+    virBufferAddLit(buf, "</enum>\n");
 
     virBufferAdjustIndent(buf, -2);
     virBufferAddLit(buf, "</poolOptions>\n");