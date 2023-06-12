e31ac985f5ab17ff8aa615ac316ad0fd64e3fbe3
Martin Kletzander <mkletzan@redhat.com>
2023-06-12 09:49:33 +0200

    meson-dist: Use shutil.copy for copying a file
    Using os.system("cp {0} {1}".format(...)) has two issues, it does not
    work on Windows, but more importantly it can cause issues in case one of
    the directories has a space in it.
    
    Signed-off-by: Martin Kletzander <mkletzan@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- scripts/meson-dist.py
+++ scripts/meson-dist.py
@@ -2,13 +2,12 @@
 
 import os
 import sys
+import shutil
 
 meson_build_root = sys.argv[1]
 file_name = sys.argv[2]
 
 meson_dist_root = os.environ['MESON_DIST_ROOT']
 
-os.system('cp {0} {1}'.format(
-    os.path.join(meson_build_root, file_name),
-    os.path.join(meson_dist_root, file_name)
-))
+shutil.copy(os.path.join(meson_build_root, file_name),
+            os.path.join(meson_dist_root, file_name))
