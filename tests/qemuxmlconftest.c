0d095c6d47dce854af07278ed5ec7062222eca2c
Andrea Bolognani <abologna@redhat.com>
2024-01-26 19:37:31 +0100

    tests: Add controller-scsi-auto
    The "auto" SCSI controller model was introduced for use in the
    ESX driver, but the QEMU driver doesn't reject the value.
    
    Add a test case showing the behavior when such a configuration
    is encountered.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Jonathon Jongsma <jjongsma@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -1586,6 +1586,7 @@ mymain(void)
     DO_TEST_CAPS_LATEST("disk-scsi-disk-vpd");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("disk-scsi-disk-vpd-build-error");
     DO_TEST_CAPS_LATEST("controller-virtio-scsi");
+    DO_TEST_CAPS_LATEST("controller-scsi-auto");
     DO_TEST_CAPS_LATEST("disk-sata-device");
     DO_TEST_CAPS_LATEST("disk-aio");
     DO_TEST_CAPS_LATEST("disk-aio-io_uring");
