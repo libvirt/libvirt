f6cc87a3395b44357d8506b7175174491a75c300
Peter Krempa <pkrempa@redhat.com>
2024-02-21 15:30:41 +0100

    qemuxmlconftest: Add test data for USB selection with 'mac99' machine on ppc64
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>
    Reviewed-by: Andrea Bolognani <abologna@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -1255,6 +1255,8 @@ mymain(void)
     DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("ppc64-pseries-minimal", "ppc64");
     DO_TEST_CAPS_ARCH_LATEST("ppc64-g3beige-minimal", "ppc64");
     DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("ppc64-g3beige-minimal", "ppc64");
+    DO_TEST_CAPS_ARCH_LATEST("ppc64-mac99-minimal", "ppc64");
+    DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("ppc64-mac99-minimal", "ppc64");
     DO_TEST_CAPS_ARCH_LATEST("s390x-ccw-minimal", "s390x");
     DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("s390x-ccw-minimal", "s390x");
 
@@ -1856,6 +1858,7 @@ mymain(void)
     DO_TEST_CAPS_ARCH_LATEST("usb-controller-default-pseries", "ppc64");
     DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("usb-controller-default-pseries", "ppc64");
     DO_TEST_CAPS_ARCH_LATEST("usb-controller-default-g3beige", "ppc64");
+    DO_TEST_CAPS_ARCH_LATEST("usb-controller-default-mac99", "ppc64");
     /* i440fx downgrades to use '-usb' if the explicit controller is not present */
     DO_TEST_FULL("usb-controller-default-unavailable-i440fx", ".x86_64-latest",
                  ARG_CAPS_ARCH, "x86_64",
@@ -1939,8 +1942,18 @@ mymain(void)
                  ARG_CAPS_VER, "latest",
                  ARG_QEMU_CAPS_DEL, QEMU_CAPS_NEC_USB_XHCI, QEMU_CAPS_DEVICE_QEMU_XHCI, QEMU_CAPS_LAST,
                  ARG_END);
-
     DO_TEST_FULL("usb-controller-default-unavailable-g3beige", ".ppc64-latest",
+                 ARG_CAPS_ARCH, "ppc64",
+                 ARG_CAPS_VER, "latest",
+                 ARG_QEMU_CAPS_DEL, QEMU_CAPS_NEC_USB_XHCI, QEMU_CAPS_PCI_OHCI, QEMU_CAPS_DEVICE_QEMU_XHCI, QEMU_CAPS_LAST,
+                 ARG_END);
+
+    DO_TEST_FULL("usb-controller-default-fallback-mac99", ".ppc64-latest",
+                 ARG_CAPS_ARCH, "ppc64",
+                 ARG_CAPS_VER, "latest",
+                 ARG_QEMU_CAPS_DEL, QEMU_CAPS_NEC_USB_XHCI, QEMU_CAPS_DEVICE_QEMU_XHCI, QEMU_CAPS_LAST,
+                 ARG_END);
+    DO_TEST_FULL("usb-controller-default-unavailable-mac99", ".ppc64-latest",
                  ARG_CAPS_ARCH, "ppc64",
                  ARG_CAPS_VER, "latest",
                  ARG_QEMU_CAPS_DEL, QEMU_CAPS_NEC_USB_XHCI, QEMU_CAPS_PCI_OHCI, QEMU_CAPS_DEVICE_QEMU_XHCI, QEMU_CAPS_LAST,
