libvirt/libvirt

commit 065054daa71f645fc83aff0271f194d326208616

virnetdevbandwidth: Compute quantum value
I've noticed couple of warning in dmesg while debugging
something:

[ 9683.973754] HTB: quantum of class 10001 is big. Consider r2q change.
[ 9683.976460] HTB: quantum of class 10002 is big. Consider r2q change.

I've read the HTB documentation and linux kernel code to find out
what's wrong. Basically we need to pass another argument
"quantum" to our tc cmd line because the default computed by HTB
does not always work in which case the warning message is printed
out.

You can read more details here:

http://luxik.cdi.cz/~devik/qos/htb/manual/userg.htm#sharing

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

src/util/virnetdevbandwidth.c
@@ -43,6 +43,29 @@ virNetDevBandwidthFree(virNetDevBandwidthPtr def)
     VIR_FREE(def);
 }
 
+static void
+virNetDevBandwidthCmdAddOptimalQuantum(virCommandPtr cmd,
+                                       const virNetDevBandwidthRate *rate)
+{
+    const unsigned long long mtu = 1500;
+    unsigned long long r2q;
+
+    /* When two or more classes compete for unused bandwidth they are each
+     * given some number of bytes before serving other competing class. This
+     * number is called quantum. It's advised in HTB docs that the number
+     * should be equal to MTU. The class quantum is computed from its rate
+     * divided by global r2q parameter. However, if rate is too small the
+     * default value will not suffice and thus we must provide our own value.
+     * */
+
+    r2q = rate->average * 1024 / 8 / mtu;
+    if (!r2q)
+        r2q = 1;
+
+    virCommandAddArg(cmd, "quantum");
+    virCommandAddArgFormat(cmd, "%llu", r2q);
+}
+
 /**
  * virNetDevBandwidthManipulateFilter:
  * @ifname: interface to operate on
@@ -280,6 +303,7 @@ virNetDevBandwidthSet(const char *ifname,
             virCommandAddArgList(cmd, "class", "add", "dev", ifname, "parent",
                                  "1:", "classid", "1:1", "htb", "rate", average,
                                  "ceil", peak ? peak : average, NULL);
+            virNetDevBandwidthCmdAddOptimalQuantum(cmd, bandwidth->in);
             if (virCommandRun(cmd, NULL) < 0)
                 goto cleanup;
         }
@@ -295,6 +319,7 @@ virNetDevBandwidthSet(const char *ifname,
         if (burst)
             virCommandAddArgList(cmd, "burst", burst, NULL);
 
+        virNetDevBandwidthCmdAddOptimalQuantum(cmd, bandwidth->in);
         if (virCommandRun(cmd, NULL) < 0)
             goto cleanup;
 
@@ -549,6 +574,7 @@ virNetDevBandwidthPlug(const char *brname,
     virCommandAddArgList(cmd, "class", "add", "dev", brname, "parent", "1:1",
                          "classid", class_id, "htb", "rate", floor,
                          "ceil", ceil, NULL);
+    virNetDevBandwidthCmdAddOptimalQuantum(cmd, bandwidth->in);
 
     if (virCommandRun(cmd, NULL) < 0)
         goto cleanup;
@@ -672,6 +698,7 @@ virNetDevBandwidthUpdateRate(const char *ifname,
     virCommandAddArgList(cmd, "class", "change", "dev", ifname,
                          "classid", class_id, "htb", "rate", rate,
                          "ceil", ceil, NULL);
+    virNetDevBandwidthCmdAddOptimalQuantum(cmd, bandwidth->in);
 
     if (virCommandRun(cmd, NULL) < 0)
         goto cleanup;

tests/virnetdevbandwidthtest.c
@@ -137,7 +137,7 @@ mymain(void)
                 (TC " qdisc del dev eth0 root\n"
                  TC " qdisc del dev eth0 ingress\n"
                  TC " qdisc add dev eth0 root handle 1: htb default 1\n"
-                 TC " class add dev eth0 parent 1: classid 1:1 htb rate 1024kbps\n"
+                 TC " class add dev eth0 parent 1: classid 1:1 htb rate 1024kbps quantum 87\n"
                  TC " qdisc add dev eth0 parent 1:1 handle 2: sfq perturb 10\n"
                  TC " filter add dev eth0 parent 1:0 protocol all prio 1 handle 1 fw flowid 1\n"));
 
@@ -157,7 +157,7 @@ mymain(void)
                 (TC " qdisc del dev eth0 root\n"
                  TC " qdisc del dev eth0 ingress\n"
                  TC " qdisc add dev eth0 root handle 1: htb default 1\n"
-                 TC " class add dev eth0 parent 1: classid 1:1 htb rate 1kbps ceil 2kbps burst 4kb\n"
+                 TC " class add dev eth0 parent 1: classid 1:1 htb rate 1kbps ceil 2kbps burst 4kb quantum 1\n"
                  TC " qdisc add dev eth0 parent 1:1 handle 2: sfq perturb 10\n"
                  TC " filter add dev eth0 parent 1:0 protocol all prio 1 handle 1 fw flowid 1\n"
                  TC " qdisc add dev eth0 ingress\n"

commit ac9c3c0b2ce4ce8b2501ec3f2e27fe2bf4dd61f4

virnetdevbandwidth.c: Put a limit to "quantum"
The "quantum" attribute of HTB is documented as:

  Number of bytes to serve from this class before the scheduler
  moves to the next class.

Since v1.3.2-rc1~225 we compute what we think is the appropriate
value and pass it on the TC command line. But kernel and
subsequently TC use uint32_t to store this value. If we compute
value outside of this type then TC fails and prints usage which
we then interpret as an error message. Needlessly long error
message. While there's not much we can do about the latter, we
can put a cap on the value and stop tickling this behavior of TC.

Fixes: 065054daa71f645fc83aff0271f194d326208616
Resolves: https://issues.redhat.com/browse/RHEL-34112
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

src/util/virnetdevbandwidth.c
@@ -46,6 +46,7 @@ virNetDevBandwidthCmdAddOptimalQuantum(virCommand *cmd,
                                        const virNetDevBandwidthRate *rate)
 {
     const unsigned long long mtu = 1500;
+    const unsigned long long r2q_limit = UINT32_MAX;
     unsigned long long r2q;
 
     /* When two or more classes compete for unused bandwidth they are each
@@ -59,6 +60,11 @@ virNetDevBandwidthCmdAddOptimalQuantum(virCommand *cmd,
     r2q = rate->average * 1024 / 8 / mtu;
     if (!r2q)
         r2q = 1;
+
+    /* But there's an internal limit in TC (well, kernel's implementation of
+     * HTB) for quantum: it has to fit into u32. Put a cap there. */
+    if (r2q > r2q_limit)
+        r2q = r2q_limit;
 
     virCommandAddArg(cmd, "quantum");
     virCommandAddArgFormat(cmd, "%llu", r2q);