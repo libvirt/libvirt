bc70aa1df33ce91820111058e22f00eeef13036b
Michal Privoznik <mprivozn@redhat.com>
2024-04-29 09:49:17 +0200

    scripts/meson-dist.py: Get builddir from env too
    When meson runs a dist script it sets both MESON_BUILD_ROOT and
    MESON_DIST_ROOT envvars [1]. But for some reason, we took the
    former as an argument and obtained the latter via env. Well,
    obtain both via env.
    
    1: https://mesonbuild.com/Reference-manual_builtin_meson.html#mesonadd_dist_script
    Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- scripts/meson-dist.py
+++ scripts/meson-dist.py
@@ -4,9 +4,9 @@ import os
 import shutil
 import sys
 
-meson_build_root = sys.argv[1]
-file_name = sys.argv[2]
+file_name = sys.argv[1]
 
+meson_build_root = os.environ['MESON_BUILD_ROOT']
 meson_dist_root = os.environ['MESON_DIST_ROOT']
 
 shutil.copy(os.path.join(meson_build_root, file_name),
