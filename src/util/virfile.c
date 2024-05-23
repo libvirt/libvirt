libvirt/libvirt

commit 91f4ebbac81bc3829da6d5a71d7520a6fc9e358e

virsocket: Simplify virSocketSendFD()
After previous cleanups, virSocketSendFD() is but a thin wrapper
over virSocketSendMsgWithFDs(). Replace the body of the former
with a call to the latter.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/util/virfile.c
@@ -2445,9 +2445,7 @@ virFileOpenForked(const char *path, int openflags, mode_t mode,
             goto childerror;
         }
 
-        do {
-            ret = virSocketSendFD(pair[1], fd);
-        } while (ret < 0 && errno == EINTR);
+        ret = virSocketSendFD(pair[1], fd);
 
         if (ret < 0) {
             ret = -errno;

src/util/virsocket.c
@@ -382,38 +382,16 @@ vir_socket(int domain, int type, int protocol)
 /* virSocketSendFD sends the file descriptor fd along the socket
    to a process calling virSocketRecvFD on the other end.
 
-   Return 0 on success, or -1 with errno set in case of error.
+   Return 1 on success, or -1 with errno set in case of error.
 */
 int
 virSocketSendFD(int sock, int fd)
 {
     char byte = 0;
-    struct iovec iov;
-    struct msghdr msg = { 0 };
-    struct cmsghdr *cmsg;
-    char buf[CMSG_SPACE(sizeof(fd))];
+    int fds[] = { fd };
 
-    /* send at least one char */
-    iov.iov_base = &byte;
-    iov.iov_len = 1;
-    msg.msg_iov = &iov;
-    msg.msg_iovlen = 1;
-    msg.msg_name = NULL;
-    msg.msg_namelen = 0;
-
-    msg.msg_control = buf;
-    msg.msg_controllen = sizeof(buf);
-    cmsg = CMSG_FIRSTHDR(&msg);
-    cmsg->cmsg_level = SOL_SOCKET;
-    cmsg->cmsg_type = SCM_RIGHTS;
-    cmsg->cmsg_len = CMSG_LEN(sizeof(fd));
-    /* Initialize the payload: */
-    memcpy(CMSG_DATA(cmsg), &fd, sizeof(fd));
-    msg.msg_controllen = cmsg->cmsg_len;
-
-    if (sendmsg(sock, &msg, 0) != iov.iov_len)
-        return -1;
-    return 0;
+    return virSocketSendMsgWithFDs(sock, &byte, sizeof(byte),
+                                   fds, G_N_ELEMENTS(fds));
 }
 
 

commit 65cdc37a7e849a889f81458c7060a8655f53ab07

virFileOpenForked: Fix handling of return value from virSocketSendFD()
Commit 91f4ebbac81bc3829da6d5a71d7520a6fc9e358e (v10.0.0-185-g91f4ebbac8)
changed the return value of virSocketSendFD() from 0 to 1 on success.

Unfortunately in 'virFileOpenForked' the return value was used to report
the error back to the main process from the fork'd child. As process
return codes are positive only, the code negates the value of 'ret' and
reports it. This resulted in the parent thinking the process exited with
failure:

 # virsh save avocado-vt-vm1 /mnt/save
 error: Failed to save domain 'avocado-vt-vm1' to /mnt/save
 error: Error from child process creating '/mnt/save': Unknown error 255

This error reproduces on NFS mounts with 'root_squash' enabled. I've
also observed it in one specific migration case when root_squash NFS is
used with following error:

  Failed to open file '/var/lib/libvirt/images/alpine.qcow2': Unknown error 255'

To fix the issue the code is refactored so that it doesn't actually
touch the 'ret' variable needlessly and assigns to it only on failure
cases, which prevents the '1' to be propagated to the parent process as
'255' after negating and storing in the process return code.

Fixes: 91f4ebbac81bc3829da6d5a71d7520a6fc9e358e
Resolves: https://issues.redhat.com/browse/RHEL-36721
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Jiri Denemark <jdenemar@redhat.com>

src/util/virfile.c
@@ -2441,8 +2441,7 @@ virFileOpenForked(const char *path,
             created = true;
 
         /* File is successfully open. Set permissions if requested. */
-        ret = virFileOpenForceOwnerMode(path, fd, mode, uid, gid, flags);
-        if (ret < 0) {
+        if (virFileOpenForceOwnerMode(path, fd, mode, uid, gid, flags) < 0) {
             ret = -errno;
             virReportSystemError(errno,
                                  _("child process failed to force owner mode file '%1$s'"),
@@ -2450,9 +2449,7 @@ virFileOpenForked(const char *path,
             goto childerror;
         }
 
-        ret = virSocketSendFD(pair[1], fd);
-
-        if (ret < 0) {
+        if (virSocketSendFD(pair[1], fd) < 0) {
             ret = -errno;
             virReportSystemError(errno, "%s",
                                  _("child process failed to send fd to parent"));