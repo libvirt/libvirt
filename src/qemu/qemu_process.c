libvirt/libvirt

commit da95bcb6b2d9b04958e0f2603202801dd29debb8

qemu: Substract isolcpus from all online affinity
When starting a domain and there's no vCPU/emulator pinning set,
we query the list of all online physical CPUs and set affinity of
the child process (which eventually becomes QEMU) to that list.
We can't assume libvirtd itself had affinity to all online CPUs
and since affinity of the child process is inherited, we should
fix it afterwards. But that's not necessarily correct. Users
might isolate some physical CPUs and we should avoid touching
them unless explicitly told so (i.e. vCPU/emulator pinning told
us so).

Therefore, when attempting to set affinity to all online CPUs
subtract the isolated ones.

Before this commit:

  root@localhost:~# cat /sys/devices/system/cpu/isolated
  19,21,23
  root@virtlab414:~# taskset -cp $(pgrep qemu)
  pid 14835's current affinity list: 0-23

After:

  root@virtlab414:~# taskset -cp $(pgrep qemu)
  pid 17153's current affinity list: 0-18,20,22

Resolves: https://issues.redhat.com/browse/RHEL-33082
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

src/qemu/qemu_process.c
@@ -2334,6 +2334,8 @@ qemuProcessDetectIOThreadPIDs(virDomainObj *vm,
 static int
 qemuProcessGetAllCpuAffinity(virBitmap **cpumapRet)
 {
+    g_autoptr(virBitmap) isolCpus = NULL;
+
     *cpumapRet = NULL;
 
     if (!virHostCPUHasBitmap())
@@ -2341,6 +2343,13 @@ qemuProcessGetAllCpuAffinity(virBitmap **cpumapRet)
 
     if (!(*cpumapRet = virHostCPUGetOnlineBitmap()))
         return -1;
+
+    if (virHostCPUGetIsolated(&isolCpus) < 0)
+        return -1;
+
+    if (isolCpus) {
+        virBitmapSubtract(*cpumapRet, isolCpus);
+    }
 
     return 0;
 }

commit 095f22db21a3e924df9f2cd435d0f93f37c2925d

qemu_process: Issue an info message when subtracting isolcpus
In one of my previous commits I've made us substract isolcpus
from all online CPUs when setting affinity on QEMU threads. See
commit below for more info on that. Nevertheless, this is
something that surely deserves an entry in log. I've chosen INFO
priority for now. We can promote that to a regular WARN if users
complain.

Fixes: da95bcb6b2d9b04958e0f2603202801dd29debb8
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_process.c
@@ -2348,6 +2348,12 @@ qemuProcessGetAllCpuAffinity(virBitmap **cpumapRet)
         return -1;
 
     if (isolCpus) {
+        g_autofree char *isolCpusStr = virBitmapFormat(isolCpus);
+        g_autofree char *cpumapRetStr = virBitmapFormat(*cpumapRet);
+
+        VIR_INFO("Subtracting isolated CPUs %1$s from online CPUs %2$s",
+                 isolCpusStr, cpumapRetStr);
+
         virBitmapSubtract(*cpumapRet, isolCpus);
     }
 