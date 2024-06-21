397c0f4b01ae1b24806a145ffbd31a9a49126ae3
Laine Stump <laine@redhat.com>
2024-06-21 08:17:58 -0400

    network: add more firewall test cases
    This patch adds some previously missing test cases that test for
    proper firewall rule creation when the following are included in the
    network definition:
    
    * <forward dev='blah'>
    * no forward element (an "isolated" network)
    * nat port range when only ipv4 is nat-ed
    * nat port range when both ipv4 & ipv6 are nated
    
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
    Signed-off-by: Laine Stump <laine@redhat.com>

--- tests/networkxml2firewalltest.c
+++ tests/networkxml2firewalltest.c
@@ -198,6 +198,11 @@ mymain(void)
     DO_TEST("nat-ipv6");
     DO_TEST("nat-ipv6-masquerade");
     DO_TEST("route-default");
+    DO_TEST("forward-dev");
+    DO_TEST("isolated");
+    DO_TEST("forward-dev");
+    DO_TEST("nat-port-range");
+    DO_TEST("nat-port-range-ipv6");
 
     return ret == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
