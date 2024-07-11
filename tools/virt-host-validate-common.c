libvirt/libvirt

commit 1a8f646f291775d2423ce4e4df62ad69f06ab827

virt-host-validate: Detect SEV-ES and SEV-SNP
With a simple cpuid (Section "E.4.17 Function
8000_001Fh—Encrypted Memory Capabilities" in "AMD64 Architecture
Programmer’s Manual Vol. 3") we can detect whether CPU is capable
of running SEV-ES and/or SEV-SNP guests. Report these in
virt-host-validate tool.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Jiri Denemark <jdenemar@redhat.com>
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

tools/virt-host-validate-common.c
@@ -34,6 +34,7 @@
 #include "virstring.h"
 #include "virarch.h"
 #include "virutil.h"
+#include "virhostcpu.h"
 
 #define VIR_FROM_THIS VIR_FROM_NONE
 
@@ -380,9 +381,11 @@ bool virHostKernelModuleIsLoaded(const char *module)
 
 
 static int
-virHostValidateAMDSev(virValidateLevel level)
+virHostValidateAMDSev(const char *hvname,
+                      virValidateLevel level)
 {
     g_autofree char *mod_value = NULL;
+    uint32_t eax, ebx;
 
     if (virFileReadValueString(&mod_value, "/sys/module/kvm_amd/parameters/sev") < 0) {
         virValidateFail(level, "AMD Secure Encrypted Virtualization not "
@@ -402,6 +405,32 @@ virHostValidateAMDSev(virValidateLevel level)
         virValidateFail(level,
                         "AMD Secure Encrypted Virtualization appears to be "
                         "disabled in firmware.");
+        return VIR_VALIDATE_FAILURE(level);
+    }
+
+    virValidatePass();
+
+    virValidateCheck(hvname, "%s",
+                     _("Checking for AMD Secure Encrypted Virtualization-Encrypted State (SEV-ES)"));
+
+    virHostCPUX86GetCPUID(0x8000001F, 0, &eax, &ebx, NULL, NULL);
+
+    if (eax & (1U << 3)) {
+        virValidatePass();
+    } else {
+        virValidateFail(level,
+                        "AMD SEV-ES is not supported");
+        return VIR_VALIDATE_FAILURE(level);
+    }
+
+    virValidateCheck(hvname, "%s",
+                     _("Checking for AMD Secure Encrypted Virtualization-Secure Nested Paging (SEV-SNP)"));
+
+    if (eax & (1U << 4)) {
+        virValidatePass();
+    } else {
+        virValidateFail(level,
+                        "AMD SEV-SNP is not supported");
         return VIR_VALIDATE_FAILURE(level);
     }
 
@@ -459,7 +488,7 @@ int virHostValidateSecureGuests(const char *hvname,
             return VIR_VALIDATE_FAILURE(level);
         }
     } else if (hasAMDSev) {
-        int rc = virHostValidateAMDSev(level);
+        int rc = virHostValidateAMDSev(hvname, level);
 
         if (rc > 0)
             virValidatePass();

commit c9fa43c48cdb1b8505929e3287975445f3004f32

virt-host-validate: Drop extra "PASS"
If virt-host-validate is ran on a SEV-SNP capable machine, an
extra "PASS" is printed out. This is because
virHostValidateAMDSev() prints "PASS" and then returns 1
(indicating success) which in turn makes the caller
(virHostValidateSecureGuests()) print "PASS" again. Just drop the
extra printing in the caller and let virHostValidateAMDSev() do
all the printing.

Fixes: 1a8f646f291775d2423ce4e4df62ad69f06ab827
Resolves: https://issues.redhat.com/browse/RHEL-46868
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

tools/virt-host-validate-common.c
@@ -488,11 +488,7 @@ int virHostValidateSecureGuests(const char *hvname,
             return VIR_VALIDATE_FAILURE(level);
         }
     } else if (hasAMDSev) {
-        int rc = virHostValidateAMDSev(hvname, level);
-
-        if (rc > 0)
-            virValidatePass();
-        return rc;
+        return virHostValidateAMDSev(hvname, level);
     }
 
     virValidateFail(level,