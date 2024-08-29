libvirt/libvirt

commit d292c5ba1775bd70488fc1646e986c1987844549

qemu: Export a few functions
We're going to need them in a minute.

Signed-off-by: Andrea Bolognani <abologna@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/qemu/qemu_domain.c
@@ -9045,8 +9045,9 @@ qemuFindAgentConfig(virDomainDef *def)
     return NULL;
 }
 
-
-static bool
+/* You should normally avoid this function and use
+ * qemuDomainMachineIsQ35() instead. */
+bool
 qemuDomainMachineIsQ35(const char *machine,
                        const virArch arch)
 {
@@ -9062,7 +9063,9 @@ qemuDomainMachineIsQ35(const char *machine,
 }
 
 
-static bool
+/* You should normally avoid this function and use
+ * qemuDomainMachineIsI440FX() instead. */
+bool
 qemuDomainMachineIsI440FX(const char *machine,
                           const virArch arch)
 {
@@ -9179,7 +9182,10 @@ qemuDomainMachineIsMipsMalta(const char *machine,
     return false;
 }
 
-static bool
+
+/* You should normally avoid this function and use
+ * qemuDomainMachineIsXenFV() instead. */
+bool
 qemuDomainMachineIsXenFV(const char *machine,
                          const virArch arch)
 {

src/qemu/qemu_domain.h
@@ -823,10 +823,16 @@ virDomainChrDef *qemuFindAgentConfig(virDomainDef *def);
 
 /* You should normally avoid these functions and use the variant that
  * doesn't have "Machine" in the name instead. */
+bool qemuDomainMachineIsQ35(const char *machine,
+                            const virArch arch);
+bool qemuDomainMachineIsI440FX(const char *machine,
+                               const virArch arch);
 bool qemuDomainMachineIsARMVirt(const char *machine,
                                 const virArch arch);
 bool qemuDomainMachineIsPSeries(const char *machine,
                                 const virArch arch);
+bool qemuDomainMachineIsXenFV(const char *machine,
+                              const virArch arch);
 bool qemuDomainMachineHasBuiltinIDE(const char *machine,
                                     const virArch arch);
 

commit 76f6caee3c60e171e37eaa15772b02291d65011f

qemu: Fix a few comments
Fixes: d292c5ba1775bd70488fc1646e986c1987844549
Signed-off-by: Kamil Szczęk <kamil@szczek.dev>
Reviewed-by: Andrea Bolognani <abologna@redhat.com>

src/qemu/qemu_domain.c
@@ -9046,7 +9046,7 @@ qemuFindAgentConfig(virDomainDef *def)
 }
 
 /* You should normally avoid this function and use
- * qemuDomainMachineIsQ35() instead. */
+ * qemuDomainIsQ35() instead. */
 bool
 qemuDomainMachineIsQ35(const char *machine,
                        const virArch arch)
@@ -9064,7 +9064,7 @@ qemuDomainMachineIsQ35(const char *machine,
 
 
 /* You should normally avoid this function and use
- * qemuDomainMachineIsI440FX() instead. */
+ * qemuDomainIsI440FX() instead. */
 bool
 qemuDomainMachineIsI440FX(const char *machine,
                           const virArch arch)
@@ -9184,7 +9184,7 @@ qemuDomainMachineIsMipsMalta(const char *machine,
 
 
 /* You should normally avoid this function and use
- * qemuDomainMachineIsXenFV() instead. */
+ * qemuDomainIsXenFV() instead. */
 bool
 qemuDomainMachineIsXenFV(const char *machine,
                          const virArch arch)