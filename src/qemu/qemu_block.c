libvirt/libvirt

commit 24b667eeed78d2df0376a38a592ed9d8c2744bdc

qemu: block: Absorb logic from qemuBlockReopenFormat to qemuBlockReopenAccess
Move all the logic into the new function and remove the old one.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_block.c
@@ -3192,45 +3192,6 @@ qemuBlockReopenFormatMon(qemuMonitor *mon,
 
 
 /**
- * qemuBlockReopenFormat:
- * @vm: domain object
- * @src: storage source to reopen
- * @asyncJob: qemu async job type
- *
- * Invokes the 'blockdev-reopen' command on the format layer of @src. This means
- * that @src must be already properly configured for the desired outcome. The
- * nodenames of @src are used to identify the specific image in qemu.
- */
-static int
-qemuBlockReopenFormat(virDomainObj *vm,
-                      virStorageSource *src,
-                      virDomainAsyncJob asyncJob)
-{
-    qemuDomainObjPrivate *priv = vm->privateData;
-    int rc;
-
-    /* If we are lacking the object here, qemu might have opened an image with
-     * a node name unknown to us */
-    if (!src->backingStore) {
-        virReportError(VIR_ERR_OPERATION_UNSUPPORTED, "%s",
-                       _("can't reopen image with unknown presence of backing store"));
-        return -1;
-    }
-
-    if (qemuDomainObjEnterMonitorAsync(vm, asyncJob) < 0)
-        return -1;
-
-    rc = qemuBlockReopenFormatMon(priv->mon, src);
-
-    qemuDomainObjExitMonitor(vm);
-    if (rc < 0)
-        return -1;
-
-    return 0;
-}
-
-
-/**
  * qemuBlockReopenAccess:
  * @vm: domain object
  * @src: storage source to reopen
@@ -3242,23 +3203,44 @@ qemuBlockReopenFormat(virDomainObj *vm,
  *
  * Callers must use qemuBlockReopenReadWrite/qemuBlockReopenReadOnly functions.
  */
-
 static int
 qemuBlockReopenAccess(virDomainObj *vm,
                       virStorageSource *src,
                       bool readonly,
                       virDomainAsyncJob asyncJob)
 {
+    qemuDomainObjPrivate *priv = vm->privateData;
+    int rc;
+    int ret = -1;
+
     if (src->readonly == readonly)
         return 0;
 
-    src->readonly = readonly;
-    if (qemuBlockReopenFormat(vm, src, asyncJob) < 0) {
-        src->readonly = !readonly;
+    /* If we are lacking the object here, qemu might have opened an image with
+     * a node name unknown to us */
+    if (!src->backingStore) {
+        virReportError(VIR_ERR_OPERATION_UNSUPPORTED, "%s",
+                       _("can't reopen image with unknown presence of backing store"));
         return -1;
     }
 
-    return 0;
+    src->readonly = readonly;
+    /* from now on all error paths must use 'goto cleanup' */
+
+    if (qemuDomainObjEnterMonitorAsync(vm, asyncJob) < 0)
+        goto cleanup;
+
+    rc = qemuBlockReopenFormatMon(priv->mon, src);
+
+    qemuDomainObjExitMonitor(vm);
+    if (rc < 0)
+        goto cleanup;
+
+    ret = 0;
+
+ cleanup:
+    src->readonly = !readonly;
+    return ret;
 }
 
 

commit b83e87ca78afc44352b5c7a6850acdc53513728a

qemuBlockReopenAccess: Fix update of 'readonly' state
Refactors done in 24b667eeed78d2df (and also 9ec0e28e876b17df9)
broke the expected handling of the update of 'readonly' flag of a
virStorage. The source is actually set to the proper state but rolled
back to the previous state as the 'cleanup' label should have been
'error' and thus not reached on success.

Additionally some of the code paths violate the statement in the comment
after updating 'readonly' that only 'goto error' must be used.

Fixes: 24b667eeed78d2df0376a38a592ed9d8c2744bdc
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Jiri Denemark <jdenemar@redhat.com>

src/qemu/qemu_block.c
@@ -3172,7 +3172,6 @@ qemuBlockReopenAccess(virDomainObj *vm,
     g_autoptr(virJSONValue) reopenoptions = virJSONValueNewArray();
     g_autoptr(virJSONValue) srcprops = NULL;
     int rc;
-    int ret = -1;
 
     VIR_DEBUG("nodename:'%s' current-ro:'%d requested-ro='%d'",
               qemuBlockStorageSourceGetEffectiveNodename(src),
@@ -3190,39 +3189,39 @@ qemuBlockReopenAccess(virDomainObj *vm,
     }
 
     src->readonly = readonly;
-    /* from now on all error paths must use 'goto cleanup' */
+    /* from now on all error paths must use 'goto error' which restores the original state */
 
     /* based on which is the current 'effecitve' layer we must reopen the
      * appropriate blockdev */
     if (qemuBlockStorageSourceGetFormatNodename(src)) {
         if (!(srcprops = qemuBlockStorageSourceGetFormatProps(src, src->backingStore)))
-            return -1;
+            goto error;
     } else if (qemuBlockStorageSourceGetSliceNodename(src)) {
         if (!(srcprops = qemuBlockStorageSourceGetBlockdevStorageSliceProps(src, true, false)))
-            return -1;
+            goto error;
     } else {
         if (!(srcprops = qemuBlockStorageSourceGetBackendProps(src,
                                                                QEMU_BLOCK_STORAGE_SOURCE_BACKEND_PROPS_EFFECTIVE_NODE)))
-            return -1;
+            goto error;
     }
 
     if (virJSONValueArrayAppend(reopenoptions, &srcprops) < 0)
-        return -1;
+        goto error;
 
     if (qemuDomainObjEnterMonitorAsync(vm, asyncJob) < 0)
-        goto cleanup;
+        goto error;
 
     rc = qemuMonitorBlockdevReopen(qemuDomainGetMonitor(vm), &reopenoptions);
 
     qemuDomainObjExitMonitor(vm);
     if (rc < 0)
-        goto cleanup;
+        goto error;
 
-    ret = 0;
+    return 0;
 
- cleanup:
+ error:
     src->readonly = !readonly;
-    return ret;
+    return -1;
 }
 
 