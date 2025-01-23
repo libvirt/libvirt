libvirt/libvirt

commit b902cfece0db71c3421b0bfe0e05d1dbe7890c31

virsysinfo: Try reading DMI table
Add DMI support for risc-v and mips. Attempt to read dmidecode and
fall back to old behavior if that fails.

The SMBIOS specification[1] officially supports both RISC-V and LoongArch.
Some mips-based Loongson-3 processors also have SMBIOS.

[1] https://www.dmtf.org/sites/default/files/standards/documents/DSP0134_3.7.0.pdf

Signed-off-by: Brett Holman <brett.holman@canonical.com>
Reviewed-by: Kristina Hanicova <khanicov@redhat.com>
Reviewed-by: Jim Fehlig <jfehlig@suse.com>

src/util/virsysinfo.c
@@ -1249,6 +1249,8 @@ virSysinfoRead(void)
     (defined(__x86_64__) || \
      defined(__i386__) || \
      defined(__amd64__) || \
+     defined(__riscv__) || \
+     defined(__mips__) || \
      defined(__loongarch__))
     return virSysinfoReadDMI();
 #else /* WIN32 || not supported arch */

commit 3e9440dbcb39846c96ada55bd29ff6ae9fff6d93

virsysinfo: fix RISC-V detection
The correct compiler define to detect the RISC-V architecture is __riscv.

Fixes: b902cfece0db ("virsysinfo: Try reading DMI table")
Signed-off-by: Heinrich Schuchardt <heinrich.schuchardt@canonical.com>
Reviewed-by: Andrea Bolognani <abologna@redhat.com>

src/util/virsysinfo.c
@@ -1254,7 +1254,7 @@ virSysinfoRead(void)
     (defined(__x86_64__) || \
      defined(__i386__) || \
      defined(__amd64__) || \
-     defined(__riscv__) || \
+     defined(__riscv) || \
      defined(__mips__) || \
      defined(__loongarch__))
     return virSysinfoReadDMI();