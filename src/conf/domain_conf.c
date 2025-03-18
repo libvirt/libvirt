libvirt/libvirt

commit bfa7d5aaa8834c6801cacbfc6c6ea83a8ecaca9c

conf: parse optional RDP username & password
Like VNC, allow to set credentials for RDP.

Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>
Reviewed-by: Martin Kletzander <mkletzan@redhat.com>

src/conf/domain_conf.c
@@ -1994,6 +1994,7 @@ virDomainGraphicsAuthDefClear(virDomainGraphicsAuthDef *def)
     if (!def)
         return;
 
+    VIR_FREE(def->username);
     VIR_FREE(def->passwd);
 
     /* Don't free def */
@@ -11319,6 +11320,8 @@ virDomainGraphicsAuthDefParseXML(xmlNodePtr node,
     if (!def->passwd)
         return 0;
 
+    def->username = virXMLPropString(node, "username");
+
     validTo = virXMLPropString(node, "passwdValidTo");
     if (validTo) {
         g_autoptr(GDateTime) then = NULL;
@@ -11707,6 +11710,10 @@ virDomainGraphicsDefParseXMLRDP(virDomainGraphicsDef *def,
 
     if (STREQ_NULLABLE(multiUser, "yes"))
         def->data.rdp.multiUser = true;
+
+    if (virDomainGraphicsAuthDefParseXML(node, &def->data.rdp.auth,
+                                         def->type) < 0)
+        return -1;
 
     return 0;
 }
@@ -26306,6 +26313,10 @@ virDomainGraphicsAuthDefFormatAttr(virBuffer *buf,
     if (!def->passwd)
         return;
 
+    if (def->username)
+        virBufferEscapeString(buf, " username='%s'",
+                              def->username);
+
     if (flags & VIR_DOMAIN_DEF_FORMAT_SECURE)
         virBufferEscapeString(buf, " passwd='%s'",
                               def->passwd);
@@ -26579,6 +26590,8 @@ virDomainGraphicsDefFormatRDP(virBuffer *attrBuf,
         virBufferAddLit(attrBuf, " multiUser='yes'");
 
     virDomainGraphicsListenDefFormatAddr(attrBuf, glisten, flags);
+
+    virDomainGraphicsAuthDefFormatAttr(attrBuf, &def->data.rdp.auth, flags);
 
     virDomainGraphicsDefFormatListnes(childBuf, def, flags);
 }

src/conf/domain_conf.h
@@ -1903,6 +1903,7 @@ typedef enum {
 } virDomainGraphicsAuthConnectedType;
 
 struct _virDomainGraphicsAuthDef {
+    char *username;
     char *passwd;
     bool expires; /* Whether there is an expiry time set */
     time_t validTo;  /* seconds since epoch */
@@ -2027,6 +2028,7 @@ struct _virDomainGraphicsDef {
             bool autoport;
             bool replaceUser;
             bool multiUser;
+            virDomainGraphicsAuthDef auth;
         } rdp;
         struct {
             char *display;

src/conf/schemas/domaincommon.rng
@@ -4540,6 +4540,16 @@
               <ref name="addrIPorName"/>
             </attribute>
           </optional>
+          <optional>
+            <attribute name="username">
+              <text/>
+            </attribute>
+          </optional>
+          <optional>
+            <attribute name="passwd">
+              <text/>
+            </attribute>
+          </optional>
           <ref name="listenElements"/>
         </group>
         <group>

commit 815c22a3a79a6dac7abefff050bb2d5157001985

conf: also free auth for rdp in virDomainGraphicsDefFree
Signed-off-by: Ján Tomko <jtomko@redhat.com>
Fixes: bfa7d5aaa8834c6801cacbfc6c6ea83a8ecaca9c

src/conf/domain_conf.c
@@ -2032,6 +2032,7 @@ void virDomainGraphicsDefFree(virDomainGraphicsDef *def)
         break;
 
     case VIR_DOMAIN_GRAPHICS_TYPE_RDP:
+        virDomainGraphicsAuthDefClear(&def->data.rdp.auth);
         break;
 
     case VIR_DOMAIN_GRAPHICS_TYPE_DESKTOP: