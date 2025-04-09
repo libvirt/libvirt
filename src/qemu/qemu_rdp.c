libvirt/libvirt

commit f5e5a9bec9ec3e6c762f5000e3b8a0ba6a3a8c8d

qemu: add 'rdp' capability if qemu-rdp is available
Signed-off-by: Marc-André Lureau <marcandre.lureau@redhat.com>
Reviewed-by: Martin Kletzander <mkletzan@redhat.com>

src/qemu/qemu_rdp.c
@@ -411,3 +411,14 @@ qemuRdpSetCredentials(virDomainObj *vm,
                               "SetCredentials",
                               args);
 }
+
+
+bool
+qemuRdpAvailable(const char *helper)
+{
+    g_autoptr(qemuRdp) rdp = NULL;
+
+    rdp = qemuRdpNewForHelper(helper);
+
+    return rdp && qemuRdpHasFeature(rdp, QEMU_RDP_FEATURE_DBUS_ADDRESS);
+}

src/qemu/qemu_capabilities.c
@@ -6475,7 +6475,8 @@ virQEMUCapsFillDomainDeviceDiskCaps(virQEMUCaps *qemuCaps,
 
 
 void
-virQEMUCapsFillDomainDeviceGraphicsCaps(virQEMUCaps *qemuCaps,
+virQEMUCapsFillDomainDeviceGraphicsCaps(virQEMUDriverConfig *cfg,
+                                        virQEMUCaps *qemuCaps,
                                         virDomainCapsDeviceGraphics *dev)
 {
     dev->supported = VIR_TRISTATE_BOOL_YES;
@@ -6489,8 +6490,14 @@ virQEMUCapsFillDomainDeviceGraphicsCaps(virQEMUCaps *qemuCaps,
         VIR_DOMAIN_CAPS_ENUM_SET(dev->type, VIR_DOMAIN_GRAPHICS_TYPE_SPICE);
     if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_EGL_HEADLESS))
         VIR_DOMAIN_CAPS_ENUM_SET(dev->type, VIR_DOMAIN_GRAPHICS_TYPE_EGL_HEADLESS);
-    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_DISPLAY_DBUS))
-        VIR_DOMAIN_CAPS_ENUM_SET(dev->type, VIR_DOMAIN_GRAPHICS_TYPE_DBUS);
+    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_DISPLAY_DBUS)) {
+        VIR_DOMAIN_CAPS_ENUM_SET(dev->type,
+                                 VIR_DOMAIN_GRAPHICS_TYPE_DBUS);
+        if (qemuRdpAvailable(cfg->qemuRdpName)) {
+            VIR_DOMAIN_CAPS_ENUM_SET(dev->type,
+                                     VIR_DOMAIN_GRAPHICS_TYPE_RDP);
+        }
+    }
 }
 
 
@@ -6974,7 +6981,7 @@ virQEMUCapsFillDomainCaps(virQEMUDriverConfig *cfg,
     virQEMUCapsFillDomainCPUCaps(qemuCaps, hostarch, domCaps);
     virQEMUCapsFillDomainMemoryBackingCaps(qemuCaps, memoryBacking);
     virQEMUCapsFillDomainDeviceDiskCaps(qemuCaps, domCaps->machine, disk);
-    virQEMUCapsFillDomainDeviceGraphicsCaps(qemuCaps, graphics);
+    virQEMUCapsFillDomainDeviceGraphicsCaps(cfg, qemuCaps, graphics);
     virQEMUCapsFillDomainDeviceVideoCaps(qemuCaps, video);
     virQEMUCapsFillDomainDeviceHostdevCaps(qemuCaps, hostdev);
     virQEMUCapsFillDomainDeviceRNGCaps(qemuCaps, rng);

src/qemu/qemu_capabilities.h
@@ -876,7 +876,8 @@ int virQEMUCapsFillDomainCaps(virQEMUDriverConfig *cfg,
 void virQEMUCapsFillDomainMemoryBackingCaps(virQEMUCaps *qemuCaps,
                                             virDomainCapsMemoryBacking *memoryBacking);
 
-void virQEMUCapsFillDomainDeviceGraphicsCaps(virQEMUCaps *qemuCaps,
+void virQEMUCapsFillDomainDeviceGraphicsCaps(virQEMUDriverConfig *cfg,
+                                             virQEMUCaps *qemuCaps,
                                              virDomainCapsDeviceGraphics *dev);
 
 void virQEMUCapsFillDomainDeviceVideoCaps(virQEMUCaps *qemuCaps,

src/qemu/qemu_rdp.h
@@ -42,6 +42,8 @@ struct _qemuRdp {
     guint leaving_id;
 };
 
+bool qemuRdpAvailable(const char *helper);
+
 qemuRdp *qemuRdpNew(void);
 
 qemuRdp *qemuRdpNewForHelper(const char *helper);

src/qemu/qemu_validate.c
@@ -4513,9 +4513,10 @@ qemuValidateDomainDeviceDefGraphics(const virDomainGraphicsDef *graphics,
                                     virQEMUDriver *driver,
                                     virQEMUCaps *qemuCaps)
 {
+    g_autoptr(virQEMUDriverConfig) cfg = virQEMUDriverGetConfig(driver);
     virDomainCapsDeviceGraphics graphicsCaps = { 0 };
 
-    virQEMUCapsFillDomainDeviceGraphicsCaps(qemuCaps, &graphicsCaps);
+    virQEMUCapsFillDomainDeviceGraphicsCaps(cfg, qemuCaps, &graphicsCaps);
 
     if (!VIR_DOMAIN_CAPS_ENUM_IS_SET(graphicsCaps.type, graphics->type)) {
         virReportError(VIR_ERR_CONFIG_UNSUPPORTED,

tests/domaincapsdata/qemu_10.0.0-q35.x86_64+amdsev.xml
@@ -697,6 +697,7 @@
       <enum name='type'>
         <value>sdl</value>
         <value>vnc</value>
+        <value>rdp</value>
         <value>spice</value>
         <value>egl-headless</value>
         <value>dbus</value>

tests/domaincapsdata/qemu_10.0.0-q35.x86_64.xml
@@ -1558,6 +1558,7 @@
       <enum name='type'>
         <value>sdl</value>
         <value>vnc</value>
+        <value>rdp</value>
         <value>spice</value>
         <value>egl-headless</value>
         <value>dbus</value>

tests/domaincapsdata/qemu_10.0.0-tcg.x86_64+amdsev.xml
@@ -1666,6 +1666,7 @@
       <enum name='type'>
         <value>sdl</value>
         <value>vnc</value>
+        <value>rdp</value>
         <value>spice</value>
         <value>egl-headless</value>
         <value>dbus</value>

commit bd473480b443aa2561236dfd903535aa778d99a8

qemu: rdp: Fix 'qemuRdpAvailable()'
qemuRdpAvailable() is called from the capability filing code, thus:
- it must not report spurious errors
- it should not call any extra processes

We can solve the above by just checking existance of 'qemu-rdp' in the
path as:
- at the time of adding of qemuRdpAvailable() there was only one 'qemu-rdp' release
- it supported all the features
- the check can't change as we'd drop the capability

Add comments and gut the check to only check existance of the file.

Fixes: f5e5a9bec9ec3e6c762f5000e3b8a0ba6a3a8c8d
Closes: https://gitlab.com/libvirt/libvirt/-/issues/763
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
Reviewed-by: Boris Fiuczynski <fiuczy@linux.ibm.com>

src/qemu/qemu_rdp.c
@@ -413,12 +413,25 @@ qemuRdpSetCredentials(virDomainObj *vm,
 }
 
 
+/**
+ * qemuRdpAvailable:
+ * @helper: name (or path to) 'qemu-rdp' binary
+ *
+ * Returns whether 'qemu-rdp' is available.
+ *
+ * Important:
+ * This function is called from 'virQEMUDriverGetDomainCapabilities'. It must
+ * not report any errors and must not add any additional checks.
+ *
+ * This function is mocked from 'tests/testutilsqemu.c'
+ *
+ */
 bool
 qemuRdpAvailable(const char *helper)
 {
-    g_autoptr(qemuRdp) rdp = NULL;
+    g_autofree char *helperPath = NULL;
 
-    rdp = qemuRdpNewForHelper(helper);
-
-    return rdp && qemuRdpHasFeature(rdp, QEMU_RDP_FEATURE_DBUS_ADDRESS);
+    /* This function was added corresponding to the first release of 'qemu-rdp'
+     * thus checking existance of the helper binary is sufficient. */
+    return !!(helperPath = virFindFileInPath(helper));
 }