libvirt/libvirt

commit bd473480b443aa2561236dfd903535aa778d99a8

qemu: rdp: Fix 'qemuRdpAvailable()'
qemuRdpAvailable() is called from the capability filing code, thus:
- it must not report spurious errors
- it should not call any extra processes

We can solve the above by just checking existance of 'qemu-rdp' in the
path as:
- at the time of adding of qemuRdpAvailable() there was only one 'qemu-rdp' release
- it supported all the features
- the check can't change as we'd drop the capability

Add comments and gut the check to only check existance of the file.

Fixes: f5e5a9bec9ec3e6c762f5000e3b8a0ba6a3a8c8d
Closes: https://gitlab.com/libvirt/libvirt/-/issues/763
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Marc-André Lureau <marcandre.lureau@redhat.com>
Reviewed-by: Boris Fiuczynski <fiuczy@linux.ibm.com>

src/qemu/qemu_rdp.c
@@ -413,12 +413,25 @@ qemuRdpSetCredentials(virDomainObj *vm,
 }
 
 
+/**
+ * qemuRdpAvailable:
+ * @helper: name (or path to) 'qemu-rdp' binary
+ *
+ * Returns whether 'qemu-rdp' is available.
+ *
+ * Important:
+ * This function is called from 'virQEMUDriverGetDomainCapabilities'. It must
+ * not report any errors and must not add any additional checks.
+ *
+ * This function is mocked from 'tests/testutilsqemu.c'
+ *
+ */
 bool
 qemuRdpAvailable(const char *helper)
 {
-    g_autoptr(qemuRdp) rdp = NULL;
+    g_autofree char *helperPath = NULL;
 
-    rdp = qemuRdpNewForHelper(helper);
-
-    return rdp && qemuRdpHasFeature(rdp, QEMU_RDP_FEATURE_DBUS_ADDRESS);
+    /* This function was added corresponding to the first release of 'qemu-rdp'
+     * thus checking existance of the helper binary is sufficient. */
+    return !!(helperPath = virFindFileInPath(helper));
 }

commit c5a73f75bc6cae4f466d0a6344d5b3277ac9c2f4

qemu_rdp: Fix a typo existance -> existence
Fixes: bd473480b443aa2561236dfd903535aa778d99a8
Signed-off-by: Martin Kletzander <mkletzan@redhat.com>
Reviewed-by: Jiri Denemark <jdenemar@redhat.com>

src/qemu/qemu_rdp.c
@@ -432,6 +432,6 @@ qemuRdpAvailable(const char *helper)
     g_autofree char *helperPath = NULL;
 
     /* This function was added corresponding to the first release of 'qemu-rdp'
-     * thus checking existance of the helper binary is sufficient. */
+     * thus checking existence of the helper binary is sufficient. */
     return !!(helperPath = virFindFileInPath(helper));
 }