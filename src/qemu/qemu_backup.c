libvirt/libvirt

commit eb768a556db75040f7b518d198a18bd0f5d6faad

backup: Add support for passing server socket file descriptor to backup NBD server
In deployments where libvirt is containerized together with the VM it
may be hard for the management application to access listening sockets
inside the container from the outside.

This patch implements "transport='fd'" for the NBD server definition for
backups which allows to use the existing "virDomainFDAssociate()" to
pass FD to a pre-opened server socket to qemu instead of trying to
create it by qemu.

Add schema, enable the parser, add formatter and implement the actual
passing for the qemu backup code.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Spellchecked-by: Ján Tomko <jtomko@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_backup.c
@@ -761,6 +761,7 @@ qemuBackupBegin(virDomainObj *vm,
     bool reuse = (flags & VIR_DOMAIN_BACKUP_BEGIN_REUSE_EXTERNAL);
     int rc = 0;
     int ret = -1;
+    g_autoptr(qemuFDPassDirect) fdpass = NULL;
 
     virCheckFlags(VIR_DOMAIN_BACKUP_BEGIN_REUSE_EXTERNAL, -1);
 
@@ -847,6 +848,29 @@ qemuBackupBegin(virDomainObj *vm,
 
     priv->backup = g_steal_pointer(&def);
 
+    if (pull && priv->backup->server->fdgroup) {
+        virStorageSourceFDTuple *fdt = NULL;
+        VIR_AUTOCLOSE fdcopy = -1;
+
+        if (!(fdt = virHashLookup(priv->fds, priv->backup->server->fdgroup))) {
+            virReportError(VIR_ERR_INVALID_ARG,
+                           _("file descriptor group '%1$s' was not associated with the domain"),
+                           priv->backup->server->fdgroup);
+            goto endjob;
+        }
+
+        if (fdt->nfds != 1) {
+            virReportError(VIR_ERR_INVALID_ARG,
+                           _("file descriptor group '%1$s' must contain only 1 file descriptor for NBD server"),
+                           priv->backup->server->fdgroup);
+            goto endjob;
+        }
+
+        priv->backup->server->qemu_fdname = g_strdup("libvirt-backup-nbd");
+        fdcopy = dup(fdt->fds[0]);
+        fdpass = qemuFDPassDirectNew(priv->backup->server->qemu_fdname, &fdcopy);
+    }
+
     if (qemuDomainObjEnterMonitorAsync(vm, VIR_ASYNC_JOB_BACKUP) < 0)
         goto endjob;
 
@@ -856,6 +880,9 @@ qemuBackupBegin(virDomainObj *vm,
 
         if (rc == 0 && tlsProps)
             rc = qemuMonitorAddObject(priv->mon, &tlsProps, &tlsAlias);
+
+        if (rc == 0 && fdpass)
+            rc = qemuFDPassDirectTransferMonitor(fdpass, priv->mon);
 
         if (rc == 0) {
             if ((rc = qemuMonitorNBDServerStart(priv->mon, priv->backup->server, tlsAlias)) == 0)

src/conf/backup_conf.c
@@ -228,7 +228,7 @@ virDomainBackupDefParseXML(xmlXPathContextPtr ctxt,
 
         def->server = g_new0(virStorageNetHostDef, 1);
 
-        if (virDomainStorageNetworkParseHost(node, def->server, false) < 0)
+        if (virDomainStorageNetworkParseHost(node, def->server, true) < 0)
             return NULL;
 
         if (def->server->transport == VIR_STORAGE_NET_HOST_TRANS_RDMA) {
@@ -388,6 +388,7 @@ virDomainBackupDefFormat(virBuffer *buf,
         if (def->server->port)
             virBufferAsprintf(&serverAttrBuf, " port='%u'", def->server->port);
         virBufferEscapeString(&serverAttrBuf, " socket='%s'", def->server->socket);
+        virBufferEscapeString(&serverAttrBuf, " fdgroup='%s'", def->server->fdgroup);
     }
 
     virXMLFormatElement(&childBuf, "server", &serverAttrBuf, NULL);

src/conf/schemas/domainbackup.rng
@@ -90,6 +90,12 @@
                       <ref name="absFilePath"/>
                     </attribute>
                   </group>
+                  <group>
+                    <attribute name="transport">
+                      <value>fd</value>
+                    </attribute>
+                    <attribute name="fdgroup"/>
+                  </group>
                 </choice>
               </element>
               <ref name="backupDisksPull"/>

commit 3d05fe7aa31e58ec8a7286ccc9524638a7741326

qemuBackupPrepare: Actually allow 'VIR_STORAGE_NET_HOST_TRANS_FD'
While I've actually implemented support for FD passing the NBD server
socket in eb768a556db I managed to misplace the hunk allowing the 'FD'
transport in the validation code, rendering the whole feature useless.

Fix the validation logic to make the feature usable.

Fixes: eb768a556db75040f7b518d198a18bd0f5d6faad
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_backup.c
@@ -86,8 +86,10 @@ qemuBackupPrepare(virDomainBackupDef *def)
             /* TODO: Do we need to mess with selinux? */
             break;
 
-        case VIR_STORAGE_NET_HOST_TRANS_RDMA:
         case VIR_STORAGE_NET_HOST_TRANS_FD:
+            break;
+
+        case VIR_STORAGE_NET_HOST_TRANS_RDMA:
         case VIR_STORAGE_NET_HOST_TRANS_LAST:
             virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
                            _("unexpected transport in <domainbackup>"));