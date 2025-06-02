libvirt/libvirt

commit f2023e8018fe18550ad6aec66fe72bd1376f8522

utils: Canonicalize paths before comparing them
In virFileIsSharedFSOverride() we compare a path against a list
of overrides looking for a match.

All overrides are canonicalized ahead of time though, so e.g.
/var/run/foo will be turned into /run/foo due to /var/run being
a symlink on modern Linux systems. But the path we're trying to
match with the overrides doesn't get the same treatment, so in
this scenario the comparison will always fail.

Canonicalizing the path as well solves the issue.

Resolves: https://issues.redhat.com/browse/RHEL-79165
Signed-off-by: Andrea Bolognani <abologna@redhat.com>
Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

src/util/virfile.c
@@ -3823,10 +3823,13 @@ virFileIsSharedFSOverride(const char *path,
     if (!path || path[0] != '/' || !overrides)
         return false;
 
-    if (g_strv_contains((const char *const *) overrides, path))
-        return true;
+    /* Overrides have been canonicalized ahead of time, so we need to
+     * do the same for the provided path or we'll never be able to
+     * find a match if symlinks are involved */
+    dirpath = virFileCanonicalizePath(path);
 
-    dirpath = g_strdup(path);
+    if (g_strv_contains((const char *const *) overrides, dirpath))
+        return true;
 
     /* Continue until we've scanned the entire path */
     while (p != dirpath) {

commit 94fb348d670f612c0b58901c9829b4eec81faa50

util: Fix virFileIsSharedFSOverride on nonexistent paths
Commit v11.0.0-162-gf2023e8018 added path canonicalization to
virFileIsSharedFSOverride to make sure we can properly match shared
filesystem override paths which include symlinks. But
virFileCanonicalizePath only works on existing paths, while
virFileIsSharedFSOverride may be called on paths that do not exist yet.
Matching paths against overrides has always worked even for nonexistent
paths. To fix the regression we need to first get the longest existing
sub-path and canonicalize only this part and use the result for
searching in overrides. Clearly any portion of the path we dynamically
create is not going to end up on a different filesystem to what the
parent directory is stored in. So checking just the existing parent is
enough.

https://issues.redhat.com/browse/RHEL-86592

Fixes: f2023e8018fe18550ad6aec66fe72bd1376f8522
Signed-off-by: Jiri Denemark <jdenemar@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/util/virfile.c
@@ -3821,15 +3821,26 @@ virFileIsSharedFSOverride(const char *path,
                           char *const *overrides)
 {
     g_autofree char *dirpath = NULL;
+    g_autofree char *existing = NULL;
     char *p = NULL;
 
     if (!path || path[0] != '/' || !overrides)
         return false;
 
+    /* We only care about the longest existing sub-path. Further components
+     * may will later be created by libvirt will not magically become a shared
+     * filesystem. */
+    if (!(existing = virFileGetExistingParent(path)))
+        return false;
+
     /* Overrides have been canonicalized ahead of time, so we need to
      * do the same for the provided path or we'll never be able to
      * find a match if symlinks are involved */
-    dirpath = virFileCanonicalizePath(path);
+    if (!(dirpath = virFileCanonicalizePath(existing))) {
+        VIR_DEBUG("Cannot canonicalize parent '%s' of path '%s'",
+                  existing, path);
+        return false;
+    }
 
     if (g_strv_contains((const char *const *) overrides, dirpath))
         return true;