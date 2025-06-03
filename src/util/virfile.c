libvirt/libvirt

commit 94fb348d670f612c0b58901c9829b4eec81faa50

util: Fix virFileIsSharedFSOverride on nonexistent paths
Commit v11.0.0-162-gf2023e8018 added path canonicalization to
virFileIsSharedFSOverride to make sure we can properly match shared
filesystem override paths which include symlinks. But
virFileCanonicalizePath only works on existing paths, while
virFileIsSharedFSOverride may be called on paths that do not exist yet.
Matching paths against overrides has always worked even for nonexistent
paths. To fix the regression we need to first get the longest existing
sub-path and canonicalize only this part and use the result for
searching in overrides. Clearly any portion of the path we dynamically
create is not going to end up on a different filesystem to what the
parent directory is stored in. So checking just the existing parent is
enough.

https://issues.redhat.com/browse/RHEL-86592

Fixes: f2023e8018fe18550ad6aec66fe72bd1376f8522
Signed-off-by: Jiri Denemark <jdenemar@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/util/virfile.c
@@ -3821,15 +3821,26 @@ virFileIsSharedFSOverride(const char *path,
                           char *const *overrides)
 {
     g_autofree char *dirpath = NULL;
+    g_autofree char *existing = NULL;
     char *p = NULL;
 
     if (!path || path[0] != '/' || !overrides)
         return false;
 
+    /* We only care about the longest existing sub-path. Further components
+     * may will later be created by libvirt will not magically become a shared
+     * filesystem. */
+    if (!(existing = virFileGetExistingParent(path)))
+        return false;
+
     /* Overrides have been canonicalized ahead of time, so we need to
      * do the same for the provided path or we'll never be able to
      * find a match if symlinks are involved */
-    dirpath = virFileCanonicalizePath(path);
+    if (!(dirpath = virFileCanonicalizePath(existing))) {
+        VIR_DEBUG("Cannot canonicalize parent '%s' of path '%s'",
+                  existing, path);
+        return false;
+    }
 
     if (g_strv_contains((const char *const *) overrides, dirpath))
         return true;

commit 2b1393357c72b3baf04eefc35929002c62bcce80

util: Move virFileGetExistingParent out of ifdef __linux__
The function is called by virFileIsSharedFSOverride which is not Linux
specific and thus building on anything but Linux failes.

Fixes: 94fb348d670f612c0b58901c9829b4eec81faa50
Signed-off-by: Jiri Denemark <jdenemar@redhat.com>
Reviewed-by: Martin Kletzander <mkletzan@redhat.com>

src/util/virfile.c
@@ -3444,6 +3444,34 @@ virFileRemoveLastComponent(char *path)
         path[0] = '\0';
 }
 
+
+static char *
+virFileGetExistingParent(const char *path)
+{
+    g_autofree char *dirpath = g_strdup(path);
+    char *p = NULL;
+
+    /* Try less and less of the path until we get to a directory we can access.
+     * Even if we don't have 'x' permission on any directory in the path on the
+     * NFS server (assuming it's NFS), we will be able to stat the mount point.
+     */
+    while (!virFileExists(dirpath) && p != dirpath) {
+        if (!(p = strrchr(dirpath, '/'))) {
+            virReportSystemError(EINVAL,
+                                 _("Invalid relative path '%1$s'"), path);
+            return NULL;
+        }
+
+        if (p == dirpath)
+            *(p + 1) = '\0';
+        else
+            *p = '\0';
+    }
+
+    return g_steal_pointer(&dirpath);
+}
+
+
 #ifdef __linux__
 
 # ifndef NFS_SUPER_MAGIC
@@ -3554,33 +3582,6 @@ virFileIsSharedFsFUSE(const char *path,
         return 1;
 
     return 0;
-}
-
-
-static char *
-virFileGetExistingParent(const char *path)
-{
-    g_autofree char *dirpath = g_strdup(path);
-    char *p = NULL;
-
-    /* Try less and less of the path until we get to a directory we can access.
-     * Even if we don't have 'x' permission on any directory in the path on the
-     * NFS server (assuming it's NFS), we will be able to stat the mount point.
-     */
-    while (!virFileExists(dirpath) && p != dirpath) {
-        if (!(p = strrchr(dirpath, '/'))) {
-            virReportSystemError(EINVAL,
-                                 _("Invalid relative path '%1$s'"), path);
-            return NULL;
-        }
-
-        if (p == dirpath)
-            *(p + 1) = '\0';
-        else
-            *p = '\0';
-    }
-
-    return g_steal_pointer(&dirpath);
 }
 
 