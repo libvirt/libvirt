7747c1eeb7ded9980bb31c34f01e52893e4b4159
Peter Krempa <pkrempa@redhat.com>
2025-06-19 14:27:26 +0200

    qemusecuritytest: Use 'disk-usb-device' case instead of 'disk-cdrom-bus-other'
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Jiri Denemark <jdenemar@redhat.com>

--- tests/qemusecuritytest.c
+++ tests/qemusecuritytest.c
@@ -207,7 +207,7 @@ mymain(void)
     DO_TEST_DOMAIN("disk-backing-chains-noindex");
     DO_TEST_DOMAIN("disk-cache");
     DO_TEST_DOMAIN("disk-cdrom");
-    DO_TEST_DOMAIN("disk-cdrom-bus-other");
+    DO_TEST_DOMAIN("disk-usb-device");
     DO_TEST_DOMAIN("disk-cdrom-network");
     DO_TEST_DOMAIN("disk-cdrom-tray");
     DO_TEST_DOMAIN("disk-copy_on_read");
