3b43fa08ffa58d786d781f675419f151481ddb45
Peter Krempa <pkrempa@redhat.com>
2025-07-18 16:49:11 +0200

    virSecuritySELinuxGetSecurityMountOptions: refactor printing
    Fix linebreaks and remove the use of ternary operator.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- src/security/security_selinux.c
+++ src/security/security_selinux.c
@@ -3526,23 +3526,24 @@ virSecuritySELinuxGetSecurityMountOptions(virSecurityManager *mgr,
 {
     char *opts = NULL;
     virSecurityLabelDef *secdef;
+    const char *imagelabel = NULL;
 
     if ((secdef = virDomainDefGetSecurityLabelDef(def, SECURITY_SELINUX_NAME))) {
         if (!secdef->imagelabel)
             secdef->imagelabel = virSecuritySELinuxGenImageLabel(mgr, def);
 
         if (secdef->imagelabel) {
-            opts = g_strdup_printf(
-                                   ",context=\"%s\"",
+            opts = g_strdup_printf(",context=\"%s\"",
                                    (const char*) secdef->imagelabel);
         }
+
+        imagelabel = secdef->imagelabel;
     }
 
     if (!opts)
         opts = g_strdup("");
 
-    VIR_DEBUG("imageLabel=%s opts=%s",
-              secdef ? secdef->imagelabel : "(null)", opts);
+    VIR_DEBUG("imageLabel=%s opts=%s", NULLSTR(imagelabel), opts);
     return opts;
 }
 
