fab45749f820ef50108bc697f40b847abe19a037
Andrea Bolognani <abologna@redhat.com>
2025-07-22 14:54:54 +0200

    tests: Drop coverage for versatilepb on aarch64
    We already test the machine type on armv7l and realview on
    aarch64, so these handful of test cases can be dropped without
    negatively impacting our coverage.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>
    Reviewed-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Ján Tomko <jtomko@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -1382,8 +1382,6 @@ mymain(void)
     DO_TEST_CAPS_LATEST_ABI_UPDATE("x86_64-q35-minimal");
     DO_TEST_CAPS_ARCH_LATEST("aarch64-virt-minimal", "aarch64");
     DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("aarch64-virt-minimal", "aarch64");
-    DO_TEST_CAPS_ARCH_LATEST("aarch64-versatilepb-minimal", "aarch64");
-    DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("aarch64-versatilepb-minimal", "aarch64");
     DO_TEST_CAPS_ARCH_LATEST("armv7l-versatilepb-minimal", "armv7l");
     DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("armv7l-versatilepb-minimal", "armv7l");
     DO_TEST_CAPS_ARCH_LATEST("aarch64-realview-minimal", "aarch64");
@@ -2121,25 +2119,13 @@ mymain(void)
                  ARG_END);
 
     /* controller selection tests for various uncommon machine types */
-    DO_TEST_CAPS_ARCH_LATEST("usb-controller-default-versatilepb-aarch64", "aarch64");
-    DO_TEST_FULL("usb-controller-default-fallback-versatilepb-aarch64", ".aarch64-latest",
-                 ARG_CAPS_ARCH, "aarch64",
-                 ARG_CAPS_VER, "latest",
-                 ARG_QEMU_CAPS_DEL, QEMU_CAPS_PIIX3_USB_UHCI, QEMU_CAPS_LAST,
-                 ARG_END);
-    DO_TEST_FULL("usb-controller-default-unavailable-versatilepb-aarch64", ".aarch64-latest",
-                 ARG_CAPS_ARCH, "aarch64",
-                 ARG_CAPS_VER, "latest",
-                 ARG_QEMU_CAPS_DEL, QEMU_CAPS_PIIX3_USB_UHCI, QEMU_CAPS_PCI_OHCI, QEMU_CAPS_LAST,
-                 ARG_END);
-
-    DO_TEST_CAPS_ARCH_LATEST("usb-controller-default-versatilepb-armv7l", "armv7l");
-    DO_TEST_FULL("usb-controller-default-fallback-versatilepb-armv7l", ".armv7l-latest",
+    DO_TEST_CAPS_ARCH_LATEST("usb-controller-default-versatilepb", "armv7l");
+    DO_TEST_FULL("usb-controller-default-fallback-versatilepb", ".armv7l-latest",
                  ARG_CAPS_ARCH, "armv7l",
                  ARG_CAPS_VER, "latest",
                  ARG_QEMU_CAPS_DEL, QEMU_CAPS_PIIX3_USB_UHCI, QEMU_CAPS_LAST,
                  ARG_END);
-    DO_TEST_FULL("usb-controller-default-unavailable-versatilepb-armv7l", ".armv7l-latest",
+    DO_TEST_FULL("usb-controller-default-unavailable-versatilepb", ".armv7l-latest",
                  ARG_CAPS_ARCH, "armv7l",
                  ARG_CAPS_VER, "latest",
                  ARG_FLAGS, FLAG_EXPECT_FAILURE,
