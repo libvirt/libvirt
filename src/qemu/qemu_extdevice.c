libvirt/libvirt

commit 1e9054b9c79d721a55f413c2983c5370044f8f60

qemu: complete vhostuser + passt support
<interface type='vhostuser'><backend type='passt'/> needs to run the
passt command just as is done for interface type='user', but then add
vhostuser bits to the qemu commandline/monitor command.

There are some changes to the parsing/validation along with changes to
the vhostuser codepath do do the extra stuff for passt. I tried
keeping them separated into different patches, but then the unit test
failed in a strange way deep down in the bowels of the commandline
generation, so this patch both 1) makes the final changes to
parsing/formatting and 2) adds passt stuff at appropriate places for
vhostuser (as well as making a couple of things *not* happen when the
passt backend is chosen). The result is that you can now have:

    <interface type='vhostuser'>
      <backend type='passt'/>
      ...
    </interface>

Then as long as you also have the following as a subelement of
<domain>:

    <memoryBacking>
      <access mode='shared'/>
    </memoryBacking>

your passt interfaces will benefit from the greatly improved
efficiency of a vhost-user data path, and all without requiring
special privileges or capabilities *anywhere* (i.e. it works for
unprivileged libvirt (qemu:///session) as well as privileged libvirt).

Resolves: https://issues.redhat.com/browse/RHEL-69455
Signed-off-by: Laine Stump <laine@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_extdevice.c
@@ -212,13 +212,15 @@ qemuExtDevicesStart(virQEMUDriver *driver,
     for (i = 0; i < def->nnets; i++) {
         virDomainNetDef *net = def->nets[i];
 
-        if (net->type != VIR_DOMAIN_NET_TYPE_USER)
+        if (net->type != VIR_DOMAIN_NET_TYPE_USER &&
+            net->type != VIR_DOMAIN_NET_TYPE_VHOSTUSER) {
             continue;
+        }
 
         if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
             if (qemuPasstStart(vm, net) < 0)
                 return -1;
-        } else {
+        } else if (net->type == VIR_DOMAIN_NET_TYPE_USER) {
             if (qemuSlirpStart(vm, net, incomingMigration) < 0)
                 return -1;
         }

src/qemu/qemu_hotplug.c
@@ -1262,10 +1262,23 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
         if (!(charDevAlias = qemuAliasChardevFromDevAlias(net->info.alias)))
             goto cleanup;
 
-        if (virNetDevOpenvswitchGetVhostuserIfname(net->data.vhostuser->data.nix.path,
-                                                   net->data.vhostuser->data.nix.listen,
-                                                   &net->ifname) < 0)
-            goto cleanup;
+        if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
+
+            /* vhostuser needs socket path in this location, and when
+             * backend is passt, the path is derived from other info,
+             * not taken from config.
+             */
+            g_free(net->data.vhostuser->data.nix.path);
+            net->data.vhostuser->data.nix.path = qemuPasstCreateSocketPath(vm, net);
+
+            if (qemuPasstStart(vm, net) < 0)
+                goto cleanup;
+        } else {
+            if (virNetDevOpenvswitchGetVhostuserIfname(net->data.vhostuser->data.nix.path,
+                                                       net->data.vhostuser->data.nix.listen,
+                                                       &net->ifname) < 0)
+                goto cleanup;
+        }
 
         if (qemuSecuritySetNetdevLabel(driver, vm, net) < 0)
             goto cleanup;

src/conf/domain_conf.c
@@ -9457,9 +9457,25 @@ virDomainNetBackendParseXML(xmlNodePtr node,
     g_autofree char *tap = virXMLPropString(node, "tap");
     g_autofree char *vhost = virXMLPropString(node, "vhost");
 
-    /* The VIR_DOMAIN_NET_BACKEND_DEFAULT really means 'use hypervisor's
-     * builtin SLIRP'. It's reported in domain caps and thus we need to accept
-     * it. Hence VIR_XML_PROP_NONE instead of VIR_XML_PROP_NONZERO. */
+    /* In the case of NET_TYPE_USER, backend type can be unspecified
+     * (i.e. VIR_DOMAIN_NET_BACKEND_DEFAULT) and that means 'use
+     * hypervisor's builtin SLIRP (or if that isn't available, use
+     * passt)'. Similarly, it can also be left unspecified in the case
+     * of NET_TYPE_VHOSTUSER, and then it means "use the traditional
+     * vhost-user backend (which auto-detects between connecting to a
+     * socket created by OVS, or connecting to a standalone socket
+     * used (mostly in testing) to connect the vhost-user interface of
+     * one guest directly to the vhost-user interface of another
+     * guest.
+     *
+     * If backend type is set to 'passt', then in both cases a passt
+     * process will be started, and libvirt will connect that to the
+     * guest interface (either communicating everything over the
+     * socket created by passt using a specific-to-passt protocol
+     * (interface type='user'>), or by using the socket for control
+     * plane messages and shared memory for data using the vhost-user
+     * protocol (<interface type='vhostuser'>)).
+     */
     if (virXMLPropEnum(node, "type", virDomainNetBackendTypeFromString,
                        VIR_XML_PROP_NONE, &def->backend.type) < 0) {
         return -1;
@@ -24616,7 +24632,11 @@ virDomainNetDefFormat(virBuffer *buf,
             break;
 
         case VIR_DOMAIN_NET_TYPE_VHOSTUSER:
-            if (def->data.vhostuser->type == VIR_DOMAIN_CHR_TYPE_UNIX) {
+            if (def->data.vhostuser->type == VIR_DOMAIN_CHR_TYPE_UNIX &&
+                def->backend.type != VIR_DOMAIN_NET_BACKEND_PASST) {
+                /* in the case of BACKEND_PASST, the values of all of these are either
+                 * fixed (type, mode, reconnect), or derived from elsewhere (path)
+                 */
                 virBufferAddLit(&sourceAttrBuf, " type='unix'");
                 virBufferEscapeString(&sourceAttrBuf, " path='%s'",
                                       def->data.vhostuser->data.nix.path);
@@ -24627,7 +24647,6 @@ virDomainNetDefFormat(virBuffer *buf,
                     virDomainChrSourceReconnectDefFormat(&sourceChildBuf,
                                                          &def->data.vhostuser->data.nix.reconnect);
                 }
-
             }
             break;
 
@@ -24689,14 +24708,13 @@ virDomainNetDefFormat(virBuffer *buf,
         }
 
         case VIR_DOMAIN_NET_TYPE_USER:
-            if (def->backend.type == VIR_DOMAIN_NET_BACKEND_PASST)
-                virBufferEscapeString(&sourceAttrBuf, " dev='%s'", def->sourceDev);
-            break;
-
         case VIR_DOMAIN_NET_TYPE_NULL:
         case VIR_DOMAIN_NET_TYPE_LAST:
             break;
         }
+
+        if (def->backend.type == VIR_DOMAIN_NET_BACKEND_PASST)
+            virBufferEscapeString(&sourceAttrBuf, " dev='%s'", def->sourceDev);
 
         if (def->hostIP.nips || def->hostIP.nroutes) {
             if (virDomainNetIPInfoFormat(&sourceChildBuf, &def->hostIP) < 0)

src/conf/domain_validate.c
@@ -2163,67 +2163,46 @@ virDomainNetDefValidate(const virDomainNetDef *net)
         return -1;
     }
 
-    if (net->type != VIR_DOMAIN_NET_TYPE_USER) {
+    if (net->type != VIR_DOMAIN_NET_TYPE_USER &&
+        net->type != VIR_DOMAIN_NET_TYPE_VHOSTUSER) {
         if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
             virReportError(VIR_ERR_INTERNAL_ERROR, "%s",
-                           _("The 'passt' backend can only be used with interface type='user'"));
+                           _("The 'passt' backend can only be used with interface type='user' or type='vhostuser'"));
             return -1;
         }
     }
 
-    if (net->nPortForwards > 0 &&
-        (net->type != VIR_DOMAIN_NET_TYPE_USER ||
-         (net->type == VIR_DOMAIN_NET_TYPE_USER &&
-          net->backend.type != VIR_DOMAIN_NET_BACKEND_PASST))) {
-        virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
-                       _("The <portForward> element can only be used with <interface type='user'> and its 'passt' backend"));
-        return -1;
+    if (net->nPortForwards > 0) {
+        size_t p;
+
+        if ((net->type != VIR_DOMAIN_NET_TYPE_USER &&
+            net->type != VIR_DOMAIN_NET_TYPE_VHOSTUSER) ||
+            net->backend.type != VIR_DOMAIN_NET_BACKEND_PASST) {
+            virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                           _("The <portForward> element can only be used with the 'passt' backend of interface type='user' or type='vhostuser'"));
+            return -1;
+        }
+
+        for (p = 0; p < net->nPortForwards; p++) {
+            size_t r;
+            virDomainNetPortForward *pf = net->portForwards[p];
+
+            for (r = 0; r < pf->nRanges; r++) {
+                virDomainNetPortForwardRange *range = pf->ranges[r];
+
+                if (!range->start
+                    && (range->end || range->to
+                        || range->exclude != VIR_TRISTATE_BOOL_ABSENT)) {
+                    virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
+                                   _("The 'range' of a 'portForward' requires 'start' attribute if 'end', 'to', or 'exclude' is specified"));
+                    return -1;
+                }
+            }
+        }
     }
 
     if (!virNetDevBandwidthValidate(net->bandwidth)) {
         return -1;
-    }
-
-    switch (net->type) {
-    case VIR_DOMAIN_NET_TYPE_USER:
-        if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
-            size_t p;
-
-            for (p = 0; p < net->nPortForwards; p++) {
-                size_t r;
-                virDomainNetPortForward *pf = net->portForwards[p];
-
-                for (r = 0; r < pf->nRanges; r++) {
-                    virDomainNetPortForwardRange *range = pf->ranges[r];
-
-                    if (!range->start
-                        && (range->end || range->to
-                            || range->exclude != VIR_TRISTATE_BOOL_ABSENT)) {
-                        virReportError(VIR_ERR_CONFIG_UNSUPPORTED, "%s",
-                                       _("The 'range' of a 'portForward' requires 'start' attribute if 'end', 'to', or 'exclude' is specified"));
-                        return -1;
-                    }
-                }
-            }
-        }
-        break;
-
-    case VIR_DOMAIN_NET_TYPE_VHOSTUSER:
-    case VIR_DOMAIN_NET_TYPE_NETWORK:
-    case VIR_DOMAIN_NET_TYPE_VDPA:
-    case VIR_DOMAIN_NET_TYPE_BRIDGE:
-    case VIR_DOMAIN_NET_TYPE_CLIENT:
-    case VIR_DOMAIN_NET_TYPE_SERVER:
-    case VIR_DOMAIN_NET_TYPE_MCAST:
-    case VIR_DOMAIN_NET_TYPE_UDP:
-    case VIR_DOMAIN_NET_TYPE_INTERNAL:
-    case VIR_DOMAIN_NET_TYPE_DIRECT:
-    case VIR_DOMAIN_NET_TYPE_HOSTDEV:
-    case VIR_DOMAIN_NET_TYPE_VDS:
-    case VIR_DOMAIN_NET_TYPE_ETHERNET:
-    case VIR_DOMAIN_NET_TYPE_NULL:
-    case VIR_DOMAIN_NET_TYPE_LAST:
-        break;
     }
 
     return 0;

src/conf/schemas/domaincommon.rng
@@ -3486,8 +3486,36 @@
           </attribute>
           <interleave>
             <optional>
-              <ref name="unixSocketSource"/>
-            </optional>
+              <element name="source">
+                <optional>
+                  <attribute name="type">
+                    <value>unix</value>
+                  </attribute>
+                </optional>
+                <optional>
+                  <attribute name="path">
+                    <ref name="absFilePath"/>
+                  </attribute>
+                </optional>
+                <optional>
+                  <attribute name="mode">
+                    <choice>
+                      <value>server</value>
+                      <value>client</value>
+                    </choice>
+                  </attribute>
+                </optional>
+                <optional>
+                  <attribute name="dev">
+                    <ref name="deviceName"/>
+                  </attribute>
+                </optional>
+                <optional>
+                  <ref name="reconnect"/>
+                </optional>
+                <empty/>
+              </element>
+             </optional>
             <ref name="interface-options"/>
           </interleave>
         </group>

src/qemu/qemu_command.c
@@ -8649,11 +8649,12 @@ qemuBuildInterfaceCommandLine(virQEMUDriver *driver,
         if (qemuInterfaceVhostuserConnect(cmd, net, qemuCaps) < 0)
             goto cleanup;
 
-        if (virNetDevOpenvswitchGetVhostuserIfname(net->data.vhostuser->data.nix.path,
+        if (net->backend.type != VIR_DOMAIN_NET_BACKEND_PASST &&
+            virNetDevOpenvswitchGetVhostuserIfname(net->data.vhostuser->data.nix.path,
                                                    net->data.vhostuser->data.nix.listen,
-                                                   &net->ifname) < 0)
+                                                   &net->ifname) < 0) {
             goto cleanup;
-
+        }
         break;
 
     case VIR_DOMAIN_NET_TYPE_VDPA:

src/qemu/qemu_passt.c
@@ -180,6 +180,9 @@ qemuPasstStart(virDomainObj *vm,
 
     virCommandClearCaps(cmd);
 
+    if (virDomainNetGetActualType(net) == VIR_DOMAIN_NET_TYPE_VHOSTUSER)
+        virCommandAddArg(cmd, "--vhost-user");
+
     virCommandAddArgList(cmd,
                          "--one-off",
                          "--socket", passtSocketName,

src/qemu/qemu_process.c
@@ -64,6 +64,7 @@
 #include "qemu_backup.h"
 #include "qemu_dbus.h"
 #include "qemu_snapshot.h"
+#include "qemu_passt.h"
 
 #include "cpu/cpu.h"
 #include "cpu/cpu_x86.h"
@@ -5931,12 +5932,23 @@ qemuProcessPrepareDomainNetwork(virDomainObj *vm)
             }
             break;
 
+        case VIR_DOMAIN_NET_TYPE_VHOSTUSER:
+            if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
+                /* when using the passt backend, the path of the
+                 * unix socket is always derived from other info
+                 * *not* manually given in the config, but all the
+                 * vhostuser code looks for it there.
+                 */
+                g_free(net->data.vhostuser->data.nix.path);
+                net->data.vhostuser->data.nix.path = qemuPasstCreateSocketPath(vm, net);
+            }
+            break;
+
         case VIR_DOMAIN_NET_TYPE_DIRECT:
         case VIR_DOMAIN_NET_TYPE_BRIDGE:
         case VIR_DOMAIN_NET_TYPE_NETWORK:
         case VIR_DOMAIN_NET_TYPE_ETHERNET:
         case VIR_DOMAIN_NET_TYPE_USER:
-        case VIR_DOMAIN_NET_TYPE_VHOSTUSER:
         case VIR_DOMAIN_NET_TYPE_SERVER:
         case VIR_DOMAIN_NET_TYPE_CLIENT:
         case VIR_DOMAIN_NET_TYPE_MCAST:
@@ -5948,7 +5960,6 @@ qemuProcessPrepareDomainNetwork(virDomainObj *vm)
         case VIR_DOMAIN_NET_TYPE_LAST:
             break;
         }
-
     }
     return 0;
 }

commit 82b794ca120fcc2fa2afd2f5213c498438480460

qemu: fix multiple missing setup/teardown of passt process for interface type='vhostuser'
passt networking support was originally added only for <interface
type='user'>, and all of the codepaths leading to qemuPasst*()
functions were protected with

   if (net->type == VIR_DOMAIN_NET_TYPE_USER &&
       net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST)

When support was later added to use a vhost-user socket to connect
between the passt process and qemu process, *some* of the conditionals
similar to the above were changed to be

   if ((net->type == VIR_DOMAIN_NET_TYPE_USER ||
        net->type == VIR_DOMAIN_NET_TYPE_VHOSTUSER) &&
       net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST)

As a matter of fact, enough of these places were changed to make
passt+vhostuser work. However I missed a few places that resulted in
the passt process not being properly shutdown/cleaned up when the
interface type was vhostuser, and also as far as I can see from
examining the code, the passt process wasn't being added to the cgroup
for the domain.

We could fix these problems by adding the extra condition to all the
missing places (checking for either 'user' or 'vhostuser' as well as
for backend type of 'passt'), but since validation already guarantees
that if backend type='passt' then the interface type MUST be either
'user' or 'vhostuser', it's really just adding extra code for no good
purpose (and would leave open the possibility of the same problem
recurring in the future if a different interface type begins using
passt as well). So the better solution is to not bother checking
net->type at all in those locations - if backend type is 'passt' then
we call the passt-related code.

Resolves: https://issues.redhat.com/browse/RHEL-80285
Resolves: https://issues.redhat.com/browse/RHEL-92842
Fixes: 1e9054b9c79d721a55f413c2983c5370044f8f60
Signed-off-by: Laine Stump <laine@redhat.com>
Reviewed-by: Martin Kletzander <mkletzan@redhat.com>

src/qemu/qemu_extdevice.c
@@ -213,11 +213,6 @@ qemuExtDevicesStart(virQEMUDriver *driver,
     for (i = 0; i < def->nnets; i++) {
         virDomainNetDef *net = def->nets[i];
 
-        if (net->type != VIR_DOMAIN_NET_TYPE_USER &&
-            net->type != VIR_DOMAIN_NET_TYPE_VHOSTUSER) {
-            continue;
-        }
-
         if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
             if (qemuPasstStart(vm, net) < 0)
                 return -1;
@@ -310,10 +305,8 @@ qemuExtDevicesStop(virQEMUDriver *driver,
         if (slirp)
             qemuSlirpStop(slirp, vm, driver, net);
 
-        if (net->type == VIR_DOMAIN_NET_TYPE_USER &&
-            net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
+        if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST)
             qemuPasstStop(vm, net);
-        }
 
         if (actualType == VIR_DOMAIN_NET_TYPE_ETHERNET && net->downscript)
             virNetDevRunEthernetScript(net->ifname, net->downscript);
@@ -373,8 +366,7 @@ qemuExtDevicesHasDevice(virDomainDef *def)
         if (QEMU_DOMAIN_NETWORK_PRIVATE(net)->slirp)
             return true;
 
-        if (net->type == VIR_DOMAIN_NET_TYPE_USER &&
-            net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST)
+        if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST)
             return true;
     }
 
@@ -455,8 +447,7 @@ qemuExtDevicesSetupCgroup(virQEMUDriver *driver,
         if (slirp && qemuSlirpSetupCgroup(slirp, cgroup) < 0)
             return -1;
 
-        if (net->type == VIR_DOMAIN_NET_TYPE_USER &&
-            net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST &&
+        if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST &&
             qemuPasstSetupCgroup(vm, net, cgroup) < 0) {
             return -1;
         }

src/qemu/qemu_hotplug.c
@@ -1298,8 +1298,6 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
 
             qemuPasstPrepareVhostUser(vm, net);
 
-            if (qemuPasstStart(vm, net) < 0)
-                goto cleanup;
         } else {
             if (virNetDevOpenvswitchGetVhostuserIfname(net->data.vhostuser->data.nix.path,
                                                        net->data.vhostuser->data.nix.listen,
@@ -1313,13 +1311,8 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
         break;
 
     case VIR_DOMAIN_NET_TYPE_USER:
-        if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
-
-            if (qemuPasstStart(vm, net) < 0)
-                goto cleanup;
-
-        } else if (!priv->disableSlirp &&
-                   virQEMUCapsGet(priv->qemuCaps, QEMU_CAPS_DBUS_VMSTATE)) {
+        if (!priv->disableSlirp &&
+            virQEMUCapsGet(priv->qemuCaps, QEMU_CAPS_DBUS_VMSTATE)) {
 
             if (qemuInterfacePrepareSlirp(driver, net) < 0)
                 goto cleanup;
@@ -1353,6 +1346,12 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
         virReportError(VIR_ERR_OPERATION_UNSUPPORTED,
                        _("hotplug of interface type of %1$s is not implemented yet"),
                        virDomainNetTypeToString(actualType));
+        goto cleanup;
+    }
+
+    /* both NET_TYPE_USER and NET_TYPE_VHOSTUSER might use passt */
+    if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST &&
+        qemuPasstStart(vm, net) < 0) {
         goto cleanup;
     }
 
@@ -1539,10 +1538,8 @@ qemuDomainAttachNetDevice(virQEMUDriver *driver,
     if (QEMU_DOMAIN_NETWORK_PRIVATE(net)->slirp)
         qemuSlirpStop(QEMU_DOMAIN_NETWORK_PRIVATE(net)->slirp, vm, driver, net);
 
-    if (net->type == VIR_DOMAIN_NET_TYPE_USER &&
-        net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
+    if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST)
         qemuPasstStop(vm, net);
-    }
 
     qemuDomainObjEnterMonitor(vm);
     if (charDevPlugged &&
@@ -5112,10 +5109,8 @@ qemuDomainRemoveNetDevice(virQEMUDriver *driver,
     if (QEMU_DOMAIN_NETWORK_PRIVATE(net)->slirp)
         qemuSlirpStop(QEMU_DOMAIN_NETWORK_PRIVATE(net)->slirp, vm, driver, net);
 
-    if (net->type == VIR_DOMAIN_NET_TYPE_USER &&
-        net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST) {
+    if (net->backend.type == VIR_DOMAIN_NET_BACKEND_PASST)
         qemuPasstStop(vm, net);
-    }
 
     virDomainAuditNet(vm, net, NULL, "detach", true);
 