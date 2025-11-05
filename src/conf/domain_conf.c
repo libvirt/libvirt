libvirt/libvirt

commit 19fc614d531fb75877abb59baaf33bc1add4d483

conf: Add configuration option for timed disk statistics collection
QEMU supports collection of disk statistics in configurable time
windows. Add support for enabling this feature to the conf parser.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/conf/domain_conf.c
@@ -8297,6 +8297,8 @@ static int
 virDomainDiskDefDriverParseXML(virDomainDiskDef *def,
                                xmlNodePtr cur)
 {
+    xmlNodePtr statisticsNode;
+
     def->driverName = virXMLPropString(cur, "name");
 
     if (virXMLPropEnum(cur, "cache", virDomainDiskCacheTypeFromString,
@@ -8345,6 +8347,26 @@ virDomainDiskDefDriverParseXML(virDomainDiskDef *def,
 
     if (virDomainIothreadMappingDefParse(cur, &def->iothreads) < 0)
         return -1;
+
+    if ((statisticsNode = virXMLNodeGetSubelement(cur, "statistics"))) {
+        g_autoptr(GPtrArray) statisticNodes = NULL;
+
+        statisticNodes = virXMLNodeGetSubelementList(statisticsNode, "statistic");
+
+        if (statisticNodes->len > 0) {
+            size_t i;
+
+            def->statistics = g_new0(unsigned int, statisticNodes->len + 1);
+
+            for (i = 0; i < statisticNodes->len; i++) {
+                if (virXMLPropUInt(g_ptr_array_index(statisticNodes, i),
+                                   "interval", 10,
+                                   VIR_XML_PROP_REQUIRED | VIR_XML_PROP_NONZERO,
+                                   def->statistics + i) < 0)
+                    return -1;
+            }
+        }
+    }
 
     if (virXMLPropEnum(cur, "detect_zeroes",
                        virDomainDiskDetectZeroesTypeFromString,
@@ -23863,6 +23885,18 @@ virDomainDiskDefFormatDriver(virBuffer *buf,
     }
 
     virDomainIothreadMappingDefFormat(&childBuf, disk->iothreads);
+
+    if (disk->statistics) {
+        g_auto(virBuffer) statisticsChildBuf = VIR_BUFFER_INIT_CHILD(&childBuf);
+        size_t i;
+
+        for (i = 0; disk->statistics[i] > 0; i++)
+            virBufferAsprintf(&statisticsChildBuf, "<statistic interval='%u'/>\n",
+                              disk->statistics[i]);
+
+        virXMLFormatElement(&childBuf, "statistics", NULL, &statisticsChildBuf);
+    }
+
 
     virXMLFormatElement(buf, "driver", &attrBuf, &childBuf);
 }

src/conf/domain_conf.h
@@ -592,6 +592,8 @@ struct _virDomainDiskDef {
     virDomainDiskDiscard discard;
     unsigned int iothread; /* unused = 0, > 0 specific thread # */
     GSList *iothreads; /* List of virDomainIothreadMappingDef */
+    unsigned int *statistics; /* Optional, zero terminated list of intervals to
+                                collect statistics for */
     virDomainDiskDetectZeroes detect_zeroes;
     virTristateSwitch discard_no_unref;
     char *domain_name; /* backend domain name */

src/conf/schemas/domaincommon.rng
@@ -2742,6 +2742,17 @@
         <optional>
           <ref name="iothreadMapping"/>
         </optional>
+        <optional>
+          <element name="statistics">
+            <zeroOrMore>
+              <element name="statistic">
+                <attribute name="interval">
+                  <ref name="unsignedInt"/>
+                </attribute>
+              </element>
+            </zeroOrMore>
+          </element>
+        </optional>
       </interleave>
     </element>
   </define>

commit 9b7ef8058b18c7e90448b33664b25392fc600fee

conf: Don't leak virDomainDiskDef's 'statistics' member
In commit 19fc614d531f I've added an option to configure statistics but
forgot to free it once the disk definition struct is freed.

Fixes: 19fc614d531fb75877abb59baaf33bc1add4d483
Signed-off-by: Peter Krempa <pkrempa@redhat.com>

src/conf/domain_conf.c
@@ -2444,6 +2444,7 @@ virDomainDiskDefFree(virDomainDiskDef *def)
     virDomainDeviceInfoClear(&def->info);
     virObjectUnref(def->privateData);
     g_slist_free_full(def->iothreads, (GDestroyNotify) virDomainIothreadMappingDefFree);
+    g_free(def->statistics);
 
     if (def->throttlefilters) {
         size_t i;