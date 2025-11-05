libvirt/libvirt

commit e4d058866e9563756349de6b3f451a53e64ca872

qemu: passt: Use 'reconnect-ms' instead of 'reconnect' with new qemus
The 'reconnect' field of 'stream' network backend type is about to be
deprecated so libvirt will need to start using 'reconnect-ms'.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_passt.c
@@ -104,12 +104,14 @@ qemuPasstAddNetProps(virDomainObj *vm,
         return -1;
     }
 
-    /* a narrow range of QEMU releases support -netdev stream, but
-     * don't support its "reconnect" option
-     */
-    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_NETDEV_STREAM_RECONNECT) &&
-        virJSONValueObjectAdd(netprops, "u:reconnect", 5, NULL) < 0) {
-        return -1;
+    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_NETDEV_STREAM_RECONNECT)) {
+        if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_NETDEV_STREAM_RECONNECT_MILISECONDS)) {
+            if (virJSONValueObjectAdd(netprops, "u:reconnect-ms", 5000, NULL) < 0)
+                return -1;
+        } else {
+            if (virJSONValueObjectAdd(netprops, "u:reconnect", 5, NULL) < 0)
+                return -1;
+        }
     }
 
     return 0;

commit ed0f48935285c99bc374420c8c0cee927527334c

qemu: capabilities: Fix logic for formatting 'reconnect' parameter
In commit e4d058866e9 I've converted the code to use the modern
'reconnect-ms' parameter instead of 'reconnect' but messed up the logic
for the time when 'reconnect' will be removed.

We need to check QEMU_CAPS_NETDEV_STREAM_RECONNECT_MILISECONDS
individually and not based on QEMU_CAPS_NETDEV_STREAM_RECONNECT.

Fix the logic as upstream qemu now removed 'reconnect'.

Fixes: e4d058866e9563756349de6b3f451a53e64ca872
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_passt.c
@@ -104,17 +104,15 @@ qemuPasstAddNetProps(virDomainObj *vm,
         return -1;
     }
 
-    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_NETDEV_STREAM_RECONNECT)) {
-        if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_NETDEV_STREAM_RECONNECT_MILISECONDS)) {
-            if (virJSONValueObjectAdd(netprops, "u:reconnect-ms",
-                                      QEMU_PASST_RECONNECT_TIMEOUT * 1000, NULL) < 0) {
-                return -1;
-            }
-        } else {
-            if (virJSONValueObjectAdd(netprops, "u:reconnect",
-                                      QEMU_PASST_RECONNECT_TIMEOUT, NULL) < 0) {
-                return -1;
-            }
+    if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_NETDEV_STREAM_RECONNECT_MILISECONDS)) {
+        if (virJSONValueObjectAdd(netprops, "u:reconnect-ms",
+                                  QEMU_PASST_RECONNECT_TIMEOUT * 1000, NULL) < 0) {
+            return -1;
+        }
+    } else if (virQEMUCapsGet(qemuCaps, QEMU_CAPS_NETDEV_STREAM_RECONNECT)) {
+        if (virJSONValueObjectAdd(netprops, "u:reconnect",
+                                  QEMU_PASST_RECONNECT_TIMEOUT, NULL) < 0) {
+            return -1;
         }
     }
 