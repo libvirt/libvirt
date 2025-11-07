libvirt/libvirt

commit 15c9ca383c2a814c61cc4ed16b4dad91221d8129

scripts: use subprocess.run instead of os.system
The subprocess.run command avoids using the shell and so is robust
should sys.argv contain any whitespace or unexpected shell meta
characters.

Reviewed-by: Michal Privoznik <mprivozn@redhat.com>
Signed-off-by: Daniel P. Berrangé <berrange@redhat.com>

scripts/check-file-access.py
@@ -23,6 +23,7 @@
 
 import os
 import re
+import subprocess
 import sys
 import tempfile
 
@@ -36,11 +37,9 @@ permitted_file = os.path.join(abs_srcdir, 'permitted_file_access.txt')
 
 os.environ['VIR_TEST_FILE_ACCESS_OUTPUT'] = access_file
 
-test = ' '.join(sys.argv[1:])
+proc = subprocess.run(sys.argv[1:])
 
-ret = os.system(test)
-
-if ret != 0 or os.read(access_fd, 10) == b'':
+if proc.returncode != 0 or os.read(access_fd, 10) == b'':
     os.close(access_fd)
     os.remove(access_file)
     sys.exit(ret)

commit a088734d570e916c35a53447e281b9d04fc2a380

scripts: do not use uninitialized variable
A previous commit removed the 'ret' variable when
switching to subprocess.run, but did not adjust
the exit code.

Fixes: 15c9ca383c2a814c61cc4ed16b4dad91221d8129
Signed-off-by: Ján Tomko <jtomko@redhat.com>

scripts/check-file-access.py
@@ -42,7 +42,7 @@ proc = subprocess.run(sys.argv[1:])
 if proc.returncode != 0 or os.read(access_fd, 10) == b'':
     os.close(access_fd)
     os.remove(access_file)
-    sys.exit(ret)
+    sys.exit(proc.returncode)
 
 known_actions = ["open", "fopen", "access", "stat", "lstat", "connect"]
 