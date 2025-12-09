38c4c3f654473c6779b7afd00eb802b5550efb15
Andrea Bolognani <abologna@redhat.com>
2025-12-10 00:02:52 +0100

    tests: Add firmware-auto-efi-enrolled-keys-aarch64
    This test case demonstrates how to automatically configure an
    aarch64 guest so that Secure Boot support is available and only
    signed operating systems are allowed to boot.
    
    It currently fails because there is no firmware descriptor that
    describes a suitable firmware build yet. That will change in a
    future commit.
    
    In addition to the latest version, the test case is also executed
    against QEMU 8.2.0 specifically. This version of the test case is
    intended to fail, because the uefi-vars device that we need to
    support Secure Boot on aarch64 was not yet available in that
    version of QEMU. The exact error message will change down the
    line.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -1657,6 +1657,8 @@ mymain(void)
     DO_TEST_CAPS_LATEST("firmware-auto-efi-secboot");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-no-secboot");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-enrolled-keys");
+    DO_TEST_CAPS_ARCH_LATEST_FAILURE("firmware-auto-efi-enrolled-keys-aarch64", "aarch64");
+    DO_TEST_CAPS_ARCH_VER_FAILURE("firmware-auto-efi-enrolled-keys-aarch64", "aarch64", "8.2.0");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-no-enrolled-keys");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("firmware-auto-efi-enrolled-keys-no-secboot");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-smm-off");
