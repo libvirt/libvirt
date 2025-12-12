26035762a96d77b8c78fa56a3a62a45ae4914597
Andrea Bolognani <abologna@redhat.com>
2025-12-12 16:15:34 +0100

    tests: Add firmware-manual-bios-rw
    This test case demonstrates a flaw in the XML validation process.
    
    ROM images are by definition read-only, so attempting to use one
    as read/write should have resulted in the domain XML being
    rejected.
    
    The issue will be addressed in an upcoming commit.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -1527,6 +1527,7 @@ mymain(void)
     DO_TEST_CAPS_LATEST("firmware-manual-bios");
     DO_TEST_CAPS_LATEST("firmware-manual-bios-stateless");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("firmware-manual-bios-not-stateless");
+    DO_TEST_CAPS_LATEST("firmware-manual-bios-rw");
     DO_TEST_CAPS_LATEST("firmware-manual-efi");
     DO_TEST_CAPS_LATEST("firmware-manual-efi-features");
     DO_TEST_CAPS_LATEST_ABI_UPDATE_PARSE_ERROR("firmware-manual-efi-features");
