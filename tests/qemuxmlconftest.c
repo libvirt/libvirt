ab92d773397b2b344bb568690baa30951408d43a
Andrea Bolognani <abologna@redhat.com>
2025-12-15 19:56:19 +0100

    tests: Add firmware-auto-efi-format-nvram-raw-loader-path
    This test case demonstrates an issue with the current
    implementation of firmware autoselection.
    
    libvirt would normally be able to find the firmware descriptor
    for the binary mentioned in the domain XML, but the fact that at
    the same time we're asking for the NVRAM file to be of a
    different format throws a spanner in the works.
    
    Of course there is no requirement for the format of the NVRAM
    file to match that of the NVRAM template, so the fact that
    libvirt is unable to produce a working configuration out of this
    input is an issues that will be addressed in an upcoming commit.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -1610,6 +1610,7 @@ mymain(void)
     DO_TEST_CAPS_LATEST("firmware-auto-efi-format-nvram-qcow2-network-nbd");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-format-nvram-raw");
     DO_TEST_CAPS_LATEST_ABI_UPDATE("firmware-auto-efi-format-nvram-raw");
+    DO_TEST_CAPS_LATEST_FAILURE("firmware-auto-efi-format-nvram-raw-loader-path");
     DO_TEST_CAPS_ARCH_LATEST("firmware-auto-efi-format-loader-raw", "aarch64");
     DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("firmware-auto-efi-format-loader-raw", "aarch64");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-format-mismatch");
