a4f33d72287f68789f1d288d78b872d42dfe3b12
Andrea Bolognani <abologna@redhat.com>
2025-12-15 20:29:11 +0100

    tests: Add firmware-auto-efi-format-nvram-raw-nvramtemplate-path
    This test case demonstrates an issue with the current
    implementation of firmware autoselection.
    
    There is no requirement for the format of the NVRAM file (raw in
    this case) to match that of the NVRAM template (qcow2 in this
    case), and yet libvirt incorrectly rejects the configuration.
    
    The issue will be addressed in an upcoming commit.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -1611,6 +1611,7 @@ mymain(void)
     DO_TEST_CAPS_LATEST("firmware-auto-efi-format-nvram-raw");
     DO_TEST_CAPS_LATEST_ABI_UPDATE("firmware-auto-efi-format-nvram-raw");
     DO_TEST_CAPS_LATEST_FAILURE("firmware-auto-efi-format-nvram-raw-loader-path");
+    DO_TEST_CAPS_LATEST_FAILURE("firmware-auto-efi-format-nvram-raw-nvramtemplate-path");
     DO_TEST_CAPS_ARCH_LATEST("firmware-auto-efi-format-loader-raw", "aarch64");
     DO_TEST_CAPS_ARCH_LATEST_ABI_UPDATE("firmware-auto-efi-format-loader-raw", "aarch64");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-format-mismatch");
