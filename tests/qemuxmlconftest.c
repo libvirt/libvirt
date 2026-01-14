a1476a5afcfb1fed5e145a3fcfa98690a89b9c2f
Peter Krempa <pkrempa@redhat.com>
2026-01-14 09:28:32 +0100

    qemuxmlconftest: Prepare 'fd-memory-numa-topology4' for removal of 'pc-i440fx-5.0'
    Per commit message of 3f390db2e2ed33cda8e which added the
    'fd-memory-numa-topology4' case, it's supposed to test memory
    preallocation setting in the NUMA setup.
    
    Now the 5.0 machine type is the last one supporting the old-style NUMA
    config and thus also the only way to test the formatting of
    '--mem-prealloc' commandline option for qemu, which was since replaced
    by a property in the memory backend object.
    
    Thus to preserve the idea of this test, add
    'fd-memory-numa-topology4-old-machine' test case pinned to qemu-10.2,
    which will use pc-i440fx-5.0 and modernize the normal case using the
    latest qemu version by dropping the specific machine version.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -3104,6 +3104,10 @@ mymain(void)
     DO_TEST_CAPS_LATEST("fd-memory-numa-topology");
     DO_TEST_CAPS_LATEST("fd-memory-numa-topology2");
     DO_TEST_CAPS_LATEST("fd-memory-numa-topology3");
+    /* qemu-10.2 is the last one supporting pc-i440fx-5.0 machine which uses the
+     * old NUMA topology setup and thus the only way to test the old style of
+     * formatting --mem-prealloc */
+    DO_TEST_CAPS_VER("fd-memory-numa-topology4-old-machine", "10.2.0");
     DO_TEST_CAPS_LATEST("fd-memory-numa-topology4");
 
     DO_TEST_CAPS_LATEST("fd-memory-no-numa-topology");
