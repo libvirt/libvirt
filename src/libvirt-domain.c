libvirt/libvirt

commit 3640245db7d72bf8e05df726587625a6328c895e

RPC: Allow HW address in remote_domain_interface struct to be NULL
Not all NICs (esp. the virtual ones like TUN) must have a hardware
address. Teach our RPC that it's possible.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>

src/libvirt-domain.c
@@ -11460,7 +11460,8 @@ virDomainFSInfoFree(virDomainFSInfoPtr info)
  *  ... do something with returned values, for example:
  *  for (i = 0; i < ifaces_count; i++) {
  *      printf("name: %s", ifaces[i]->name);
- *      printf(" hwaddr: %s", ifaces[i]->hwaddr);
+ *      if (ifaces[i]->hwaddr)
+ *          printf(" hwaddr: %s", ifaces[i]->hwaddr);
  *
  *      for (j = 0; j < ifaces[i]->naddrs; j++) {
  *          virDomainIPAddressPtr ip_addr = ifaces[i]->addrs + j;

daemon/remote.c
@@ -6525,7 +6525,9 @@ remoteSerializeDomainInterface(virDomainInterfacePtr *ifaces,
         if ((VIR_STRDUP(iface_ret->name, iface->name)) < 0)
             goto cleanup;
 
-        if ((VIR_STRDUP(iface_ret->hwaddr, iface->hwaddr)) < 0)
+        if (iface->hwaddr &&
+            (VIR_ALLOC(iface_ret->hwaddr) < 0 ||
+             VIR_STRDUP(*iface_ret->hwaddr, iface->hwaddr) < 0))
             goto cleanup;
 
         if (iface->naddrs > REMOTE_DOMAIN_IP_ADDR_MAX) {
@@ -6561,7 +6563,10 @@ remoteSerializeDomainInterface(virDomainInterfacePtr *ifaces,
         for (i = 0; i < ifaces_count; i++) {
             remote_domain_interface *iface_ret = &(ret->ifaces.ifaces_val[i]);
             VIR_FREE(iface_ret->name);
-            VIR_FREE(iface_ret->hwaddr);
+            if (iface_ret->hwaddr) {
+                VIR_FREE(*iface_ret->hwaddr);
+                VIR_FREE(iface_ret->hwaddr);
+            }
             for (j = 0; j < iface_ret->addrs.addrs_len; j++) {
                 remote_domain_ip_addr *ip_addr =
                     &(iface_ret->addrs.addrs_val[j]);

include/libvirt/libvirt-domain.h
@@ -3760,7 +3760,7 @@ typedef struct _virDomainInterface virDomainInterface;
 typedef virDomainInterface *virDomainInterfacePtr;
 struct _virDomainInterface {
     char *name;                     /* interface name */
-    char *hwaddr;                   /* hardware address */
+    char *hwaddr;                   /* hardware address, may be NULL */
     unsigned int naddrs;            /* number of items in @addrs */
     virDomainIPAddressPtr addrs;    /* array of IP addresses */
 };

src/remote/remote_driver.c
@@ -7963,7 +7963,8 @@ remoteDomainInterfaceAddresses(virDomainPtr dom,
         if (VIR_STRDUP(iface->name, iface_ret->name) < 0)
             goto cleanup;
 
-        if (VIR_STRDUP(iface->hwaddr, iface_ret->hwaddr) < 0)
+        if (iface_ret->hwaddr &&
+            VIR_STRDUP(iface->hwaddr, *iface_ret->hwaddr) < 0)
             goto cleanup;
 
         if (iface_ret->addrs.addrs_len > REMOTE_DOMAIN_IP_ADDR_MAX) {

src/remote/remote_protocol.x
@@ -3191,7 +3191,7 @@ struct remote_domain_ip_addr {
 
 struct remote_domain_interface {
     remote_nonnull_string name;
-    remote_nonnull_string hwaddr;
+    remote_string hwaddr;
     remote_domain_ip_addr addrs<REMOTE_DOMAIN_IP_ADDR_MAX>;
 };
 

src/remote_protocol-structs
@@ -2646,7 +2646,7 @@ struct remote_domain_ip_addr {
 };
 struct remote_domain_interface {
         remote_nonnull_string      name;
-        remote_nonnull_string      hwaddr;
+        remote_string              hwaddr;
         struct {
                 u_int              addrs_len;
                 remote_domain_ip_addr * addrs_val;

commit 3a10f435b75400a3c83feb8b8c27250d5665fc40

libvirt-domain: Fix documentation of virDomainInterfaceAddresses()
Ever since of v1.2.14-rc1~105 the hwaddr member of
_virDomainInterface struct can be NULL. And this is documented
inside the struct. But then the public API documents it can never
be NULL which is obviously wrong. Fix the public API
documentation.

Fixes: 3640245db7d72bf8e05df726587625a6328c895e
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/libvirt-domain.c
@@ -12881,7 +12881,7 @@ virDomainFSInfoFree(virDomainFSInfoPtr info)
  * might be unset (e.g. VIR_DOMAIN_INTERFACE_ADDRESSES_SRC_ARP does not
  * set IP address prefix as ARP table does not have any notion of that).
  *
- * @ifaces->name and @ifaces->hwaddr are never NULL.
+ * @ifaces->name is never NULL, and @ifaces->hwaddr may be NULL.
  *
  * The caller *must* free @ifaces when no longer needed. Usual use case
  * looks like this: