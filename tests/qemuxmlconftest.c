0653a2eae2ef7751f2d4f3dae20ff55332a90cc7
Andrea Bolognani <abologna@redhat.com>
2026-01-22 14:45:28 +0100

    tests: Add firmware-auto-efi-varstore-aarch64
    This test case demonstrates how to explicitly opt into using
    the uefi-vars device for an aarch64 guest.
    
    Normally the firmware autoselection process will pick a UEFI
    build that is loaded via pflash, but by including the <varstore>
    element in the input XML we can tell the QEMU driver that we
    want want the uefi-vars device to be used instead.
    
    Currently this results in an error, because the firmware
    autoselection algorithm doesn't yet know how to properly handle
    the scenario. A future commit will address this and make things
    work as expected.
    
    Signed-off-by: Andrea Bolognani <abologna@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>
    Acked-by: Gerd Hoffmann <kraxel@redhat.com>
    Reviewed-by: Daniel P. Berrangé <berrange@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -1672,6 +1672,7 @@ mymain(void)
     DO_TEST_CAPS_LATEST("firmware-auto-efi-nvram-network-nbd");
     DO_TEST_CAPS_LATEST("firmware-auto-efi-nvram-network-iscsi");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("firmware-auto-efi-varstore-q35");
+    DO_TEST_CAPS_ARCH_LATEST_PARSE_ERROR("firmware-auto-efi-varstore-aarch64", "aarch64");
 
     DO_TEST_CAPS_LATEST("firmware-auto-efi-format-loader-qcow2");
     DO_TEST_CAPS_LATEST_PARSE_ERROR("firmware-auto-efi-format-loader-qcow2-rom");
