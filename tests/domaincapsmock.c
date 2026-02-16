libvirt/libvirt

commit 63434db800b921e96fec424a667b7161af1f02de

qemu: Fill iommufd domain capability
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

tests/domaincapsdata/qemu_9.0.0.sparc.xml
@@ -90,6 +90,9 @@
         <value>default</value>
         <value>vfio</value>
       </enum>
+      <enum name='iommufd'>
+        <value>no</value>
+      </enum>
     </hostdev>
     <rng supported='yes'>
       <enum name='model'/>

tests/domaincapsdata/qemu_9.2.0-hvf.aarch64+hvf.xml
@@ -151,6 +151,9 @@
         <value>default</value>
         <value>vfio</value>
       </enum>
+      <enum name='iommufd'>
+        <value>no</value>
+      </enum>
     </hostdev>
     <rng supported='yes'>
       <enum name='model'>

tests/domaincapsmock.c
@@ -16,11 +16,13 @@
 
 #include <config.h>
 
+#include "virfile.h"
 #include "virhostcpu.h"
 #include "virhostmem.h"
+#include "viriommufd.h"
+#include "virmock.h"
 
 #if WITH_QEMU
-# include "virmock.h"
 # include "qemu/qemu_capabilities.h"
 #endif
 
@@ -77,4 +79,17 @@ virHostMemGetTHPSize(unsigned long long *size)
     /* Pretend Transparent Huge Page size is 2MiB. */
     *size = 2048;
     return 0;
+}
+
+static bool (*real_virFileExists)(const char *path);
+
+bool
+virFileExists(const char *path)
+{
+    VIR_MOCK_REAL_INIT(virFileExists);
+
+    if (STREQ(path, VIR_IOMMU_DEV_PATH))
+        return true;
+
+    return real_virFileExists(path);
 }

commit 75db232f90af2b2acd95266dd21290c967e3c625

tests: properly mock VFIO and IOMMU checks
Fixes: 63434db800b921e96fec424a667b7161af1f02de
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>

src/util/virutil.h
@@ -141,7 +141,7 @@ unsigned long long virMemoryLimitTruncate(unsigned long long value);
 bool virMemoryLimitIsSet(unsigned long long value);
 unsigned long long virMemoryMaxValue(bool ulong) ATTRIBUTE_MOCKABLE;
 
-bool virHostHasIOMMU(void);
+bool virHostHasIOMMU(void) ATTRIBUTE_MOCKABLE;
 
 char *virHostGetDRMRenderNode(void) ATTRIBUTE_MOCKABLE;
 

tests/domaincapsdata/qemu_9.0.0.sparc.xml
@@ -86,10 +86,7 @@
         <value>scsi</value>
       </enum>
       <enum name='capsType'/>
-      <enum name='pciBackend'>
-        <value>default</value>
-        <value>vfio</value>
-      </enum>
+      <enum name='pciBackend'/>
       <enum name='iommufd'>
         <value>no</value>
       </enum>

tests/domaincapsdata/qemu_9.2.0-hvf.aarch64+hvf.xml
@@ -147,10 +147,7 @@
         <value>scsi</value>
       </enum>
       <enum name='capsType'/>
-      <enum name='pciBackend'>
-        <value>default</value>
-        <value>vfio</value>
-      </enum>
+      <enum name='pciBackend'/>
       <enum name='iommufd'>
         <value>no</value>
       </enum>

tests/domaincapsmock.c
@@ -21,6 +21,7 @@
 #include "virhostmem.h"
 #include "viriommufd.h"
 #include "virmock.h"
+#include "virutil.h"
 
 #if WITH_QEMU
 # include "qemu/qemu_capabilities.h"
@@ -91,5 +92,14 @@ virFileExists(const char *path)
     if (STREQ(path, VIR_IOMMU_DEV_PATH))
         return true;
 
+    if (STREQ(path, "/dev/vfio/vfio"))
+        return true;
+
     return real_virFileExists(path);
+}
+
+bool
+virHostHasIOMMU(void)
+{
+    return true;
 }

tests/domaincapstest.c
@@ -104,14 +104,6 @@ fillQemuCaps(virDomainCaps *domCaps,
                                   false) < 0)
         return -1;
 
-    /* The function above tries to query host's VFIO capabilities by calling
-     * virHostdevHostSupportsPassthroughVFIO() which, however, can't be
-     * successfully mocked as they are not exposed as internal APIs. Therefore,
-     * instead of mocking set the expected values here by hand. */
-    VIR_DOMAIN_CAPS_ENUM_SET(domCaps->hostdev.pciBackend,
-                             VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_DEFAULT,
-                             VIR_DEVICE_HOSTDEV_PCI_DRIVER_NAME_VFIO);
-
     /* As of f05b6a918e28 we are expecting to see OVMF_CODE.fd file which
      * may not exists everywhere. */
     while (loader->values.nvalues)