libvirt/libvirt

commit 7b5566ce67b18a2bebe68fdb07e046f25185f8d3

qemu: Re-activate block nodes before storage operations
Upcoming patches will modify how we treat inactive block nodes so that
we can properly deactivate nodes for 'manual' disk snapshot mode.

Re-activate the nodes before operations requiring them. This includes
also query operations where we e.g. probe bitmaps.

Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

src/qemu/qemu_migration.c
@@ -2919,6 +2919,9 @@ qemuMigrationSrcBeginPhase(virQEMUDriver *driver,
          vm->newDef && !qemuDomainVcpuHotplugIsInOrder(vm->newDef)))
         cookieFlags |= QEMU_MIGRATION_COOKIE_CPU_HOTPLUG;
 
+    if (qemuBlockNodesEnsureActive(vm, vm->job->asyncJob) < 0)
+        return NULL;
+
     return qemuMigrationSrcBeginXML(vm, xmlin,
                                     cookieout, cookieoutlen, cookieFlags,
                                     migrate_disks, flags);

src/qemu/qemu_backup.c
@@ -823,6 +823,9 @@ qemuBackupBegin(virDomainObj *vm,
     if (qemuBackupBeginPrepareTLS(vm, cfg, def, &tlsProps, &tlsSecretProps) < 0)
         goto endjob;
 
+    if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_BACKUP) < 0)
+        goto endjob;
+
     actions = virJSONValueNewArray();
 
     /* The 'chk' checkpoint must be rolled back if the transaction command

src/qemu/qemu_checkpoint.c
@@ -189,6 +189,9 @@ qemuCheckpointDiscardBitmaps(virDomainObj *vm,
 
     actions = virJSONValueNewArray();
 
+    if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_NONE) < 0)
+        return -1;
+
     if (!(blockNamedNodeData = qemuBlockGetNamedNodeData(vm, VIR_ASYNC_JOB_NONE)))
         return -1;
 
@@ -411,6 +414,9 @@ qemuCheckpointRedefineValidateBitmaps(virDomainObj *vm,
     if (virDomainObjCheckActive(vm) < 0)
         return -1;
 
+    if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_NONE) < 0)
+        return -1;
+
     if (!(blockNamedNodeData = qemuBlockGetNamedNodeData(vm, VIR_ASYNC_JOB_NONE)))
         return -1;
 
@@ -514,6 +520,9 @@ qemuCheckpointCreate(virQEMUDriver *driver,
     int rc;
 
     if (qemuCheckpointCreateCommon(driver, vm, def, &actions, &chk) < 0)
+        return NULL;
+
+    if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_NONE) < 0)
         return NULL;
 
     qemuDomainObjEnterMonitor(vm);
@@ -649,6 +658,9 @@ qemuCheckpointGetXMLDescUpdateSize(virDomainObj *vm,
         return -1;
 
     if (virDomainObjCheckActive(vm) < 0)
+        goto endjob;
+
+    if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_NONE) < 0)
         goto endjob;
 
     if (!(nodedataMerge = qemuBlockGetNamedNodeData(vm, VIR_ASYNC_JOB_NONE)))

src/qemu/qemu_driver.c
@@ -13820,6 +13820,9 @@ qemuDomainBlockPullCommon(virDomainObj *vm,
         speed <<= 20;
     }
 
+    if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_NONE) < 0)
+        goto endjob;
+
     if (!(job = qemuBlockJobDiskNewPull(vm, disk, baseSource, flags)))
         goto endjob;
 
@@ -14390,6 +14393,9 @@ qemuDomainBlockCopyCommon(virDomainObj *vm,
         goto endjob;
     }
 
+    if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_NONE) < 0)
+        goto endjob;
+
     /* pre-create the image file. This is required so that libvirt can properly
      * label the image for access by qemu */
     if (!existing) {
@@ -14794,6 +14800,9 @@ qemuDomainBlockCommit(virDomainPtr dom,
         baseSource = topSource->backingStore;
     else if (!(baseSource = virStorageSourceChainLookup(disk->src, topSource,
                                                         base, disk->dst, NULL)))
+        goto endjob;
+
+    if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_NONE) < 0)
         goto endjob;
 
     job = qemuBlockCommit(vm, disk, baseSource, topSource, top_parent,

src/qemu/qemu_snapshot.c
@@ -2066,6 +2066,9 @@ qemuSnapshotCreate(virDomainObj *vm,
 
     /* actually do the snapshot */
     if (virDomainObjIsActive(vm)) {
+        if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_SNAPSHOT) < 0)
+            goto error;
+
         if (flags & VIR_DOMAIN_SNAPSHOT_CREATE_DISK_ONLY ||
             virDomainSnapshotObjGetDef(snap)->memory == VIR_DOMAIN_SNAPSHOT_LOCATION_EXTERNAL) {
             /* external full system or disk snapshot */
@@ -4094,6 +4097,9 @@ qemuSnapshotDiscardImpl(virDomainObj *vm,
                     return -1;
             }
         } else {
+            if (qemuBlockNodesEnsureActive(vm, VIR_ASYNC_JOB_SNAPSHOT) < 0)
+                return -1;
+
             if (virDomainSnapshotIsExternal(snap)) {
                 if (qemuSnapshotDiscardExternal(vm, snap, externalData) < 0)
                     return -1;

commit 4537c0b8708851136b8246b1869c6008a56240ae

qemuMigrationSrcBeginPhase: Don't call 'qemuBlockNodesEnsureActive' with offline VM
Commits 7b5566ce67b18a and f879d5f40385358 ( v11.8.0-92-gf879d5f403 )
moved around code for re-activating block backends after migration.
While previously it was done when migration failed now we do it when we
need qemu to do some block operations.

'qemuBlockNodesEnsureActive' is thus called also when 'VIR_MIGRATE_OFFLINE'
is used. This doesn't cause failure similar to previous patch only due
to a conincidence as 'qemuCaps' wasn't initialized yet and thus we
assume that QEMU doesn't support 'blockdev-set-active' and skip all
monitor code.

Make the code more robust and explicit by calling
'qemuBlockNodesEnsureActive' only on active VMs during migration.

Fixes: 7b5566ce67b18a2bebe68fdb07e046f25185f8d3
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Jiri Denemark <jdenemar@redhat.com>

src/qemu/qemu_migration.c
@@ -2899,7 +2899,8 @@ qemuMigrationSrcBeginPhase(virQEMUDriver *driver,
          vm->newDef && !qemuDomainVcpuHotplugIsInOrder(vm->newDef)))
         cookieFlags |= QEMU_MIGRATION_COOKIE_CPU_HOTPLUG;
 
-    if (qemuBlockNodesEnsureActive(vm, vm->job->asyncJob) < 0)
+    if (virDomainObjIsActive(vm) &&
+        qemuBlockNodesEnsureActive(vm, vm->job->asyncJob) < 0)
         return NULL;
 
     return qemuMigrationSrcBeginXML(vm, xmlin,