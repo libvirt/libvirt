libvirt/libvirt

commit 36cf50f85b9c8f3351332dc5302401e4b06176e1

qemu: driver: Setup 'virPortAllocator' for backup NBD server ports
Signed-off-by: Lucas Amaral <lucaaamaral@gmail.com>
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

src/qemu/qemu_driver.c
@@ -719,6 +719,12 @@ qemuStateInitialize(bool privileged,
                                   cfg->migrationPortMax)) == NULL)
         goto error;
 
+    if ((qemu_driver->backupPorts =
+         virPortAllocatorRangeNew(_("backup"),
+                                  cfg->backupPortMin,
+                                  cfg->backupPortMax)) == NULL)
+        goto error;
+
     if (qemuSecurityInit(qemu_driver) < 0)
         goto error;
 

src/qemu/qemu_conf.h
@@ -342,6 +342,9 @@ struct _virQEMUDriver {
     /* Immutable pointer, immutable object */
     virPortAllocatorRange *migrationPorts;
 
+    /* Immutable pointer, immutable object */
+    virPortAllocatorRange *backupPorts;
+
     /* Immutable pointer, lockless APIs */
     virSysinfoDef *hostsysinfo;
 

commit 4a363bdfe95361f0f45c83d7eeac9f43b77bdd84

qemu: driver: Don't leak 'qemu_driver->backupPorts'
 ==2413993== 23 (16 direct, 7 indirect) bytes in 1 blocks are definitely lost in loss record 1,238 of 4,225
 ==2413993==    at 0x48FC683: calloc (vg_replace_malloc.c:1678)
 ==2413993==    by 0x4ED0A51: g_malloc0 (in /usr/lib64/libglib-2.0.so.0.8600.4)
 ==2413993==    by 0x49A62CF: virPortAllocatorRangeNew (virportallocator.c:104)
 ==2413993==    by 0xEA35F27: qemuStateInitialize (qemu_driver.c:723)
 ==2413993==    by 0x4BABC76: virStateInitialize (libvirt.c:667)
 ==2413993==    by 0x40148D4: daemonRunStateInit (remote_daemon.c:583)
 ==2413993==    by 0x49BB8A5: virThreadHelper (virthread.c:256)
 ==2413993==    by 0x52DD463: start_thread (in /usr/lib64/libc.so.6)
 ==2413993==    by 0x5360413: clone (in /usr/lib64/libc.so.6)

Fixes: 36cf50f85b9c8f3351332dc5302401e4b06176e1
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

src/qemu/qemu_driver.c
@@ -1037,6 +1037,7 @@ qemuStateCleanup(void)
     virLockManagerPluginUnref(qemu_driver->lockManager);
     virSysinfoDefFree(qemu_driver->hostsysinfo);
     virPortAllocatorRangeFree(qemu_driver->migrationPorts);
+    virPortAllocatorRangeFree(qemu_driver->backupPorts);
     virPortAllocatorRangeFree(qemu_driver->webSocketPorts);
     virPortAllocatorRangeFree(qemu_driver->rdpPorts);
     virPortAllocatorRangeFree(qemu_driver->remotePorts);