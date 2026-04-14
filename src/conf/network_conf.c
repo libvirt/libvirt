libvirt/libvirt

commit d9b34ad12b2da231431a761b03ca038cdd44bd42

network: Format <ip/> element using virXMLFormatElement()
Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Pavel Hrdina <phrdina@redhat.com>

src/conf/network_conf.c
@@ -2065,40 +2065,40 @@ static int
 virNetworkIPDefFormat(virBuffer *buf,
                       const virNetworkIPDef *def)
 {
-    virBufferAddLit(buf, "<ip");
+    virBuffer ipAttrBuf = VIR_BUFFER_INITIALIZER;
+    virBuffer ipChildBuf = VIR_BUFFER_INIT_CHILD(buf);
 
     if (def->family)
-        virBufferAsprintf(buf, " family='%s'", def->family);
+        virBufferAsprintf(&ipAttrBuf, " family='%s'", def->family);
     if (VIR_SOCKET_ADDR_VALID(&def->address)) {
         g_autofree char *addr = virSocketAddrFormat(&def->address);
         if (!addr)
             return -1;
-        virBufferAsprintf(buf, " address='%s'", addr);
+        virBufferAsprintf(&ipAttrBuf, " address='%s'", addr);
     }
     if (VIR_SOCKET_ADDR_VALID(&def->netmask)) {
         g_autofree char *addr = virSocketAddrFormat(&def->netmask);
         if (!addr)
             return -1;
-        virBufferAsprintf(buf, " netmask='%s'", addr);
+        virBufferAsprintf(&ipAttrBuf, " netmask='%s'", addr);
     }
     if (def->prefix > 0)
-        virBufferAsprintf(buf, " prefix='%u'", def->prefix);
+        virBufferAsprintf(&ipAttrBuf, " prefix='%u'", def->prefix);
 
     if (def->localPTR) {
-        virBufferAsprintf(buf, " localPtr='%s'",
+        virBufferAsprintf(&ipAttrBuf, " localPtr='%s'",
                           virTristateBoolTypeToString(def->localPTR));
     }
 
-    virBufferAddLit(buf, ">\n");
-    virBufferAdjustIndent(buf, 2);
-    virBufferEscapeString(buf, "<tftp root='%s'/>\n",
+    virBufferEscapeString(&ipChildBuf, "<tftp root='%s'/>\n",
                           def->tftproot);
     if ((def->nranges || def->nhosts)) {
+        virBuffer dhcpChildBuf = VIR_BUFFER_INIT_CHILD(&ipChildBuf);
         size_t i;
-        virBufferAddLit(buf, "<dhcp>\n");
-        virBufferAdjustIndent(buf, 2);
 
         for (i = 0; i < def->nranges; i++) {
+            virBuffer rangeAttrBuf = VIR_BUFFER_INITIALIZER;
+            virBuffer rangeChildBuf = VIR_BUFFER_INIT_CHILD(&dhcpChildBuf);
             virSocketAddrRange addr = def->ranges[i].addr;
             virNetworkDHCPLeaseTimeDef *lease = def->ranges[i].lease;
             g_autofree char *saddr = NULL;
@@ -2110,75 +2110,69 @@ virNetworkIPDefFormat(virBuffer *buf,
             if (!(eaddr = virSocketAddrFormat(&addr.end)))
                 return -1;
 
-            virBufferAsprintf(buf, "<range start='%s' end='%s'",
+            virBufferAsprintf(&rangeAttrBuf, " start='%s' end='%s'",
                               saddr, eaddr);
             if (lease) {
-                virBufferAddLit(buf, ">\n");
-                virBufferAdjustIndent(buf, 2);
                 if (!lease->expiry) {
-                    virBufferAddLit(buf, "<lease expiry='0'/>\n");
+                    virBufferAddLit(&rangeChildBuf, "<lease expiry='0'/>\n");
                 } else {
-                    virBufferAsprintf(buf, "<lease expiry='%llu' unit='%s'/>\n",
+                    virBufferAsprintf(&rangeChildBuf, "<lease expiry='%llu' unit='%s'/>\n",
                                       lease->expiry,
                                       virNetworkDHCPLeaseTimeUnitTypeToString(lease->unit));
                 }
-                virBufferAdjustIndent(buf, -2);
-                virBufferAddLit(buf, "</range>\n");
-            } else {
-                virBufferAddLit(buf, "/>\n");
             }
+
+            virXMLFormatElement(&dhcpChildBuf, "range", &rangeAttrBuf, &rangeChildBuf);
         }
         for (i = 0; i < def->nhosts; i++) {
+            virBuffer hostAttrBuf = VIR_BUFFER_INITIALIZER;
+            virBuffer hostChildBuf = VIR_BUFFER_INIT_CHILD(&dhcpChildBuf);
             virNetworkDHCPLeaseTimeDef *lease = def->hosts[i].lease;
-            virBufferAddLit(buf, "<host");
+
             if (def->hosts[i].mac)
-                virBufferAsprintf(buf, " mac='%s'", def->hosts[i].mac);
+                virBufferAsprintf(&hostAttrBuf, " mac='%s'", def->hosts[i].mac);
             if (def->hosts[i].id)
-                virBufferAsprintf(buf, " id='%s'", def->hosts[i].id);
+                virBufferAsprintf(&hostAttrBuf, " id='%s'", def->hosts[i].id);
             if (def->hosts[i].name)
-                virBufferAsprintf(buf, " name='%s'", def->hosts[i].name);
+                virBufferAsprintf(&hostAttrBuf, " name='%s'", def->hosts[i].name);
             if (VIR_SOCKET_ADDR_VALID(&def->hosts[i].ip)) {
                 g_autofree char *ipaddr = virSocketAddrFormat(&def->hosts[i].ip);
                 if (!ipaddr)
                     return -1;
 
-                virBufferAsprintf(buf, " ip='%s'", ipaddr);
+                virBufferAsprintf(&hostAttrBuf, " ip='%s'", ipaddr);
             }
             if (lease) {
-                virBufferAddLit(buf, ">\n");
-                virBufferAdjustIndent(buf, 2);
                 if (!lease->expiry) {
-                    virBufferAddLit(buf, "<lease expiry='0'/>\n");
+                    virBufferAddLit(&hostChildBuf, "<lease expiry='0'/>\n");
                 } else {
-                    virBufferAsprintf(buf, "<lease expiry='%llu' unit='%s'/>\n",
+                    virBufferAsprintf(&hostChildBuf, "<lease expiry='%llu' unit='%s'/>\n",
                                       lease->expiry,
                                       virNetworkDHCPLeaseTimeUnitTypeToString(lease->unit));
                 }
-                virBufferAdjustIndent(buf, -2);
-                virBufferAddLit(buf, "</host>\n");
-            } else {
-                virBufferAddLit(buf, "/>\n");
             }
+
+            virXMLFormatElement(&dhcpChildBuf, "host", &hostAttrBuf, &hostChildBuf);
         }
         if (def->bootfile) {
-            virBufferEscapeString(buf, "<bootp file='%s'",
-                                  def->bootfile);
+            virBuffer bootpAttrBuf = VIR_BUFFER_INITIALIZER;
+
+            virBufferEscapeString(&bootpAttrBuf, " file='%s'", def->bootfile);
             if (VIR_SOCKET_ADDR_VALID(&def->bootserver)) {
                 g_autofree char *ipaddr = virSocketAddrFormat(&def->bootserver);
                 if (!ipaddr)
                     return -1;
 
-                virBufferEscapeString(buf, " server='%s'", ipaddr);
+                virBufferEscapeString(&bootpAttrBuf, " server='%s'", ipaddr);
             }
-            virBufferAddLit(buf, "/>\n");
 
+            virXMLFormatElement(&dhcpChildBuf, "bootp", &bootpAttrBuf, NULL);
         }
-        virBufferAdjustIndent(buf, -2);
-        virBufferAddLit(buf, "</dhcp>\n");
+
+        virXMLFormatElement(&ipChildBuf, "dhcp", NULL, &dhcpChildBuf);
     }
 
-    virBufferAdjustIndent(buf, -2);
-    virBufferAddLit(buf, "</ip>\n");
+    virXMLFormatElement(buf, "ip", &ipAttrBuf, &ipChildBuf);
 
     return 0;
 }

tests/networkxml2xmlupdateout/nat-network-dns-hosts-modified.xml
@@ -24,6 +24,5 @@
       <hostname>names</hostname>
     </host>
   </dns>
-  <ip address='192.168.122.1' netmask='255.255.255.0'>
-  </ip>
+  <ip address='192.168.122.1' netmask='255.255.255.0'/>
 </network>

tests/networkxml2xmlupdateout/nat-network-dns-more-hosts.xml
@@ -26,6 +26,5 @@
       <hostname>names</hostname>
     </host>
   </dns>
-  <ip address='192.168.122.1' netmask='255.255.255.0'>
-  </ip>
+  <ip address='192.168.122.1' netmask='255.255.255.0'/>
 </network>

tests/networkxml2xmlupdateout/nat-network-dns-srv-record.xml
@@ -15,12 +15,8 @@
       <host mac='00:16:3e:3e:a9:1a' name='b.example.com' ip='192.168.122.11'/>
     </dhcp>
   </ip>
-  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'>
-  </ip>
-  <ip family='ipv4' address='10.24.10.1'>
-  </ip>
+  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'/>
+  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'/>
+  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'/>
+  <ip family='ipv4' address='10.24.10.1'/>
 </network>

tests/networkxml2xmlupdateout/nat-network-dns-srv-records.xml
@@ -22,12 +22,8 @@
       <host mac='00:16:3e:3e:a9:1a' name='b.example.com' ip='192.168.122.11'/>
     </dhcp>
   </ip>
-  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'>
-  </ip>
-  <ip family='ipv4' address='10.24.10.1'>
-  </ip>
+  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'/>
+  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'/>
+  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'/>
+  <ip family='ipv4' address='10.24.10.1'/>
 </network>

tests/networkxml2xmlupdateout/nat-network-dns-txt-modify-ok.xml
@@ -15,12 +15,8 @@
       <host mac='00:16:3e:3e:a9:1a' name='b.example.com' ip='192.168.122.11'/>
     </dhcp>
   </ip>
-  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'>
-  </ip>
-  <ip family='ipv4' address='10.24.10.1'>
-  </ip>
+  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'/>
+  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'/>
+  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'/>
+  <ip family='ipv4' address='10.24.10.1'/>
 </network>

tests/networkxml2xmlupdateout/nat-network-dns-txt-none.xml
@@ -12,12 +12,8 @@
       <host mac='00:16:3e:3e:a9:1a' name='b.example.com' ip='192.168.122.11'/>
     </dhcp>
   </ip>
-  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'>
-  </ip>
-  <ip family='ipv4' address='10.24.10.1'>
-  </ip>
+  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'/>
+  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'/>
+  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'/>
+  <ip family='ipv4' address='10.24.10.1'/>
 </network>

tests/networkxml2xmlupdateout/nat-network-dns-txt-records.xml
@@ -16,12 +16,8 @@
       <host mac='00:16:3e:3e:a9:1a' name='b.example.com' ip='192.168.122.11'/>
     </dhcp>
   </ip>
-  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'>
-  </ip>
-  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'>
-  </ip>
-  <ip family='ipv4' address='10.24.10.1'>
-  </ip>
+  <ip family='ipv4' address='192.168.123.1' netmask='255.255.255.0'/>
+  <ip family='ipv6' address='2001:db8:ac10:fe01::1' prefix='64'/>
+  <ip family='ipv6' address='2001:db8:ac10:fd01::1' prefix='64'/>
+  <ip family='ipv4' address='10.24.10.1'/>
 </network>

commit f2434e8bd29e78e05583d258cc20ec78497f8f20

virNetworkIPDefFormat: Fix memory leaks
Use g_auto() for every virBuffer in this function to make sure none of
them will leak memory. It is not necessary to use on all of them because
for some of the buffers virXMLFormatElement() is called before any
return from the function but for consistency reasons it's better to use
g_auto() for all cases.

Fixes: d9b34ad12b2da231431a761b03ca038cdd44bd42
Signed-off-by: Pavel Hrdina <phrdina@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

src/conf/network_conf.c
@@ -2065,8 +2065,8 @@ static int
 virNetworkIPDefFormat(virBuffer *buf,
                       const virNetworkIPDef *def)
 {
-    virBuffer ipAttrBuf = VIR_BUFFER_INITIALIZER;
-    virBuffer ipChildBuf = VIR_BUFFER_INIT_CHILD(buf);
+    g_auto(virBuffer) ipAttrBuf = VIR_BUFFER_INITIALIZER;
+    g_auto(virBuffer) ipChildBuf = VIR_BUFFER_INIT_CHILD(buf);
 
     if (def->family)
         virBufferAsprintf(&ipAttrBuf, " family='%s'", def->family);
@@ -2093,12 +2093,12 @@ virNetworkIPDefFormat(virBuffer *buf,
     virBufferEscapeString(&ipChildBuf, "<tftp root='%s'/>\n",
                           def->tftproot);
     if ((def->nranges || def->nhosts)) {
-        virBuffer dhcpChildBuf = VIR_BUFFER_INIT_CHILD(&ipChildBuf);
+        g_auto(virBuffer) dhcpChildBuf = VIR_BUFFER_INIT_CHILD(&ipChildBuf);
         size_t i;
 
         for (i = 0; i < def->nranges; i++) {
-            virBuffer rangeAttrBuf = VIR_BUFFER_INITIALIZER;
-            virBuffer rangeChildBuf = VIR_BUFFER_INIT_CHILD(&dhcpChildBuf);
+            g_auto(virBuffer) rangeAttrBuf = VIR_BUFFER_INITIALIZER;
+            g_auto(virBuffer) rangeChildBuf = VIR_BUFFER_INIT_CHILD(&dhcpChildBuf);
             virSocketAddrRange addr = def->ranges[i].addr;
             virNetworkDHCPLeaseTimeDef *lease = def->ranges[i].lease;
             g_autofree char *saddr = NULL;
@@ -2125,8 +2125,8 @@ virNetworkIPDefFormat(virBuffer *buf,
             virXMLFormatElement(&dhcpChildBuf, "range", &rangeAttrBuf, &rangeChildBuf);
         }
         for (i = 0; i < def->nhosts; i++) {
-            virBuffer hostAttrBuf = VIR_BUFFER_INITIALIZER;
-            virBuffer hostChildBuf = VIR_BUFFER_INIT_CHILD(&dhcpChildBuf);
+            g_auto(virBuffer) hostAttrBuf = VIR_BUFFER_INITIALIZER;
+            g_auto(virBuffer) hostChildBuf = VIR_BUFFER_INIT_CHILD(&dhcpChildBuf);
             virNetworkDHCPLeaseTimeDef *lease = def->hosts[i].lease;
 
             if (def->hosts[i].mac)
@@ -2155,7 +2155,7 @@ virNetworkIPDefFormat(virBuffer *buf,
             virXMLFormatElement(&dhcpChildBuf, "host", &hostAttrBuf, &hostChildBuf);
         }
         if (def->bootfile) {
-            virBuffer bootpAttrBuf = VIR_BUFFER_INITIALIZER;
+            g_auto(virBuffer) bootpAttrBuf = VIR_BUFFER_INITIALIZER;
 
             virBufferEscapeString(&bootpAttrBuf, " file='%s'", def->bootfile);
             if (VIR_SOCKET_ADDR_VALID(&def->bootserver)) {