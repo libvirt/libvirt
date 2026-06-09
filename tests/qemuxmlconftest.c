a30a5a89e7702c7ae31344893735442385ce5777
Peter Krempa <pkrempa@redhat.com>
2026-06-09 14:33:04 +0200

    qemuxmlconftest: Add test cases for configs asking for 'virtio-gpu-gl' or 'virtio-vga-gl' without the capability
    The capability check in 'qemuValidateDomainDeviceDefVideo' which
    validates whether a <video> definition with acceleration enabled is
    possible is only aggregate, thus validates that any '-gl' video backend
    is available.
    
    Since qemu compiles each backend into a separate module it's possible to
    have an installation where 'virtio-gpu-pci-gl' exist but 'virtio-vga-gl'
    doesn't and it will not be rejected at validation. The commandline
    though will generate a device *without* the '-gl' which is ABI
    incompatible with the counterpart which does have '-gl', but the VM
    starts. If such a VM is then migrated to a deployment which does have
    the '-gl' variant available, migration will fail because qemu will
    generate the '-gl' device as we don't record this fact in the XML.
    
    This test case captures this situation which will be fixed later.
    
    Signed-off-by: Peter Krempa <pkrempa@redhat.com>
    Reviewed-by: Michal Privoznik <mprivozn@redhat.com>

--- tests/qemuxmlconftest.c
+++ tests/qemuxmlconftest.c
@@ -2642,6 +2642,16 @@ mymain(void)
     DO_TEST_CAPS_LATEST("video-virtio-blob-on");
     DO_TEST_CAPS_LATEST("video-virtio-blob-off");
     DO_TEST_CAPS_LATEST("video-virtio-vga-gpu-gl");
+    DO_TEST_FULL("video-virtio-vga-gpu-gl", ".x86_64-latest.QEMU_CAPS_VIRTIO_VGA_GL-disabled",
+                 ARG_CAPS_ARCH, "x86_64",
+                 ARG_CAPS_VER, "latest",
+                 ARG_QEMU_CAPS_DEL, QEMU_CAPS_VIRTIO_VGA_GL, QEMU_CAPS_LAST,
+                 ARG_END);
+    DO_TEST_FULL("video-virtio-vga-gpu-gl", ".x86_64-latest.QEMU_CAPS_VIRTIO_GPU_GL_PCI-disabled",
+                 ARG_CAPS_ARCH, "x86_64",
+                 ARG_CAPS_VER, "latest",
+                 ARG_QEMU_CAPS_DEL, QEMU_CAPS_VIRTIO_GPU_GL_PCI, QEMU_CAPS_LAST,
+                 ARG_END);
     DO_TEST_CAPS_LATEST("video-virtio-edid-none");
     DO_TEST_CAPS_LATEST("video-virtio-edid-off");
     DO_TEST_CAPS_LATEST("video-virtio-edid-on");
