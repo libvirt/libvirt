libvirt/libvirt

commit db7b0b12b7b07b8a0313161e3cbedb38a74e5954

qemu_process: Be nicer to killing QEMU when probing caps
The qemuProcessQMPStop() function is intended to kill this dummy
QEMU process we started only for querying capabilities.
Nevertheless, it may be not plain QEMU binary we executed, but
in fact it may be a memcheck tool (e.g. valgrind) that executes
QEMU later. By switching to virProcessKillPainfully() we allow
this wrapper tool to exit gracefully.

Another up side is that virProcessKillPainfully() reports an
error so no need for us to VIR_ERROR() ourselves.

Signed-off-by: Michal Privoznik <mprivozn@redhat.com>
Reviewed-by: Martin Kletzander <mkletzan@redhat.com>

src/qemu/qemu_process.c
@@ -9131,11 +9131,8 @@ qemuProcessQMPStop(qemuProcessQMP *proc)
 
     if (proc->pid != 0) {
         VIR_DEBUG("Killing QMP caps process %lld", (long long)proc->pid);
-        if (virProcessKill(proc->pid, SIGKILL) < 0 && errno != ESRCH)
-            VIR_ERROR(_("Failed to kill process %lld: %s"),
-                      (long long)proc->pid,
-                      g_strerror(errno));
-
+        virProcessKillPainfully(proc->pid, true);
+        virResetLastError();
         proc->pid = 0;
     }
 

commit 5c351c896565acfde0819d195689dcdf19868c1f

qemuProcessQMPStop: Preserve error when killing qemu used for capability probing
When probing of capabilities fails 'qemuProcessQMPStop' kills the qemu
process, but also resets the error. The virResetLastError() was
originally intended to reset the error from 'virProcessKillPainfully',
it also resets any probing error resulting in the following:

  $ virsh start VM
  error: Failed to start domain 'VM'
  error: An error occurred, but the cause is unknown

Keep the error clearing as this might be called on success but preserve
the original error around it.

Fixes: db7b0b12b7b07b8a0313161e3cbedb38a74e5954
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/qemu/qemu_process.c
@@ -10239,6 +10239,8 @@ static qemuMonitorCallbacks callbacks = {
 static void
 qemuProcessQMPStop(qemuProcessQMP *proc)
 {
+    virErrorPtr err;
+
     if (proc->mon) {
         virObjectUnlock(proc->mon);
         g_clear_pointer(&proc->mon, qemuMonitorClose);
@@ -10255,10 +10257,12 @@ qemuProcessQMPStop(qemuProcessQMP *proc)
     virDomainObjEndAPI(&proc->vm);
 
     if (proc->pid != 0) {
+        virErrorPreserveLast(&err);
         VIR_DEBUG("Killing QMP caps process %lld", (long long)proc->pid);
         virProcessKillPainfully(proc->pid, true);
         virResetLastError();
         proc->pid = 0;
+        virErrorRestore(&err);
     }
 
     if (proc->pidfile)