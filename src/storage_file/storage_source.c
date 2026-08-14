libvirt/libvirt

commit 0a3d177d9bd6cc608cc8e5769188e5f45f70ee62

storage file: fill in src->dataFileStore during file probe
Signed-off-by: Nikolai Barybin <nikolai.barybin@virtuozzo.com>
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Peter Krempa <pkrempa@redhat.com>

src/storage_file/storage_source.c
@@ -544,6 +544,39 @@ virStorageSourceNewFromBacking(virStorageSource *parent,
 
 
 /**
+ * virStorageSourceNewFromDataFile:
+ * @parent: storage source parent
+ *
+ * Creates a storage source which describes the data file image of @parent.
+ * Returned storage source format is VIR_STORAGE_FILE_RAW, and, unlike
+ * backing storage creation, readonly flag is copied from @parent.
+ */
+static virStorageSource *
+virStorageSourceNewFromDataFile(virStorageSource *parent)
+{
+    g_autoptr(virStorageSource) dataFile = NULL;
+    int rc;
+
+    if ((rc = virStorageSourceNewFromChild(parent,
+                                           parent->dataFileRaw,
+                                           &dataFile)) < 0)
+        return NULL;
+
+    if (rc == 1) {
+        virReportError(VIR_ERR_OPERATION_FAILED,
+                       _("can't use data file definition '%1$s'"),
+                       parent->dataFileRaw);
+        return NULL;
+    }
+
+    dataFile->format = VIR_STORAGE_FILE_RAW;
+    dataFile->readonly = parent->readonly;
+
+    return g_steal_pointer(&dataFile);
+}
+
+
+/**
  * @src: disk source definition structure
  * @fd: file descriptor
  * @sb: stat buffer
@@ -1389,6 +1422,14 @@ virStorageSourceGetMetadataRecurse(virStorageSource *src,
             VIR_FREE(src->backingStoreRaw);
             return -2;
         }
+    }
+
+    /* The image we're detecting on can have a '<dataStore>' explicitly
+     * configured without a '<backingStore/>' terminator so we must not
+     * overwrite it */
+    if (!src->dataFileStore && src->dataFileRaw) {
+        if (!(src->dataFileStore = virStorageSourceNewFromDataFile(src)))
+            return -1;
     }
 
     if (src->backingStoreRaw) {

commit 7daf35474716702739f409e8bbea68800229955d

virStorageSourceGetMetadataRecurse: Fix format probing exception for images with 'data_file'
The image metadata crawler code generally forbids qcow2 images which
have a 'backing_file' but don't specify a 'backing_file_fmt' header as
we don't want to probe the format due to security implications.

There's one notable exception for the last image in the chain which can
be format probed if it doesn't have another 'backing_file'.

As the comment in 'virStorageSourceGetMetadataRecurse' suggests we don't
want to allow the probe if anything probed would influence access to
more resources. Unfortunately that didn't involve 'data_file' header
which gives more access.

Fortunately there is no way for a guest OS having a 'raw' disk and
access to libvirt's snapshot API to abuse this as libvirt's snapshot API
does specify the header. Other cases where an arbitrary image is passed
to libvirt can 'backing_file'+'backing_file_fmt' directly to access
arbitrary file on disk so this bug is no worse in this regard.

Nevertheless it's a bug which should be fixed.

Closes: https://gitlab.com/libvirt/libvirt/-/work_items/904
Fixes: 0a3d177d9bd6cc608cc8e5769188e5f45f70ee62
Signed-off-by: Peter Krempa <pkrempa@redhat.com>
Reviewed-by: Ján Tomko <jtomko@redhat.com>

src/storage_file/storage_source.c
@@ -1426,9 +1426,10 @@ virStorageSourceGetMetadataRecurse(virStorageSource *src,
     /* If we probed the format we MUST ensure that nothing else than the current
      * image is considered for security labelling and/or recursion. */
     if (orig_format == VIR_STORAGE_FILE_AUTO) {
-        if (src->backingStoreRaw) {
+        if (src->backingStoreRaw || src->dataFileRaw) {
             src->format = VIR_STORAGE_FILE_RAW;
             VIR_FREE(src->backingStoreRaw);
+            VIR_FREE(src->dataFileRaw);
             return -2;
         }
     }

tests/virstoragetest.c
@@ -499,7 +499,7 @@ mymain(void)
                VIR_STORAGE_FILE_QCOW2, EXP_PASS);
     TEST_CHAIN("qcow2datafile-qcow2_qcow2-datafile-auto",
                abs_srcdir "/virstoragetestdata/images/qcow2_datafile-auto.qcow2",
-               VIR_STORAGE_FILE_QCOW2, EXP_PASS);
+               VIR_STORAGE_FILE_QCOW2, EXP_FAIL);
 
     /* broken qcow2 with a 'data_file' which is an empty string */
     TEST_CHAIN("qcow2-datafile-broken",

tests/virstoragetestdata/out/qcow2datafile-qcow2_qcow2-datafile-auto
@@ -1,31 +1,0 @@
-path:ABS_SRCDIR/virstoragetestdata/images/qcow2_datafile-auto.qcow2
-backingStoreRaw: datafile.qcow2
-backingStoreRawFormat: <null>(-1)
-dataFileRaw: <null>
-capacity: 1024
-encryption: 0
-relPath:<null>
-type:file
-format:qcow2
-protocol:none
-hostname:<null>
-
-path:ABS_SRCDIR/virstoragetestdata/images/datafile.qcow2
-backingStoreRaw: <null>
-backingStoreRawFormat: none(0)
-dataFileRaw: raw
-capacity: 1024
-encryption: 0
-relPath:datafile.qcow2
-type:file
-format:qcow2
-protocol:none
-hostname:<null>
-
- dataFileStoreSource for 'ABS_SRCDIR/virstoragetestdata/images/datafile.qcow2':
-  path: ABS_SRCDIR/virstoragetestdata/images/raw
-  capacity: 0
-  encryption: 0
-  type:file
-  format:raw
-